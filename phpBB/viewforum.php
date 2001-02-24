7ee58291852f86696c3c02bec35560c76310787b
Paul S. Owen <psotfx@users.sourceforge.net>
2001-02-24 14:05:52 +0000

    Fixed some layout issues
    git-svn-id: file:///svn/phpbb/trunk@44 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/viewforum.php
+++ phpBB/viewforum.php
@@ -6,8 +6,7 @@
  *   copyright            : (C) 2001 The phpBB Group        
  *   email                : support@phpbb.com                           
  *                                                          
- *   $Id$                                                           
- *                                                            
+ *   $Id$                                                      *                                                            
  * 
  ***************************************************************************/ 
 
@@ -28,9 +27,9 @@ include('common.'.$phpEx);
 // If not give them a nice error page.
 if(isset($forum_id))
 {
-	$sql = "SELECT f.forum_type, f.forum_name 
-		 FROM ".FORUMS_TABLE." f 
-		 WHERE forum_id = '$forum_id'";
+	$sql = "SELECT f.forum_type, f.forum_name
+		FROM ".FORUMS_TABLE." f 
+		WHERE forum_id = '$forum_id'";
 }
 else 
 {
@@ -56,6 +55,7 @@ if(!$forum_row)
 
 $forum_name = stripslashes($forum_row[0]["forum_name"]);
 $forum_moderators = "<a href=\"profile.$phpEx?mode=viewprofile&user_id=1\">james</a>";
+
 $pagetype = "viewforum";
 $page_title = "View Forum - $forum_name";
 include('page_header.'.$phpEx);
@@ -70,10 +70,13 @@ if(!isset($start))
    $start = 0;
 }
 
-$sql = "SELECT t.*, u.username, p.post_time FROM " . TOPICS_TABLE ." t, ". USERS_TABLE. " u 
-         LEFT JOIN ".POSTS_TABLE." p ON p.post_id = t.topic_last_post_id
-	 WHERE t.forum_id = '$forum_id' AND t.topic_poster = u.user_id 
-	 ORDER BY topic_time DESC LIMIT $start, $topics_per_page";
+$sql = "SELECT t.*, u.username, p.post_time
+	FROM " . TOPICS_TABLE ." t, ". USERS_TABLE. " u
+	LEFT JOIN ".POSTS_TABLE." p ON p.post_id = t.topic_last_post_id
+	WHERE t.forum_id = '$forum_id'
+		AND t.topic_poster = u.user_id
+	ORDER BY topic_time DESC
+	LIMIT $start, $topics_per_page";
 if(!$t_result = $db->sql_query($sql))
 {
    error_die($db, QUERY_ERROR);
@@ -82,34 +85,32 @@ $total_topics = $db->sql_numrows();
 
 if($total_topics)
 {
-   $topic_rowset = $db->sql_fetchrowset($t_result);
-   for($x = 0; $x < $total_topics; $x++)
-     {
-	$topic_title = stripslashes($topic_rowset[$x]["topic_title"]);
-	$topic_id = $topic_rowset[$x]["topic_id"];
-	$replies = $topic_rowset[$x]["topic_replies"];
-	$views = $topic_rowset[$x]["topic_views"];
-	$last_post_time = date($date_format, $topic_rowset[$x]["post_time"]);
-	$last_post_user = $topic_rowset[$x]["username"];
-	$folder_img = "<img src=\"images/folder.gif\">";
-	$template->set_var(array("FORUM_ID" => $forum_id,
-				 "TOPIC_ID" => $topic_id,
-				 "FOLDER" => $folder_img, 
-				 "REPLIES" => $replies,
-				 "TOPIC_TITLE" => $topic_title,
-				 "VIEWS" => $views,
-				 "LAST_POST" => $last_post_time . "<br>" . $last_post_user));
-	$template->parse("topics", "topicrow",  true);
-     }
-   $template->pparse("output", array("topics", "body"));
+	$topic_rowset = $db->sql_fetchrowset($t_result);
+	for($x = 0; $x < $total_topics; $x++)
+	{
+		$topic_title = stripslashes($topic_rowset[$x]["topic_title"]);
+		$topic_id = $topic_rowset[$x]["topic_id"];
+		$replies = $topic_rowset[$x]["topic_replies"];
+		$views = $topic_rowset[$x]["topic_views"];
+		$last_post_time = date($date_format, $topic_rowset[$x]["post_time"]);
+		$last_post_user = $topic_rowset[$x]["username"];
+		$folder_img = "<img src=\"images/folder.gif\">";
+		$template->set_var(array("FORUM_ID" => $forum_id, 
+			"TOPIC_ID" => $topic_id,
+			"FOLDER" => $folder_img, 
+			"REPLIES" => $replies,
+			"TOPIC_TITLE" => $topic_title,
+			"VIEWS" => $views,
+			"LAST_POST" => $last_post_time . "<br>" . $last_post_user));
+		$template->parse("topics", "topicrow",  true);
+	}
+	$template->pparse("output", array("topics", "body"));
 }
 else
 {
-   error_die($db, NO_POSTS);
+	error_die($db, NO_POSTS);
 }
 			       
-
 include('page_tail.'.$phpEx);
-
 
 ?>
