0642a9f062c788a4801bb010ed455aac719f0455
James Atkinson <thefinn@users.sourceforge.net>
2001-03-09 18:20:41 +0000

    me == moron...
    git-svn-id: file:///svn/phpbb/trunk@92 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/viewtopic.php
+++ phpBB/viewtopic.php
@@ -23,13 +23,13 @@
 include('extension.inc');
 include('common.'.$phpEx);
 
-if(!isset($HTTP_GET_VARS['topic_id']))  // For backward compatibility
+if(!isset($HTTP_GET_VARS['topic']))  // For backward compatibility
 {
 	$topic_id = $HTTP_GET_VARS[POST_TOPIC_URL];
 }
 else
 {
-	$topic_id = $HTTP_GET_VARS['topic_id'];
+	$topic_id = $HTTP_GET_VARS['topic'];
 }
 
 $is_moderator = 0;
