8f097ef156fc541e8d4d0fcb8f90a17b60e30bfd
Bart van Bragt <bartvb@users.sourceforge.net>
2001-04-16 19:22:48 +0000

    Added a mod otherwise viewforum craps out
    git-svn-id: file:///svn/phpbb/trunk@166 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/db/mysql_basic.sql
+++ phpBB/db/mysql_basic.sql
@@ -7,5 +7,7 @@ INSERT INTO phpbb_categories VALUES (1,'Test category 1','1');
 
 INSERT INTO phpbb_forums VALUES (1,'Test Forum 1','This is just a test forum, nothing special here.',1,1,1,1,0,0,0);
 
+INSERT INTO phpbb_forum_mods VALUES (1,1,0);
+
 INSERT INTO phpbb_users VALUES (1,'admin','Jan 09, 2001','21232f297a57a5a743894a0e4a801fc3','admin@yourdomain.com','','','','','','This is just a stupid sig',1,1,'','','',0,1,0,0,0,0,'',4,'','','',0);
 INSERT INTO phpbb_users VALUES (-1,'Anonymous','May 12, 1978','','','','','','','','',0,0,'','','',0,0,0,0,0,0,'',0,'','','',0);
