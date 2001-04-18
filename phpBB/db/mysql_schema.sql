bc7a2fbc9a7348927769fc078caf79801742fc51
James Atkinson <thefinn@users.sourceforge.net>
2001-04-18 06:35:16 +0000

    Latest mysql schema dump
    git-svn-id: file:///svn/phpbb/trunk@173 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/db/mysql_schema.sql
+++ phpBB/db/mysql_schema.sql
@@ -1,344 +1,307 @@
+# MySQL dump 8.13
 #
-# phpBB2 mysql DB schema - phpBB team 2001
-#
-#
-# $Id$
-#
+# Host: localhost    Database: phpBBtest
+#--------------------------------------------------------
+# Server version	3.23.35
 
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_banlist'
 #
 
 CREATE TABLE phpbb_banlist (
-   ban_id int(10) NOT NULL auto_increment,
-   ban_userid int(10),
-   ban_ip int(11),
-   ban_start int(10),
-   ban_end int(10),
-   ban_time_type int(10),
-   PRIMARY KEY (ban_id)
-);
+  ban_id int(10) NOT NULL auto_increment,
+  ban_userid int(10) default NULL,
+  ban_ip int(11) default NULL,
+  ban_start int(10) default NULL,
+  ban_end int(10) default NULL,
+  ban_time_type int(10) default NULL,
+  PRIMARY KEY  (ban_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_categories'
 #
 
 CREATE TABLE phpbb_categories (
-   cat_id int(10) NOT NULL auto_increment,
-   cat_title varchar(100),
-   cat_order varchar(10),
-   PRIMARY KEY (cat_id)
-);
+  cat_id int(10) NOT NULL auto_increment,
+  cat_title varchar(100) default NULL,
+  cat_order varchar(10) default NULL,
+  PRIMARY KEY  (cat_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_config'
 #
 
 CREATE TABLE phpbb_config (
-   config_id int(10) NOT NULL auto_increment,
-   sitename varchar(100),
-   allow_html tinyint(3),
-   allow_bbcode tinyint(3),
-   allow_sig tinyint(3),
-   allow_namechange tinyint(3),
-   selected int(2) DEFAULT '0' NOT NULL,
-   posts_per_page int(10),
-   hot_threshold int(10),
-   topics_per_page int(10),
-   allow_theme_create int(10),
-   override_themes tinyint(3),
-   email_sig varchar(255),
-   email_from varchar(100),
-   default_dateformat varchar(20),
-   default_lang varchar(255),
-   system_timezone smallint(6),
-   PRIMARY KEY (config_id),
-   UNIQUE selected (selected)
-);
+  config_id int(10) NOT NULL auto_increment,
+  sitename varchar(100) default NULL,
+  allow_html tinyint(3) default NULL,
+  allow_bbcode tinyint(3) default NULL,
+  allow_sig tinyint(3) default NULL,
+  allow_namechange tinyint(3) default NULL,
+  selected int(2) NOT NULL default '0',
+  posts_per_page int(10) default NULL,
+  hot_threshold int(10) default NULL,
+  topics_per_page int(10) default NULL,
+  allow_theme_create int(10) default NULL,
+  override_themes tinyint(3) default NULL,
+  email_sig varchar(255) default NULL,
+  email_from varchar(100) default NULL,
+  default_dateformat varchar(20) default NULL,
+  default_lang varchar(255) default NULL,
+  system_timezone smallint(6) default NULL,
+  PRIMARY KEY  (config_id),
+  UNIQUE KEY selected (selected)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_disallow'
 #
 
 CREATE TABLE phpbb_disallow (
-   disallow_id int(10) NOT NULL auto_increment,
-   disallow_username varchar(50),
-   PRIMARY KEY (disallow_id)
-);
+  disallow_id int(10) NOT NULL auto_increment,
+  disallow_username varchar(50) default NULL,
+  PRIMARY KEY  (disallow_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_forum_access'
 #
 
 CREATE TABLE phpbb_forum_access (
-   forum_id int(10) DEFAULT '0' NOT NULL,
-   user_id int(10) DEFAULT '0' NOT NULL,
-   can_post tinyint(1) DEFAULT '0' NOT NULL,
-   PRIMARY KEY (forum_id, user_id)
-);
+  forum_id int(10) NOT NULL default '0',
+  user_id int(10) NOT NULL default '0',
+  can_post tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (forum_id,user_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_forum_mods'
 #
 
 CREATE TABLE phpbb_forum_mods (
-   forum_id int(10) DEFAULT '0' NOT NULL,
-   user_id int(10) DEFAULT '0' NOT NULL,
-   mod_notify tinyint(3)
-);
+  forum_id int(10) NOT NULL default '0',
+  user_id int(10) NOT NULL default '0',
+  mod_notify tinyint(3) default NULL
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_forums'
 #
 
 CREATE TABLE phpbb_forums (
-   forum_id int(10) NOT NULL auto_increment,
-   forum_name varchar(150),
-   forum_desc text,
-   forum_access tinyint(3),
-   cat_id int(10),
-   forum_order int(11) DEFAULT '1' NOT NULL,
-   forum_type tinyint(3),
-   forum_posts int(11) DEFAULT '0' NOT NULL,
-   forum_topics tinyint(4) DEFAULT '0' NOT NULL,
-   forum_last_post_id int(11) DEFAULT '0' NOT NULL,
-   PRIMARY KEY (forum_id),
-   KEY forum_id (forum_id),
-   KEY forums_order (forum_order),
-   KEY cat_id (cat_id)
-);
+  forum_id int(10) NOT NULL auto_increment,
+  forum_name varchar(150) default NULL,
+  forum_desc text,
+  forum_access tinyint(3) default NULL,
+  cat_id int(10) default NULL,
+  forum_order int(11) NOT NULL default '1',
+  forum_type tinyint(3) default NULL,
+  forum_posts int(11) NOT NULL default '0',
+  forum_topics tinyint(4) NOT NULL default '0',
+  forum_last_post_id int(11) NOT NULL default '0',
+  PRIMARY KEY  (forum_id),
+  KEY forum_id (forum_id),
+  KEY forums_order (forum_order),
+  KEY cat_id (cat_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_headermetafooter'
 #
-# Do we really need this anymore with templating?
-#
 
 CREATE TABLE phpbb_headermetafooter (
-   header text,
-   meta text,
-   footer text
-);
+  header text,
+  meta text,
+  footer text
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_posts'
 #
 
 CREATE TABLE phpbb_posts (
-   post_id int(10) NOT NULL auto_increment,
-   topic_id int(10) DEFAULT '0' NOT NULL,
-   forum_id int(10) DEFAULT '0' NOT NULL,
-   poster_id int(10) DEFAULT '0' NOT NULL,
-   post_time int(10) DEFAULT '0' NOT NULL,
-   poster_ip int(10) DEFAULT '0' NOT NULL,
-   bbcode_uid varchar(10) DEFAULT '' NOT NULL,
-   PRIMARY KEY (post_id),
-   KEY forum_id (forum_id),
-   KEY topic_id (topic_id),
-   KEY poster_id (poster_id)
-);
+  post_id int(10) NOT NULL auto_increment,
+  topic_id int(10) NOT NULL default '0',
+  forum_id int(10) NOT NULL default '0',
+  poster_id int(10) NOT NULL default '0',
+  post_time int(10) NOT NULL default '0',
+  poster_ip int(10) NOT NULL default '0',
+  bbcode_uid varchar(10) NOT NULL default '',
+  PRIMARY KEY  (post_id),
+  KEY forum_id (forum_id),
+  KEY topic_id (topic_id),
+  KEY poster_id (poster_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_posts_text'
 #
 
 CREATE TABLE phpbb_posts_text (
-   post_id int(10) DEFAULT '0' NOT NULL,
-   post_text text,
-   PRIMARY KEY (post_id)
-);
+  post_id int(10) NOT NULL default '0',
+  post_text text,
+  PRIMARY KEY  (post_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_priv_msgs'
 #
 
 CREATE TABLE phpbb_priv_msgs (
-   msg_id int(10) NOT NULL auto_increment,
-   from_userid int(10) DEFAULT '0' NOT NULL,
-   to_userid int(10) DEFAULT '0' NOT NULL,
-   msg_time int(10) DEFAULT '0' NOT NULL,
-   poster_ip int(10) DEFAULT '0' NOT NULL,
-   msg_status int(10) DEFAULT '0' NOT NULL,
-   msg_text text NOT NULL,
-   PRIMARY KEY (msg_id),
-   KEY to_userid (to_userid)
-);
+  msg_id int(10) NOT NULL auto_increment,
+  from_userid int(10) NOT NULL default '0',
+  to_userid int(10) NOT NULL default '0',
+  msg_time int(10) NOT NULL default '0',
+  poster_ip int(10) NOT NULL default '0',
+  msg_status int(10) NOT NULL default '0',
+  msg_text text NOT NULL,
+  PRIMARY KEY  (msg_id),
+  KEY to_userid (to_userid)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_ranks'
 #
 
 CREATE TABLE phpbb_ranks (
-   rank_id int(11) NOT NULL auto_increment,
-   rank_title varchar(50) NOT NULL,
-   rank_min int(11) DEFAULT '0' NOT NULL,
-   rank_max int(11) DEFAULT '0' NOT NULL,
-   rank_special tinyint(4) DEFAULT '0',
-   rank_image varchar(255),
-   PRIMARY KEY (rank_id),
-   KEY rank_min (rank_min),
-   KEY rank_max (rank_max),
-   KEY rank_id (rank_id)
-);
+  rank_id int(11) NOT NULL auto_increment,
+  rank_title varchar(50) NOT NULL default '',
+  rank_min int(11) NOT NULL default '0',
+  rank_max int(11) NOT NULL default '0',
+  rank_special tinyint(4) default '0',
+  rank_image varchar(255) default NULL,
+  PRIMARY KEY  (rank_id),
+  KEY rank_min (rank_min),
+  KEY rank_max (rank_max),
+  KEY rank_id (rank_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_session'
 #
 
 CREATE TABLE phpbb_session (
-   session_id int(10) unsigned DEFAULT '0' NOT NULL,
-   session_user_id int(10) DEFAULT '0' NOT NULL,
-   session_time int(10) unsigned DEFAULT '0' NOT NULL,
-   session_ip int(10) DEFAULT '0' NOT NULL,
-   session_page int(10) DEFAULT '0' NOT NULL,
-   session_logged_in tinyint(1) DEFAULT '0' NOT NULL,
-   PRIMARY KEY (session_id)
-);
+  session_id int(10) unsigned NOT NULL default '0',
+  session_user_id int(10) NOT NULL default '0',
+  session_time int(10) unsigned NOT NULL default '0',
+  session_ip int(10) default NULL,
+  session_page int(10) NOT NULL default '0',
+  session_logged_in tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (session_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_themes'
 #
 
 CREATE TABLE phpbb_themes (
-   theme_id int(10) NOT NULL auto_increment,
-   theme_name varchar(35),
-   bgcolor varchar(10),
-   textcolor varchar(10),
-   color1 varchar(10),
-   color2 varchar(10),
-   table_bgcolor varchar(10),
-   header_image varchar(50),
-   newtopic_image varchar(50),
-   reply_image varchar(50),
-   linkcolor varchar(15),
-   vlinkcolor varchar(15),
-   theme_default int(2) DEFAULT '0',
-   fontface varchar(100),
-   fontsize1 varchar(5),
-   fontsize2 varchar(5),
-   fontsize3 varchar(5),
-   fontsize4 varchar(5),
-   tablewidth varchar(10),
-   replylocked_image varchar(255),
-   PRIMARY KEY (theme_id)
-);
+  theme_id int(10) NOT NULL auto_increment,
+  theme_name varchar(35) default NULL,
+  bgcolor varchar(10) default NULL,
+  textcolor varchar(10) default NULL,
+  color1 varchar(10) default NULL,
+  color2 varchar(10) default NULL,
+  table_bgcolor varchar(10) default NULL,
+  header_image varchar(50) default NULL,
+  newtopic_image varchar(50) default NULL,
+  reply_image varchar(50) default NULL,
+  linkcolor varchar(15) default NULL,
+  vlinkcolor varchar(15) default NULL,
+  theme_default int(2) default '0',
+  fontface varchar(100) default NULL,
+  fontsize1 varchar(5) default NULL,
+  fontsize2 varchar(5) default NULL,
+  fontsize3 varchar(5) default NULL,
+  fontsize4 varchar(5) default NULL,
+  tablewidth varchar(10) default NULL,
+  replylocked_image varchar(255) default NULL,
+  PRIMARY KEY  (theme_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_topics'
 #
 
 CREATE TABLE phpbb_topics (
-   topic_id int(10) NOT NULL auto_increment,
-   topic_title varchar(100) NOT NULL,
-   topic_poster int(10) DEFAULT '0' NOT NULL,
-   topic_time int(10) DEFAULT '0' NOT NULL,
-   topic_views int(10) DEFAULT '0' NOT NULL,
-   topic_replies int(11) DEFAULT '0' NOT NULL,
-   forum_id int(10) DEFAULT '0' NOT NULL,
-   topic_status tinyint(3) DEFAULT '0' NOT NULL,
-   topic_notify tinyint(3) DEFAULT '0',
-   topic_last_post_id int(11) DEFAULT '0' NOT NULL,
-   PRIMARY KEY (topic_id),
-   KEY forum_id (forum_id),
-   KEY topic_id (topic_id)
-);
+  topic_id int(10) NOT NULL auto_increment,
+  topic_title varchar(100) NOT NULL default '',
+  topic_poster int(10) NOT NULL default '0',
+  topic_time int(10) NOT NULL default '0',
+  topic_views int(10) NOT NULL default '0',
+  topic_replies int(11) NOT NULL default '0',
+  forum_id int(10) NOT NULL default '0',
+  topic_status tinyint(3) NOT NULL default '0',
+  topic_notify tinyint(3) default '0',
+  topic_last_post_id int(11) NOT NULL default '0',
+  PRIMARY KEY  (topic_id),
+  KEY forum_id (forum_id),
+  KEY topic_id (topic_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_users'
 #
 
 CREATE TABLE phpbb_users (
-   user_id int(11) NOT NULL auto_increment,
-   username varchar(40) NOT NULL,
-   user_regdate varchar(20) NOT NULL,
-   user_password varchar(32) NOT NULL,
-   user_email varchar(255),
-   user_icq varchar(15),
-   user_website varchar(100),
-   user_occ varchar(100),
-   user_from varchar(100),
-   user_intrest varchar(150),
-   user_sig varchar(255),
-   user_viewemail tinyint(3),
-   user_theme int(11),
-   user_aim varchar(255),
-   user_yim varchar(255),
-   user_msnm varchar(255),
-   user_posts int(11) DEFAULT '0',
-   user_attachsig tinyint(3),
-   user_desmile tinyint(3),
-   user_html tinyint(3),
-   user_bbcode tinyint(3),
-   user_rank int(11) DEFAULT '0',
-   user_avatar varchar(100),
-   user_level int(11) DEFAULT '1',
-   user_lang varchar(255),
-   user_actkey varchar(32),
-   user_newpasswd varchar(32),
-   user_notify tinyint(3),
-   PRIMARY KEY (user_id),
-   KEY user_id (user_id)
-);
+  user_id int(11) NOT NULL auto_increment,
+  username varchar(40) NOT NULL default '',
+  user_regdate varchar(20) NOT NULL default '',
+  user_password varchar(32) NOT NULL default '',
+  user_email varchar(255) default NULL,
+  user_icq varchar(15) default NULL,
+  user_website varchar(100) default NULL,
+  user_occ varchar(100) default NULL,
+  user_from varchar(100) default NULL,
+  user_intrest varchar(150) default NULL,
+  user_sig varchar(255) default NULL,
+  user_viewemail tinyint(3) default NULL,
+  user_theme int(11) default NULL,
+  user_aim varchar(255) default NULL,
+  user_yim varchar(255) default NULL,
+  user_msnm varchar(255) default NULL,
+  user_posts int(11) default '0',
+  user_attachsig tinyint(3) default NULL,
+  user_desmile tinyint(3) default NULL,
+  user_html tinyint(3) default NULL,
+  user_bbcode tinyint(3) default NULL,
+  user_rank int(11) default '0',
+  user_avatar varchar(100) default NULL,
+  user_level int(11) default '1',
+  user_lang varchar(255) default NULL,
+  user_actkey varchar(32) default NULL,
+  user_newpasswd varchar(32) default NULL,
+  user_notify tinyint(3) default NULL,
+  user_timezone int(4) default NULL,
+  user_active int(2) default NULL,
+  PRIMARY KEY  (user_id),
+  KEY user_id (user_id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_whosonline'
 #
 
 CREATE TABLE phpbb_whosonline (
-   id int(3) NOT NULL auto_increment,
-   ip varchar(255),
-   name varchar(255),
-   count varchar(255),
-   date varchar(255),
-   username varchar(40),
-   forum int(10),
-   PRIMARY KEY (id)
-);
+  id int(3) NOT NULL auto_increment,
+  ip varchar(255) default NULL,
+  name varchar(255) default NULL,
+  count varchar(255) default NULL,
+  date varchar(255) default NULL,
+  username varchar(40) default NULL,
+  forum int(10) default NULL,
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
 
-
-# --------------------------------------------------------
 #
 # Table structure for table 'phpbb_words'
 #
 
 CREATE TABLE phpbb_words (
-   word_id int(10) NOT NULL auto_increment,
-   word varchar(100) NOT NULL,
-   replacement varchar(100) NOT NULL,
-   PRIMARY KEY (word_id)
-);
-
+  word_id int(10) NOT NULL auto_increment,
+  word varchar(100) NOT NULL default '',
+  replacement varchar(100) NOT NULL default '',
+  PRIMARY KEY  (word_id)
+) TYPE=MyISAM;
 
