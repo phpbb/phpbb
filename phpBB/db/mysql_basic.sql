756427797d8892210b4bc29b0f26aa27a7ee58c9
James Atkinson <thefinn@users.sourceforge.net>
2001-04-18 06:42:01 +0000

    Updated mysql_basic file with new user fields
    git-svn-id: file:///svn/phpbb/trunk@174 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/db/mysql_basic.sql
+++ phpBB/db/mysql_basic.sql
@@ -9,5 +9,5 @@ INSERT INTO phpbb_forums VALUES (1,'Test Forum 1','This is just a test forum, no
 
 INSERT INTO phpbb_forum_mods VALUES (1,1,0);
 
-INSERT INTO phpbb_users VALUES (1,'admin','Jan 09, 2001','21232f297a57a5a743894a0e4a801fc3','admin@yourdomain.com','','','','','','This is just a stupid sig',1,1,'','','',0,1,0,0,0,0,'',4,'','','',0);
-INSERT INTO phpbb_users VALUES (-1,'Anonymous','May 12, 1978','','','','','','','','',0,0,'','','',0,0,0,0,0,0,'',0,'','','',0);
+INSERT INTO phpbb_users VALUES (1,'admin','Jan 09, 2001','21232f297a57a5a743894a0e4a801fc3','admin@yourdomain.com','','','','','','This is just a stupid sig',1,1,'','','',0,1,0,0,0,0,'',4,'','','',0,'-8',1);
+INSERT INTO phpbb_users VALUES (-1,'Anonymous','May 12, 1978','','','','','','','','',0,0,'','','',0,0,0,0,0,0,'',0,'','','',0,'-8',1);
