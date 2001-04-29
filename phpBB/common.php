bec72db2be02cef9790b3331b69e970502c5ee5d
Paul S. Owen <psotfx@users.sourceforge.net>
2001-04-29 20:16:08 +0000

    Grab default_theme setting from DB
    git-svn-id: file:///svn/phpbb/trunk@214 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/common.php
+++ phpBB/common.php
@@ -93,6 +93,7 @@ else
 	$email_sig = stripslashes($config["email_sig"]);
 	$email_from = $config["email_from"];
 	$default_lang = $config["default_lang"];
+	$default_theme = $config['default_theme'];
 	$require_activation = $config["require_activation"];
 	$sys_timezone = $config["system_timezone"];
 	$sys_template = $config['sys_template'];
