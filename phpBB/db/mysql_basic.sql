2d6c9177442e6509841d0c2dc7a93268e21c2935
the_systech <the_systech@users.sourceforge.net>
2001-05-07 14:55:59 +0000

    Fixed value orders to match column orders
    git-svn-id: file:///svn/phpbb/trunk@264 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/db/mysql_basic.sql
+++ phpBB/db/mysql_basic.sql
@@ -4,20 +4,20 @@
 # $id: mysql_basic.sql,v 1.10 2001/04/19 17:26:24 psotfx Exp $
 
 # -- Config
-INSERT INTO phpbb_config VALUES (1,1,'Test forum',1,1,1,1,1,25,10,50,0,0,'With regards, the Admin','admin@yoursite.com','d M Y h:i:s a','english',0,'Default');
+INSERT INTO phpbb_config VALUES (1,1,'Test forum',1,1,1,1,1,0,10,50,25,'With regards, the Admin','admin@yoursite.com',1,'english','d M Y h:i:s a',0,'Default');
 
 # -- Categories
 INSERT INTO phpbb_categories VALUES (1,'Test category 1','1');
 
 # -- Forums
-INSERT INTO phpbb_forums VALUES (1,'Test Forum 1','This is just a test forum, nothing special here.',1,1,1,1,0,0,0);
+INSERT INTO phpbb_forums VALUES (1,1,'Test Forum 1','This is just a test forum, nothing special here.',1,1,1,0,0,0);
 
 # -- Forum Mods
 INSERT INTO phpbb_forum_mods VALUES (1,1,0);
 
 # -- Users
-INSERT INTO phpbb_users VALUES (1,1,'admin',NOW(),'21232f297a57a5a743894a0e4a801fc3','admin@yourdomain.com','','','','','','','This is just a stupid sig',1,1,'','','',0,1,0,0,0,0,'',4,'','','',0,'-8',1, '');
-INSERT INTO phpbb_users VALUES (-1,1,'Anonymous',NOW(),'','','','','','','','','',0,0,'','','',0,0,0,0,0,0,'',0,'','','',0,'-8',1,'Default');
+INSERT INTO phpbb_users VALUES (1,1,'admin',NOW(),'21232f297a57a5a743894a0e4a801fc3','','','','-8','d M Y H:i','0','','4','admin@yourdomain.com','','','','','','This is just a stupid sig',1,1,'','','',0,1,0,0,0,'','',0);
+INSERT INTO phpbb_users VALUES (-1,1,'Anonymous',NOW(),'','','Default','','-8','d M Y H:i',0,'',1,'','','','','','','',0,1,'','','',0,0,0,0,0,'','',0);
 
 # -- Themes
 INSERT INTO phpbb_themes (themes_id, themes_name, head_stylesheet, body_background, body_bgcolor, body_text, body_link, body_vlink, body_alink, body_hlink, tr_color1, tr_color2, tr_color3, th_color1, th_color2, th_color3, td_color1, td_color2, td_color3, fontface1, fontface2, fontface3, fontsize1, fontsize2, fontsize3, fontcolor1, fontcolor2, fontcolor3, img1, img2, img3, img4) VALUES ( '1', 'PSO-Wheat', '', '', 'FFFFFF', '000000', '002266', '004411', '', '', '', '', '', '001100', 'E5CCA5', 'D4A294', 'EBE4D9', 'DAD1C4', '', 'verdana,serif', 'arial,helvetica', 'courier', '1', '2', '3', '000000', '', '', '', '', '', '');
