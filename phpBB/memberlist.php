3f4ea178d63fc8752d7cfb12935cd33ee970589a
Paul S. Owen <psotfx@users.sourceforge.net>
2001-05-12 11:43:48 +0000

    Fixed various errors, added issets, removed unneeded sql error messages
    git-svn-id: file:///svn/phpbb/trunk@275 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/memberlist.php
+++ phpBB/memberlist.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *                                common.php
+ *                              memberlist.php
  *                            -------------------
  *   begin                : Friday, May 11, 2001
  *   copyright            : (C) 2001 The phpBB Group
@@ -35,33 +35,55 @@ init_userprefs($userdata);
 
 include('includes/page_header.'.$phpEx);
 
-if(!$start)
+if(!isset($HTTP_GET_VARS['start']))
 {
 	$start = 0;
 }
-switch($mode)
+if(isset($HTTP_GET_VARS['mode']))
 {
-	case 'top10':
-		$sql = "SELECT username, user_id, user_viewemail, user_posts, user_regdate, user_from, user_website, user_email
-				  FROM ".USERS_TABLE." WHERE user_id != ".ANONYMOUS." AND user_level != ".DELETED." ORDER BY user_posts ASC LIMIT 10";
-		
-	break;
-	case 'alpha':
-		$sql = "SELECT username, user_id, user_viewemail, user_posts, user_regdate, user_from, user_website, user_email
-				  FROM ".USERS_TABLE." WHERE user_id != ".ANONYMOUS." AND user_level != ".DELETED." ORDER BY username ASC LIMIT $start, ".$board_config['topics_per_page'];
-	break;
-	default:
-		$sql = "SELECT username, user_id, user_viewemail, user_posts, user_regdate, user_from, user_website, user_email
-				  FROM ".USERS_TABLE." 	WHERE user_id != ".ANONYMOUS." AND user_level != ".DELETED." ORDER BY user_id ASC LIMIT $start, ".$board_config['topics_per_page'];
-	break;
+	switch($mode)
+	{
+		case 'topten':
+			$sql = "SELECT username, user_id, user_viewemail, user_posts, user_regdate, user_from, user_website, user_email 
+				FROM ".USERS_TABLE." 
+				WHERE user_id != ".ANONYMOUS." 
+					AND user_level != ".DELETED." 
+				ORDER BY user_posts DESC 
+				LIMIT 10";
+			break;
+		case 'alpha':
+			$sql = "SELECT username, user_id, user_viewemail, user_posts, user_regdate, user_from, user_website, user_email 
+				FROM ".USERS_TABLE." 
+				WHERE user_id != ".ANONYMOUS." 
+					AND user_level != ".DELETED." 
+				ORDER BY username ASC 
+				LIMIT $start, ".$board_config['topics_per_page'];
+			break;
+		default:
+			$sql = "SELECT username, user_id, user_viewemail, user_posts, user_regdate, user_from, user_website, user_email
+				FROM ".USERS_TABLE." 
+				WHERE user_id != ".ANONYMOUS." 
+					AND user_level != ".DELETED." 
+				ORDER BY user_id ASC 
+				LIMIT $start, ".$board_config['topics_per_page'];
+			break;
+	}
+}
+else
+{
+	$sql = "SELECT username, user_id, user_viewemail, user_posts, user_regdate, user_from, user_website, user_email
+		FROM ".USERS_TABLE." 
+		WHERE user_id != ".ANONYMOUS." 
+			AND user_level != ".DELETED." 
+		ORDER BY user_id ASC 
+		LIMIT $start, ".$board_config['topics_per_page'];
 }
 
 if(!$result = $db->sql_query($sql))
 {
 	if(DEBUG)
 	{
-		$error = $db->sql_error();
-		error_die(SQL_QUERY, "Error getting memberlist.<br>Reason: ".$error['message']."<br>Query: $sql.", __LINE__, __FILE__);
+		error_die(SQL_QUERY, "Error getting memberlist.", __LINE__, __FILE__);
 	}
 	else
 	{
@@ -70,18 +92,20 @@ if(!$result = $db->sql_query($sql))
 }
 if(($selected_members = $db->sql_numrows($result)) > 0)
 {
-	$template->set_filenames(array("body" => "memberlist_body.tpl"));
-	$template->assign_vars(array("U_VIEW_TOP10" => append_sid("memberlist.$phpEx?mode=top10"),
-											"U_SORTALPHA" => append_sid("memberlist.$phpEx?mode=alpha"),
-											"L_VIEW_TOP10" => $l_top10,
-											"L_SORTALPHA" => $l_alpha,
-											"L_EMAIL" => $l_email,
-											"L_WEBSITE" => $l_website,
-											"L_FROM" => $l_from));
+	$template->set_filenames(array(
+		"body" => "memberlist_body.tpl"));
+	$template->assign_vars(array(
+		"U_VIEW_TOP10" => append_sid("memberlist.$phpEx?mode=topten"),
+		"U_SORTALPHA" => append_sid("memberlist.$phpEx?mode=alpha"),
+		"L_VIEW_TOP10" => $l_top10,
+		"L_SORTALPHA" => $l_alpha,
+		"L_EMAIL" => $l_email,
+		"L_WEBSITE" => $l_website,
+		"L_FROM" => $l_from));
 											
 	$members = $db->sql_fetchrowset($result);
 
-	for($x = $start; $x < $selected_members; $x++)
+	for($x = 0; $x < $selected_members; $x++)
 	{
 		unset($email);
 		$username = stripslashes($members[$x]['username']);
@@ -119,25 +143,27 @@ if(($selected_members = $db->sql_numrows($result)) > 0)
 			$row_color = "#".$theme['td_color2'];
 		}
 		$template->assign_block_vars("memberrow", array(
-											  "ROW_COLOR" => $row_color,
-											  "U_VIEWPROFILE" => append_sid("profile.$phpEx?mode=viewprofile&".POST_USERS_URL."=".$user_id),
-											  "USERNAME" => $username,
-											  "FROM" => $from,
-											  "JOINED" => $joined,
-											  "POSTS" => $posts,
-											  "EMAIL" => $email,
-											  "WEBSITE" => $url));
+			  "ROW_COLOR" => $row_color,
+			  "U_VIEWPROFILE" => append_sid("profile.$phpEx?mode=viewprofile&".POST_USERS_URL."=".$user_id),
+			  "USERNAME" => $username,
+			  "FROM" => $from,
+			  "JOINED" => $joined,
+			  "POSTS" => $posts,
+			  "EMAIL" => $email,
+			  "WEBSITE" => $url));
 	}
 	
-	if($mode != "top10")
+	if($mode != "topten")
 	{
-		$sql = "SELECT count(*) AS total FROM ".USERS_TABLE." WHERE user_id != ".ANONYMOUS." AND user_level != ".DELETED;
+		$sql = "SELECT count(*) AS total 
+			FROM ".USERS_TABLE." 
+			WHERE user_id != ".ANONYMOUS." 
+				AND user_level != ".DELETED;
 		if(!$count_result = $db->sql_query($sql))
 		{
 			if(DEBUG)
 			{
-				$error = $db->sql_error();
-				error_die(SQL_QUERY, "Error getting total users<br>Reason: ".$error['message']."<br>Query: $sql", __LINE__, __FILE__);
+				error_die(SQL_QUERY, "Error getting total users.", __LINE__, __FILE__);
 			}
 			else
 			{
@@ -146,16 +172,17 @@ if(($selected_members = $db->sql_numrows($result)) > 0)
 		}
 		else
 		{
-			$total = $db->sql_fetchrowset($count_result);
-			$total_members = $total[0]['total'];
-			$pagination = generate_pagination("memberlist.$phpEx?mode=$mode", $total_members, $board_config['topics_per_page'], $start, TRUE)."&nbsp;";
+			$total = $db->sql_fetchrow($count_result);
+			$total_members = $total['total'];
+			$pagination = generate_pagination("memberlist.$phpEx?mode=$mode", $total_members, $board_config['topics_per_page'], $start)."&nbsp;";
 		}
 	}
 	else
 	{
 		$pagination = "&nbsp;";
 	}
-	$template->assign_vars(array("PAGINATION" => $pagination));
+	$template->assign_vars(array(
+		"PAGINATION" => $pagination));
 	$template->pparse("body");
 }
 
