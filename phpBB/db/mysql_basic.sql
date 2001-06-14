e1e0b9c29b8d77461a50c0b484e4a843bd869a94
Paul S. Owen <psotfx@users.sourceforge.net>
2001-06-14 17:00:14 +0000

    Fixed some problems ... maybe - Part 3 A Sequel too Far
    git-svn-id: file:///svn/phpbb/trunk@495 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/db/mysql_basic.sql
+++ phpBB/db/mysql_basic.sql
@@ -24,8 +24,8 @@ INSERT INTO phpbb_ranks (rank_id, rank_title, rank_min, rank_max, rank_special, 
 INSERT INTO phpbb_ranks (rank_id, rank_title, rank_min, rank_max, rank_special, rank_image) VALUES ( '7', 'Has No Life', '200', '99999', '0', '');
 
 # -- Groups
-INSERT INTO phpbb_groups (group_id, group_name, group_note, single_user) VALUES (1, 'Anonymous', 'Personal User', 1);
-INSERT INTO phpbb_groups (group_id, group_name, group_note, single_user) VALUES (2, 'Admin', 'Personal User', 1);
+INSERT INTO phpbb_groups (group_id, group_name, group_description, group_single_user) VALUES (1, 'Anonymous', 'Personal User', 1);
+INSERT INTO phpbb_groups (group_id, group_name, group_description, group_single_user) VALUES (2, 'Admin', 'Personal User', 1);
 
 # -- User -> Group
 INSERT INTO phpbb_user_group (group_id, user_id) VALUES (1, -1);
@@ -35,10 +35,10 @@ INSERT INTO phpbb_user_group (group_id, user_id) VALUES (2, 2);
 INSERT INTO phpbb_auth_forums (forum_id, auth_view, auth_read, auth_post, auth_reply, auth_edit, auth_delete, auth_announce, auth_sticky, auth_votecreate, auth_vote) VALUES (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 # -- User Access (admin is set as ... an admin)
-INSERT INTO phpbb_auth_access (group_id, forum_id, auth_view, auth_read, auth_post, auth_reply, auth_edit, auth_delete, auth_announce, auth_sticky, auth_votecreate, auth_vote, auth_mod, auth_admin) VALUES (2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1);
+INSERT INTO phpbb_auth_access (group_id, forum_id, auth_view, auth_read, auth_post, auth_reply, auth_edit, auth_delete, auth_announce, auth_sticky, auth_votecreate, auth_vote, auth_mod, auth_admin) VALUES (2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1);
 
 # -- Demo Topic
-INSERT INTO phpbb_topics (topic_id, topic_title, topic_poster, topic_time, topic_views, topic_replies, forum_id, topic_status, topic_type, topic_notify, topic_last_post_id) VALUES (1, 'Demo Topic', 1, NOW(), 0, 0, 1, 0, 0, 0, 1);
+INSERT INTO phpbb_topics (topic_id, topic_title, topic_poster, topic_time, topic_views, topic_replies, forum_id, topic_status, topic_type, topic_notify, topic_last_post_id) VALUES (1, 'Demo Topic', 2, NOW(), 0, 0, 1, 0, 0, 0, 1);
 
 # -- Demo Post
 INSERT INTO phpbb_posts (post_id, topic_id, forum_id, poster_id, post_time, post_username, poster_ip) VALUES (1, 1, 1, 2, NOW(), '', '7F000001');
