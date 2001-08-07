2124505c10fc35a6c2d26e6ae2da93f5c7f90179
the_systech <the_systech@users.sourceforge.net>
2001-08-07 21:21:02 +0000

    Rollback from 1.16 and refix mysql_error thing...
    git-svn-id: file:///svn/phpbb/trunk@812 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/admin/admin_db_utilities.php
+++ phpBB/admin/admin_db_utilities.php
@@ -1056,7 +1056,7 @@ if( isset($HTTP_GET_VARS['perform']) || isset($HTTP_POST_VARS['perform']) )
 							if(!$result && ( !(SQL_LAYER == 'postgres' && eregi("drop table", $sql) ) ) )
 							{
 								include('page_header_admin.'.$phpEx);
-								message_die(GENERAL_ERROR, "Error importing backup file", "", __LINE__, __FILE__, mysql_error() ."<br>". $sql);
+								message_die(GENERAL_ERROR, "Error importing backup file", "", __LINE__, __FILE__, $sql);
 							}
 						}
 					}
