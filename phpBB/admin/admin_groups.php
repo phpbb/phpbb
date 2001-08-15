f8d5fd6422867811aae8058aca9799633318255b
dougk_ff7 <dougk_ff7@users.sourceforge.net>
2001-08-15 00:30:31 +0000

    Group admin isn't finished, but I felt like committing it anyway...
    git-svn-id: file:///svn/phpbb/trunk@878 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/admin/admin_groups.php
+++ phpBB/admin/admin_groups.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************  
- *                                 
+ *                             admin_groups.php
  *                            -------------------                         
  *   begin                : Saturday, Feb 13, 2001 
  *   copyright            : (C) 2001 The phpBB Group        
@@ -33,8 +33,103 @@ if($setmodules == 1)
 //
 // Include required files, get $phpEx and check permissions
 //
-require('pagestart.inc');
+$phpbb_root_path = "./../";
+include($phpbb_root_path . 'extension.inc');
+include($phpbb_root_path . 'common.'.$phpEx);
 
-echo "Nothing to see here, move on people...<br>\n";
+//
+// Start session management
+//
+$userdata = session_pagestart($user_ip, PAGE_INDEX, $session_length);
+init_userprefs($userdata);
+//
+// End session management
+//
+if( !$userdata['session_logged_in'] )
+{
+	header("Location: ../login.$phpEx?forward_page=admin/");
+}
+else if( $userdata['user_level'] != ADMIN )
+{
+	message_die(GENERAL_MESSAGE, $lang['Not_admin']);
+}
+
+if( isset($mode) )
+{
+
+	//
+	// Ok they are editing a group or creating a new group
+	//
+
+	
+}
+else if( isset($updategroup) )
+{
+	//
+	// Ok, they are submitting a group, let's save the data based on if it's new or editing
+	//
+	switch($mode)
+		case 'update':
+		
+		break;
+		
+		case 'new':
+		
+		break;
+		
+		case 'delete':
+		
+		break;
+		
+		case 'default':
+		message_die(GENERAL_ERROR, $lang['Group_mode_not_selected']);
+		break;
+}
+else
+{
+	//
+	// Default group selection box
+	//
+	// This should be altered on the final system 
+	//
+
+	$sql = "SELECT group_id, group_name
+		FROM " . GROUPS_TABLE . " 
+		WHERE group_single_user <> " . TRUE . "
+		ORCER BY group_name";
+	if(!$result = $db->sql_query($sql))
+	{
+//		message_die(GENERAL_ERROR, "Error getting group information", "", __LINE__, __FILE__, $sql);
+	}
+	if( !$db->sql_numrows($result) )
+	{
+//		message_die(GENERAL_MESSAGE, "No groups exist.");
+	}
+	
+	$select_list = "<select name=\"group\">";
+	for($i = 0; $i < count($user_list); $i++)
+	{
+		$select_list .= "<option value=\"" . $group_list[$i]['group_id'] . "\">" . $group_list[$i]['group_name'] . "</option>";
+	}
+	$select_list .= "</select>";
+
+	include('page_header_admin.'.$phpEx);
+
+	$template->set_filenames(array(
+		"body" => "admin/group_select_body.tpl")
+	);
+
+	$template->assign_vars(array(
+		"L_GROUP_TITLE" => $lang['Group'] . " " . $lang['Administration'], 
+		"L_GROUP_EXPLAIN" => $lang['Group_admin_explain'], 
+		"L_GROUP_SELECT" => $lang['Select_a'] . " " . $lang['Group'], 
+		"L_LOOK_UP" => $lang['Look_up'] . " " . $lang['Group'], 
+
+		"S_GROUP_ACTION" => append_sid("admin_groups.$phpEx"), 
+		"S_GROUP_SELECT" => $select_list)
+	);
+
+}
+include($phpbb_root_path . 'includes/page_tail.'.$phpEx);
 
 ?>
---
+++ phpBB/templates/PSO/admin/group_select_body.tpl
@@ -0,0 +1,20 @@
+
+<br clear="all" />
+
+<h1>{L_GROUP_TITLE}</h1>
+
+<p>{L_GROUP_EXPLAIN}</p>
+
+<table cellspacing="1" cellpadding="4" border="0" align="center">
+	<tr>
+		<td class="cat" align="center"><span class="cattitle"><b>{L_AUTH_SELECT}</b></span></td>
+	</tr>
+	<tr><form method="get" action="{S_GROUP_ACTION}"><input type="hidden" mode="editgroup">
+		<td class="row1" align="center">{S_GROUP_SELECT}&nbsp;&nbsp;<input type="submit" value="{L_LOOK_UP}">&nbsp;</td>
+	</form></tr>
+	<tr>
+		<td class="row2" align="center"><a href="admin_groups.{phpEx}?mode=newgroup">{L_GROUP_NEW}</a></td>
+	</tr>
+</table>
+
+<br clear="all" />
