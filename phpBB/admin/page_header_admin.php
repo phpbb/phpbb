ccb121a1a56818fe04c32ca94ee8b2e3ff67f730
Paul S. Owen <psotfx@users.sourceforge.net>
2001-09-01 22:04:44 +0000

    Added additional cache control headers
    git-svn-id: file:///svn/phpbb/trunk@962 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/admin/page_header_admin.php
+++ phpBB/admin/page_header_admin.php
@@ -155,6 +155,9 @@ $template->assign_vars(array(
 	"T_SPAN_CLASS3" => $theme['span_class3'])
 );
 
+header ("Cache-Control: no-store, no-cache, must-revalidate");
+header ("Cache-Control: pre-check=0, post-check=0, max-age=0");
+header ("Pragma: no-cache");
 header ("Expires: " . gmdate("D, d M Y H:i:s", time()) . " GMT");
 header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 
--- phpBB/includes/page_header.php
+++ phpBB/includes/page_header.php
@@ -286,6 +286,9 @@ if(!$userdata['session_logged_in'])
 	$template->assign_block_vars("loginbox", array());
 }
 
+header ("Cache-Control: no-store, no-cache, must-revalidate");
+header ("Cache-Control: pre-check=0, post-check=0, max-age=0");
+header ("Pragma: no-cache");
 header ("Expires: " . gmdate("D, d M Y H:i:s", time()) . " GMT");
 header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 
