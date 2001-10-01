dcbeb2f43ca54e65e0fab64fd5d9d59f4d97b01b
Paul S. Owen <psotfx@users.sourceforge.net>
2001-10-01 23:56:01 +0000

    This is getting boring ... I FTP, or think I do and yet still these files get through unchanged ...
    git-svn-id: file:///svn/phpbb/trunk@1115 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/functions.php
+++ phpBB/includes/functions.php
@@ -893,12 +893,12 @@ function obtain_word_list(&$orig_word, &$replacement_word)
 //
 function username_search($calling_script, $search_match, $enable_window = 0, $default_list = "")
 {
-	global $db, $template, $lang;
+	global $db, $template, $lang, $theme;
 
 	if( $enable_window )
 	{
 		$template->set_filenames(array(
-			"body" => "mini_search.tpl")
+			"minisearchbody" => "mini_search.tpl")
 		);
 	}
 
@@ -941,7 +941,50 @@ function username_search($calling_script, $search_match, $enable_window = 0, $de
 
 	if( $enable_window )
 	{
+		// 
+		// This theme var setup is very very temporary, completely
+		// forgot about it, doh!
+		//
 		$template->assign_vars(array(
+			"T_HEAD_STYLESHEET" => $theme['head_stylesheet'],
+			"T_BODY_BACKGROUND" => $theme['body_background'],
+			"T_BODY_BGCOLOR" => "#".$theme['body_bgcolor'],
+			"T_BODY_TEXT" => "#".$theme['body_text'],
+			"T_BODY_LINK" => "#".$theme['body_link'],
+			"T_BODY_VLINK" => "#".$theme['body_vlink'],
+			"T_BODY_ALINK" => "#".$theme['body_alink'],
+			"T_BODY_HLINK" => "#".$theme['body_hlink'],
+			"T_TR_COLOR1" => "#".$theme['tr_color1'],
+			"T_TR_COLOR2" => "#".$theme['tr_color2'],
+			"T_TR_COLOR3" => "#".$theme['tr_color3'],
+			"T_TR_CLASS1" => $theme['tr_class1'],
+			"T_TR_CLASS2" => $theme['tr_class2'],
+			"T_TR_CLASS3" => $theme['tr_class3'],
+			"T_TH_COLOR1" => "#".$theme['th_color1'],
+			"T_TH_COLOR2" => "#".$theme['th_color2'],
+			"T_TH_COLOR3" => "#".$theme['th_color3'],
+			"T_TH_CLASS1" => $theme['th_class1'],
+			"T_TH_CLASS2" => $theme['th_class2'],
+			"T_TH_CLASS3" => $theme['th_class3'],
+			"T_TD_COLOR1" => "#".$theme['td_color1'],
+			"T_TD_COLOR2" => "#".$theme['td_color2'],
+			"T_TD_COLOR3" => "#".$theme['td_color3'],
+			"T_TD_CLASS1" => $theme['td_class1'],
+			"T_TD_CLASS2" => $theme['td_class2'],
+			"T_TD_CLASS3" => $theme['td_class3'],
+			"T_FONTFACE1" => $theme['fontface1'],
+			"T_FONTFACE2" => $theme['fontface2'],
+			"T_FONTFACE3" => $theme['fontface3'],
+			"T_FONTSIZE1" => $theme['fontsize1'],
+			"T_FONTSIZE2" => $theme['fontsize2'],
+			"T_FONTSIZE3" => $theme['fontsize3'],
+			"T_FONTCOLOR1" => "#".$theme['fontcolor1'],
+			"T_FONTCOLOR2" => "#".$theme['fontcolor2'],
+			"T_FONTCOLOR3" => "#".$theme['fontcolor3'],
+			"T_SPAN_CLASS1" => $theme['span_class1'],
+			"T_SPAN_CLASS2" => $theme['span_class2'],
+			"T_SPAN_CLASS3" => $theme['span_class3'],
+			
 			"L_CLOSE_WINDOW" => $lang['Close_window'], 
 			"L_SEARCH_USERNAME" => $lang['Find_username'], 
 			"L_UPDATE_USERNAME" => $lang['Select_username'], 
@@ -951,7 +994,7 @@ function username_search($calling_script, $search_match, $enable_window = 0, $de
 			"S_SEARCH_ACTION" => append_sid("$calling_script?mode=searchuser"))
 		);
 
-		$template->pparse("body");
+		$template->pparse("minisearchbody");
 	}
 
 	return($author_list);
