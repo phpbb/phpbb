30fd1f8757a54b7795b8d196b34a208d871c72f8
Paul S. Owen <psotfx@users.sourceforge.net>
2001-10-14 18:32:48 +0000

    Doug, whooops .... :)
    git-svn-id: file:///svn/phpbb/trunk@1191 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/config.php
+++ phpBB/config.php
@@ -1,19 +1,4 @@
-<?php
 
 //
-// phpBB 2.x auto-generated config file
-// Do not change anything in this file!
+// RUN install.php!
 //
-
-$dbms = "mysql";
-
-$dbhost = "127.0.0.1";
-$dbname = "devphpbb2";
-$dbuser = "dougk";
-$dbpasswd = "2d8o0u7g026";
-
-$table_prefix = "phpbb_";
-
-define('PHPBB_INSTALLED', true);
-
-?>
