40ddc64afed38f66e100b0aa0882dc3c57c05cf8
Paul S. Owen <psotfx@users.sourceforge.net>
2001-11-16 15:16:14 +0000

    Updated FAQ, links, more text, etc. ... I was bored
    git-svn-id: file:///svn/phpbb/trunk@1331 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/faq.php
+++ phpBB/faq.php
@@ -33,18 +33,87 @@ init_userprefs($userdata);
 // End session management
 //
 
-include($phpbb_root_path . 'includes/page_header.'.$phpEx);
+//
+// Load the appropriate faq file
+//
 include($phpbb_root_path . 'language/lang_' . $board_config['default_lang'] . '/lang_faq.' . $phpEx);
 
-$template->set_filenames(array(
-	"body" => "faq_body.tpl")
-);
-
-$template->assign_vars(array("L_FAQ" => $lang['FAQ']));
-
+//
+// Pull the array data from the lang pack
+//
+$j = 0;
+$counter = 0;
+$counter_2 = 0;
+$faq_block = array();
 for($i = 0; $i < count($faq); $i++)
 {
-	$template->assign_block_vars("faqrow", array("FAQ_QUESTION" => $faq[$i][0], "FAQ_ANSWER" => $faq[$i][1]));
+	if( $faq[$i][0] != "--" )
+	{
+		$faq_block[$j][$counter]['id'] = $counter_2;
+		$faq_block[$j][$counter]['question'] = $faq[$i][0];
+		$faq_block[$j][$counter]['answer'] = $faq[$i][1];
+
+		$counter++;
+		$counter_2++;
+	}
+	else
+	{
+		$counter = 0;
+		$j++;
+	}
+}
+
+//
+// Lets build a page ...
+//
+$page_title = $lang['FAQ'];
+include($phpbb_root_path . 'includes/page_header.'.$phpEx);
+
+$template->set_filenames(array(
+	"body" => "faq_body.tpl",
+	"jumpbox" => "jumpbox.tpl")
+);
+
+$jumpbox = make_jumpbox($forum_id);
+$template->assign_vars(array(
+	"L_GO" => $lang['Go'],
+	"L_JUMP_TO" => $lang['Jump_to'],
+	"L_SELECT_FORUM" => $lang['Select_forum'],
+
+	"S_JUMPBOX_LIST" => $jumpbox,
+	"S_JUMPBOX_ACTION" => append_sid("viewforum.$phpEx"))
+);
+$template->assign_var_from_handle("JUMPBOX", "jumpbox");
+
+$template->assign_vars(array(
+	"L_FAQ" => $lang['FAQ'])
+);
+
+for($i = 0; $i < count($faq_block); $i++)
+{
+	if( count($faq_block[$i]) )
+	{
+		$template->assign_block_vars("faq_block", array(
+			"BLOCK_TITLE" => $faq_block[$i]['title'])
+		);
+		$template->assign_block_vars("faq_block_link", array( 
+			"BLOCK_TITLE" => $faq_block[$i]['title'])
+		);
+
+		for($j = 0; $j < count($faq_block[$i]); $j++)
+		{
+			$template->assign_block_vars("faq_block.faq_row", array(
+				"FAQ_QUESTION" => $faq_block[$i][$j]['question'], 
+				"FAQ_ANSWER" => $faq_block[$i][$j]['answer'], 
+				"U_FAQ_ID" => $faq_block[$i][$j]['id'])
+			);
+
+			$template->assign_block_vars("faq_block_link.faq_row_link", array(
+				"FAQ_LINK" => $faq_block[$i][$j]['question'], 
+				"U_FAQ_LINK" => "#" . $faq_block[$i][$j]['id'])
+			);
+		}
+	}
 }
 
 $template->pparse("body");
--- phpBB/templates/subSilver/faq_body.tpl
+++ phpBB/templates/subSilver/faq_body.tpl
@@ -1,16 +1,51 @@
-<table border="0" cellpadding="3" cellspacing="1" width="100%" class="forumline">
+
+<table width="100%" cellspacing="2" cellpadding="2" border="0">
   <tr> 
-	<th class="thHead">{L_FAQ}</th>
+	<td align="left" valign="bottom" nowrap="nowrap"><span class="nav"><a href="{U_INDEX}" class="nav">{SITENAME}&nbsp;{L_INDEX}</a></span></td>
   </tr>
-  <!-- BEGIN faqrow -->  
+</table>
+
+<table border="0" cellpadding="3" cellspacing="1" width="100%" class="forumline" align="center">
+	<tr>
+		<th class="thHead">{L_FAQ}</th>
+	</tr>
+	<tr>
+		<td align="left" valign="top" class="row1"><table cellspacing="3" cellpadding="0" border="0">
+			<tr> 
+				<td><br /></td>
+			</tr>
+			<!-- BEGIN faq_block_link -->
+			<!-- BEGIN faq_row_link -->  
+			<tr>
+				<td><span class="gen"><a href="{faq_block_link.faq_row_link.U_FAQ_LINK}">{faq_block_link.faq_row_link.FAQ_LINK}</a></span></td>
+			</tr>
+			<!-- END faq_row_link -->
+			<tr> 
+				<td><br /></td>
+			</tr>
+			<!-- END faq_block_link -->
+		</table></td>
+	</tr>
+	<tr> 
+		<td height="28" class="cat">&nbsp;</td>
+	</tr>
+	<!-- BEGIN faq_block -->
+	<!-- BEGIN faq_row -->  
+	<tr> 
+		<td align="left" valign="top" class="row1"><span class="postbody"><a name="{faq_block.faq_row.U_FAQ_ID}"></a><b>{faq_block.faq_row.FAQ_QUESTION}</b><br /><span class="postbody">{faq_block.faq_row.FAQ_ANSWER}</span></td>
+	</tr>
+	<tr>
+		<td height="1" class="spaceRow"><img src="templates/subSilver/images/spacer.gif" alt="" width="1" height="1" /></td>
+	</tr>
+	<!-- END faq_row -->
+	<tr> 
+		<td height="28" class="cat">&nbsp;</td>
+	</tr>
+	<!-- END faq_block -->
+</table>
+
+<table width="100%" cellspacing="2" border="0" align="center">
   <tr> 
-	<td width="22%" align="left" valign="top" class="row1"><span class="name"><b>{faqrow.FAQ_QUESTION}</b></span><br />
-	  <br />
-	  <span class="postbody">{faqrow.FAQ_ANSWER}</span>
-	</td>
+	  <td align="right" valign="middle" nowrap="nowrap"><span class="gensmall">{S_TIMEZONE}</span><br /><br />{JUMPBOX}</td> 
   </tr>
-  <tr> 
-	<td colspan="2" height="1" class="spaceRow"><img src="templates/subSilver/images/spacer.gif" alt="" width="1" height="1" /></td>
-  </tr>
-  <!-- END faqrow -->
 </table>
