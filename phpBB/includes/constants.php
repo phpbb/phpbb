bd4f94c0a91771ceed1a674d310476bef4311dfa
Paul S. Owen <psotfx@users.sourceforge.net>
2002-01-25 00:39:16 +0000

    Fix defines for warnings
    git-svn-id: file:///svn/phpbb/trunk@1937 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/constants.php
+++ phpBB/includes/constants.php
@@ -2,8 +2,8 @@
 /***************************************************************************
  *                               includes.php
  *                            -------------------
- *   begin                : Saturday, Feb 13, 2001
- *   copyright            : (C) 2001 The phpBB Group
+ *   begin                : Saturday', Feb 13', 2001
+ *   copyright            : ('C) 2001 The phpBB Group
  *   email                : support@phpbb.com
  *
  *   $Id$
@@ -15,130 +15,130 @@
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ *   the Free Software Foundation; either version 2 of the License', or
+ *   ('at your option) any later version.
  *
  ***************************************************************************/
 
 // Debug Level
-define(DEBUG, 1); // Debugging on
-//define(DEBUG, 0); // Debugging off
+define('DEBUG', 1); // Debugging on
+//define('DEBUG', 0); // Debugging off
 
 
 // User Levels <- Do not change the values of USER or ADMIN
-define(DELETED, -1);
-define(ANONYMOUS, -1);
+define('DELETED', -1);
+define('ANONYMOUS', -1);
 
-define(USER, 0);
-define(ADMIN, 1);
-define(MOD, 2);
+define('USER', 0);
+define('ADMIN', 1);
+define('MOD', 2);
 
 
 // User related
-define(USER_ACTIVATION_NONE, 0);
-define(USER_ACTIVATION_SELF, 1);
-define(USER_ACTIVATION_ADMIN, 2);
+define('USER_ACTIVATION_NONE', 0);
+define('USER_ACTIVATION_SELF', 1);
+define('USER_ACTIVATION_ADMIN', 2);
 
-define(USER_AVATAR_NONE, 0);
-define(USER_AVATAR_UPLOAD, 1);
-define(USER_AVATAR_REMOTE, 2);
-define(USER_AVATAR_GALLERY, 3);
+define('USER_AVATAR_NONE', 0);
+define('USER_AVATAR_UPLOAD', 1);
+define('USER_AVATAR_REMOTE', 2);
+define('USER_AVATAR_GALLERY', 3);
 
 
 // Group settings
-define(GROUP_OPEN, 0);
-define(GROUP_CLOSED, 1);
-define(GROUP_HIDDEN, 2);
+define('GROUP_OPEN', 0);
+define('GROUP_CLOSED', 1);
+define('GROUP_HIDDEN', 2);
 
 
 // Forum state
-define(FORUM_UNLOCKED, 0);
-define(FORUM_LOCKED, 1);
+define('FORUM_UNLOCKED', 0);
+define('FORUM_LOCKED', 1);
 
 
 // Topic status
-define(TOPIC_UNLOCKED, 0);
-define(TOPIC_LOCKED, 1);
-define(TOPIC_MOVED, 2);
-define(TOPIC_WATCH_NOTIFIED, 1);
-define(TOPIC_WATCH_UN_NOTIFIED, 0);
+define('TOPIC_UNLOCKED', 0);
+define('TOPIC_LOCKED', 1);
+define('TOPIC_MOVED', 2);
+define('TOPIC_WATCH_NOTIFIED', 1);
+define('TOPIC_WATCH_UN_NOTIFIED', 0);
 
 
 // Topic types
-define(POST_NORMAL, 0);
-define(POST_STICKY, 1);
-define(POST_ANNOUNCE, 2);
-define(POST_GLOBAL_ANNOUNCE, 3);
+define('POST_NORMAL', 0);
+define('POST_STICKY', 1);
+define('POST_ANNOUNCE', 2);
+define('POST_GLOBAL_ANNOUNCE', 3);
 
 
 // SQL codes
-define(BEGIN_TRANSACTION, 1);
-define(END_TRANSACTION, 2);
+define('BEGIN_TRANSACTION', 1);
+define('END_TRANSACTION', 2);
 
 
 // Error codes
-define(GENERAL_MESSAGE, 200);
-define(GENERAL_ERROR, 202);
-define(CRITICAL_MESSAGE, 203);
-define(CRITICAL_ERROR, 204);
+define('GENERAL_MESSAGE', 200);
+define('GENERAL_ERROR', 202);
+define('CRITICAL_MESSAGE', 203);
+define('CRITICAL_ERROR', 204);
 
 
 // Private messaging
-define(PRIVMSGS_READ_MAIL, 0);
-define(PRIVMSGS_NEW_MAIL, 1);
-define(PRIVMSGS_SENT_MAIL, 2);
-define(PRIVMSGS_SAVED_IN_MAIL, 3);
-define(PRIVMSGS_SAVED_OUT_MAIL, 4);
+define('PRIVMSGS_READ_MAIL', 0);
+define('PRIVMSGS_NEW_MAIL', 1);
+define('PRIVMSGS_SENT_MAIL', 2);
+define('PRIVMSGS_SAVED_IN_MAIL', 3);
+define('PRIVMSGS_SAVED_OUT_MAIL', 4);
 
 
 // URL PARAMETERS
-define(POST_TOPIC_URL, 't');
-define(POST_CAT_URL, 'c');
-define(POST_FORUM_URL, 'f');
-define(POST_USERS_URL, 'u');
-define(POST_POST_URL, 'p');
-define(POST_GROUPS_URL, 'g');
+define('POST_TOPIC_URL', 't');
+define('POST_CAT_URL', 'c');
+define('POST_FORUM_URL', 'f');
+define('POST_USERS_URL', 'u');
+define('POST_POST_URL', 'p');
+define('POST_GROUPS_URL', 'g');
 
 // Session parameters
-define(SESSION_METHOD_COOKIE, 100);
-define(SESSION_METHOD_GET, 101);
+define('SESSION_METHOD_COOKIE', 100);
+define('SESSION_METHOD_GET', 101);
 
 
 // Page numbers for session handling
-define(PAGE_INDEX, 0);
-define(PAGE_LOGIN, -1);
-define(PAGE_SEARCH, -2);
-define(PAGE_REGISTER, -3);
-define(PAGE_PROFILE, -4);
-define(PAGE_VIEWONLINE, -6);
-define(PAGE_VIEWMEMBERS, -7);
-define(PAGE_FAQ, -8);
-define(PAGE_POSTING, -9);
-define(PAGE_PRIVMSGS, -10);
-define(PAGE_GROUPCP, -11);
-define(PAGE_TOPIC_OFFSET, 5000);
+define('PAGE_INDEX', 0);
+define('PAGE_LOGIN', -1);
+define('PAGE_SEARCH', -2);
+define('PAGE_REGISTER', -3);
+define('PAGE_PROFILE', -4);
+define('PAGE_VIEWONLINE', -6);
+define('PAGE_VIEWMEMBERS', -7);
+define('PAGE_FAQ', -8);
+define('PAGE_POSTING', -9);
+define('PAGE_PRIVMSGS', -10);
+define('PAGE_GROUPCP', -11);
+define('PAGE_TOPIC_OFFSET', 5000);
 
 
 // Auth settings
-define(AUTH_LIST_ALL, 0);
-define(AUTH_ALL, 0);
+define('AUTH_LIST_ALL', 0);
+define('AUTH_ALL', 0);
 
-define(AUTH_REG, 1);
-define(AUTH_ACL, 2);
-define(AUTH_MOD, 3);
-define(AUTH_ADMIN, 5);
+define('AUTH_REG', 1);
+define('AUTH_ACL', 2);
+define('AUTH_MOD', 3);
+define('AUTH_ADMIN', 5);
 
-define(AUTH_VIEW, 1);
-define(AUTH_READ, 2);
-define(AUTH_POST, 3);
-define(AUTH_REPLY, 4);
-define(AUTH_EDIT, 5);
-define(AUTH_DELETE, 6);
-define(AUTH_ANNOUNCE, 7);
-define(AUTH_STICKY, 8);
-define(AUTH_POLLCREATE, 9);
-define(AUTH_VOTE, 10);
-define(AUTH_ATTACH, 11);
+define('AUTH_VIEW', 1);
+define('AUTH_READ', 2);
+define('AUTH_POST', 3);
+define('AUTH_REPLY', 4);
+define('AUTH_EDIT', 5);
+define('AUTH_DELETE', 6);
+define('AUTH_ANNOUNCE', 7);
+define('AUTH_STICKY', 8);
+define('AUTH_POLLCREATE', 9);
+define('AUTH_VOTE', 10);
+define('AUTH_ATTACH', 11);
 
 
 // Table names
