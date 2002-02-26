10ef4498dc063245e94bb658b91dc775d9c1fe81
the_systech <the_systech@users.sourceforge.net>
2002-02-26 16:35:02 +0000

    Added false for the optional replace parameter of the header for Cache-Control.. This might actually fix some issues with gzip compression and win98/IIS...
    git-svn-id: file:///svn/phpbb/trunk@2217 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/admin/page_header_admin.php
+++ phpBB/admin/page_header_admin.php
@@ -160,7 +160,7 @@ $template->assign_vars(array(
 );
 
 header ("Cache-Control: no-store, no-cache, must-revalidate");
-header ("Cache-Control: pre-check=0, post-check=0, max-age=0");
+header ("Cache-Control: pre-check=0, post-check=0, max-age=0", false);
 header ("Pragma: no-cache");
 header ("Expires: " . gmdate("D, d M Y H:i:s", time()) . " GMT");
 header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
--- phpBB/includes/page_header.php
+++ phpBB/includes/page_header.php
@@ -468,7 +468,7 @@ else
 }
 
 header ("Cache-Control: no-store, no-cache, must-revalidate");
-header ("Cache-Control: pre-check=0, post-check=0, max-age=0");
+header ("Cache-Control: pre-check=0, post-check=0, max-age=0", false);
 header ("Pragma: no-cache");
 header ("Expires: " . gmdate("D, d M Y H:i:s", time()) . " GMT");
 header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
