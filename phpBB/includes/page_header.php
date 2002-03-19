129beaebc3b8b9bceb3483b5c79857954b621ac3
the_systech <the_systech@users.sourceforge.net>
2002-03-19 21:10:14 +0000

    Fix for AOL browser issues as well as some IE blank Gzip pages..
    git-svn-id: file:///svn/phpbb/trunk@2358 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/page_header.php
+++ phpBB/includes/page_header.php
@@ -478,8 +478,7 @@ else
 	}
 }
 
-header ("Cache-Control: no-store, no-cache, must-revalidate");
-header ("Cache-Control: pre-check=0, post-check=0, max-age=0", false);
+header ("Cache-Control: no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0");
 header ("Pragma: no-cache");
 header ("Expires: " . gmdate("D, d M Y H:i:s", time()) . " GMT");
 header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
