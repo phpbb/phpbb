8e3f83af92f2f54a6de74ed1a680b06b3d3d28da
Paul S. Owen <psotfx@users.sourceforge.net>
2002-03-22 13:48:43 +0000

    Probably get moaned at for this but it's consistent with the non-admin files and prevents it being 'viewed' by nosey persons without need for .htaccess
    git-svn-id: file:///svn/phpbb/trunk@2395 89ea8834-ac86-4346-8a33-228a782c2dd0

---
+++ phpBB/admin/pagestart.php
@@ -0,0 +1,56 @@
+<?php
+/***************************************************************************
+ *                               pagestart.php
+ *                            -------------------
+ *   begin                : Thursday, Aug 2, 2001
+ *   copyright            : (C) 2001 The phpBB Group
+ *   email                : support@phpbb.com
+ *
+ *   $Id$
+ *
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
+if ( !defined('IN_PHPBB') )
+{
+	die("Hacking attempt");
+}
+
+define('IN_ADMIN', true);
+
+include($phpbb_root_path . 'common.'.$phpEx);
+
+//
+// Start session management
+//
+$userdata = session_pagestart($user_ip, PAGE_INDEX);
+init_userprefs($userdata);
+//
+// End session management
+//
+if( !$userdata['session_logged_in'] )
+{
+	header("Location: ../" . append_sid("login.$phpEx?redirect=admin/"));
+}
+else if( $userdata['user_level'] != ADMIN )
+{
+	message_die(GENERAL_MESSAGE, $lang['Not_admin']);
+}
+
+if ( empty($no_page_header) )
+{
+	// Not including the pageheader can be neccesarry if META tags are
+	// needed in the calling script.
+	include('page_header_admin.'.$phpEx);
+}
+
+?>
