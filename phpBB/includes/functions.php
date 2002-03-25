5b176a2effab90fa28a91fbbd9821453d562d1b1
Paul S. Owen <psotfx@users.sourceforge.net>
2002-03-25 12:39:17 +0000

    Fix bad word cockup ... oh look the censor failed and let cockup through, oh, and again ... geez sourceforge needs better censors for its CVS system
    git-svn-id: file:///svn/phpbb/trunk@2426 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/functions.php
+++ phpBB/includes/functions.php
@@ -445,8 +445,8 @@ function obtain_word_list(&$orig_word, &$replacement_word)
 	{
 		do 
 		{
-			$orig_word[] = '#(\W)' . str_replace('\*', '\w*?', preg_quote($row['word']) ) . '(\W)#is';
-			$replacement_word[] = '\1' . $row['replacement'] . '\2';
+			$orig_word[] = "#\b(" . str_replace("\*", "\w*?", phpbb_preg_quote($row['word'], "#")) . ")\b#i";
+			$replacement_word[] = $row['replacement'];
 		}
 		while ( $row = $db->sql_fetchrow($result) );
 	}
