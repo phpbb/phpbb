427be2243a251598e8a9629d8e63f3430b0294d2
Paul S. Owen <psotfx@users.sourceforge.net>
2002-04-07 00:51:47 +0000

    Minor changes
    git-svn-id: file:///svn/phpbb/branches/phpBB-2_0_0@2499 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/admin/admin_mass_email.php
+++ phpBB/admin/admin_mass_email.php
@@ -33,7 +33,7 @@ if( !empty($setmodules) )
 // Load default header
 //
 $no_page_header = TRUE;
-$phpbb_root_path = "../";
+$phpbb_root_path = '../';
 require($phpbb_root_path . 'extension.inc');
 require('pagestart.' . $phpEx);
 
@@ -43,8 +43,8 @@ require('pagestart.' . $phpEx);
 //
 @set_time_limit(1200);
 
-$message = "";
-$subject = "";
+$message = '';
+$subject = '';
 
 //
 // Do the job ...
@@ -66,11 +66,14 @@ if( isset($HTTP_POST_VARS['submit']) )
 		$sql = "SELECT user_email 
 			FROM " . USERS_TABLE;
 	}
-
-	if( !$result = $db->sql_query($sql) )
+	if( !($result = $db->sql_query($sql)) )
 	{
-		message_die(GENERAL_ERROR, "Coult not select group members!", __LINE__, __FILE__, $sql);
+		message_die(GENERAL_ERROR, 'Could not select group members', '', __LINE__, __FILE__, $sql);
 	}
+
+
+
+
 
 	if( !$db->sql_numrows($result) )
 	{
@@ -81,9 +84,13 @@ if( isset($HTTP_POST_VARS['submit']) )
 	}
 
 	$email_list = $db->sql_fetchrowset($g_result);
+
+
+
 	
-	$subject = stripslashes($HTTP_POST_VARS["subject"]);
-	$message = stripslashes($HTTP_POST_VARS["message"]);
+
+	$subject = stripslashes($HTTP_POST_VARS['subject']);
+	$message = stripslashes($HTTP_POST_VARS['message']);
 	
 	//
 	// Error checking needs to go here ... if no subject and/or
@@ -107,39 +114,35 @@ if( isset($HTTP_POST_VARS['submit']) )
 		}
 		$emailer = new emailer($board_config['smtp_delivery']);
 	
-		$email_headers = "From: " . $board_config['board_email'] . "\n";
+		$email_headers = 'From: ' . $board_config['board_email'] . "\n";
 
-		$bcc_list = "";
+		$bcc_list = '';
 		for($i = 0; $i < count($email_list); $i++)
 		{
-			if( $bcc_list != "" )
-			{
-				$bcc_list .= ", ";
-			}
-			$bcc_list .= $email_list[$i]['user_email'];
+			$bcc_list .= ( ( $bcc_list != '' ) ? ', ' : '' ) . $email_list[$i]['user_email'];
 		}
 		$email_headers .= "Bcc: $bcc_list\n";
 		
-		$email_headers .= "Return-Path: " . $userdata['board_email'] . "\n";
-		$email_headers .= "X-AntiAbuse: Board servername - " . $server_name . "\n";
-		$email_headers .= "X-AntiAbuse: User_id - " . $userdata['user_id'] . "\n";
-		$email_headers .= "X-AntiAbuse: Username - " . $userdata['username'] . "\n";
-		$email_headers .= "X-AntiAbuse: User IP - " . decode_ip($user_ip) . "\r\n";
+		$email_headers .= 'Return-Path: ' . $userdata['board_email'] . "\n";
+		$email_headers .= 'X-AntiAbuse: Board servername - ' . $server_name . "\n";
+		$email_headers .= 'X-AntiAbuse: User_id - ' . $userdata['user_id'] . "\n";
+		$email_headers .= 'X-AntiAbuse: Username - ' . $userdata['username'] . "\n";
+		$email_headers .= 'X-AntiAbuse: User IP - ' . decode_ip($user_ip) . "\r\n";
 
-		$emailer->use_template("admin_send_email");
+		$emailer->use_template('admin_send_email');
 		$emailer->email_address($board_config['board_email']);
 		$emailer->set_subject($subject);
 		$emailer->extra_headers($email_headers);
 
 		$emailer->assign_vars(array(
-			"SITENAME" => $board_config['sitename'], 
-			"BOARD_EMAIL" => $board_config['board_email'], 
-			"MESSAGE" => $message)
+			'SITENAME' => $board_config['sitename'], 
+			'BOARD_EMAIL' => $board_config['board_email'], 
+			'MESSAGE' => $message)
 		);
 		$emailer->send();
 		$emailer->reset();
 
-		$message = $lang['Email_sent'] . "<br /><br />" . sprintf($lang['Click_return_admin_index'],  "<a href=\"" . append_sid("index.$phpEx?pane=right") . "\">", "</a>");
+		$message = $lang['Email_sent'] . '<br /><br />' . sprintf($lang['Click_return_admin_index'],  '<a href="' . append_sid("index.$phpEx?pane=right") . '">', '</a>');
 
 		message_die(GENERAL_MESSAGE, $message);
 	}
@@ -152,18 +155,21 @@ if( isset($HTTP_POST_VARS['submit']) )
 $sql = "SELECT group_id, group_name 
 	FROM ".GROUPS_TABLE . "  
 	WHERE group_single_user <> 1";
-$g_result = $db->sql_query($sql);
-$group_list = $db->sql_fetchrowset($g_result);
-
-$select_list = '<select name = "' . POST_GROUPS_URL . '">';
-$select_list .= '<option value = "-1">' . $lang['All_users'] . '</option>';
-
-for($i = 0;$i < count($group_list); $i++)
+if ( !($result = $db->sql_query($sql)) ) 
 {
-	$select_list .= "<option value = \"" . $group_list[$i]['group_id'];
-	$select_list .= "\">" . $group_list[$i]['group_name'] . "</option>";
+	message_die(GENERAL_ERROR, 'Could not obtain list of groups', '', __LINE__, __FILE__, $sql);
 }
-$select_list .= "</select>";
+
+$select_list = '<select name = "' . POST_GROUPS_URL . '"><option value = "-1">' . $lang['All_users'] . '</option>';
+if ( $row = $db->sql_fetchrow($result) )
+{
+	do
+	{
+		$select_list .= '<option value = "' . $row['group_id'] . '">' . $row['group_name'] . '</option>';
+	}
+	while ( $row = $db->sql_fetchrow($result) );
+}
+$select_list .= '</select>';
 
 //
 // Generate page
@@ -171,24 +177,24 @@ $select_list .= "</select>";
 include('page_header_admin.'.$phpEx);
 
 $template->set_filenames(array(
-	"body" => "admin/user_email_body.tpl")
+	'body' => 'admin/user_email_body.tpl')
 );
 
 $template->assign_vars(array(
-	"MESSAGE" => $message,
-	"SUBJECT" => $subject, 
+	'MESSAGE' => $message,
+	'SUBJECT' => $subject, 
 
-	"L_EMAIL_TITLE" => $lang['Email'],
-	"L_EMAIL_EXPLAIN" => $lang['Mass_email_explain'],
-	"L_COMPOSE" => $lang['Compose'],
-	"L_RECIPIENTS" => $lang['Recipients'],
-	"L_EMAIL_SUBJECT" => $lang['Subject'],
-	"L_EMAIL_MSG" => $lang['Message'],
-	"L_EMAIL" => $lang['Email'],
-	"L_NOTICE" => $notice,
+	'L_EMAIL_TITLE' => $lang['Email'],
+	'L_EMAIL_EXPLAIN' => $lang['Mass_email_explain'],
+	'L_COMPOSE' => $lang['Compose'],
+	'L_RECIPIENTS' => $lang['Recipients'],
+	'L_EMAIL_SUBJECT' => $lang['Subject'],
+	'L_EMAIL_MSG' => $lang['Message'],
+	'L_EMAIL' => $lang['Email'],
+	'L_NOTICE' => $notice,
 
-	"S_USER_ACTION" => append_sid('admin_mass_email.'.$phpEx),
-	"S_GROUP_SELECT" => $select_list)
+	'S_USER_ACTION' => append_sid('admin_mass_email.'.$phpEx),
+	'S_GROUP_SELECT' => $select_list)
 );
 
 $template->pparse('body');
