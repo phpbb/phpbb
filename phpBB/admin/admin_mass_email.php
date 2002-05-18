f585bec511e2105ea6a5d4be11aa33ad84120af7
Paul S. Owen <psotfx@users.sourceforge.net>
2002-05-18 02:20:09 +0000

    Well, after accidently mass emailing members of my own board (at least twice :oops: ) I think this is behaving now ...
    git-svn-id: file:///svn/phpbb/branches/phpBB-2_0_0@2594 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/admin/admin_mass_email.php
+++ phpBB/admin/admin_mass_email.php
@@ -117,11 +117,11 @@ if ( isset($HTTP_POST_VARS['submit']) )
 		$emailer = new emailer($board_config['smtp_delivery']);
 	
 		$email_headers = 'Return-Path: ' . $userdata['board_email'] . "\r\nFrom: " . $board_config['board_email'] . "\r\n";
-		$email_headers .= "Bcc: $bcc_list\r\n";
-		$email_headers .= 'X-AntiAbuse: Board servername - ' . $server_name . "\r\n";
+		$email_headers .= 'X-AntiAbuse: Board servername - ' . $board_config['server_name'] . "\r\n";
 		$email_headers .= 'X-AntiAbuse: User_id - ' . $userdata['user_id'] . "\r\n";
 		$email_headers .= 'X-AntiAbuse: Username - ' . $userdata['username'] . "\r\n";
 		$email_headers .= 'X-AntiAbuse: User IP - ' . decode_ip($user_ip) . "\r\n";
+		$email_headers .= "Bcc: $bcc_list\r\n";
 
 		$emailer->use_template('admin_send_email');
 		$emailer->email_address($board_config['board_email']);
