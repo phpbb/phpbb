375470282542dce9e059a3ec70374a878cb68610
Paul S. Owen <psotfx@users.sourceforge.net>
2002-08-05 00:48:32 +0000

    what tintyint? I see no tintyint ....
    git-svn-id: file:///svn/phpbb/trunk@2819 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/install/schemas/mysql_schema.sql
+++ phpBB/install/schemas/mysql_schema.sql
@@ -614,7 +614,7 @@ CREATE TABLE phpbb_users (
    user_id mediumint(8) NOT NULL auto_increment,
 
    user_active tinyint(1) DEFAULT '1',
-   user_founder tintyint(1) DEFAULT '0' NOT NULL, 
+   user_founder tinyint(1) DEFAULT '0' NOT NULL, 
    user_ip varchar(40), 
    user_regdate int(11) DEFAULT '0' NOT NULL, 
    username varchar(30) NOT NULL,
