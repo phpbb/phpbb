4b896717ccfa594315836a139e7664a5228a2925
dougk_ff7 <dougk_ff7@users.sourceforge.net>
2002-08-07 17:20:31 +0000

    Oops, forgot to define $board_config as a global....
    git-svn-id: file:///svn/phpbb/branches/phpBB-2_0_0@2841 89ea8834-ac86-4346-8a33-228a782c2dd0

---
+++ phpBB/config.php
@@ -0,0 +1,28 @@
+<?php
+
+//
+// phpBB 2.x auto-generated config file
+// Do not change anything in this file!
+//
+
+$dbms = 'mysql';
+
+$dbhost = 'localhost';
+$dbname = 'phpbb2devboard';
+$dbuser = 'dougk';
+$dbpasswd = 'dougkff7';
+
+$table_prefix = 'phpbb_';
+
+/*
+$dbhost = 'localhost';
+$dbname = 'devphpbb2';
+$dbuser = 'dougk';
+$dbpasswd = 'dougkff7';
+
+$table_prefix = 'phpbb$_';
+*/
+
+define('PHPBB_INSTALLED', true);
+
+?>
