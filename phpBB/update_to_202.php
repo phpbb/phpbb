2bb3912a6c75638522009d810bfc56b9707dab35
Bart van Bragt <bartvb@users.sourceforge.net>
2002-11-27 22:45:43 +0000

    Fixed bug #753, group_signle_user, typo
    git-svn-id: file:///svn/phpbb/branches/phpBB-2_0_0@3120 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/update_to_202.php
+++ phpBB/update_to_202.php
@@ -286,7 +286,7 @@ switch ( $row['config_value'] )
 				$sql[] = "CREATE TABLE Tmp_" . GROUPS_TABLE . " 
 					(group_id int IDENTITY (1, 1) NOT NULL, group_type smallint NULL, group_name varchar(50) NOT NULL, group_description varchar(255) NOT NULL, group_moderator int NULL, group_single_user smallint NOT NULL) ON [PRIMARY]";
 				$sql[] = "INSERT INTO Tmp_" . GROPUS_TABLE . " (group_id, group_type, group_name, group_description, group_moderator, group_single_user)
-						SELECT group_id, group_type, group_name, group_description, group_moderator, group_signle_user FROM " . GROUPS_TABLE . " TABLOCKX";
+						SELECT group_id, group_type, group_name, group_description, group_moderator, group_single_user FROM " . GROUPS_TABLE . " TABLOCKX";
 				$sql[] = "DROP TABLE " . GROUPS_TABLE;
 				$sql[] = "EXECUTE sp_rename N'Tmp_" . GROUPS_TABLE . "', N'" . GROUPS_TABLE . "', 'OBJECT'";
 				$sql[] = "ALTER TABLE " . GROUPS_TABLE . " ADD 
@@ -399,7 +399,7 @@ switch ( $row['config_value'] )
 				$sql[] = "CREATE TABLE Tmp_" . GROUPS_TABLE . " 
 					(group_id int IDENTITY (1, 1) NOT NULL, group_type smallint NULL, group_name varchar(50) NOT NULL, group_description varchar(255) NOT NULL, group_moderator int NULL, group_single_user smallint NOT NULL) ON [PRIMARY]";
 				$sql[] = "INSERT INTO Tmp_" . GROPUS_TABLE . " (group_id, group_type, group_name, group_description, group_moderator, group_single_user)
-						SELECT group_id, group_type, group_name, group_description, group_moderator, group_signle_user FROM " . GROUPS_TABLE . " TABLOCKX";
+						SELECT group_id, group_type, group_name, group_description, group_moderator, group_single_user FROM " . GROUPS_TABLE . " TABLOCKX";
 				$sql[] = "DROP TABLE " . GROUPS_TABLE;
 				$sql[] = "EXECUTE sp_rename N'Tmp_" . GROUPS_TABLE . "', N'" . GROUPS_TABLE . "', 'OBJECT'";
 				$sql[] = "ALTER TABLE " . GROUPS_TABLE . " ADD 
@@ -430,7 +430,7 @@ switch ( $row['config_value'] )
 				$sql[] = "CREATE TABLE Tmp_" . GROUPS_TABLE . " 
 					(group_id int IDENTITY (1, 1) NOT NULL, group_type smallint NULL, group_name varchar(50) NOT NULL, group_description varchar(255) NOT NULL, group_moderator int NULL, group_single_user smallint NOT NULL) ON [PRIMARY]";
 				$sql[] = "INSERT INTO Tmp_" . GROPUS_TABLE . " (group_id, group_type, group_name, group_description, group_moderator, group_single_user)
-						SELECT group_id, group_type, group_name, group_description, group_moderator, group_signle_user FROM " . GROUPS_TABLE . " TABLOCKX";
+						SELECT group_id, group_type, group_name, group_description, group_moderator, group_single_user FROM " . GROUPS_TABLE . " TABLOCKX";
 				$sql[] = "DROP TABLE " . GROUPS_TABLE;
 				$sql[] = "EXECUTE sp_rename N'Tmp_" . GROUPS_TABLE . "', N'" . GROUPS_TABLE . "', 'OBJECT'";
 				$sql[] = "ALTER TABLE " . GROUPS_TABLE . " ADD 
