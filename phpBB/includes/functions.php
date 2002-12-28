f50e2d0df9c290486198b35f33a3cbf4dbc24b17
Paul S. Owen <psotfx@users.sourceforge.net>
2002-12-28 12:21:55 +0000

    Server port in wrong place in URL
    git-svn-id: file:///svn/phpbb/branches/phpBB-2_0_0@3246 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/functions.php
+++ phpBB/includes/functions.php
@@ -693,20 +693,20 @@ function redirect($url)
 
 	$server_protocol = ($board_config['cookie_secure']) ? 'https://' : 'http://';
 	$server_name = preg_replace('/^\/?(.*?)\/?$/', '\1', trim($board_config['server_name']));
-	$script_name = preg_replace('/^\/?(.*?)\/?$/', '/\1', trim($board_config['script_path']));
 	$server_port = ($board_config['server_port'] <> 80) ? ':' . trim($board_config['server_port']) . '/' : '/';
+	$script_name = preg_replace('/^\/?(.*?)\/?$/', '/\1', trim($board_config['script_path']));
 	$url = preg_replace('/^\/?(.*?\/)?$/', '\1', trim($url));
 
 	// Redirect via an HTML form for PITA webservers
 	if (@preg_match('/Microsoft|WebSTAR|Xitami/', getenv('SERVER_SOFTWARE')))
 	{
-		header('Refresh: 0; URL=' . $server_protocol . $server_name . $script_name . $server_port . $url);
-		echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><meta http-equiv="refresh" content="0; url=' . $server_protocol . $server_name . $script_name . $server_port . $url . '"><title>Redirect</title></head><body><div align="center">If your browser does not support meta redirection please click <a href="' . $server_protocol . $server_name . $script_name . $server_port . $url . '">HERE</a> to be redirected</div></body></html>';
+		header('Refresh: 0; URL=' . $server_protocol . $server_name . $server_port . $script_name . $url);
+		echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><meta http-equiv="refresh" content="0; url=' . $server_protocol . $server_name . $server_port . $script_name . $url . '"><title>Redirect</title></head><body><div align="center">If your browser does not support meta redirection please click <a href="' . $server_protocol . $server_name . $server_port . $script_name . $url . '">HERE</a> to be redirected</div></body></html>';
 		exit;
 	}
 
 	// Behave as per HTTP/1.1 spec for others
-	header('Location: ' . $server_protocol . $server_name . $script_name . $server_port . $url);
+	header('Location: ' . $server_protocol . $server_name . $server_port . $script_name . $url);
 	exit;
 }
 
