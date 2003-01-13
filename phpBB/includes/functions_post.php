eeb00fa1ec0a4cfde6e72cf01ba727016c0d0ed1
Paul S. Owen <psotfx@users.sourceforge.net>
2003-01-13 22:03:24 +0000

    Change to prevent on handlers messed up handling of any link containing "on" :D Fixed, hopefully retaining control of on handlers in all cases
    git-svn-id: file:///svn/phpbb/branches/phpBB-2_0_0@3320 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/functions_post.php
+++ phpBB/includes/functions_post.php
@@ -72,7 +72,7 @@ function prepare_message($message, $html_on, $bbcode_on, $smile_on, $bbcode_uid 
 				for ($i = 0; $i < sizeof($allowed_html_tags); $i++)
 				{
 					$match_tag = trim($allowed_html_tags[$i]);
-					if (preg_match('#^<\/?' . $match_tag . '(?!(.*?)((style)|(on[\w]+?))[\s]*?)#i', $hold_string))
+					if (preg_match('#^<\/?' . $match_tag . '(?!(.*?)((style)|( on[\w]+?[\s]?=))[\s]*?)#i', $hold_string))
 					{
 						$tagallowed = true;
 					}
