237daacdf1e0772fad7267e3b4106b37161f3f56
Paul S. Owen <psotfx@users.sourceforge.net>
2003-03-23 16:58:29 +0000

    Tidy things up a little more
    git-svn-id: file:///svn/phpbb/trunk@3700 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/adm/admin_phpinfo.php
+++ phpBB/adm/admin_phpinfo.php
@@ -43,12 +43,16 @@ phpinfo(INFO_GENERAL | INFO_CONFIGURATION | INFO_MODULES | INFO_VARIABLES);
 $phpinfo = ob_get_contents(); 
 ob_end_clean(); 
 
+// Here we play around a little with the PHP Info HTML to try and stylise
+// it along phpBB's lines ... hopefully without breaking anything. The idea
+// for this was nabbed from the PHP annotated manual
 preg_match_all('#<body[^>]*>(.*)</body>#siU', $phpinfo, $output); 
 $output = preg_replace('#<table#', '<table class="bg" align="center"', $output[1][0]);
 $output = preg_replace('#(\w),(\w)#', '\1, \2', $output);
 $output = preg_replace('#border="0" cellpadding="3" width="600"#', 'border="0" cellspacing="1" cellpadding="4" width="95%"', $output);
-$output = preg_replace('#<td>#', '<td class="row1">', $output);
-$output = preg_replace('#class="e"#', 'class="row1"', $output);
+$output = preg_replace('#<tr class="v"><td>(.*?<a .*?</a>)(.*?)</td></tr>#s', '<tr class="row1"><td><table width="100%" cellspacing="0" cellpadding="0" border="0"><tr><td>\2</td><td>\1</td></tr></table></td></tr>', $output);
+$output = preg_replace('#<td>#', '<td style="{background-color: #9999cc;}">', $output);
+$output = preg_replace('#class="e"#', 'class="row1" nowrap="nowrap"', $output);
 $output = preg_replace('#class="v"#', 'class="row2"', $output);
 $output = preg_replace('# class="h"#', '', $output);
 $output = preg_replace('#<hr />#', '', $output);
@@ -57,6 +61,8 @@ $output = $output[1][0];
 
 page_header($user->lang['PHP_INFO']);
 
+echo '<h1>' . $user->lang['PHP_INFO'] . '</h1>';
+echo '<p>' . $user->lang['PHP_INFO_EXPLAIN'] . '</p>';
 echo $output; 
 
 page_footer();
