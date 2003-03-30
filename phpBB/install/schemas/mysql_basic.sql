853cea315fbcda7d0826aa216ecbdc423d29abbc
Paul S. Owen <psotfx@users.sourceforge.net>
2003-03-30 16:11:50 +0000

    Couple of changes to f_ permissions
    git-svn-id: file:///svn/phpbb/trunk@3757 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/install/schemas/mysql_basic.sql
+++ phpBB/install/schemas/mysql_basic.sql
@@ -124,8 +124,9 @@ INSERT INTO phpbb_auth_options (auth_value, is_local) VALUES ('f_email', 1);
 INSERT INTO phpbb_auth_options (auth_value, is_local) VALUES ('f_rate', 1);
 INSERT INTO phpbb_auth_options (auth_value, is_local) VALUES ('f_print', 1);
 INSERT INTO phpbb_auth_options (auth_value, is_local) VALUES ('f_ignoreflood', 1);
-INSERT INTO phpbb_auth_options (auth_value, is_local) VALUES ('f_ignorequeue', 1);
 INSERT INTO phpbb_auth_options (auth_value, is_local) VALUES ('f_postcount', 1);
+INSERT INTO phpbb_auth_options (auth_value, is_local) VALUES ('f_moderate', 1);
+INSERT INTO phpbb_auth_options (auth_value, is_local) VALUES ('f_report', 1);
 
 INSERT INTO phpbb_auth_options (auth_value, is_local, is_global) VALUES ('m_', 1, 1);
 INSERT INTO phpbb_auth_options (auth_value, is_local, is_global) VALUES ('m_edit', 1, 1);
