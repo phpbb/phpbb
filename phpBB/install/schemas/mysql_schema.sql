d806c0a55ff7d829902cb9c09851a3e73d254bd1
Paul S. Owen <psotfx@users.sourceforge.net>
2003-06-03 22:05:49 +0000

    it's dhn's fault
    git-svn-id: file:///svn/phpbb/trunk@4078 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/install/schemas/mysql_schema.sql
+++ phpBB/install/schemas/mysql_schema.sql
@@ -868,6 +868,7 @@ CREATE TABLE phpbb_users (
    user_allow_viewonline tinyint(1) DEFAULT '1' NOT NULL,
    user_allow_viewemail tinyint(1) DEFAULT '1' NOT NULL,
    user_avatar varchar(100) DEFAULT '' NOT NULL,
+   user_avatar_type tinyint(2) DEFAULT '0' NOT NULL, 
    user_avatar_width tinyint(4) UNSIGNED DEFAULT '0' NOT NULL,
    user_avatar_height tinyint(4) UNSIGNED DEFAULT '0' NOT NULL,
    user_sig text DEFAULT '' NOT NULL,
