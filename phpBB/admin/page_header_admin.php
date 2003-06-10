ae6b2b7e89117bd4f3a3ea8a5f7e9eb7f3919100
Meik Sievertsen <acydburn@phpbb.com>
2003-06-10 20:48:19 +0000

    hrm, different storage of config value and user timezone value. ;)
    git-svn-id: file:///svn/phpbb/branches/phpBB-2_0_0@4113 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/admin/page_header_admin.php
+++ phpBB/admin/page_header_admin.php
@@ -66,7 +66,7 @@ $template->set_filenames(array(
 
 // Format Timezone. We are unable to use array_pop here, because of PHP3 compatibility
 $l_timezone = explode('.', $board_config['board_timezone']);
-$l_timezone = ($l_timezone[count($l_timezone)-1] != 0) ? $lang[sprintf('%.1f', $board_config['board_timezone'])] : $lang[number_format($board_config['board_timezone'])];
+$l_timezone = (count($l_timezone) > 1 && $l_timezone[count($l_timezone)-1] != 0) ? $lang[sprintf('%.1f', $board_config['board_timezone'])] : $lang[number_format($board_config['board_timezone'])];
 
 //
 // The following assigns all _common_ variables that may be used at any point
