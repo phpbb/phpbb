bc4ff9836950ed1a8b69e1421438fabe4bb7f770
Paul S. Owen <psotfx@users.sourceforge.net>
2003-06-10 00:40:14 +0000

    Couple more header changes
    git-svn-id: file:///svn/phpbb/branches/phpBB-2_0_0@4107 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/page_header.php
+++ phpBB/includes/page_header.php
@@ -468,14 +468,14 @@ else
 // cope with private cache control setting
 if (!empty($_SERVER['SERVER_SOFTWARE']) && strstr($_SERVER['SERVER_SOFTWARE'], 'Apache/2'))
 {
-	header ('Cache-Control: no-cache, pre-check=0, post-check=0, max-age=0');
+	header ('Cache-Control: no-cache, pre-check=0, post-check=0');
 }
 else
 {
 	header ('Cache-Control: private, pre-check=0, post-check=0, max-age=0');
 }
-header ('Expires: ' . gmdate('D, d M Y H:i:s', time()) . ' GMT');
-header ('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+header ('Expires: 0');
+header ('Pragma: no-cache');
 
 $template->pparse('overall_header');
 
