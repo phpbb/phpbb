37a12a09a365e97830b5c68315598ef0039f4f2b
Paul S. Owen <psotfx@users.sourceforge.net>
2004-03-21 15:02:57 +0000

    An update from Jonathan
    git-svn-id: file:///svn/phpbb/trunk@4871 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/develop/fix_files.sh
+++ phpBB/develop/fix_files.sh
@@ -7,18 +7,21 @@
 #
 # UPDATE: 7/31/2001: fix so that it doesn't touch things in the images directory
 #
+# UPDATE: 12/15/2003: Fix so that it doesn't touch any "non-text" files
+#
 
 find . > FILELIST.$$
 grep -sv FILELIST FILELIST.$$ > FILELIST2.$$
 grep -sv $(basename $0) FILELIST2.$$ > FILELIST.$$
 grep -sv "^\.$" FILELIST.$$ > FILELIST2.$$
-grep -sv "images" FILELIST2.$$ > FILELIST
+file -f FILELIST2.$$  |grep text | sed -e 's/^\([^\:]*\)\:.*$/\1/' > FILELIST
+file -f FILELIST2.$$  |grep -sv text | sed -e 's/^\([^\:]*\)\:.*$/Not Modifying file: \1/'
 rm FILELIST2.$$
 rm FILELIST.$$
 
 for i in $(cat FILELIST); do
 	if [ -f $i ]; then  	 
-  		sed -e s/
+		sed -e s/
 //g $i > $i.tmp
   		mv $i.tmp $i
 	fi	
