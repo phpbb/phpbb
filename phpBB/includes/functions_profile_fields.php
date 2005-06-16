67113ee028a0baa82c6b031e0e3dc1e9fa09608c
Bart van Bragt <bartvb@users.sourceforge.net>
2005-06-16 21:22:28 +0000

    Fixed a problem with editing bbcode in textfields
    git-svn-id: file:///svn/phpbb/trunk@5162 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/functions_profile_fields.php
+++ phpBB/includes/functions_profile_fields.php
@@ -564,7 +564,7 @@ class custom_profile
 			include_once($phpbb_root_path . 'includes/functions_posting.'.$phpEx);
 			$message_parser = new parse_message();
 			$message_parser->message = $value;
-			$message_parser->decode_message($user->profile_fields[$profile_row['field_ident'] . '_bbcode_uid']);
+			$message_parser->decode_message($user->profile_fields[str_replace('pf_', '', $profile_row['field_ident']) . '_bbcode_uid']);
 			$value = $message_parser->message;
 		}
 
