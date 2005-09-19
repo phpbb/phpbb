3c488dab682f2e8a4f765ce5e33bbf93e4d5e776
Graham Eames <grahamje@users.sourceforge.net>
2005-09-19 20:49:06 +0000

    Apply translation info fix to the admin panel as well
    git-svn-id: file:///svn/phpbb/branches/phpBB-2_0_0@5214 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/admin/page_footer_admin.php
+++ phpBB/admin/page_footer_admin.php
@@ -36,7 +36,7 @@ $template->set_filenames(array(
 
 $template->assign_vars(array(
 	'PHPBB_VERSION' => ($userdata['user_level'] == ADMIN && $userdata['user_id'] != ANONYMOUS) ? '2' . $board_config['version'] : '', 
-	'TRANSLATION_INFO' => $lang['TRANSLATION_INFO'])
+	'TRANSLATION_INFO' => (isset($lang['TRANSLATION_INFO'])) ? $lang['TRANSLATION_INFO'] : ((isset($lang['TRANSLATION'])) ? $lang['TRANSLATION'] : ''))
 );
 
 $template->pparse('page_footer');
