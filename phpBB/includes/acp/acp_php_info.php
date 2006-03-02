9313494e6864eba1067329b11d881a6f39875c9b
David M <davidmj@users.sourceforge.net>
2006-03-02 01:05:32 +0000

    This should work.. maybe... :P
    git-svn-id: file:///svn/phpbb/trunk@5596 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/acp/acp_php_info.php
+++ phpBB/includes/acp/acp_php_info.php
@@ -33,47 +33,19 @@ class acp_php_info
 		$phpinfo = ob_get_contents(); 
 		ob_end_clean(); 
 
-		// Get used layout
-		$layout = (preg_match('#bgcolor#i', $phpinfo)) ? 'old' : 'new';
-
 		// Here we play around a little with the PHP Info HTML to try and stylise
 		// it along phpBB's lines ... hopefully without breaking anything. The idea
 		// for this was nabbed from the PHP annotated manual
-		preg_match_all('#<body[^>]*>(.*)</body>#siU', $phpinfo, $output); 
+		preg_match_all('#<body[^>]*>(.*)</body>#si', $phpinfo, $output); 
 
-		switch ($layout)
-		{
-			case 'old':
-				$output = preg_replace('#<table#', '<table', $output[1][0]);
-				$output = preg_replace('# bgcolor="\#(\w){6}"#', '', $output);
-				$output = preg_replace('#(\w),(\w)#', '\1, \2', $output);
-				$output = preg_replace('#border="0" cellpadding="3" cellspacing="1" width="600"#', 'border="0" cellspacing="1" cellpadding="4" width="95%"', $output);
-				$output = preg_replace('#<tr valign="top"><td align="left">(.*?<a .*?</a>)(.*?)</td></tr>#s', '<tr class="row1"><td style="{background-color: #9999cc;}"><table width="100%" cellspacing="0" cellpadding="0" border="0"><tr><td style="{background-color: #9999cc;}">\2</td><td style="{background-color: #9999cc;}">\1</td></tr></table></td></tr>', $output);
-				$output = preg_replace('#<tr valign="baseline"><td[ ]{0,1}><b>(.*?)</b>#', '<tr><td class="row1" nowrap="nowrap">\1', $output);
-				$output = preg_replace('#<td align="(center|left)">#', '<td class="row2">', $output);
-				$output = preg_replace('#<td>#', '<td class="row2">', $output);
-				$output = preg_replace('#valign="middle"#', '', $output);
-				$output = preg_replace('#<tr >#', '<tr>', $output);
-				$output = preg_replace('#<hr(.*?)>#', '', $output);
-				$output = preg_replace('#<h1 align="center">#i', '<h1>', $output);
-				$output = preg_replace('#<h2 align="center">#i', '<h2>', $output);
-			break;
-
-			case 'new':
-				$output = preg_replace('#(\w),(\w)#', '\1, \2', $output[1][0]);
-				$output = preg_replace('#<tr class="v"><td>(.*?<a .*?</a>)(.*?)</td></tr>#s', '<tr class="row1"><td><table class="type2"><tr><td>\2</td><td>\1</td></tr></table></td></tr>', $output);
-				$output = preg_replace('#class="e"#', 'class="row1"', $output);
-				$output = preg_replace('#class="v"#', 'class="row2"', $output);
-				$output = preg_replace('#class="h"#', '', $output);
-				$output = preg_replace('#<hr />#', '', $output);
-				$output = preg_replace('#<table [^<]+>#i', '<table>', $output);
-				$output = preg_replace('#<img border="0"#i', '<img', $output);
-				$output = str_replace(array('<font', '</font>'), array('<span', '</span>'), $output);
-				
-				preg_match_all('#<div class="center">(.*)</div>#siU', $output, $output); 
-				$output = $output[1][0];
-			break;
-		}
+		$output = $output[1][0];
+		$output = preg_replace('#<tr class="v"><td>(.*?<a[^>]*><img[^>]*></a>)(.*?)</td></tr>#s', '<tr class="row1"><td><table class="type2"><tr><td>\2</td><td>\1</td></tr></table></td></tr>', $output);
+		$output = preg_replace('#<table[^>]+>#i', '<table>', $output);
+		$output = preg_replace('#<img border="0"#i', '<img', $output);
+		$output = str_replace(array('class="e"', 'class="v"', 'class="h"', '<hr />', '<font', '</font>'), array('class="row1"', 'class="row2"', '', '', '<span', '</span>'), $output);
+		
+		preg_match_all('#<div class="center">(.*)</div>#siU', $output, $output); 
+		$output = $output[1][0];
 
 		$template->assign_var('PHPINFO', $output);
 	}
