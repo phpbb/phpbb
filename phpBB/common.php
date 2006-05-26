a230337cd27d77982cc24b895eb3bc6c0cf89dd6
Graham Eames <grahamje@users.sourceforge.net>
2006-05-26 17:46:59 +0000

    Revert an accidental change. It makes no functional difference to anything but will avoid a code change
    git-svn-id: file:///svn/phpbb/branches/phpBB-2_0_0@5970 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/common.php
+++ phpBB/common.php
@@ -24,8 +24,8 @@ if ( !defined('IN_PHPBB') )
 	die("Hacking attempt");
 }
 
+//
 error_reporting  (E_ERROR | E_WARNING | E_PARSE); // This will NOT report uninitialized variables
-
 set_magic_quotes_runtime(0); // Disable magic_quotes_runtime
 
 // The following code (unsetting globals)
