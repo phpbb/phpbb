7e917eb5d258e5e93d1dcf8d0726020e8cd649b5
Graham Eames <grahamje@users.sourceforge.net>
2006-06-10 14:56:39 +0000

    Hide search links if you are not allowed to use them (if we're not sure, then they are displayed just to be safe)
    git-svn-id: file:///svn/phpbb/trunk@6039 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/functions.php
+++ phpBB/includes/functions.php
@@ -2595,7 +2595,7 @@ function page_header($page_title = '')
 		'S_CONTENT_DIR_RIGHT'	=> $user->lang['RIGHT'],
 		'S_TIMEZONE'			=> ($user->data['user_dst'] || ($user->data['user_id'] == ANONYMOUS && $config['board_dst'])) ? sprintf($user->lang['ALL_TIMES'], $user->lang['tz'][$tz], $user->lang['tz']['dst']) : sprintf($user->lang['ALL_TIMES'], $user->lang['tz'][$tz], ''),
 		'S_DISPLAY_ONLINE_LIST'	=> ($config['load_online']) ? 1 : 0,
-		'S_DISPLAY_SEARCH'		=> ($config['load_search']) ? 1 : 0,
+		'S_DISPLAY_SEARCH'		=> (!$config['load_search']) ? 0 : (isset($auth) ? ($auth->acl_get('u_search') && $auth->acl_getf_global('f_search')) : 1),
 		'S_DISPLAY_PM'			=> ($config['allow_privmsg'] && $user->data['is_registered']) ? 1 : 0,
 		'S_DISPLAY_MEMBERLIST'	=> (isset($auth)) ? $auth->acl_get('u_viewprofile') : 0,
 		'S_NEW_PM'				=> ($s_privmsg_new) ? 1 : 0,
