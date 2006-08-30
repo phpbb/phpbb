d64e44b8786ef0c219860838ca101bd7f44d4ece
Graham Eames <grahamje@users.sourceforge.net>
2006-08-30 19:33:55 +0000

    Error if no post in a forum
    git-svn-id: file:///svn/phpbb/trunk@6336 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/functions_display.php
+++ phpBB/includes/functions_display.php
@@ -314,7 +314,7 @@ function display_forums($root_data = '', $display_moderators = true, $return_mod
 		}
 		else
 		{
-			$last_post_time = $last_poster = $last_poster_url = $last_post_url = '';
+			$last_post_time = $last_poster = $last_poster_colour = $last_poster_url = $last_post_url = '';
 		}
 
 		// Output moderator listing ... if applicable
