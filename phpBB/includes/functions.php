883bfbf981a58c357a8dd1d0c6754488971f7cb0
Dominik Dröscher <dhn2@users.sourceforge.net>
2006-11-27 12:56:35 +0000

    blah ..
    git-svn-id: file:///svn/phpbb/trunk@6669 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/functions.php
+++ phpBB/includes/functions.php
@@ -686,7 +686,7 @@ function tz_select($default = '', $truncate = false)
 	{
 		if ($truncate)
 		{
-			$zone_trunc = truncate_string($zone, 50, false, "...");
+			$zone_trunc = truncate_string($zone, 50, false, '...');
 		} 
 		else
 		{
