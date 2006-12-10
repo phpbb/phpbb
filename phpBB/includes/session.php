96858686ff822a6b83c3c4b1704bfed5c9766906
Nils Adermann <naderman@naderman.de>
2006-12-10 21:33:01 +0000

    Haha, slowly it's starting to look stupid ...
    git-svn-id: file:///svn/phpbb/trunk@6746 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/session.php
+++ phpBB/includes/session.php
@@ -166,7 +166,7 @@ class session
 			foreach ($ips as $ip)
 			{
 				// check IPv4 first, the IPv6 is hopefully only going to be used very seldomly
-				if (!empty($ip) && !preg_match($ipv4, $this->forwarded_for) && !preg_match($ipv6, $ip))
+				if (!empty($ip) && !preg_match($ipv4, $ip) && !preg_match($ipv6, $ip))
 				{
 					if (!defined('DEBUG_EXTRA'))
 					{
