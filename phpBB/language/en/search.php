2851b9ea80318e056486785a0a5e1d2ee33ec3de
Dominik Dröscher <dhn2@users.sourceforge.net>
2007-01-03 20:10:53 +0000

    #6712 - Posts in search results with empty subjects get a Jump to post message
    git-svn-id: file:///svn/phpbb/trunk@6836 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/language/en/search.php
+++ phpBB/language/en/search.php
@@ -45,6 +45,8 @@ $lang = array_merge($lang, array(
 	'IGNORED_TERMS'			=> 'ignored',
 	'IGNORED_TERMS_EXPLAIN'	=> 'The following words in your search query were ignored: <strong>%s</strong>',
 
+	'JUMP_TO_POST'			=> 'Jump to post',
+
 	'NO_KEYWORDS'			=> 'You must specify at least one word to search for. Each word must consist of at least %d characters and must not contain more than %d characters excluding wildcards.',
 	'NO_RECENT_SEARCHES'	=> 'No searches have been carried out recently',
 	'NO_SEARCH'				=> 'Sorry but you are not permitted to use the search system.',
