2ebc300a5ac4c85fb03063d01887f52852e07da7
Nils Adermann <naderman@naderman.de>
2007-03-07 18:27:30 +0000

    forgot a file
    git-svn-id: file:///svn/phpbb/trunk@7145 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/language/en/acp/search.php
+++ phpBB/language/en/acp/search.php
@@ -76,7 +76,7 @@ $lang = array_merge($lang, array(
 
 	'SEARCH_GUEST_INTERVAL'					=> 'Guest search flood interval',
 	'SEARCH_GUEST_INTERVAL_EXPLAIN'			=> 'Number of seconds guests must wait between searches. If one guest searches all others have to wait until the time interval passed.',
-	'SEARCH_INDEX_CREATE_REDIRECT'			=> 'All posts up to post id %d have been indexed.<br />Indexing in progress ...',
+	'SEARCH_INDEX_CREATE_REDIRECT'			=> 'All posts up to post id %d have been indexed. <br /> %d posts in this step and %f posts per second<br />Indexing in progress ...',
 	'SEARCH_INDEX_DELETE_REDIRECT'			=> 'All posts up to post id %d have been removed from the search index.<br /> Deleting in progress ...',
 	'SEARCH_INDEX_CREATED'					=> 'Successfully indexed all posts in the board database.',
 	'SEARCH_INDEX_REMOVED'					=> 'Successfully deleted the search index for this backend.',
