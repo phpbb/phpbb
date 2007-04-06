3f6b104312b703701272f5253e391b37c3fcef6b
Meik Sievertsen <acydburn@phpbb.com>
2007-04-06 17:49:47 +0000

    add proper die() statements although /develop is never ever shipped with phpBB...
    git-svn-id: file:///svn/phpbb/trunk@7288 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/develop/regex.php
+++ phpBB/develop/regex.php
@@ -1,4 +1,13 @@
 <?php
+//
+// Security message:
+//
+// This script is potentially dangerous.
+// Remove or comment the next line (die(".... ) to enable this script.
+// Do NOT FORGET to either remove this script or disable it after you have used it.
+//
+die("Please read the first lines of this script for instructions on how to enable it");
+
 
 // IP regular expressions
 
--- phpBB/develop/utf_normalizer_test.php
+++ phpBB/develop/utf_normalizer_test.php
@@ -13,6 +13,15 @@ if (php_sapi_name() != 'cli')
 	die("This program must be run from the command line.\n");
 }
 
+//
+// Security message:
+//
+// This script is potentially dangerous.
+// Remove or comment the next line (die(".... ) to enable this script.
+// Do NOT FORGET to either remove this script or disable it after you have used it.
+//
+die("Please read the first lines of this script for instructions on how to enable it");
+
 set_time_limit(0);
 error_reporting(E_ALL);
 
