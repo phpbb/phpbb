ee6ea033cfc618880205524a6ab988072b5e36c8
David M <davidmj@users.sourceforge.net>
2007-05-14 19:00:11 +0000

    #10903
    git-svn-id: file:///svn/phpbb/trunk@7574 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/acp/acp_bbcodes.php
+++ phpBB/includes/acp/acp_bbcodes.php
@@ -75,7 +75,7 @@ class acp_bbcodes
 				$display_on_posting = request_var('display_on_posting', 0);
 
 				$bbcode_match = request_var('bbcode_match', '');
-				$bbcode_tpl = htmlspecialchars_decode(request_var('bbcode_tpl', ''));
+				$bbcode_tpl = htmlspecialchars_decode(request_var('bbcode_tpl', '', true));
 				$bbcode_helpline = request_var('bbcode_helpline', '', true);
 			break;
 		}
