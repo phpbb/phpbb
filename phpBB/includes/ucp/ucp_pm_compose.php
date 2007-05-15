a366a30f053da1770ef144ec483779cc48431f5b
David M <davidmj@users.sourceforge.net>
2007-05-15 22:30:12 +0000

    #10972
    git-svn-id: file:///svn/phpbb/trunk@7594 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/ucp/ucp_pm_compose.php
+++ phpBB/includes/ucp/ucp_pm_compose.php
@@ -553,7 +553,7 @@ function compose_pm($id, $mode, $action)
 		{
 			if (!$subject)
 			{
-				$error[] = $user->lang['EMPTY_SUBJECT'];
+				$error[] = $user->lang['NO_SUBJECT'];
 			}
 
 			if (!sizeof($address_list))
