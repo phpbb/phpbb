1ade9361894fe686e155f9b026dff6d5818f0538
Jonathan Stanley <shs@users.sourceforge.net>
2007-05-17 14:10:32 +0000

    #10946
    git-svn-id: file:///svn/phpbb/trunk@7617 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/language/en/help_bbcode.php
+++ phpBB/language/en/help_bbcode.php
@@ -60,7 +60,7 @@ $help = array(
 	),
 	array(
 		0 => 'Outputting code or fixed width data',
-		1 => 'If you want to output a piece of code or in fact anything that requires a fixed width, eg. Courier type font you should enclose the text in <strong>[code][/code]</strong> tags, eg.<br /><br /><strong>[code]</strong>echo &quot;This is some code&quot;;<strong>[/code]</strong><br /><br />All formatting used within <strong>[code][/code]</strong> tags is retained when you later view it.'
+		1 => 'If you want to output a piece of code or in fact anything that requires a fixed width, eg. Courier type font you should enclose the text in <strong>[code][/code]</strong> tags, eg.<br /><br /><strong>[code]</strong>echo &quot;This is some code&quot;;<strong>[/code]</strong><br /><br />All formatting used within <strong>[code][/code]</strong> tags is retained when you later view it. PHP syntax highlighting can be enabled using <strong>[code=php][/code]</strong> and is recommended when posting PHP code samples as it improves readability.'
 	),
 	array(
 		0 => '--',
