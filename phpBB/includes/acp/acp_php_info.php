a694c17791eb0375bc9259397a68849a35145281
David M <davidmj@users.sourceforge.net>
2007-07-10 17:46:47 +0000

    #12777
    git-svn-id: file:///svn/phpbb/trunk@7864 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/acp/acp_php_info.php
+++ phpBB/includes/acp/acp_php_info.php
@@ -45,7 +45,16 @@ class acp_php_info
 		}
 
 		$output = $output[1][0];
-		$output = preg_replace('#<tr class="v"><td>(.*?<a[^>]*><img[^>]*></a>)(.*?)</td></tr>#s', '<tr class="row1"><td><table class="type2"><tr><td>\2</td><td>\1</td></tr></table></td></tr>', $output);
+
+		// expose_php can make the image not exist
+		if (preg_match('#<a[^>]*><img[^>]*></a>#', $output))
+		{
+			$output = preg_replace('#<tr class="v"><td>(.*?<a[^>]*><img[^>]*></a>)(.*?)</td></tr>#s', '<tr class="row1"><td><table class="type2"><tr><td>\2</td><td>\1</td></tr></table></td></tr>', $output);
+		}
+		else
+		{
+			$output = preg_replace('#<tr class="v"><td>(.*?)</td></tr>#s', '<tr class="row1"><td><table class="type2"><tr><td>\1</td></tr></table></td></tr>', $output);
+		}
 		$output = preg_replace('#<table[^>]+>#i', '<table>', $output);
 		$output = preg_replace('#<img border="0"#i', '<img', $output);
 		$output = str_replace(array('class="e"', 'class="v"', 'class="h"', '<hr />', '<font', '</font>'), array('class="row1"', 'class="row2"', '', '', '<span', '</span>'), $output);
