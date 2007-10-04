e2ac95febf5d01ab0321e797fb0c7043b73245af
Meik Sievertsen <acydburn@phpbb.com>
2007-10-04 12:33:31 +0000

    *** empty log message ***
    git-svn-id: file:///svn/phpbb/trunk@8132 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/mcp/mcp_topic.php
+++ phpBB/includes/mcp/mcp_topic.php
@@ -1,12 +1,20 @@
 <?php
-/** 
+/**
 *
 * @package mcp
 * @version $Id$
-* @copyright (c) 2005 phpBB Group 
-* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+* @copyright (c) 2005 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
 */
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
 
 /**
 * View topic in MCP
