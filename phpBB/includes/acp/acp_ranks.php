4c63ca4684d3c530ad3051899da593ae04bfd73f
Henry Sudhof <kellanved@phpbb.com>
2007-10-14 10:07:52 +0000

    #14725 One check too many
    git-svn-id: file:///svn/phpbb/trunk@8176 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/acp/acp_ranks.php
+++ phpBB/includes/acp/acp_ranks.php
@@ -139,11 +139,6 @@ class acp_ranks
 			case 'edit':
 			case 'add':
 
-				if (!check_form_key($form_name))
-				{
-					trigger_error($user->lang['FORM_INVALID']. adm_back_link($this->u_action), E_USER_WARNING);
-				}
-
 				$data = $ranks = $existing_imgs = array();
 				
 				$sql = 'SELECT *
