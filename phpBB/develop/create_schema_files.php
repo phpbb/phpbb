6dfa77098619c461649dcef22aac142227d3653b
David M <davidmj@users.sourceforge.net>
2008-01-06 13:48:28 +0000

    I plan on getting a perfect commit one of these days... Today is not that day :P
    git-svn-id: file:///svn/phpbb/trunk@8309 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/develop/create_schema_files.php
+++ phpBB/develop/create_schema_files.php
@@ -12,7 +12,7 @@
 * If you overwrite the original schema files please make sure you save the file with UNIX linefeeds.
 */
 
-//die("Please read the first lines of this script for instructions on how to enable it");
+die("Please read the first lines of this script for instructions on how to enable it");
 
 @set_time_limit(0);
 
@@ -239,7 +239,7 @@ $supported_dbms = array('firebird', 'mssql', 'mysql', 'db2', 'oracle', 'postgres
 
 foreach ($supported_dbms as $dbms)
 {
-	$fp = fopen($schema_path . '' . $dbms . '_schema.sql', 'wt');
+	$fp = fopen($schema_path . '_' . $dbms . '_schema.sql', 'wt');
 
 	$line = '';
 
