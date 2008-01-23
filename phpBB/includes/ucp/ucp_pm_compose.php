7fed5dd1666c2d54b87dd8e46dadcbb167e702a7
Henry Sudhof <kellanved@phpbb.com>
2008-01-23 14:32:15 +0000

    cross-checkin for the TRUNK
    git-svn-id: file:///svn/phpbb/trunk@8333 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/ucp/ucp_pm_compose.php
+++ phpBB/includes/ucp/ucp_pm_compose.php
@@ -465,7 +465,8 @@ function compose_pm($id, $mode, $action)
 					'forum_id'		=> 0,
 					'save_time'		=> $current_time,
 					'draft_subject'	=> $subject,
-					'draft_message'	=> $message)
+					'draft_message'	=> $message
+					)
 				);
 				$db->sql_query($sql);
 
@@ -488,6 +489,8 @@ function compose_pm($id, $mode, $action)
 					'g'			=> $to_group_id,
 					'p'			=> $msg_id)
 				);
+				$s_hidden_fields .= build_address_field($address_list);
+
 
 				confirm_box(false, 'SAVE_DRAFT', $s_hidden_fields);
 			}
@@ -541,7 +544,7 @@ function compose_pm($id, $mode, $action)
 
 	if ($submit || $preview || $refresh)
 	{
-		if (!check_form_key('ucp_pm_compose'))
+		if (($submit || $preview) && !check_form_key('ucp_pm_compose'))
 		{
 			$error[] = $user->lang['FORM_INVALID'];
 		}
@@ -888,15 +891,9 @@ function compose_pm($id, $mode, $action)
 	}
 
 	// Build hidden address list
-	$s_hidden_address_field = '';
-	foreach ($address_list as $type => $adr_ary)
-	{
-		foreach ($adr_ary as $id => $field)
-		{
-			$s_hidden_address_field .= '<input type="hidden" name="address_list[' . (($type == 'u') ? 'u' : 'g') . '][' . (int) $id . ']" value="' . (($field == 'to') ? 'to' : 'bcc') . '" />';
-		}
-	}
-
+	$s_hidden_address_field = build_address_field($address_list);
+	 
+	
 	$bbcode_checked		= (isset($enable_bbcode)) ? !$enable_bbcode : (($config['allow_bbcode'] && $auth->acl_get('u_pm_bbcode')) ? !$user->optionget('bbcode') : 1);
 	$smilies_checked	= (isset($enable_smilies)) ? !$enable_smilies : (($config['allow_smilies'] && $auth->acl_get('u_pm_smilies')) ? !$user->optionget('smilies') : 1);
 	$urls_checked		= (isset($enable_urls)) ? !$enable_urls : 0;
@@ -1115,6 +1112,22 @@ function handle_message_list_actions(&$address_list, &$error, $remove_u, $remove
 			}
 		}
 	}
+}
+
+/** 
+* Build the hidden field for the recipients. Needed, as the variable is not read via request_var.
+*/
+function build_address_field($address_list)
+{
+	$s_hidden_address_field = '';
+	foreach ($address_list as $type => $adr_ary)
+	{
+		foreach ($adr_ary as $id => $field)
+		{
+			$s_hidden_address_field .= '<input type="hidden" name="address_list[' . (($type == 'u') ? 'u' : 'g') . '][' . (int) $id . ']" value="' . (($field == 'to') ? 'to' : 'bcc') . '" />';
+		}
+	}
+	return $s_hidden_address_field;
 }
 
 /**
