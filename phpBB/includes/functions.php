0a5c435102dbdfce9b773c5ba4c0a91e42141108
Meik Sievertsen <acydburn@phpbb.com>
2008-02-23 11:26:23 +0000

    Fix Custom BBCode {EMAIL}-Token usage - #21155
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@8387 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/functions.php
+++ phpBB/includes/functions.php
@@ -2756,7 +2756,7 @@ function get_preg_expression($mode)
 	switch ($mode)
 	{
 		case 'email':
-			return '[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*[a-z]+';
+			return '[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.(?:[a-z0-9\-]+\.)*[a-z]+';
 		break;
 
 		case 'bbcode_htm':
