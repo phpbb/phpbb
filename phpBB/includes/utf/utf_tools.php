29eb7129987e129877529144816f32704eb724f0
David M <davidmj@users.sourceforge.net>
2008-04-20 05:16:42 +0000

    #21885
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@8510 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/utf/utf_tools.php
+++ phpBB/includes/utf/utf_tools.php
@@ -1840,7 +1840,7 @@ function utf8_clean_string($text)
 /**
 * A wrapper for htmlspecialchars($value, ENT_COMPAT, 'UTF-8')
 */
-function utf8_htmlspecialchars(&$value)
+function utf8_htmlspecialchars($value)
 {
 	return htmlspecialchars($value, ENT_COMPAT, 'UTF-8');
 }
