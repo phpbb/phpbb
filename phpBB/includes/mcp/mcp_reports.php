9640f5b1083e31f57271c4084e09fc5717f185c6
Meik Sievertsen <acydburn@phpbb.com>
2008-04-21 13:17:30 +0000

    remove the _f_ portion from redirect urls now they no longer exist
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@8520 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/mcp/mcp_reports.php
+++ phpBB/includes/mcp/mcp_reports.php
@@ -450,15 +450,15 @@ function close_report($report_id_list, $mode, $action)
 
 	if ($action == 'delete' && strpos($user->data['session_page'], 'mode=report_details') !== false)
 	{
-		$redirect = request_var('redirect', build_url(array('mode', '_f_', 'r', 'quickmod')) . '&amp;mode=reports');
+		$redirect = request_var('redirect', build_url(array('mode', 'r', 'quickmod')) . '&amp;mode=reports');
 	}
 	else if ($action == 'close' && !request_var('r', 0))
 	{
-		$redirect = request_var('redirect', build_url(array('mode', '_f_', 'p', 'quickmod')) . '&amp;mode=reports');
+		$redirect = request_var('redirect', build_url(array('mode', 'p', 'quickmod')) . '&amp;mode=reports');
 	}
 	else
 	{
-		$redirect = request_var('redirect', build_url(array('_f_', 'quickmod')));
+		$redirect = request_var('redirect', build_url(array('quickmod')));
 	}
 	$success_msg = '';
 	$forum_ids = array();
