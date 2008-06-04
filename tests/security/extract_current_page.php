e8e807f79750e68c3fa1757958e3674bb9cd9aaf
Meik Sievertsen <acydburn@phpbb.com>
2008-06-04 11:06:51 +0000

    adding missing props
    git-svn-id: file:///svn/phpbb/trunk@8588 89ea8834-ac86-4346-8a33-228a782c2dd0

--- tests/security/extract_current_page.php
+++ tests/security/extract_current_page.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package testing
-* @version $Id: request_var.php 8549 2008-05-04 22:54:16Z naderman $
+* @version $Id$
 * @copyright (c) 2008 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
