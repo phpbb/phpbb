fe2343965bf88a3e695bb93e7d45da8d9261ccf2
Henry Sudhof <kellanved@phpbb.com>
2008-06-09 11:59:43 +0000

    merge
    git-svn-id: file:///svn/phpbb/trunk@8629 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/acp/acp_icons.php
+++ phpBB/includes/acp/acp_icons.php
@@ -435,7 +435,7 @@ class acp_icons
 					default:
 						$suc_lang = $lang;
 				}
-				$errormsgs = '<br />';
+				$errormsgs = '';
 				foreach ($errors as $img => $error)
 				{
 					$errormsgs .= '<br />' . sprintf($user->lang[$error], $img);
@@ -446,7 +446,7 @@ class acp_icons
 				}
 				else
 				{
-					trigger_error($user->lang[$suc_lang . '_ADDED'] . $errormsgs .adm_back_link($this->u_action), $level);
+					trigger_error($user->lang[$suc_lang . '_ADDED'] . $errormsgs . adm_back_link($this->u_action), $level);
 				}
 
 			break;
