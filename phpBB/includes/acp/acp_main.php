3892e7330adb2464293428e58f882f7d18fc8558
Marek A. R <aptx@phpbb.com>
2008-06-19 17:51:11 +0000

    Fix merge to use the new constants
    git-svn-id: file:///svn/phpbb/trunk@8660 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/acp/acp_main.php
+++ phpBB/includes/acp/acp_main.php
@@ -63,7 +63,7 @@ class acp_main
 			if ($action === 'admlogout')
 			{
 				$user->unset_admin();
-				$redirect_url = append_sid("{$phpbb_root_path}index.$phpEx");
+				$redirect_url = append_sid(PHPBB_ROOT_PATH . 'index.' . PHP_EXT);
 				meta_refresh(3, $redirect_url);
 				trigger_error($user->lang['ADM_LOGGED_OUT'] . '<br /><br />' . sprintf($user->lang['RETURN_INDEX'], '<a href="' . $redirect_url . '">', '</a>'));
 			}
