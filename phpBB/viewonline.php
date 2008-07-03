a6fb65baece408a709cc1b623e9c32a53c28ff0d
Meik Sievertsen <acydburn@phpbb.com>
2008-07-03 12:43:49 +0000

    #29895 - prevent php notice on viewonline
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@8677 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/viewonline.php
+++ phpBB/viewonline.php
@@ -225,8 +225,9 @@ while ($row = $db->sql_fetchrow($result))
 				{
 					case 'posting':
 						preg_match('#mode=([a-z]+)#', $row['session_page'], $on_page);
+						$posting_mode = (!empty($on_page[1])) ? $on_page[1] : '';
 
-						switch ($on_page[1])
+						switch ($posting_mode)
 						{
 							case 'reply':
 							case 'quote':
