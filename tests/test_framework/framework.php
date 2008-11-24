81c44f83514770a0753efcfc75e3ba028ac84d18
Nils Adermann <naderman@naderman.de>
2008-11-24 13:41:39 +0000

    - added a PHPUnit version check
    git-svn-id: file:///svn/phpbb/trunk@9112 89ea8834-ac86-4346-8a33-228a782c2dd0

--- tests/test_framework/framework.php
+++ tests/test_framework/framework.php
@@ -1,5 +1,11 @@
 <?php
 
-require_once 'PHPUnit/Framework.php';
+// require at least PHPUnit 3.3.0
+require_once 'PHPUnit/Runner/Version.php';
+if (version_compare(PHPUnit_Runner_Version::id(), '3.3.0', '<'))
+{
+	trigger_error('PHPUnit >= 3.3.0 required');
+}
 
+require_once 'PHPUnit/Framework.php';
 require_once 'test_framework/phpbb_test_case.php';
