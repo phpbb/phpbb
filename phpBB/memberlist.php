phpbb/phpbb

commit 127750f89d114b7ab9a2e9442c7b3bd28084c5df

Do not display COPPA group in memberlist find member dialog if COPPA disabled (Bug #37175)
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9075 89ea8834-ac86-4346-8a33-228a782c2dd0

phpBB/memberlist.php
@@ -1276,11 +1276,21 @@ switch ($mode)
 			$s_group_select = '<option value="0"' . ((!$group_selected) ? ' selected="selected"' : '') . '>&nbsp;</option>';
 			$group_ids = array();
 
+			/**
+			* @todo add this to a separate function (function is responsible for returning the groups the user is able to see based on the users group membership)
+			*/
+
 			if ($auth->acl_gets('a_group', 'a_groupadd', 'a_groupdel'))
 			{
 				$sql = 'SELECT group_id, group_name, group_type
-					FROM ' . GROUPS_TABLE . '
-					ORDER BY group_name ASC';
+					FROM ' . GROUPS_TABLE;
+
+				if (!$config['coppa_enable'])
+				{
+					$sql .= " WHERE group_name <> 'REGISTERED_COPPA'";
+				}
+
+				$sql .= ' ORDER BY group_name ASC';
 			}
 			else
 			{
@@ -1292,8 +1302,14 @@ switch ($mode)
 							AND ug.user_id = ' . $user->data['user_id'] . '
 							AND ug.user_pending = 0
 						)
-					WHERE (g.group_type <> ' . GROUP_HIDDEN . ' OR ug.user_id = ' . $user->data['user_id'] . ')
-					ORDER BY g.group_name ASC';
+					WHERE (g.group_type <> ' . GROUP_HIDDEN . ' OR ug.user_id = ' . $user->data['user_id'] . ')';
+
+				if (!$config['coppa_enable'])
+				{
+					$sql .= " WHERE group_name <> 'REGISTERED_COPPA'";
+				}
+
+				$sql .= ' ORDER BY g.group_name ASC';
 			}
 			$result = $db->sql_query($sql);
 

commit 41ad04c1550bd01a9eaa0a65e9d44e928194cc35

Fix a regression from r9075
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9156 89ea8834-ac86-4346-8a33-228a782c2dd0

phpBB/memberlist.php
@@ -1306,7 +1306,7 @@ switch ($mode)
 
 				if (!$config['coppa_enable'])
 				{
-					$sql .= " WHERE group_name <> 'REGISTERED_COPPA'";
+					$sql .= " AND g.group_name <> 'REGISTERED_COPPA'";
 				}
 
 				$sql .= ' ORDER BY g.group_name ASC';