23de8dd0ce9249105711dfbf2d28e7042476af8b
Meik Sievertsen <acydburn@phpbb.com>
2008-12-05 11:36:51 +0000

    make it obvious that even if the files got uploaded on an update that it is not finished yet... by replacing "Check files again" with "Continue update now"
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9177 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/language/en/install.php
+++ phpBB/language/en/install.php
@@ -379,7 +379,8 @@ $lang = array_merge($lang, array(
 	'COLLECTED_INFORMATION_EXPLAIN'	=> 'The list below shows information about the files needing an update. Please read the information in front of every status block to see what they mean and what you may need to do to perform a successful update.',
 	'COLLECTING_FILE_DIFFS'			=> 'Collecting file differences',
 	'COMPLETE_LOGIN_TO_BOARD'		=> 'You should now <a href="../ucp.php?mode=login">login to your board</a> and check if everything is working fine. Do not forget to delete, rename or move your install directory!',
-	'CONTINUE_UPDATE_NOW'			=> 'Continue the update process now',
+	'CONTINUE_UPDATE_NOW'			=> 'Continue the update process now',		// Shown within the database update script at the end if called from the updater
+	'CONTINUE_UPDATE'				=> 'Continue update now',					// Shown after file upload to indicate the update process is not yet finished
 	'CURRENT_FILE'					=> 'Beginning of current original file',
 	'CURRENT_VERSION'				=> 'Current version',
 
