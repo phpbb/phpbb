4cd13cf424497d660dc45bb3bee25173b64ce30f
Meik Sievertsen <acydburn@phpbb.com>
2008-12-28 23:37:41 +0000

    change index.php to a working state once the other changes are in...
    git-svn-id: file:///svn/phpbb/trunk@9243 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/index.php
+++ phpBB/index.php
@@ -21,23 +21,18 @@ include(PHPBB_ROOT_PATH . 'common.' . PHP_EXT);
 include(PHPBB_ROOT_PATH . 'includes/functions_display.' . PHP_EXT);
 
 // Start session management
-$user->session_begin();
-$auth->acl($user->data);
-$user->setup('viewforum');
+phpbb::$user->init();
+phpbb::$user->setup('viewforum');
 
-display_forums('', $config['load_moderators']);
+display_forums('', phpbb::$config['load_moderators']);
 
 // Set some stats, get posts count from forums data if we... hum... retrieve all forums data
-$total_posts	= $config['num_posts'];
-$total_topics	= $config['num_topics'];
-$total_users	= $config['num_users'];
-
-$l_total_user_s = ($total_users == 0) ? 'TOTAL_USERS_ZERO' : 'TOTAL_USERS_OTHER';
-$l_total_post_s = ($total_posts == 0) ? 'TOTAL_POSTS_ZERO' : 'TOTAL_POSTS_OTHER';
-$l_total_topic_s = ($total_topics == 0) ? 'TOTAL_TOPICS_ZERO' : 'TOTAL_TOPICS_OTHER';
+$total_posts	= phpbb::$config['num_posts'];
+$total_topics	= phpbb::$config['num_topics'];
+$total_users	= phpbb::$config['num_users'];
 
 // Grab group details for legend display
-if ($auth->acl_gets('a_group', 'a_groupadd', 'a_groupdel'))
+if (phpbb::$acl->acl_gets('a_group', 'a_groupadd', 'a_groupdel'))
 {
 	$sql = 'SELECT group_id, group_name, group_colour, group_type
 		FROM ' . GROUPS_TABLE . '
@@ -51,46 +46,46 @@ else
 		LEFT JOIN ' . USER_GROUP_TABLE . ' ug
 			ON (
 				g.group_id = ug.group_id
-				AND ug.user_id = ' . $user->data['user_id'] . '
+				AND ug.user_id = ' . phpbb::$user->data['user_id'] . '
 				AND ug.user_pending = 0
 			)
 		WHERE g.group_legend = 1
-			AND (g.group_type <> ' . GROUP_HIDDEN . ' OR ug.user_id = ' . $user->data['user_id'] . ')
+			AND (g.group_type <> ' . GROUP_HIDDEN . ' OR ug.user_id = ' . phpbb::$user->data['user_id'] . ')
 		ORDER BY g.group_name ASC';
 }
-$result = $db->sql_query($sql);
+$result = phpbb::$db->sql_query($sql);
 
 $legend = array();
-while ($row = $db->sql_fetchrow($result))
+while ($row = phpbb::$db->sql_fetchrow($result))
 {
 	$colour_text = ($row['group_colour']) ? ' style="color:#' . $row['group_colour'] . '"' : '';
-	$group_name = ($row['group_type'] == GROUP_SPECIAL) ? $user->lang['G_' . $row['group_name']] : $row['group_name'];
+	$group_name = ($row['group_type'] == GROUP_SPECIAL) ? phpbb::$user->lang['G_' . $row['group_name']] : $row['group_name'];
 
-	if ($row['group_name'] == 'BOTS' || ($user->data['user_id'] != ANONYMOUS && !$auth->acl_get('u_viewprofile')))
+	if ($row['group_name'] == 'BOTS' || (phpbb::$user->data['user_id'] != ANONYMOUS && !$auth->acl_get('u_viewprofile')))
 	{
 		$legend[] = '<span' . $colour_text . '>' . $group_name . '</span>';
 	}
 	else
 	{
-		$legend[] = '<a' . $colour_text . ' href="' . append_sid('memberlist', 'mode=group&amp;g=' . $row['group_id']) . '">' . $group_name . '</a>';
+		$legend[] = '<a' . $colour_text . ' href="' . phpbb::$url->append_sid('memberlist', 'mode=group&amp;g=' . $row['group_id']) . '">' . $group_name . '</a>';
 	}
 }
-$db->sql_freeresult($result);
+phpbb::$db->sql_freeresult($result);
 
 $legend = implode(', ', $legend);
 
 // Generate birthday list if required ...
 $birthday_list = '';
-if ($config['load_birthdays'] && $config['allow_birthdays'])
+if (phpbb::$config['load_birthdays'] && phpbb::$config['allow_birthdays'])
 {
-	$now = getdate(time() + $user->timezone + $user->dst - date('Z'));
+	$now = getdate(time() + phpbb::$user->timezone + phpbb::$user->dst - date('Z'));
 	$sql = 'SELECT user_id, username, user_colour, user_birthday
 		FROM ' . USERS_TABLE . "
-		WHERE user_birthday LIKE '" . $db->sql_escape(sprintf('%2d-%2d-', $now['mday'], $now['mon'])) . "%'
+		WHERE user_birthday LIKE '" . phpbb::$db->sql_escape(sprintf('%2d-%2d-', $now['mday'], $now['mon'])) . "%'
 			AND user_type IN (" . phpbb::USER_NORMAL . ', ' . phpbb::USER_FOUNDER . ')';
-	$result = $db->sql_query($sql);
+	$result = phpbb::$db->sql_query($sql);
 
-	while ($row = $db->sql_fetchrow($result))
+	while ($row = phpbb::$db->sql_fetchrow($result))
 	{
 		$birthday_list .= (($birthday_list != '') ? ', ' : '') . get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']);
 
@@ -99,35 +94,35 @@ if ($config['load_birthdays'] && $config['allow_birthdays'])
 			$birthday_list .= ' (' . ($now['year'] - $age) . ')';
 		}
 	}
-	$db->sql_freeresult($result);
+	phpbb::$db->sql_freeresult($result);
 }
 
 // Assign index specific vars
-$template->assign_vars(array(
-	'TOTAL_POSTS'	=> sprintf($user->lang[$l_total_post_s], $total_posts),
-	'TOTAL_TOPICS'	=> sprintf($user->lang[$l_total_topic_s], $total_topics),
-	'TOTAL_USERS'	=> sprintf($user->lang[$l_total_user_s], $total_users),
-	'NEWEST_USER'	=> sprintf($user->lang['NEWEST_USER'], get_username_string('full', $config['newest_user_id'], $config['newest_username'], $config['newest_user_colour'])),
+phpbb::$template->assign_vars(array(
+	'TOTAL_POSTS'	=> phpbb::$user->lang('TOTAL_POSTS_COUNT', $total_posts),
+	'TOTAL_TOPICS'	=> phpbb::$user->lang('TOTAL_TOPICS_COUNT', $total_topics),
+	'TOTAL_USERS'	=> phpbb::$user->lang('TOTAL_USERS_COUNT', $total_users),
+	'NEWEST_USER'	=> phpbb::$user->lang('NEWEST_USER', get_username_string('full', phpbb::$config['newest_user_id'], phpbb::$config['newest_username'], phpbb::$config['newest_user_colour'])),
 
 	'LEGEND'		=> $legend,
 	'BIRTHDAY_LIST'	=> $birthday_list,
 
-	'FORUM_IMG'				=> $user->img('forum_read', 'NO_NEW_POSTS'),
-	'FORUM_NEW_IMG'			=> $user->img('forum_unread', 'NEW_POSTS'),
-	'FORUM_LOCKED_IMG'		=> $user->img('forum_read_locked', 'NO_NEW_POSTS_LOCKED'),
-	'FORUM_NEW_LOCKED_IMG'	=> $user->img('forum_unread_locked', 'NO_NEW_POSTS_LOCKED'),
+	'FORUM_IMG'				=> phpbb::$user->img('forum_read', 'NO_NEW_POSTS'),
+	'FORUM_NEW_IMG'			=> phpbb::$user->img('forum_unread', 'NEW_POSTS'),
+	'FORUM_LOCKED_IMG'		=> phpbb::$user->img('forum_read_locked', 'NO_NEW_POSTS_LOCKED'),
+	'FORUM_NEW_LOCKED_IMG'	=> phpbb::$user->img('forum_unread_locked', 'NO_NEW_POSTS_LOCKED'),
 
-	'S_LOGIN_ACTION'			=> append_sid('ucp', 'mode=login'),
-	'S_DISPLAY_BIRTHDAY_LIST'	=> ($config['load_birthdays']) ? true : false,
+	'S_LOGIN_ACTION'			=> phpbb::$url->append_sid('ucp', 'mode=login'),
+	'S_DISPLAY_BIRTHDAY_LIST'	=> (phpbb::$config['load_birthdays']) ? true : false,
 
-	'U_MARK_FORUMS'		=> ($user->data['is_registered'] || $config['load_anon_lastread']) ? append_sid('index', 'hash=' . generate_link_hash('global') . '&amp;mark=forums') : '',
-	'U_MCP'				=> ($auth->acl_get('m_') || $auth->acl_getf_global('m_')) ? append_sid('mcp', 'i=main&amp;mode=front', true, $user->session_id) : '')
+	'U_MARK_FORUMS'		=> (phpbb::$user->is_registered || phpbb::$config['load_anon_lastread']) ? append_sid('index', 'hash=' . generate_link_hash('global') . '&amp;mark=forums') : '',
+	'U_MCP'				=> (phpbb::$acl->acl_get('m_') || phpbb::$acl->acl_getf_global('m_')) ? phpbb::$url->append_sid('mcp', 'i=main&amp;mode=front', true, phpbb::$user->session_id) : '')
 );
 
 // Output page
-page_header($user->lang['INDEX']);
+page_header(phpbb::$user->lang['INDEX']);
 
-$template->set_filenames(array(
+phpbb::$template->set_filenames(array(
 	'body' => 'index_body.html')
 );
 
