66a9d467ced3242c8be4601afd3b93bd4e76316b
Meik Sievertsen <acydburn@phpbb.com>
2009-04-29 14:52:43 +0000

    only pre-fill forums if in forum mode (else it is too irritating to see the forum select skipped on user/group selection screens)
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9496 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/acp/acp_permissions.php
+++ phpBB/includes/acp/acp_permissions.php
@@ -762,7 +762,14 @@ class acp_permissions
 
 		$this->log_action($mode, 'add', $permission_type, $ug_type, $ug_ids, $forum_ids);
 
-		trigger_error($user->lang['AUTH_UPDATED'] . adm_back_link($this->u_action . '&amp;forum_id[]=' . implode('&amp;forum_id[]=', $forum_ids)));
+		if ($mode == 'setting_forum_local' || $mode == 'setting_mod_local')
+		{
+			trigger_error($user->lang['AUTH_UPDATED'] . adm_back_link($this->u_action . '&amp;forum_id[]=' . implode('&amp;forum_id[]=', $forum_ids)));
+		}
+		else
+		{
+			trigger_error($user->lang['AUTH_UPDATED'] . adm_back_link($this->u_action));
+		}
 	}
 
 	/**
@@ -829,7 +836,14 @@ class acp_permissions
 
 		$this->log_action($mode, 'del', $permission_type, $ug_type, (($ug_type == 'user') ? $user_id : $group_id), (sizeof($forum_id) ? $forum_id : array(0 => 0)));
 
-		trigger_error($user->lang['AUTH_UPDATED'] . adm_back_link($this->u_action . '&amp;forum_id[]=' . implode('&amp;forum_id[]=', $forum_id)));
+		if ($mode == 'setting_forum_local' || $mode == 'setting_mod_local')
+		{
+			trigger_error($user->lang['AUTH_UPDATED'] . adm_back_link($this->u_action . '&amp;forum_id[]=' . implode('&amp;forum_id[]=', $forum_id)));
+		}
+		else
+		{
+			trigger_error($user->lang['AUTH_UPDATED'] . adm_back_link($this->u_action));
+		}
 	}
 
 	/**
