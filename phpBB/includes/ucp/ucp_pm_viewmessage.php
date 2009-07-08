1c6e99848f9663792051c6bd84b6159b5ad57a4e
Ruslan Uzdenov <rxu@mail.ru>
2009-07-08 12:50:45 +0000

    Fixed Bug #40155 - Postcount of 0 not showing up in PMs Authorised by: AcydBurn
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9733 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/ucp/ucp_pm_viewmessage.php
+++ phpBB/includes/ucp/ucp_pm_viewmessage.php
@@ -178,7 +178,7 @@ function view_message($id, $mode, $folder_id, $msg_id, $folder, $message_row)
 		'RANK_IMG'			=> $user_info['rank_image'],
 		'AUTHOR_AVATAR'		=> (isset($user_info['avatar'])) ? $user_info['avatar'] : '',
 		'AUTHOR_JOINED'		=> $user->format_date($user_info['user_regdate']),
-		'AUTHOR_POSTS'		=> (!empty($user_info['user_posts'])) ? $user_info['user_posts'] : '',
+		'AUTHOR_POSTS'		=> (int) $user_info['user_posts'],
 		'AUTHOR_FROM'		=> (!empty($user_info['user_from'])) ? $user_info['user_from'] : '',
 
 		'ONLINE_IMG'		=> (!$config['load_onlinetrack']) ? '' : ((isset($user_info['online']) && $user_info['online']) ? $user->img('icon_user_online', $user->lang['ONLINE']) : $user->img('icon_user_offline', $user->lang['OFFLINE'])),
