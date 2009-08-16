389663872784bd4c411e9b3a813107ec51e70b11
Henry Sudhof <kellanved@phpbb.com>
2009-08-16 20:49:22 +0000

    append_sid
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9996 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/captcha/plugins/captcha_abstract.php
+++ phpBB/includes/captcha/plugins/captcha_abstract.php
@@ -95,7 +95,7 @@ class phpbb_default_captcha
 		}
 		else
 		{
-			$link = append_sid($phpbb_root_path . 'ucp.' . $phpEx . '?mode=confirm&amp;confirm_id=' . $this->confirm_id . '&amp;type=' . $this->type);
+			$link = append_sid($phpbb_root_path . 'ucp.' . $phpEx,  'mode=confirm&amp;confirm_id=' . $this->confirm_id . '&amp;type=' . $this->type);
 			$explain = ($this->type != CONFIRM_POST) ? sprintf($user->lang['CONFIRM_EXPLAIN'], '<a href="mailto:' . htmlspecialchars($config['board_contact']) . '">', '</a>') : $user->lang['POST_CONFIRM_EXPLAIN'];
 			
 			$template->assign_vars(array(
@@ -129,7 +129,7 @@ class phpbb_default_captcha
 
 		// acp_captcha has a delivery function; let's use it
 		$template->assign_vars(array(
-			'CONFIRM_IMAGE'		=> append_sid($phpbb_admin_path . 'index.' . $phpEx . '?captcha_demo=1&amp;mode=visual&amp;i=' . $id . '&amp;select_captcha=' . $this->get_class_name()) . $variables,
+			'CONFIRM_IMAGE'		=> append_sid($phpbb_admin_path . 'index.' . $phpEx, 'captcha_demo=1&amp;mode=visual&amp;i=' . $id . '&amp;select_captcha=' . $this->get_class_name()) . $variables,
 			'CONFIRM_ID'		=> $this->confirm_id,
 		));
 
