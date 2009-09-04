ba2fb956de1affbd924c07b020385413077bbe8a
Meik Sievertsen <acydburn@phpbb.com>
2009-09-04 14:49:41 +0000

    spelling
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@10101 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/language/en/install.php
+++ phpBB/language/en/install.php
@@ -470,7 +470,7 @@ $lang = array_merge($lang, array(
 	'NO_VISIBLE_CHANGES'			=> 'No visible changes',
 	'NOTICE'						=> 'Notice',
 	'NUM_CONFLICTS'					=> 'Number of conflicts',
-	'NUMBER_OF_FILES_COLLECTED'		=> 'Currently differences of %1$d of %2$d files have been checked.<br />Please wait until all files are checked.',
+	'NUMBER_OF_FILES_COLLECTED'		=> 'Currently differences from %1$d of %2$d files have been checked.<br />Please wait until all files are checked.',
 
 	'OLD_UPDATE_FILES'		=> 'Update files are out of date. The update files found are for updating from phpBB %1$s to phpBB %2$s but the latest version of phpBB is %3$s.',
 
