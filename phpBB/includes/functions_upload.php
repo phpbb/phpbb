phpbb/phpbb

commit a5e4ca259aa98bc3d5abc03dcf4504c2afe91e92

Always remove temporary filename (Bug #50965)
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@10122 89ea8834-ac86-4346-8a33-228a782c2dd0

phpBB/includes/functions_upload.php
@@ -313,11 +313,8 @@ class filespec
 						if (!@move_uploaded_file($this->filename, $this->destination_file))
 						{
 							$this->error[] = sprintf($user->lang[$this->upload->error_prefix . 'GENERAL_UPLOAD_ERROR'], $this->destination_file);
-							return false;
 						}
 					}
-
-					@unlink($this->filename);
 
 				break;
 
@@ -328,11 +325,8 @@ class filespec
 						if (!@copy($this->filename, $this->destination_file))
 						{
 							$this->error[] = sprintf($user->lang[$this->upload->error_prefix . 'GENERAL_UPLOAD_ERROR'], $this->destination_file);
-							return false;
 						}
 					}
-
-					@unlink($this->filename);
 
 				break;
 
@@ -341,14 +335,21 @@ class filespec
 					if (!@copy($this->filename, $this->destination_file))
 					{
 						$this->error[] = sprintf($user->lang[$this->upload->error_prefix . 'GENERAL_UPLOAD_ERROR'], $this->destination_file);
-						return false;
 					}
-					@unlink($this->filename);
 
 				break;
 			}
 
+			// Remove temporary filename
+			@unlink($this->filename);
+
+			if (sizeof($this->error))
+			{
+				return false;
+			}
+
 			phpbb_chmod($this->destination_file, $chmod);
+			return true;
 		}
 
 		// Try to get real filesize from destination folder

commit 3b73584430bd88a655e6ec3cdca3ad56ded95b21

why am i so dumb sometimes... /me hits myself with the dumbness stick... regression from r10122
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@10141 89ea8834-ac86-4346-8a33-228a782c2dd0

phpBB/includes/functions_upload.php
@@ -349,7 +349,6 @@ class filespec
 			}
 
 			phpbb_chmod($this->destination_file, $chmod);
-			return true;
 		}
 
 		// Try to get real filesize from destination folder