phpbb/phpbb

commit 89332c00b7198b5bd07fdb98b21d4734c300da15

Fix Bug #40515 - Fix set_custom_template for database-stored styles
Authorised by: AcydBurn



git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9839 89ea8834-ac86-4346-8a33-228a782c2dd0

phpBB/includes/template.php
@@ -39,6 +39,8 @@ class template
 	var $files_inherit = array();
 	var $files_template = array();
 	var $inherit_root = '';
+	var $orig_tpl_storedb = 'undefined';
+	var $orig_tpl_inherits_id = 'undefined';
 
 	// this will hash handle names to the compiled/uncompiled code for that handle.
 	var $compiled_code = array();
@@ -55,6 +57,16 @@ class template
 		{
 			$this->root = $phpbb_root_path . 'styles/' . $user->theme['template_path'] . '/template';
 			$this->cachepath = $phpbb_root_path . 'cache/tpl_' . str_replace('_', '-', $user->theme['template_path']) . '_';
+			if ($this->orig_tpl_storedb == 'undefined')
+			{
+				$this->orig_tpl_storedb = $user->theme['template_storedb'];
+			}
+			if ($this->orig_tpl_inherits_id == 'undefined')
+			{
+				$this->orig_tpl_inherits_id = $user->theme['template_inherits_id'];
+			}
+			$user->theme['template_storedb'] = $this->orig_tpl_storedb;
+			$user->theme['template_inherits_id'] = $this->orig_tpl_inherits_id;
 
 			if ($user->theme['template_inherits_id'])
 			{
@@ -77,10 +89,12 @@ class template
 	*/
 	function set_custom_template($template_path, $template_name)
 	{
-		global $phpbb_root_path;
+		global $phpbb_root_path, $user;
 
 		$this->root = $template_path;
 		$this->cachepath = $phpbb_root_path . 'cache/ctpl_' . str_replace('_', '-', $template_name) . '_';
+		$user->theme['template_storedb'] = false;
+		$user->theme['template_inherits_id'] = false;
 
 		$this->_rootref = &$this->_tpldata['.'][0];
 

commit 697fd5a134bde47006f57b6dd8df4e2dec6a7f3d

Fix #51285 and more issues with template inherence on set_custom_template Conflict introduced with r9839 and r9823
Authorised by: AcydBurn

git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@10150 89ea8834-ac86-4346-8a33-228a782c2dd0

phpBB/includes/bbcode.php
@@ -128,7 +128,7 @@ class bbcode
 	*/
 	function bbcode_cache_init()
 	{
-		global $user, $phpbb_root_path;
+		global $phpbb_root_path, $template, $user;
 
 		if (empty($this->template_filename))
 		{
@@ -137,7 +137,7 @@ class bbcode
 
 			if (!@file_exists($this->template_filename))
 			{
-				if (isset($user->theme['template_inherits_id']) && $user->theme['template_inherits_id'])
+				if (isset($template->orig_tpl_inherits_id) && $template->orig_tpl_inherits_id)
 				{
 					$this->template_filename = $phpbb_root_path . 'styles/' . $user->theme['template_inherit_path'] . '/template/bbcode.html';
 					if (!@file_exists($this->template_filename))

phpBB/includes/functions_messenger.php
@@ -199,7 +199,7 @@ class messenger
 				$template_path .= $template_lang . '/email';
 			}
 
-			$tpl->set_custom_template($template_path, $template_lang . '_email');
+			$tpl->set_custom_template($template_path, $template_lang . '_email', 'email');
 
 			$tpl->set_filenames(array(
 				'body'		=> $template_file . '.txt',

phpBB/includes/template.php
@@ -90,7 +90,7 @@ class template
 	* Set custom template location (able to use directory outside of phpBB)
 	* @access public
 	*/
-	function set_custom_template($template_path, $template_name)
+	function set_custom_template($template_path, $template_name, $template_mode = 'template')
 	{
 		global $phpbb_root_path, $user;
 
@@ -102,8 +102,13 @@ class template
 
 		$this->root = $template_path;
 		$this->cachepath = $phpbb_root_path . 'cache/ctpl_' . str_replace('_', '-', $template_name) . '_';
-		$user->theme['template_storedb'] = false;
-		$user->theme['template_inherits_id'] = false;
+
+		// As the template-engine is used for more than the template (emails, etc.), we should not set $user->theme in all cases, but only on the real template.
+		if ($template_mode == 'template')
+		{
+			$user->theme['template_storedb'] = false;
+			$user->theme['template_inherits_id'] = false;
+		}
 
 		$this->_rootref = &$this->_tpldata['.'][0];
 