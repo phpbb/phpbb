3850de87fc430561612ca27d61855aee8ef43d1e
Henry Sudhof <kellanved@phpbb.com>
2009-09-30 15:00:44 +0000

    #52245 Authorised by: AcydBurn
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@10199 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/includes/acp/acp_users.php
+++ phpBB/includes/acp/acp_users.php
@@ -1023,7 +1023,7 @@ class acp_users
 					'U_WHOIS'		=> $this->u_action . "&amp;action=whois&amp;user_ip={$user_row['user_ip']}",
 					'U_MCP_QUEUE'	=> ($auth->acl_getf_global('m_approve')) ? append_sid("{$phpbb_root_path}mcp.$phpEx", 'i=queue', true, $user->session_id) : '',
 
-					'U_SWITCH_PERMISSIONS'	=> ($auth->acl_get('a_switchperm') && $user->data['user_id'] != $user_row['user_id']) ? append_sid("{$phpbb_root_path}ucp.$phpEx", "mode=switch_perm&amp;u={$user_row['user_id']}") : '',
+					'U_SWITCH_PERMISSIONS'	=> ($auth->acl_get('a_switchperm') && $user->data['user_id'] != $user_row['user_id']) ? append_sid("{$phpbb_root_path}ucp.$phpEx", "mode=switch_perm&amp;u={$user_row['user_id']}&amp;hash=" . generate_link_hash('switchperm')) : '',
 
 					'POSTS_IN_QUEUE'	=> $user_row['posts_in_queue'],
 					'USER'				=> $user_row['username'],
