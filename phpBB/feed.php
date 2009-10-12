cc0eadb67feb8debcf38f8e102f1c3ed63368f33
Meik Sievertsen <acydburn@phpbb.com>
2009-10-12 14:16:09 +0000

    remove unused closing div tag (Bug #52735)
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@10221 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/feed.php
+++ phpBB/feed.php
@@ -196,7 +196,7 @@ foreach ($item_vars as $row)
 		echo '<p>' . $user->lang['STATISTICS'] . ': ' . $row['statistics'] . '</p>';
 	}
 
-	echo '<hr /></div>' . "\n" . ']]></content>' . "\n";
+	echo '<hr />' . "\n" . ']]></content>' . "\n";
 	echo '</entry>' . "\n";
 }
 
