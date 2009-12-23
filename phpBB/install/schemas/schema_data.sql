736f26dea1c23ce1eff5da716292c997b6c03785
Andreas Fischer <bantu@phpbb.com>
2009-12-23 02:55:28 +0000

    Reflect changes in schema data. This should also close #55775 ;-)
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@10371 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/install/schemas/schema_data.sql
+++ phpBB/install/schemas/schema_data.sql
@@ -99,11 +99,11 @@ INSERT INTO phpbb_config (config_name, config_value) VALUES ('enable_confirm', '
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('enable_pm_icons', '1');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('enable_post_confirm', '1');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_enable', '0');
-INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_limit', '10');
+INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_limit_post', '15');
+INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_limit_topic', '10');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_overall_forums', '1');
-INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_overall_forums_limit', '15');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_overall_topics', '0');
-INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_overall_topics_limit', '15');
+INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_overall', '1');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_forum', '1');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_topic', '1');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('feed_item_statistics', '1');
