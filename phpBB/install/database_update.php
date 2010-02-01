20301d6b9f78732ac4bde379a3db847435192e81
Nils Adermann <naderman@naderman.de>
2010-02-01 00:49:35 +0000

    Change version numbers to 3.0.7 and 3.0.7-RC1 for a QA release
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_7@10467 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/install/database_update.php
+++ phpBB/install/database_update.php
@@ -8,7 +8,7 @@
 *
 */
 
-$updates_to_version = '3.0.7-dev';
+$updates_to_version = '3.0.7-RC1';
 
 // Enter any version to update from to test updates. The version within the db will not be updated.
 $debug_from_version = false;
