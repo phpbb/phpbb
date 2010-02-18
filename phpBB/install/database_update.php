19da9ac065d97907ef67e49f5edc7bbe68d4b79d
Andreas Fischer <bantu@phpbb.com>
2010-02-18 02:13:15 +0000

    Merge r10503, r10504 into 3.0.7 branch.
    git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_7@10505 89ea8834-ac86-4346-8a33-228a782c2dd0

--- phpBB/install/database_update.php
+++ phpBB/install/database_update.php
@@ -907,8 +907,10 @@ function database_update_info()
 			),
 		),
 
-		// No changes from 3.0.7-RC1 to 3.0.7
+		// No changes from 3.0.7-RC1 to 3.0.7-RC2
 		'3.0.7-RC1'		=> array(),
+		// No changes from 3.0.7-RC2 to 3.0.7
+		'3.0.7-RC2'		=> array(),
 	);
 }
 
@@ -1601,7 +1603,7 @@ function change_database_data(&$no_updates, $version)
 			$no_updates = false;
 		break;
 
-		// No changes from 3.0.7-RC1 to 3.0.7
+		// Changes from 3.0.7-RC1 to 3.0.7-RC2
 		case '3.0.7-RC1':
 
 			$sql = 'SELECT user_id, user_email, user_email_hash
@@ -1624,13 +1626,19 @@ function change_database_data(&$no_updates, $version)
 					$sql = 'UPDATE ' . USERS_TABLE . '
 						SET ' . $db->sql_build_array('UPDATE', $sql_ary) . '
 						WHERE user_id = ' . (int) $row['user_id'];
-					__sql($sql, $errored, $error_ary, ($i % 100 == 0));
+					_sql($sql, $errored, $error_ary, ($i % 100 == 0));
 
 					++$i;
 				}
 			}
 			$db->sql_freeresult($result);
 
+			$no_updates = false;
+
+		break;
+
+		// No changes from 3.0.7-RC2 to 3.0.7
+		case '3.0.7-RC2':
 		break;
 	}
 }
