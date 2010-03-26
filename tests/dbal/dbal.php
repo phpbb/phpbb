c6442ce64072fd5080d1b2f3a3d595c62ccdc59c
Joas Schilling <nickvergessen@gmx.de>
2010-03-26 21:02:56 +0100

    [feature/dbal-tests] Tests for $db->sql_query_limit()

--- tests/dbal/dbal.php
+++ tests/dbal/dbal.php
@@ -14,7 +14,7 @@ class phpbb_dbal_test extends phpbb_database_test_case
 {
 	public function getDataSet()
 	{
-		return $this->createXMLDataSet(dirname(__FILE__).'/fixtures/two_users.xml');
+		return $this->createXMLDataSet(dirname(__FILE__).'/fixtures/three_users.xml');
 	}
 
 	public function test_select_row()
@@ -38,6 +38,48 @@ class phpbb_dbal_test extends phpbb_database_test_case
 			WHERE user_id = 2');
 
 		$this->assertEquals('foobar', $db->sql_fetchfield('username_clean'));
+	}
+
+	public static function query_limit_data()
+	{
+		return array(
+			array(0, 0, array(array('username_clean' => 'barfoo'),
+				array('username_clean' => 'foobar'),
+				array('username_clean' => 'bertie'))),
+			array(0, 1, array(array('username_clean' => 'foobar'),
+				array('username_clean' => 'bertie'))),
+			array(1, 0, array(array('username_clean' => 'barfoo'))),
+			array(1, 1, array(array('username_clean' => 'foobar'))),
+			array(1, 2, array(array('username_clean' => 'bertie'))),
+			array(2, 0, array(array('username_clean' => 'barfoo'),
+				array('username_clean' => 'foobar'))),
+			array(2, 2, array(array('username_clean' => 'bertie'))),
+			array(2, 5, array()),
+			array(10, 1, array(array('username_clean' => 'foobar'),
+				array('username_clean' => 'bertie'))),
+			array(10, 5, array()),
+		);
+	}
+
+	/**
+	* @dataProvider query_limit_data
+	*/
+	public function test_query_limit($total, $offset, $expected)
+	{
+		$db = $this->new_dbal();
+
+		$result = $db->sql_query_limit('SELECT username_clean
+			FROM phpbb_users
+			ORDER BY user_id', $total, $offset);
+
+		$ary = array();
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$ary[] = $row;
+		}
+		$db->sql_freeresult($result);
+
+		$this->assertEquals($expected, $ary);
 	}
 }
 
