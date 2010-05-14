3e111bb41ad6a2ea954dd52bcf78b954114c37e9
Andreas Fischer <bantu@phpbb.com>
2010-05-14 03:21:08 +0200

    [ticket/9599] Adding network function tests.
    PHPBB3-9599

---
+++ tests/network/all_tests.php
@@ -0,0 +1,36 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2010 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+if (!defined('PHPUnit_MAIN_METHOD'))
+{
+	define('PHPUnit_MAIN_METHOD', 'phpbb_network_all_tests::main');
+}
+
+require_once 'test_framework/framework.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+class phpbb_network_all_tests
+{
+	public static function main()
+	{
+		PHPUnit_TextUI_TestRunner::run(self::suite());
+	}
+
+	public static function suite()
+	{
+		$suite = new PHPUnit_Framework_TestSuite('phpBB Network Functions');
+
+		return $suite;
+	}
+}
+
+if (PHPUnit_MAIN_METHOD == 'phpbb_network_all_tests::main')
+{
+	phpbb_network_all_tests::main();
+}
