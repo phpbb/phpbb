e09d6c6d7139375d8b645664bef295c82d585653
Andreas Fischer <bantu@phpbb.com>
2010-08-20 21:12:18 +0200

    [ticket/9780] Adding unit tests for gen_rand_string().
    PHPBB3-9780

--- tests/all_tests.php
+++ tests/all_tests.php
@@ -25,6 +25,7 @@ require_once 'text_processing/all_tests.php';
 require_once 'dbal/all_tests.php';
 require_once 'regex/all_tests.php';
 require_once 'network/all_tests.php';
+require_once 'random/all_tests.php';
 
 // exclude the test directory from code coverage reports
 PHPUnit_Util_Filter::addDirectoryToFilter('./');
@@ -48,6 +49,7 @@ class phpbb_all_tests
 		$suite->addTest(phpbb_dbal_all_tests::suite());
 		$suite->addTest(phpbb_regex_all_tests::suite());
 		$suite->addTest(phpbb_network_all_tests::suite());
+		$suite->addTest(phpbb_random_all_tests::suite());
 
 		return $suite;
 	}
