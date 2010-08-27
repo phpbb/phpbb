phpbb/phpbb

commit c1a4cb1d01dc19650219566b60671abc767af662

[ticket/7782] Send status line using refactored download/file.php logic.
PHPBB3-7782

phpBB/download/file.php
@@ -670,15 +670,7 @@ function set_modified_headers($stamp, $browser)
 	{
 		if ($last_load !== false && $last_load >= $stamp)
 		{
-			if (substr(strtolower(@php_sapi_name()),0,3) === 'cgi')
-			{
-				// in theory, we shouldn't need that due to php doing it. Reality offers a differing opinion, though
-				header('Status: 304 Not Modified', true, 304);
-			}
-			else
-			{
-				header('HTTP/1.0 304 Not Modified', true, 304);
-			}
+			send_status_line(304, 'Not Modified');
 			// seems that we need those too ... browsers
 			header('Pragma: public');
 			header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + 31536000));

phpBB/includes/functions.php
@@ -2578,6 +2578,27 @@ function meta_refresh($time, $url, $disable_cd_check = false)
 	return $url;
 }
 
+function send_status_line($code, $message)
+{
+	if (substr(strtolower(@php_sapi_name()),0,3) === 'cgi')
+	{
+		// in theory, we shouldn't need that due to php doing it. Reality offers a differing opinion, though
+		header("Status: $code $message", true, $code);
+	}
+	else
+	{
+		if (isset($_SERVER['HTTP_VERSION']))
+		{
+			$version = $_SERVER['HTTP_VERSION'];
+		}
+		else
+		{
+			$version = 'HTTP/1.0';
+		}
+		header("$version $code $message", true, $code);
+	}
+}
+
 //Form validation
 
 
@@ -3623,7 +3644,7 @@ function msg_handler($errno, $msg_text, $errfile, $errline)
 
 			if ($msg_text == 'NO_FORUM' || $msg_text == 'NO_TOPIC' || $msg_text == 'NO_USER')
 			{
-				header("HTTP/1.x 404 Not Found");
+				send_status_line(404, 'Not Found');
 			}
 
 			$msg_text = (!empty($user->lang[$msg_text])) ? $user->lang[$msg_text] : $msg_text;

commit 3afd2c6948e777fde2f43f00bff1659f2691f4d8

[ticket/9793] Make sure send_status_line() is defined when in avatar mode.
When download/file.php is in avatar mode set_modified_headers() can be called,
which then calls send_status_line() which is not available because
includes/functions.php is not included in avatar mode.

We also load includes/functions.php in the bootstrap now to solve this problem.

Regression from c1a4cb1d01dc19650219566b60671abc767af662.

PHPBB3-9793
PHPBB3-7782

phpBB/download/file.php
@@ -48,6 +48,7 @@ if (isset($_GET['avatar']))
 	require($phpbb_root_path . 'includes/cache.' . $phpEx);
 	require($phpbb_root_path . 'includes/db/' . $dbms . '.' . $phpEx);
 	require($phpbb_root_path . 'includes/constants.' . $phpEx);
+	require($phpbb_root_path . 'includes/functions.' . $phpEx);
 
 	$db = new $sql_db();
 	$cache = new cache();