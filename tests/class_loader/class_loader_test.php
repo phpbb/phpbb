9edf0d64a2735eaff6dbd15487b008bf9ef9b686
Igor Wiedler <igor@wiedler.ch>
2010-08-31 20:30:59 +0200

    [feature/auto-loading] Add test case for phpbb_dir2 => includes/dir2/dir2.php
    PHPBB3-9682

--- tests/class_loader/class_loader_test.php
+++ tests/class_loader/class_loader_test.php
@@ -44,6 +44,11 @@ class phpbb_class_loader_test extends PHPUnit_Framework_TestCase
 			$class_loader->resolve_path('phpbb_dir_subdir_class_name'),
 			'Class in a sub-directory'
 		);
+		$this->assertEquals(
+			$prefix . 'dir2/dir2.php',
+			$class_loader->resolve_path('phpbb_dir2'),
+			'Class with name of dir within dir (short class name)'
+		);
 	}
 
 	public function test_resolve_cached()
