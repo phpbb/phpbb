55b176882fe5fdf497555ccaf8cca3205aad3b17
Oleg Pudeyev <oleg@bsdpower.com>
2011-01-27 18:26:58 -0500

    [ticket/10013] Changed cache test to use tests/tmp/cache as cache directory
    This is the first step of making the test suite use a single directory
    for writing files to, this directory being tests/tmp.
    
    The cache test, instead of tests/cache/tmp, now writes to tests/tmp/cache.
    
    Also remove cache directory in setUp method, in case an earlier test run
    did not complete successfully and the cache directory was not cleaned up.
    
    Finally, this change makes the cache test take responsibility over the entire
    contents of its cache directory.
    
    PHPBB3-10013

--- tests/cache/cache_test.php
+++ tests/cache/cache_test.php
@@ -11,21 +11,53 @@ require_once __DIR__ . '/../../phpBB/includes/functions.php';
 
 class phpbb_cache_test extends phpbb_test_case
 {
+	private $cache_dir;
+
+	public function __construct()
+	{
+		$this->cache_dir = dirname(__FILE__) . '/../tmp/cache';
+	}
+
+	protected function setUp()
+	{
+		if (file_exists($this->cache_dir))
+		{
+			// cache directory possibly left after aborted
+			// or failed run earlier
+			$this->remove_cache_dir();
+		}
+		$this->create_cache_dir();
+	}
+
 	protected function tearDown()
 	{
-		$iterator = new DirectoryIterator(__DIR__ . '/tmp');
+		if (file_exists($this->cache_dir))
+		{
+			$this->remove_cache_dir();
+		}
+	}
+
+	private function create_cache_dir()
+	{
+		mkdir($this->cache_dir);
+	}
+
+	private function remove_cache_dir()
+	{
+		$iterator = new DirectoryIterator($this->cache_dir);
 		foreach ($iterator as $file)
 		{
-			if (is_file(__DIR__ . '/tmp/' . $file) && $file != '.gitkeep')
+			if ($file != '.' && $file != '..')
 			{
-				unlink(__DIR__ . '/tmp/' . $file);
+				unlink($this->cache_dir . '/' . $file);
 			}
 		}
+		rmdir($this->cache_dir);
 	}
 
 	public function test_cache_driver_file()
 	{
-		$driver = new phpbb_cache_driver_file(__DIR__ . '/tmp/');
+		$driver = new phpbb_cache_driver_file($this->cache_dir);
 		$driver->put('test_key', 'test_value');
 		$driver->save();
 
