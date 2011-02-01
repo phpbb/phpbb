9846d806f3bc719138f237b64c8ca7db961838e8
Joas Schilling <nickvergessen@gmx.de>
2011-02-01 14:54:29 +0100

    [ticket/9823] Move mock file into the mock/ folder.
    PHPBB3-9823

--- tests/functions_acp/validate_range_test.php
+++ tests/functions_acp/validate_range_test.php
@@ -7,8 +7,8 @@
 *
 */
 
-require_once dirname(__FILE__) . 'user_mock.php';
-require_once dirname(__FILE__) . '../../phpBB/includes/functions_acp.php';
+require_once dirname(__FILE__) . '/../mock/lang.php';
+require_once dirname(__FILE__) . '/../../phpBB/includes/functions_acp.php';
 
 class phpbb_functions_acp_validate_range_test extends phpbb_test_case
 {
