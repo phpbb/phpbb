f5136e78483d1686be55c9b1286e4bf62d85b683
Oleg Pudeyev <oleg@bsdpower.com>
2011-08-09 21:54:30 -0400

    [ticket/9369] Split a statement for readability.
    PHPBB3-9369

--- phpBB/includes/confirm_box.php
+++ phpBB/includes/confirm_box.php
@@ -178,7 +178,8 @@ class phpbb_confirm_box
 			return check_form_key($this->title);
 		}
 
-		$confirm = ($this->user->lang['YES'] === $this->request->variable('confirm', '', true, phpbb_request_interface::POST));
+		$confirm_value = $this->request->variable('confirm', '', true, phpbb_request_interface::POST);
+		$confirm = ($this->user->lang['YES'] === $confirm_value);
 
 		if ($this->request->is_set_post('cancel'))
 		{
