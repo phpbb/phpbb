d938d5d3994e5febd6a7530e740e24b5727d38c4
Patrick Webster <noxwizard@phpbb.com>
2011-09-04 20:22:36 -0500

    [ticket/10351] Fix Oracle's sql_column_remove()
    The correct syntax is DROP COLUMN.
    
    PHPBB3-10351

--- phpBB/includes/db/db_tools.php
+++ phpBB/includes/db/db_tools.php
@@ -1805,7 +1805,7 @@ class phpbb_db_tools
 			break;
 
 			case 'oracle':
-				$statements[] = 'ALTER TABLE ' . $table_name . ' DROP ' . $column_name;
+				$statements[] = 'ALTER TABLE ' . $table_name . ' DROP COLUMN ' . $column_name;
 			break;
 
 			case 'postgres':
