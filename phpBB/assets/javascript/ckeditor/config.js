1a69c4738cc0ca5404a3379674bbce7779e3c172
Eric Faerber <wgeric@gmail.com>
2011-10-16 22:12:06 -0600

    [feature/ckeditor] Adding CKEditor as a WYSIWYG editor for posts.
    * Users can set in the UCP whether to use the WYSIWYG editor or not
    * Can toggle between modes on the posting screen
    
    PHPBB3-10418

---
+++ phpBB/assets/javascript/ckeditor/config.js
@@ -0,0 +1,37 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	
+	config.extraPlugins = 'bbcode,buttonpre';
+	config.toolbar = [
+		{
+			name: 'basicstyles',
+			items: ['Bold', 'Italic', 'Underline', 'RemoveFormat']
+		},
+		{
+			name: 'paragraph',
+			items: ['NumberedList', 'BulletedList', 'Blockquote', 'buttonpre']
+		},
+		{
+			name: 'links',
+			items: ['Link', 'Unlink', 'Anchor']
+		},
+		{
+			name: 'insert',
+			items: ['Image', 'Flash', 'Smiley']
+		},
+		{
+			name: 'font',
+			items: ['FontSize', 'TextColor' ]
+		}
+	];
+	config.fontSize_defaultLabel = 'Normal';
+	config.fontSize_sizes = 'Tiny/50%;Small/85%;Normal/100%;Large/150%;Huge/200%';
+};
