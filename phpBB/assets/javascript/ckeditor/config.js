5ff5e2d2a0a84806b59aff9f5626ddd3f6f39d19
wGEric <wgeric@gmail.com>
2011-10-20 16:00:35 -0600

    [feature/ckeditor] Move files around in CKEditor to make it easier to configure
    * Config is now in a different file then the one provided by CKEditor. This
    makes it easier to update CKEditor.
    * Themes can define/create skins for the editor
    
    PHPBB3-10418

--- phpBB/assets/javascript/ckeditor/config.js
+++ phpBB/assets/javascript/ckeditor/config.js
@@ -8,30 +8,4 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	
-	config.extraPlugins = 'bbcode,buttonpre';
-	config.toolbar = [
-		{
-			name: 'basicstyles',
-			items: ['Bold', 'Italic', 'Underline', 'RemoveFormat']
-		},
-		{
-			name: 'paragraph',
-			items: ['NumberedList', 'BulletedList', 'Blockquote', 'buttonpre']
-		},
-		{
-			name: 'links',
-			items: ['Link', 'Unlink', 'Anchor']
-		},
-		{
-			name: 'insert',
-			items: ['Image', 'Flash', 'Smiley']
-		},
-		{
-			name: 'font',
-			items: ['FontSize', 'TextColor' ]
-		}
-	];
-	config.fontSize_defaultLabel = 'Normal';
-	config.fontSize_sizes = 'Tiny/50%;Small/85%;Normal/100%;Large/150%;Huge/200%';
 };
---
+++ phpBB/assets/javascript/editor.js
@@ -0,0 +1,90 @@
+/**
+ * Sets up the WYSIWYG editor for posts
+ */
+(function($) {	
+	$(function() {
+		var editor = null,
+			format_buttons = $('#format-buttons'),
+			colour_palette = $('#colour_palette');
+		
+		/**
+		 * creates/shows the editor
+		 */
+		function createEditor() {
+			editor = editor || (CKEDITOR.instances.message || CKEDITOR.instances.signature);
+			
+			if (editor) {
+				return;
+			}
+			
+			$('textarea#message,textarea#signature').ckeditor({
+				// config file is in the theme so that it can set a skin and change other settings
+				customConfig : phpbb.board_url + phpbb.theme_path + '/ckeditor-config.js',
+				
+				// setup ckeditor
+				extraPlugins : 'bbcode,buttonpre',
+				toolbar : [
+					{
+						name: 'basicstyles',
+						items: ['Bold', 'Italic', 'Underline', 'RemoveFormat']
+					},
+					{
+						name: 'paragraph',
+						items: ['NumberedList', 'BulletedList', 'Blockquote', 'buttonpre']
+					},
+					{
+						name: 'links',
+						items: ['Link', 'Unlink', 'Anchor']
+					},
+					{
+						name: 'insert',
+						items: ['Image', 'Flash', 'Smiley']
+					},
+					{
+						name: 'font',
+						items: ['FontSize', 'TextColor' ]
+					}
+				],
+				fontSize_defaultLabel : 'Normal',
+				fontSize_sizes : 'Tiny/50%;Small/85%;Normal/100%;Large/150%;Huge/200%'
+			});
+		}
+		
+		/**
+		 * removes/hides the editor
+		 */
+		function removeEditor() {
+			editor = editor || (CKEDITOR.instances.message || CKEDITOR.instances.signature);
+			
+			if (!editor) {
+				return;
+			}
+			
+			editor.destroy();
+			editor = null;
+		}
+		
+		// if the format buttons are hidden then the user has the WYSIWYG editor on by default
+		if (format_buttons.is(':hidden')) {
+			createEditor();
+		}
+		
+		// switches to WYSIWYG mode
+		$('#mode-wysiwyg').click(function() {
+			format_buttons.hide();
+			colour_palette.hide();
+			
+			createEditor();
+			
+			return false;
+		});
+		
+		// switches to bbcode mode
+		$('#mode-bbcode').click(function() {
+			format_buttons.show();
+			removeEditor();
+			
+			return false;
+		});
+	});
+})(jQuery);
