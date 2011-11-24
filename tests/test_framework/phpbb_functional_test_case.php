ba1603a5f0e06d30a930170bec0773a3fbe68fc2
Oleg Pudeyev <oleg@bsdpower.com>
2011-11-24 14:16:58 -0500

    [ticket/10481] Use phar:// url for requiring goutte.
    It does not seem to work on my machine without phar:// even with
    phar extension installed.
    
    PHPBB3-10481

--- tests/test_framework/phpbb_functional_test_case.php
+++ tests/test_framework/phpbb_functional_test_case.php
@@ -24,7 +24,7 @@ class phpbb_functional_test_case extends phpbb_test_case
 			self::markTestSkipped('phar extension is not loaded');
 		}
 
-		require_once __DIR__ . '/../../vendor/goutte.phar';
+		require_once 'phar://' . __DIR__ . '/../../vendor/goutte.phar';
 	}
 
 	public function setUp()
