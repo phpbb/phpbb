04ed95ca57a8b4844ebfeb9103257f9b28273b97
Vjacheslav Trushkin <arty@phpbb.com>
2011-12-08 23:35:26 +0200

    [ticket/10319] Missing hidden fields in search form
    Missing hidden fields in search form (bug added in 3.0.9)
    
    PHPBB3-10319

--- phpBB/viewtopic.php
+++ phpBB/viewtopic.php
@@ -602,6 +602,15 @@ if ($_SID)
 	$s_search_hidden_fields['sid'] = $_SID;
 }
 
+if (!empty($_EXTRA_URL))
+{
+	foreach ($_EXTRA_URL as $url_param)
+	{
+		$url_param = explode('=', $url_param, 2);
+		$s_hidden_fields[$url_param[0]] = $url_param[1];
+	}
+}
+
 // Send vars to template
 $template->assign_vars(array(
 	'FORUM_ID' 		=> $forum_id,
