769cd370f4507a979abc6e4b5d62bee2ffcac685
David King <imkingdavid@gmail.com>
2011-12-26 01:21:15 +0000

    [feature/alwaysreply] Logic: reply works with u_sendpm or u_pm_reply
    PHPBB3-10517

--- phpBB/includes/ucp/ucp_pm_compose.php
+++ phpBB/includes/ucp/ucp_pm_compose.php
@@ -155,7 +155,8 @@ function compose_pm($id, $mode, $action, $user_folders = array())
 			{
 				trigger_error('NO_AUTH_SEND_MESSAGE');
 			}
-			else if (!$auth->acl_get('u_pm_reply'))
+			// users can reply if they have either u_pm_reply OR u_sendpm.
+			else if (!($auth->acl_get('u_pm_reply') || $auth->acl_get('u_sendpm')))
 			{
 				trigger_error('NO_AUTH_REPLY_MESSAGE');
 			}
