5e52734c240e5ecfb812202cd4cae305be9290c5
Unknown <unknownbliss@phpbbdevelopers.net>
2012-01-02 15:09:38 +0000

    [ticket/9916] Changing header in non-distributed files
    PHPBB3-9916

--- phpBB/develop/create_schema_files.php
+++ phpBB/develop/create_schema_files.php
@@ -2,9 +2,8 @@
 /**
 *
 * @package phpBB3
-* @version $Id$
 * @copyright (c) 2006 phpBB Group
-* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 * This file creates new schema files for every database.
 * The filenames will be prefixed with an underscore to not overwrite the current schema files.
