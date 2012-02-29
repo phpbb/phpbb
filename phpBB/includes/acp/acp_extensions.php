5e6b4c7192a80dc29382ad06976e9ce6d5c16efd
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-02-29 21:23:50 +0000

    [ticket/10631] Adding an extensions admin
    PHPBB3-10631

---
+++ phpBB/includes/acp/acp_extensions.php
@@ -0,0 +1,265 @@
+<?php
+/**
+*
+* @package acp
+* @copyright (c) 2012 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* @package acp
+*/
+class acp_extensions
+{
+	var $u_action;
+	
+	function main()
+	{
+		// Start the page
+		global $user, $template, $request;
+		
+		$user->add_lang(array('install', 'acp/customisations'));
+		
+		$this->page_title = 'ACP_EXTENSIONS';
+		
+		$action = $request->variable('action', '');
+		$ext_name = $request->variable('ext_name', '');
+		
+		// Set action to list if not set
+		if (empty($action))
+		{
+			$action = "list";
+		}
+		
+		// What are we doing?
+		switch ($action)
+		{
+			case 'list':
+			default:
+				$this->list_enabled_exts();
+				$this->list_disabled_exts();
+				$this->tpl_name = 'acp_ext_list';
+			break;
+
+			case 'enable_pre':
+				$this->tpl_name = 'acp_ext_enable';
+				$template->assign_vars(array(
+					'PRE'		=> true,
+					'U_ENABLE'	=> $this->u_action . '&amp;action=enable&amp;ext_name=' . $ext_name,
+				   )
+				);
+			break;
+			
+			case 'enable':
+				$name = $request->variable('ext_name', '');
+				$this->enable_extension($name);
+				$this->tpl_name = 'acp_ext_enable';
+			break;
+				
+			case 'disable_pre':
+				$this->tpl_name = 'acp_ext_disable';
+				$template->assign_vars(array(
+					'PRE'		=> true,
+					'U_DISABLE'	=> $this->u_action . '&amp;action=disable&amp;ext_name=' . $ext_name,
+				   )
+				);				
+			break;	
+	
+			case 'disable':
+				$name = $request->variable('ext_name', '');
+				$this->disable_extension($name);
+				$this->tpl_name = 'acp_ext_disable';
+			break;
+		
+			case 'purge_pre':
+				$this->tpl_name = 'acp_ext_purge';
+				$template->assign_vars(array(
+					'PRE'		=> true,
+					'U_PURGE'	=> $this->u_action . '&amp;action=purge&amp;ext_name=' . $ext_name,
+				   )
+				);
+			break;		
+		
+			case 'purge':
+				$name = $request->variable('ext_name', '');
+				$this->purge_extension($name);
+				$this->tpl_name = 'acp_ext_purge';
+			break;
+		
+			case 'delete_pre':
+				$this->tpl_name = 'acp_ext_delete';
+				$template->assign_vars(array(
+					'PRE'		=> true,
+					'U_DELETE'	=> $this->u_action . '&amp;action=delete&amp;ext_name=' . $ext_name,
+				   )
+				);
+			break;
+		
+			case 'delete':
+				$name = $request->variable('ext_name', '');
+				$this->tpl_name = 'acp_ext_delete';
+			break;
+			
+			case 'details':
+				$name = $request->variable('ext_name', '');
+				$filepath = $phpbb_root_path . 'ext/' . $name . '/extension.json';
+				$this->tpl_name = 'acp_ext_details';
+				$this->get_meta_info($filepath);
+			break;
+		}
+	}
+		
+	function enable_extension($name)
+	{
+		global $phpbb_extension_manager, $template, $cache;
+		
+		$phpbb_extension_manager->enable($name);
+		$template->assign_vars(array(
+			'U_RETURN'	=> $this->u_action . '&amp;action=list',
+		));
+		$cache->purge();
+	}
+
+	function disable_extension($name)
+	{
+		global $phpbb_extension_manager, $template, $cache;
+		$phpbb_extension_manager->disable($name);
+		$template->assign_vars(array(
+			'U_RETURN'	=> $this->u_action . '&amp;action=list',
+		));
+		$cache->purge();
+	}
+
+	function purge_extension($name)
+	{
+		global $phpbb_extension_manager, $template, $cache;
+		$phpbb_extension_manager->purge($name);
+		$template->assign_vars(array(
+			'U_RETURN'	=> $this->u_action . '&amp;action=list',
+		));
+		$cache->purge();
+	}
+		
+	function list_enabled_exts()
+	{
+		global $db, $template;
+
+		$sql = 'SELECT ext_name
+			FROM ' . EXT_TABLE . '
+			WHERE ext_active= 1
+			ORDER BY ext_name ASC';
+		$result = $db->sql_query($sql);
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$template->assign_block_vars('enabled', array(
+				'EXT_NAME'		=> $row['ext_name'],
+
+				'U_DETAILS'		=> $this->u_action . '&amp;action=details&amp;ext_name=' . $row['ext_name'],
+				'U_PURGE'		=> $this->u_action . '&amp;action=purge_pre&amp;ext_name=' . $row['ext_name'],
+				'U_DISABLE'		=> $this->u_action . '&amp;action=disable_pre&amp;ext_name=' . $row['ext_name'],
+			));
+		}
+		$db->sql_freeresult($result);
+
+		return;
+	}
+	
+	function list_disabled_exts()
+	{
+		global $db, $template;
+
+		$sql = 'SELECT ext_name
+			FROM ' . EXT_TABLE . '
+			WHERE ext_active= 0
+			ORDER BY ext_name ASC';
+		$result = $db->sql_query($sql);
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$template->assign_block_vars('disabled', array(
+				'EXT_NAME'		=> $row['ext_name'],
+
+				'U_DETAILS'		=> $this->u_action . '&amp;action=details&amp;ext_name=' . $row['ext_name'],
+				'U_PURGE'		=> $this->u_action . '&amp;action=purge_pre&amp;ext_name=' . $row['ext_name'],
+				'U_DELETE'		=> $this->u_action . '&amp;action=delete_pre&amp;ext_name=' . $row['ext_name'],
+				'U_ENABLE'		=> $this->u_action . '&amp;action=enable_pre&amp;ext_name=' . $row['ext_name'],
+			));
+		}
+		$db->sql_freeresult($result);
+
+		return;
+	}
+	
+	function list_avaliable_exts()
+	{
+		$phpbb_extension_manager->load_extensions();
+		$allavailable = array_keys($phpbb_extension_manager->all_available());
+		$allconfigured = array_keys($phpbb_extension_manager->all_configured());
+		$uninstalled = array_diff($allavailable, $allconfigured);
+		
+		foreach ($uninstalled as $ext)
+		{
+			$template->assign_block_vars('disabled', array(
+				'EXT_NAME'		=> $ext['ext_name'],
+
+				'U_DETAILS'		=> $this->u_action . '&amp;action=details&amp;ext_name=' . $ext['ext_name'],
+				'U_DELETE'		=> $this->u_action . '&amp;action=delete_pre&amp;ext_name=' . $ext['ext_name'],
+				'U_ENABLE'		=> $this->u_action . '&amp;action=enable_pre&amp;ext_name=' . $ext['ext_name'],
+			));
+		}
+		
+		return;
+	}
+	
+	function get_meta_info($filepath)
+	{
+		global $template;
+		
+		$metadatafile = file_get_contents($filepath);
+		$metadata = json_decode($metadatafile,true);
+		
+		$name = $metadata["name"];
+		$type = $metadata["type"];
+		$description = $metadata["description"];
+		$homepage = $metadata["homepage"];
+		$version = $metadata["version"];
+		$time = $metadata["time"];
+		$licence = $metadata["licence"];
+		$require_php = $metadata["require"]["php"];
+		$require_phpbb = $metadata["require"]["phpbb"];
+		$display_name = $metadata["extra"]["display-name"];
+		
+		$template->assign_vars(array(
+			'NAME'			=> $name,
+			'TYPE'			=> $type,
+			'DESCRIPTION'	=> $description,
+			'HOMEPAGE'		=> $homepage,
+			'VERSION'		=> $version,
+			'TIME'			=> $time,
+			'LICENSE'		=> $licence,
+			'REQUIRE_PHP'	=> $require_php,
+			'REQUIRE_PHPBB'	=> $require_phpbb,
+			'DISPLAY_NAME'	=> $display_name,
+			)
+		);
+		
+		foreach ($metadata["authors"] as $author)
+		{
+			$template->assign_block_vars('authors', array(
+				'AUTHOR_NAME'		=> $author["name"],
+				'AUTHOR_USERNAME'	=> $author["username"],
+				'AUTHOR_EMAIL'		=> $author["email"],
+				'AUTHOR_HOMEPAGE'	=> $author["homepage"],
+				'AUTHOR_TYPE'		=> $author["type"],
+			));
+		}
+	}
+}
---
+++ phpBB/language/en/acp/customisations.php
@@ -0,0 +1,100 @@
+<?php
+/** 
+*
+* acp_mods [English]
+*
+* @package language
+* @copyright (c) 2012 phpBB Group 
+* @license http://opensource.org/licenses/gpl-2.0.php GNU Public License 
+*
+*/
+/**
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE 
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+
+$lang = array_merge($lang, array(
+	'EXTENSION'				=> 'Extension',
+	'EXTENSIONS'			=> 'Extensions',
+	'EXTENSIONS_ADMIN'		=> 'Extensions Admin',
+	'EXTENSIONS_EXPLAIN'		=> 'The Extensions Admin is a tool in your phpBB Board which allows you to manage all of your extensions. For more information about extensions please visit <a href="http://phpbb.com/mods/extensions/">this page</a> on phpBBs Offical Website.',
+	
+	'DETAILS'				=> 'Details',
+	
+	'AVALIABLE'				=> 'Avaliable',
+	'ENABLED'				=> 'Enabled',
+	'DISBALED'				=> 'Disabled',
+	
+	'ENABLE'				=> 'Enable',
+	'DISABLE'				=> 'Disable',
+	'PURGE'					=> 'Purge',
+	'DELETE'				=> 'Delete',
+	
+	'ENABLED'				=> 'Enabled',
+	'DISABLED'				=> 'Disabled',
+	'PURGED'				=> 'Purged',
+	'DELETED'				=> 'Deleted',
+	
+	'ENABLE_EXPLAIN'		=> 'Enabling an extension allows you to use it on your board.',
+	'DISABLE_EXPLAIN'		=> 'Disabling an extension keeps the files and data intact but is not working on your board.',
+	'PURGE_EXPLAIN'			=> 'Purging an extension keeps the files but not the data on your board.',
+	'DELETE_EXPLAIN'		=> 'Deleting an extension removes all traces of the extension except from your logs.',
+	
+	'ENABLE_SUCESS'			=> 'Your extension was enabled sucessfully',
+	'DISABLE_SUCESS'		=> 'Your extension was disabled sucessfully',
+	'PURGE_SUCESS'			=> 'Your extension was purged sucessfully',
+	'DELETE_SUCESS'			=> 'Your extension was deleted sucessfully',
+	
+	'ENABLE_FAIL'			=> 'Your extension could not be enabled',
+	'DISABLE_FAIL'			=> 'Your extension could not be disabled',
+	'PURGE_FAIL'			=> 'Your extension could not be purged',
+	'DELETE_FAIL'			=> 'Your extension could not be deleted',
+	
+	'EXTENSION_NAME'			=> 'Extension Name',
+	'EXTENSION_ACTIONS'			=> 'Actions',
+	'EXTENSION_OPTIONS'			=> 'Options',
+
+	'ENABLE_CONFIRM'			=> 'Are you sure you wish to enable this extension?',
+	'DISABLE_CONFIRM'			=> 'Are your sure you wish to disable this extension?',
+	'PURGE_CONFIRM'				=> 'Are you sure you wish to purge (and not disable) this extension? This action will wipe the extension data on this board and is not reversible.',
+	'DELETE_CONFIRM'			=> 'Are you sure you wish to delete (and not purge or disable) this extension? This action will remove the extension and all of its data from this board and is not reversible.',
+	
+	'WARNING'			=> 'Warning',
+	'RETURN'			=> 'Return',
+	
+	''			=> '',
+	''			=> '',
+	''			=> '',
+	''			=> '',
+	''			=> '',
+	''			=> '',
+	''			=> '',
+	''			=> '',
+	''			=> '',
+	''			=> '',
+	''			=> '',
+	''			=> '',
+));
+
+?>
