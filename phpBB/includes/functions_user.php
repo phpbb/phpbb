55897d6b32b99a56b613a5d31983aca27055db3d
Joas Schilling <nickvergessen@gmx.de>
2012-03-15 21:00:20 +0100

    [feature/ledges] Adding ledge group_user_del
    Used by phpBB Gallery
    
    PHPBB3-9550

--- phpBB/includes/functions_user.php
+++ phpBB/includes/functions_user.php
@@ -2956,7 +2956,7 @@ function group_user_add($group_id, $user_id_ary = false, $username_ary = false, 
 */
 function group_user_del($group_id, $user_id_ary = false, $username_ary = false, $group_name = false)
 {
-	global $db, $auth, $config;
+	global $db, $auth, $config, $phpbb_dispatcher;
 
 	if ($config['coppa_enable'])
 	{
@@ -3054,6 +3054,11 @@ function group_user_del($group_id, $user_id_ary = false, $username_ary = false, 
 		}
 	}
 	unset($special_group_data);
+
+	$vars = array('group_id', 'user_id_ary', 'username_ary', 'group_name');
+	$event = new phpbb_event_data(compact($vars));
+	$phpbb_dispatcher->dispatch('core.group_user_del', $event);
+	extract($event->get_data_filtered($vars));
 
 	$sql = 'DELETE FROM ' . USER_GROUP_TABLE . "
 		WHERE group_id = $group_id
