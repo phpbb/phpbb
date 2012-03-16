3339935db316859b38a820b7cf3ebff204b72d7f
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-03-16 14:58:29 +0000

    [feature/events] Adding core.common ledge
    PHPBB3-9550

--- phpBB/common.php
+++ phpBB/common.php
@@ -143,3 +143,5 @@ if (!$config['use_system_cron'])
 {
 	$cron = new phpbb_cron_manager(new phpbb_cron_task_provider($phpbb_extension_manager), $cache->get_driver());
 }
+
+$phpbb_dispatcher->dispatch('core.common');
