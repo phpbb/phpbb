5ad7138bb38d8426dbebb01d92f3224b1d520740
Shibu Lijack <shibulijack@gmail.com>
2012-03-22 15:39:05 +0530

    Updated SQL Schema
    Included a new field 'user_selecteditor' in the 'phpbb_user' to
    enable/disable visual editor

--- phpBB/install/schemas/oracle_schema.sql
+++ phpBB/install/schemas/oracle_schema.sql
@@ -1750,6 +1750,7 @@ CREATE TABLE phpbb_users (
 	user_new number(1) DEFAULT '1' NOT NULL,
 	user_reminded number(4) DEFAULT '0' NOT NULL,
 	user_reminded_time number(11) DEFAULT '0' NOT NULL,
+	user_selecteditor int(4) DEFAULT '0' NOT NULL,
 	CONSTRAINT pk_phpbb_users PRIMARY KEY (user_id),
 	CONSTRAINT u_phpbb_username_clean UNIQUE (username_clean)
 )
