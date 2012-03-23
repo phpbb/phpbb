127ff95812752445dadad5f55b9b529b1d28702f
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-03-23 12:11:45 +0000

    [feature/events] Remove core.acp_modules_modules event
    PHPBB3-9550

--- phpBB/includes/acp/info/acp_modules.php
+++ phpBB/includes/acp/info/acp_modules.php
@@ -14,27 +14,16 @@ class acp_modules_info
 {
 	function module()
 	{
-		global $phpbb_dispatcher;
-
-		$modules = array(
-			'acp'		=> array('title' => 'ACP', 'auth' => 'acl_a_modules', 'cat' => array('ACP_MODULE_MANAGEMENT')),
-			'ucp'		=> array('title' => 'UCP', 'auth' => 'acl_a_modules', 'cat' => array('ACP_MODULE_MANAGEMENT')),
-			'mcp'		=> array('title' => 'MCP', 'auth' => 'acl_a_modules', 'cat' => array('ACP_MODULE_MANAGEMENT')),
-		);
-
-		$vars = array('modules');
-		$event = new phpbb_event_data(compact($vars));
-		$phpbb_dispatcher->dispatch('core.acp_modules_modules', $event);
-		extract($event->get_data_filtered($vars));
-
-		$data = array(
+		return array(
 			'filename'	=> 'acp_modules',
 			'title'		=> 'ACP_MODULE_MANAGEMENT',
 			'version'	=> '1.0.0',
-			'modes'		=> $modules,
+			'modes'		=> array(
+				'acp'		=> array('title' => 'ACP', 'auth' => 'acl_a_modules', 'cat' => array('ACP_MODULE_MANAGEMENT')),
+				'ucp'		=> array('title' => 'UCP', 'auth' => 'acl_a_modules', 'cat' => array('ACP_MODULE_MANAGEMENT')),
+				'mcp'		=> array('title' => 'MCP', 'auth' => 'acl_a_modules', 'cat' => array('ACP_MODULE_MANAGEMENT')),
+			),
 		);
-
-		return $data;
 	}
 
 	function install()
