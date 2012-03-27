ec33ed62a590194e827173f3c797b44255eaf8bd
David King <imkingdavid@gmail.com>
2012-03-27 19:03:24 -0400

    [feature/qrpreview] Change conflicting form ID and use proper HTML code for &
    PHPBB3-10726

--- phpBB/language/en/viewtopic.php
+++ phpBB/language/en/viewtopic.php
@@ -61,7 +61,7 @@ $lang = array_merge($lang, array(
 
 	'FILE_NOT_FOUND_404'	=> 'The file <strong>%s</strong> does not exist.',
 	'FORK_TOPIC'			=> 'Copy topic',
-	'FULL_EDITOR'			=> 'Full Editor & Preview',
+	'FULL_EDITOR'			=> 'Full Editor &amp; Preview',
 	
 	'LINKAGE_FORBIDDEN'		=> 'You are not authorised to view, download or link from/to this site.',
 	'LOGIN_NOTIFY_TOPIC'	=> 'You have been notified about this topic, please login to view it.',
