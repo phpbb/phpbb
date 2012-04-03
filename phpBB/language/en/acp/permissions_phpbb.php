d24f857cfb33cded3214c885e9f6f53ad4f48aec
David King <imkingdavid@gmail.com>
2012-04-02 21:50:19 -0400

    [feature/revisions] Add local moderator permission
    PHPBB3-10755

--- phpBB/language/en/acp/permissions_phpbb.php
+++ phpBB/language/en/acp/permissions_phpbb.php
@@ -177,6 +177,7 @@ $lang = array_merge($lang, array(
 	'acl_m_approve'		=> array('lang' => 'Can approve posts', 'cat' => 'post_actions'),
 	'acl_m_report'		=> array('lang' => 'Can close and delete reports', 'cat' => 'post_actions'),
 	'acl_m_chgposter'	=> array('lang' => 'Can change post author', 'cat' => 'post_actions'),
+	'acl_m_revisions'	=> array('lang' => 'Can view post revisions', 'cat' => 'post_actions'),
 
 	'acl_m_move'	=> array('lang' => 'Can move topics', 'cat' => 'topic_actions'),
 	'acl_m_lock'	=> array('lang' => 'Can lock topics', 'cat' => 'topic_actions'),
