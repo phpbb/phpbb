811ba4953e6c0d6a260599f136ea40722e31f930
Joseph Warner <hardolaf@gmail.com>
2012-04-13 12:47:53 -0400

    [ticket/10533] Add user topics, percent, and per day to view profile.
    This change adds the same information about topics to the user profile
    that already exists about posts. This also modifies the display of the
    search user's topics feature on the view profile page to look more in-line
    with the same options that exist for posts.
    
    PHPBB3-10533

--- phpBB/language/en/common.php
+++ phpBB/language/en/common.php
@@ -566,7 +566,7 @@ $lang = array_merge($lang, array(
 	'SEARCH_UNANSWERED'			=> 'View unanswered posts',
 	'SEARCH_UNREAD'				=> 'View unread posts',
 	'SEARCH_USER_POSTS'			=> 'Search user’s posts',
-    'SEARCH_USER_TOPICS'		=> 'Search user’s topics',
+	'SEARCH_USER_TOPICS'			=> 'Search user’s topics',
 	'SECONDS'					=> 'Seconds',
 	'SELECT'					=> 'Select',
 	'SELECT_ALL_CODE'			=> 'Select all',
@@ -671,10 +671,13 @@ $lang = array_merge($lang, array(
 		1	=> '%d private message in total',
 		2	=> '%d private messages in total',
 	),
+	'TOPIC_DAY'		=> '%.2f topics per day',
+	'TOPIC_PCT'		=> '%.2f%% of all topics',
 	'TOTAL_POSTS'		=> 'Total posts',
 	'TOTAL_POSTS_COUNT'	=> array(
 		2	=> 'Total posts <strong>%d</strong>',
 	),
+	
 	'TOPIC_REPORTED'	=> 'This topic has been reported',
 	'TOTAL_TOPICS'		=> array(
 		2	=> 'Total topics <strong>%d</strong>',
@@ -682,6 +685,7 @@ $lang = array_merge($lang, array(
 	'TOTAL_USERS'		=> array(
 		2	=> 'Total members <strong>%d</strong>',
 	),
+	'TOTAL_USER_TOPICS'	=> 'Total topics',
 	'TRACKED_PHP_ERROR'	=> 'Tracked PHP errors: %s',
 
 	'UNABLE_GET_IMAGE_SIZE'	=> 'It was not possible to determine the dimensions of the image.',
