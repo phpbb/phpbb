4b200e5d330fe117092c5d52878886563c56319d
Hari Sankar R <hsr@theinglorio.us>
2012-04-19 12:21:50 +0530

    [ticket/10770] Use a lightbox for image attachments
    Use a lightbox to display image attachments in a page.
    
    PHPBB3-10770

--- phpBB/assets/javascript/core.js
+++ phpBB/assets/javascript/core.js
@@ -585,4 +585,29 @@ phpbb.add_ajax_callback('toggle_link', function() {
 	el.parent().attr('class', toggle_class);
 });
 
+/**
+ * Mother function for the lightbox implementation.
+ */
+phpbb.attachment_lightbox = function() {
+	this.init = function() {
+		if(this.sources === undefined) {
+			return this.addSources();
+		}
+		else {
+			return this.enable();
+		}
+	}
+	
+	this.addSources() {
+		this.sources = [];
+		this.imageAlts = [];
+		$('.attach-image img').each(function(i, image){
+			this.sources.push(image.src);
+			this.imageAlts.push(image.alt);
+		});
+		
+		return this.sources;
+	}
+}
+
 })(jQuery); // Avoid conflicts with other libraries
