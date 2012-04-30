56474da92c50428b4b14fb219d8541432439a0ab
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-04-30 17:27:58 +0100

    [ticket/10857] Adding information about items requiring a moderators attention
    With thanks to RMcGirr83 for his Moderator Needed MOD.
    
    PHPBB3-10857

--- phpBB/includes/functions_display.php
+++ phpBB/includes/functions_display.php
@@ -1322,3 +1322,134 @@ function get_user_avatar($avatar, $avatar_type, $avatar_width, $avatar_height, $
 	$avatar_img .= $avatar;
 	return '<img src="' . (str_replace(' ', '%20', $avatar_img)) . '" width="' . $avatar_width . '" height="' . $avatar_height . '" alt="' . ((!empty($user->lang[$alt])) ? $user->lang[$alt] : $alt) . '" />';
 }
+
+/**
+ * Fetches the number of items which need a moderators attention
+ */
+public function mcp_quick_info()
+{
+	global $auth, $cache, $db, $template, $user, $phpEx, $phpbb_root_path;
+
+	$allow = false;
+
+	// Check for approve and report permissions
+	if ($auth->acl_getf_global('m_approve') && $auth->acl_getf_global('m_report'))
+	{
+		$sql_where = ' WHERE (' . $db->sql_in_set('forum_id', get_forum_list('m_approve')) . ' or ' . $db->sql_in_set('forum_id', get_forum_list('m_report')) . ') AND (post_reported = 1 or post_approved = 0)';
+		$allow = true;
+	}
+	elseif ($auth->acl_getf_global('m_approve'))
+	{
+		$sql_where = ' WHERE ' . $db->sql_in_set('forum_id', get_forum_list('m_approve')) . ' AND post_approved = 0';
+		$allow = true;
+	}
+	elseif ($auth->acl_getf_global('m_report'))
+	{
+		$sql_where = ' WHERE ' . $db->sql_in_set('forum_id', get_forum_list('m_report')) . ' AND post_reported = 1';
+		$allow = true;
+	}
+
+	if ($allow)
+	{
+		$reported_posts_count = $unapproved_posts_count = $unapproved_topics_count = $reported_pms = 0;
+
+		// Topic Approval
+		if ($auth->acl_getf_global('m_approve'))
+		{
+			$sql = 'SELECT topic_first_post_id
+						FROM ' . TOPICS_TABLE . '
+					WHERE ' . $db->sql_in_set('forum_id', get_forum_list('m_approve')) . ' AND topic_approved = 0';
+			$result = $db->sql_query($sql);
+
+			$unapproved_topics_array = array();
+			while ($row = $db->sql_fetchrow($result))
+			{
+				$unapproved_topics_array[] = (int) $row['topic_first_post_id'];
+				$unapproved_topics_count++;
+			}
+			$db->sql_freeresult($result);
+
+			if(sizeof($unapproved_topics_array))
+			{
+				$sql_where .= ' AND ' . $db->sql_in_set('post_id', $unapproved_topics_array, true);
+			}
+		}
+
+		// Reported Posts and Approved Posts
+		$sql = 'SELECT post_reported, post_approved
+					FROM ' . POSTS_TABLE .
+				$sql_where . ' OR (forum_id = 0 and post_reported = 1)';
+		$result = $db->sql_query($sql);
+
+		while ($row = $db->sql_fetchrow($result))
+		{
+			// count the reported posts
+			if ($row['post_reported'])
+			{
+				$reported_posts_count++;
+			}
+			// count the unapproved posts
+			if (!$row['post_approved'])
+			{
+				$unapproved_posts_count++;
+			}
+		}
+		$db->sql_freeresult($result);
+
+		// Lets use this data
+		if ($reported_posts_count || $unapproved_posts_count || $unapproved_topics_count)
+		{
+			$l_reported_posts_count = $reported_posts_count ? (($reported_posts_count == 1) ? $user->lang['MODERATOR_NEEDED_REPORTED_POST'] : $user->lang['MODERATOR_NEEDED_REPORTED_POSTS']) : '';
+			$total_reported_posts = sprintf($l_reported_posts_count, $reported_posts_count);
+			$l_unapproved_topics_count = $unapproved_topics_count ? (($unapproved_topics_count == 1) ? $user->lang['MODERATOR_NEEDED_APPROVE_TOPIC'] : $user->lang['MODERATOR_NEEDED_APPROVE_TOPICS']) : '';
+			$total_unapproved_topics = sprintf($l_unapproved_topics_count, $unapproved_topics_count);
+			$l_unapproved_posts_count = $unapproved_posts_count ? (($unapproved_posts_count == 1) ? $user->lang['MODERATOR_NEEDED_APPROVE_POST'] : $user->lang['MODERATOR_NEEDED_APPROVE_POSTS']) : '';
+			$total_unapproved_posts = sprintf($l_unapproved_posts_count, $unapproved_posts_count);
+
+			// And export variables to the template
+			$template->assign_vars(array(
+				'TOTAL_MODERATOR_REPORTS'		=> $total_reported_posts,
+				'TOTAL_MODERATOR_POSTS'			=> $total_unapproved_posts,
+				'TOTAL_MODERATOR_TOPICS'    	=> $total_unapproved_topics,
+
+				'U_MODERATOR_REPORTS'			=> append_sid("{$phpbb_root_path}mcp.$phpEx", 'i=reports&amp;mode=reports', true, $user->session_id),
+				'U_MODERATOR_APPROVE_POSTS'		=> append_sid("{$phpbb_root_path}mcp.$phpEx", 'i=queue&amp;mode=unapproved_posts', true, $user->session_id),
+				'U_MODERATOR_APPROVE_TOPICS'    => append_sid("{$phpbb_root_path}mcp.$phpEx", 'i=queue&amp;mode=unapproved_topics', true, $user->session_id),
+			));
+		}
+    }
+
+    // Don't forget PMs
+	if ($auth->acl_getf_global('m_'))
+	{
+		$reported_pms = 0;
+
+		// Lets have a cache
+		if (($reported_pms = $cache->get('_reported_pms')) === false)
+		{
+			$sql = 'SELECT COUNT(msg_id) AS pms_count
+				FROM ' . PRIVMSGS_TABLE . '
+				WHERE message_reported = 1';
+			$result = $db->sql_query($sql);
+			$reported_pms = (int) $db->sql_fetchfield('pms_count');
+			$db->sql_freeresult($result);
+
+			// Cache for a few minutes to optimize. Might add to topics/posts later
+			$cache->put('_reported_pms', $reported_pms, 300);
+		}
+
+		// Dump to the template again
+		if ($reported_pms)
+		{
+			$l_reported_pms_count = $reported_pms ? (($reported_pms == 1) ? $user->lang['MODERATOR_NEEDED_REPORTED_PM'] : $user->lang['MODERATOR_NEEDED_REPORTED_PMS']) : '';
+			$total_reported_pms = 	sprintf($l_reported_pms_count, $reported_pms);
+
+			$template->assign_vars(array(
+				'TOTAL_MODERATOR_PMS'			=> $total_reported_pms,
+				'U_MODERATOR_PMS'				=> append_sid("{$phpbb_root_path}mcp.$phpEx", 'i=pm_reports&amp;mode=pm_reports', true, $user->session_id),
+			));
+		}
+	}
+
+	return;
+}
