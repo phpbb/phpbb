ffabfefe459c8fd94bf0706766134dd821d294a7
David King <imkingdavid@gmail.com>
2012-05-22 10:14:20 -0400

    [ticket/10906] Add setting for last post topic title in schema_data.sql
    PHPBB3-10906

--- phpBB/install/schemas/schema_data.sql
+++ phpBB/install/schemas/schema_data.sql
@@ -88,6 +88,7 @@ INSERT INTO phpbb_config (config_name, config_value) VALUES ('dbms_version', '')
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('default_dateformat', 'D M d, Y g:i a');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('default_style', '1');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('display_last_edited', '1');
+INSERT INTO phpbb_config (config_name, config_value) VALUES ('display_last_subject', '1');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('display_order', '0');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('edit_time', '0');
 INSERT INTO phpbb_config (config_name, config_value) VALUES ('delete_time', '0');
