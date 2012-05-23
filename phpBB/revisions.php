113b25c758ab974afdafcc7c0859b3a232222caf
David King <imkingdavid@gmail.com>
2012-05-23 13:26:54 -0400

    [feature/revisions] Rework revision comparison
    PHPBB3-10755

--- phpBB/revisions.php
+++ phpBB/revisions.php
@@ -24,34 +24,29 @@ $user->setup('viewtopic');
 
 // Initialize variables
 $post_id = $request->variable('p', 0);
-// Single revision to view
-$revision_id = $request->variable('r', 0);
-// Starting revision for comparison
-$from = $request->variable('from', 0);
-// Ending revision for comparison
-$to = $request->variable('to', 0);
+// Revision id(s)
+// To view a single revision, just its ID
+// To compare two revisions, should be: #...# e.g. 3...14
+$revision_id = $request->variable('r', '0');
 
-if ($revision_id || ($to && !$from))
+if ($revision_id)
 {
-	$revision_id = $revision_id ?: $to;
-	$revision = new phpbb_revisions_revision($revision_id);
+	$compare = true;
 
-	if ($user->data['user_id'] != $revision->get_poster_id() || !$auth->acl_get('m_revisions'))
+	preg_match('/([0-9]+)\.{3}([0-9]+)/', $revision_id, $match);
+	// If we have a range, like 1...2, show a comparison
+	// Otherwise, just show the one revision, compared to the previous one
+	if (!empty($match))
 	{
-		// @todo - create this language entry
-		trigger_error('NO_AUTH_VIEW_REVISIONS');
+		$revision_from = new phpbb_revisions_revision($match[1]);
+		$revision_to = new phpbb_revisions_revision($match[2]);
 	}
-
-	$template->assign_vars(array(
-		'SUBJECT'	=> $revision->get('subject'),
-		'TEXT'		=> $revision->get('text'),
-		'TIME'		=> $user->format_date($revision->get('time')),
-	));
-}
-else if ($to && $from)
-{
-	$revision_to = new phpbb_revisions_revision($to);
-	$revision_from = new phpbb_revisions_revision($from);
+	else
+	{
+		$revision_id = (int) $revision_id;
+		$revision_to = new phpbb_revisions_revision($revision_id);
+		$revision_from = $revision_to->load_previous();
+	}
 
 	if ($user->data['user_id'] != $revision_to->get_poster_id() || !$auth->acl_get('m_revisions'))
 	{
@@ -66,14 +61,10 @@ else if ($to && $from)
 		trigger_error('REVISIONS_FROM_DIFFERENT_POSTS');
 	}
 
-	$comparison = $revision_to->compare_to($revision_from);
-
 	$template->assign_vars(array(
-		'COMPARISON'	=> true,
-		'DIFF'			=> $comparison,
+		'COMPARISON'	=> $compare,
+		'DIFF'			=> $revision_to->compare_to($revision_from),
 	));
-
-	$tpl_body = 'revisions_body.html';
 }
 else if ($post_id)
 {
@@ -103,12 +94,12 @@ else if ($post_id)
 		));
 	}
 }
-if (!$tpl_body)
+else
 {
 	trigger_error('NO_REVISIONS');
 }
-page_header('Revisions');
+page_header('POST_REVISIONS');
 $template->set_filenames(array(
-	'body'	=> $tpl_body,
+	'body'	=> 'revisions_body.html',
 ));
 page_footer();
