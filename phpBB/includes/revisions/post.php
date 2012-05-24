ee4061ec71a38b60ef25ef5df4e16c6c0f9efb11
David King <imkingdavid@gmail.com>
2012-05-24 17:20:30 -0400

    [feature/revisions] Rework logic for revision comparison
    The revision compare page is being structure similar to the GitHub Compare View
    
    PHPBB3-10755

--- phpBB/includes/revisions/post.php
+++ phpBB/includes/revisions/post.php
@@ -5,8 +5,7 @@ class phpbb_revisions_post
 	private $db;
 
 	private $post_id;
-	private $forum_id;
-	private $poster_id;
+	private $post_data;
 	private $revisions = array();
 
 	/**
@@ -18,7 +17,32 @@ class phpbb_revisions_post
 		$this->db = $db;
 
 		$this->post_id = (int) $id;
+
+		$this->load_post_data();
 	}
+
+	/**
+	* Load post data into an array
+	*
+	* @return array Array of post data from database
+	*/
+	public function load_post_data()
+	{
+		if (!$this->post_id)
+		{
+			return false;
+		}
+
+		$sql = 'SELECT *
+			FROM ' . POSTS_TABLE . '
+			WHERE post_id = ' . (int) $this->post_id;
+		$result = $this->db->sql_query($sql);
+		$this->post_data = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
+
+		return $this->post_data;
+	}
+
 
 	/**
 	* Retrieve all revisions from a specified post from database
@@ -37,20 +61,26 @@ class phpbb_revisions_post
 			return $this->revisions;
 		}
 
-		$sql = 'SELECT r.*, p.*
+		$sql = 'SELECT r.*, u.username, u.user_colour
 			FROM ' . POST_REVISIONS_TABLE . ' r
-			LEFT JOIN ' . POSTS_TABLE . ' p
-				ON p.post_id = r.post_id
-			WHERE p.post_id = ' . (int) $this->post_id . '
-			ORDER BY revision_id DESC';
+			LEFT JOIN ' . USERS_TABLE . ' u
+				ON u.user_id = r.user_id
+			WHERE r.post_id = ' . (int) $this->post_id . '
+			ORDER BY r.revision_id DESC';
 		$result = $this->db->sql_query($sql);
 		while ($row = $this->db->sql_fetchrow($result))
 		{
+			// Some $post_data we already have from loading the post data can be put here
+			// because we need it for set_data() in the revision object
+			$row['poster_id'] = $this->post_data['poster_id'];
+			$row['forum_id'] = $this->post_data['forum_id'];
+			$row['enable_bbcode'] = $this->post_data['enable_bbcode'];
+			$row['enable_smilies'] = $this->post_data['enable_smilies'];
+			$row['enable_magic_url'] = $this->post_data['enable_magic_url'];
+			
 			$rev = new phpbb_revisions_revision($row['revision_id'], false);
 			$rev->set_data($row);
-			$this->revisions[] = $rev;
-			$this->poster_id = $this->poster_id ?: $row['poster_id'];
-			$this->forum_id = $this->forum_id ?: $row['forum_id'];
+			$this->revisions[$row['revision_id']] = $rev;
 		}
 		$this->db->sql_freeresult($result);
 
@@ -90,5 +120,25 @@ class phpbb_revisions_post
 		}
 
 		return null;
+	}
+
+	/**
+	 * Custom sort function used by usort() to order a post's revisions by ther ID's
+	 *
+	 * @param phpbb_revisions_revision $a First comparison argument
+	 * @param phpbb_revisions_revision $b Second comparison argument
+	 * @return int 0 for equal, 1 for a greater than b, -1 for b greater than a
+	 */
+	static public function sort_post_revisions(phpbb_revisions_revision $a, phpbb_revisions_revision $b)
+	{
+		$a_order = $a->get('id');
+		$b_order = $b->get('id');
+
+		if ($a_order == $b_order)
+		{
+			return 0;
+		}
+
+		return ($a_order > $b_order) ? 1 : -1;
 	}
 }
