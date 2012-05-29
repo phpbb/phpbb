af67ac9c8ba5617c5c4c012232581d0fc6150c2e
Joseph Warner <Hardolaf@phpbb.com>
2012-05-29 19:28:20 -0400

    [feature/auth-refactoring] Modify usage of zend storage
    Changes as directed by naderman during code review.
    
    PHPBB3-9734

---
+++ phpBB/includes/auth/zend_storage.php
@@ -0,0 +1,99 @@
+<?php
+/**
+*
+* @package phpBB3
+* @copyright (c) 2005 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * Implements the storage methods of the ZendFramework OpenID Consumer code.
+ * This allows the saving of user tokens and logins for federated logins.
+ *
+ * @package phpBB3
+ */
+class phpbb_auth_zend_storage extends \Zend\OpenId\Consumer\Storage\AbstractStorage
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function addAssociation($url, $handle, $macFunc, $secret, $expires)
+	{
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getAssociation($url, &$handle, &$macFunc, &$secret, &$expires)
+	{
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getAssociationByHandle($handle, &$url, &$macFunc, &$secret, &$expires)
+	{
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function delAssociation($url)
+	{
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function addDiscoveryInfo($id, $realId, $server, $version, $expires)
+	{
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getDiscoveryInfo($id, &$realId, &$server, &$version, &$expires)
+	{
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function delDiscoveryInfo($id)
+	{
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function isUniqueNonce($provider, $nonce)
+	{
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function purgeNonces($date = null)
+	{
+
+	}
+}
+
+?>
