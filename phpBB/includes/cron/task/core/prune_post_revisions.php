4e3b47bf457fbcfac599a3424ecff418c9309b9e
David King <imkingdavid@gmail.com>
2012-05-30 17:35:22 -0400

    [feature/revisions] Add cron pruning of excess revisions
    PHPBB3-10755

---
+++ phpBB/includes/cron/task/core/prune_post_revisions.php
@@ -0,0 +1,109 @@
+<?php
+/**
+*
+* @package phpBB3
+* @copyright (c) 2010 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* Prune all forums cron task.
+*
+* It is intended to be invoked from system cron.
+* This task will find all forums for which pruning is enabled, and will
+* prune all forums as necessary.
+*
+* @package phpBB3
+*/
+class phpbb_cron_task_core_prune_post_revisions extends phpbb_cron_task_base
+{
+	/**
+	* Runs this cron task.
+	*
+	* @return void
+	*/
+	public function run()
+	{
+		global $phpbb_root_path, $phpEx, $db;
+
+		$prune_revision_ids = array();
+
+		if ($config['post_revisions_max_age'])
+		{
+			// First we get all revision IDs of revisions that are older than the date in the board settings
+			$earliest_allowed = time() - $config['post_revisions_max_age'];
+
+			$sql = 'SELECT revision_id
+				FROM ' . POST_REVISIONS_TABLE . '
+				WHERE revision_time < ' . (int) $earliest_allowed;
+			$result = $db->sql_query($sql);
+			while ($row = $db->sql_fetchrow($result))
+			{
+				$prune_revision_ids[] = $row['revision_id'];
+			}
+			$db->sql_freeresult($result);
+		}
+
+		if ($config['post_revisions_max_age'])
+		{
+			$overloaded_posts = array();
+
+			// Now we get post IDs of posts with > the max number of revisions 
+			$sql = 'SELECT post_id, post_edit_count
+				FROM ' . POSTS_TABLE . '
+				WHERE post_edit_count > ' . (int) $config['revisions_per_post_max'];
+			$result = $db->sql_query($sql);
+			while($row = $db->sql_fetchrow($result))
+			{
+				$id	= $row['post_id'];
+				$excess	= $row['post_edit_count'] - $config['revisions_per_post_max'];
+
+				// Query in a loop? Uh oh! But I can't find a better way...
+				$inner_sql = 'SELECT revision_id
+					FROM ' . POST_REVISIONS_TABLE . '
+					WHERE post_id = ' . (int) $id . '
+					ORDER BY revision_id ASC
+					LIMIT ' . (int) $excess;
+				$inner_result = $db->sql_query($inner_sql);
+				while ($inner_row = $db->sql_fetchrow($inner_result))
+				{
+					$prune_revision_ids[] = $inner_row['revision_id'];
+				}
+				$db->sql_freeresult($inner_result);
+			}
+			$db->sql_freeresult($result);
+		}
+
+		$prune_revision_ids = array_unique($prune_revision_ids);
+
+		// Finally, if we have any revisions that meet the criteria, we delete them
+		if (!empty($prune_revision_ids))
+		{
+			$sql = 'DELETE FROM ' . POST_REVISIONS_TABLE . '
+				WHERE ' . $db->sql_in_set('revision_id', $prune_revision_ids);
+			$result = $db->sql_query($sql);
+		}
+	}
+
+	/**
+	* Returns whether this cron task can run, given current board configuration.
+	*
+	* This cron task will only run when system cron is utilised.
+	*
+	* @return bool
+	*/
+	public function is_runnable()
+	{
+		global $config;
+		return $config['use_system_cron'] && $config['track_post_revisions'] && ($config['revisions_per_post_max'] || $config['post_revisions_max_age']);
+	}
+}
