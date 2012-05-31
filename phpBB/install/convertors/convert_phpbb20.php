d8ddda512180be56100a890410dece8046aade28
Andreas Fischer <bantu@phpbb.com>
2012-05-31 20:19:28 +0200

    [develop-olympus] Bumping version numbers to final for 3.0.11 releases.

--- phpBB/install/convertors/convert_phpbb20.php
+++ phpBB/install/convertors/convert_phpbb20.php
@@ -32,7 +32,7 @@ unset($dbpasswd);
 $convertor_data = array(
 	'forum_name'	=> 'phpBB 2.0.x',
 	'version'		=> '1.0.3',
-	'phpbb_version'	=> '3.0.10',
+	'phpbb_version'	=> '3.0.11',
 	'author'		=> '<a href="http://www.phpbb.com/">phpBB Group</a>',
 	'dbms'			=> $dbms,
 	'dbhost'		=> $dbhost,
