876b1d4d29ed3d68cac8daecee74b5406978f025
David King <imkingdavid@gmail.com>
2012-06-12 12:38:58 -0400

    [ticket/10537] Fall back on u_download after removing u_pm_download
    PHPBB3-10537

--- phpBB/includes/mcp/mcp_pm_reports.php
+++ phpBB/includes/mcp/mcp_pm_reports.php
@@ -123,7 +123,7 @@ class mcp_pm_reports
 				$message = bbcode_nl2br($message);
 				$message = smiley_text($message);
 
-				if ($pm_info['message_attachment'])
+				if ($pm_info['message_attachment'] && $auth->acl_get('u_download'))
 				{
 					$sql = 'SELECT *
 						FROM ' . ATTACHMENTS_TABLE . '
