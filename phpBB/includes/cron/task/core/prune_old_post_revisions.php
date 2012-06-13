0fd32f6085b0392f8e3311c61fe989b44675251b
David King <imkingdavid@gmail.com>
2012-06-13 11:35:42 -0400

    [feature/revisions] Split up IDs in cron between multiple queries
    PHPBB3-10755

--- phpBB/includes/cron/task/core/prune_old_post_revisions.php
+++ phpBB/includes/cron/task/core/prune_old_post_revisions.php
@@ -32,29 +32,39 @@ class phpbb_cron_task_core_prune_old_post_revisions extends phpbb_cron_task_base
 	*/
 	public function run()
 	{
-		global $phpbb_root_path, $phpEx, $db;
+		global $phpbb_root_path, $phpEx, $db, $config;
 
 		$prune_revision_ids = array();
 
-		if ($config['post_revisions_max_age'])
+		// This allows us to split up the IDs so that we don't try to delete a ton all at once
+		$iteration = 0;
+		// This is how many IDs to have per iteration
+		$ids_per_iteration = 20;
+
+		$sql = 'SELECT revision_id
+			FROM ' . POST_REVISIONS_TABLE . '
+			WHERE revision_time < ' . (time() - $config['post_revisions_max_age']);
+		$result = $db->sql_query($sql);
+		while ($row = $db->sql_fetchrow($result))
 		{
-			$sql = 'SELECT revision_id
-				FROM ' . POST_REVISIONS_TABLE . '
-				WHERE revision_time < ' . (time() - $config['post_revisions_max_age']);
-			$result = $db->sql_query($sql);
-			while ($row = $db->sql_fetchrow($result))
+			if (sizeof($prune_revision_ids[$iteration]) == $ids_per_iteration)
 			{
-				$prune_revision_ids[] = $row['revision_id'];
+				$iteration++;
 			}
-			$db->sql_freeresult($result);
+
+			$prune_revision_ids[$iteration][] = $row['revision_id'];
 		}
+		$db->sql_freeresult($result);
 
 		// Finally, if we have any revisions that meet the criteria, we delete them
 		if (!empty($prune_revision_ids))
 		{
-			$sql = 'DELETE FROM ' . POST_REVISIONS_TABLE . '
-				WHERE ' . $db->sql_in_set('revision_id', $prune_revision_ids);
-			$result = $db->sql_query($sql);
+			for($i = 0; $i < $iteration; $i++)
+			{
+				$sql = 'DELETE FROM ' . POST_REVISIONS_TABLE . '
+					WHERE ' . $db->sql_in_set('revision_id', $prune_revision_ids[$i]);
+				$result = $db->sql_query($sql);
+			}
 		}
 	}
 
