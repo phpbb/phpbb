dcd7d962960ade175400cbb9b8e00575e8735a22
Joas Schilling <nickvergessen@gmx.de>
2012-06-19 19:08:53 +0200

    [feature/new-tz-handling] Use asset in acp and subsilver2 and remove duplicates
    PHPBB3-9558

--- phpBB/adm/style/timezone.js
+++ phpBB/adm/style/timezone.js
@@ -1,35 +1,11 @@
-/**
-* Hide the optgroups that are not the selected timezone
-*
-* @param	bool	keep_selection		Shall we keep the value selected, or shall the user be forced to repick one.
-*/
-function phpbb_switch_tz_date(keep_selection)
-{
-	$('#timezone > optgroup').css("display", "none");
-	$("#timezone > optgroup[label='" + $('#tz_date').val() + "']").css("display", "block");
+(function($) { // Avoid conflicts with other libraries
 
-	if ($("#timezone > optgroup[label='" + $('#tz_date').val() + "'] > option").size() == 1)
-	{
-		// If there is only one timezone for the selected date, we just select that automatically.
-		$("#timezone > optgroup[label='" + $('#tz_date').val() + "'] > option:first").attr("selected", true);
-		keep_selection = true;
-	}
+$('#tz_date').change(function() {
+	phpbb.timezone_switch_date(false);
+});
 
-	if (typeof keep_selection !== 'undefined')
-	{
-		if (!keep_selection)
-		{
-			$('#timezone > option:first').attr("selected", true);
-		}
-	}
-}
+$(document).ready(
+	phpbb.timezone_enable_date_selection
+);
 
-/**
-* Display the date/time select
-*/
-function phpbb_enable_tz_dates()
-{
-	$('#tz_select_date').css("display", "block");
-}
-
-phpbb_enable_tz_dates();
+})(jQuery); // Avoid conflicts with other libraries
--- phpBB/styles/subsilver2/template/timezone.js
+++ phpBB/styles/subsilver2/template/timezone.js
@@ -1,108 +1,19 @@
-/**
-* Hide the optgroups that are not the selected timezone
-*
-* @param	bool	keep_selection		Shall we keep the value selected, or shall the user be forced to repick one.
-*/
-function phpbb_switch_tz_date(keep_selection)
-{
-	$('#timezone > optgroup').css("display", "none");
-	$("#timezone > optgroup[label='" + $('#tz_date').val() + "']").css("display", "block");
+(function($) { // Avoid conflicts with other libraries
 
-	if ($("#timezone > optgroup[label='" + $('#tz_date').val() + "'] > option").size() == 1)
-	{
-		// If there is only one timezone for the selected date, we just select that automatically.
-		$("#timezone > optgroup[label='" + $('#tz_date').val() + "'] > option:first").attr("selected", true);
-		keep_selection = true;
-	}
-
-	if (typeof keep_selection !== 'undefined')
-	{
-		if (!keep_selection)
-		{
-			$('#timezone > option:first').attr("selected", true);
-		}
-	}
-}
-
-/**
-* Display the date/time select
-*/
-function phpbb_enable_tz_dates()
-{
-	$('#tz_select_date').css("display", "block");
-}
-
-/**
-* Preselect a date/time or suggest one, if it is not picked.
-*
-* @param	bool	force_selector		Shall we select the suggestion?
-*/
-function phpbb_preselect_tz_select(force_selector)
-{
-
-	// The offset returned here is in minutes and negated.
-	// http://www.w3schools.com/jsref/jsref_getTimezoneOffset.asp
-	var offset = (new Date()).getTimezoneOffset();
-	if (offset < 0)
-	{
-		var sign = '+';
-		offset = -offset;
-	}
-	else
-	{
-		var sign = '-';
-	}
-	var minutes = offset % 60;
-	var hours = (offset - minutes) / 60;
-	if (hours < 10)
-	{
-		hours = '0' + hours.toString();
-	}
-	else
-	{
-		hours = hours.toString();
-	}
-	if (minutes < 10)
-	{
-		minutes = '0' + minutes.toString();
-	}
-	else
-	{
-		minutes = minutes.toString();
-	}
-
-	var prefix = 'GMT' + sign + hours + ':' + minutes;
-	var prefix_length = prefix.length;
-
-	var selector_options = $('#tz_date > option');
-	for (var i = 0; i < selector_options.length; ++i)
-	{
-		var option = selector_options[i];
-		if (option.value.substring(0, prefix_length) == prefix)
-		{
-			if ($('#tz_date').val() != option.value && !force_selector)
-			{
-				// We do not select the option for the user, but notify him,
-				// that we would suggest a different setting.
-				$('#tz_select_date_suggest').css("display", "inline");
-				$('#tz_select_date_suggest').attr("title", $('#tz_select_date_suggest').attr('data-l-suggestion').replace("%s", option.innerHTML));
-				$('#tz_select_date_suggest').attr("value", $('#tz_select_date_suggest').attr('data-l-suggestion').replace("%s", option.innerHTML.substring(0, 9)));
-				phpbb_switch_tz_date(true);
-			}
-			else
-			{
-				option.selected = true;
-				phpbb_switch_tz_date(!force_selector);
-				$('#tz_select_date_suggest').css("display", "none");
-			}
-			break;
-		}
-	}
-}
-
-$('#tz_select_date_suggest').click(function(){
-	phpbb_preselect_tz_select(true, '');
+$('#tz_date').change(function() {
+	phpbb.timezone_switch_date(false);
 });
 
-phpbb_enable_tz_dates();
-phpbb_preselect_tz_select($('#tz_select_date_suggest').attr('data-is-registration') == "true");
+$('#tz_select_date_suggest').click(function(){
+	phpbb.timezone_preselect_select(true);
+});
+
+$(document).ready(
+	phpbb.timezone_enable_date_selection
+);
+
+$(document).ready(
+	phpbb.timezone_preselect_select($('#tz_select_date_suggest').attr('data-is-registration') == 'true')
+);
+
+})(jQuery); // Avoid conflicts with other libraries
