13f4bfabbeab77698f06c3431931b73ebedc587c
Igor Wiedler <igor@wiedler.ch>
2012-06-27 21:00:00 +0200

    [feature/avatars] Fixup avatars.js rewrite
    PHPBB3-10018

--- phpBB/styles/prosilver/template/avatars.js
+++ phpBB/styles/prosilver/template/avatars.js
@@ -3,13 +3,13 @@
 "use strict";
 
 function avatar_simplify() {
-    $('#av_options').hide();
+    $('#av_options > div').hide();
 
     var selected = $('#avatar_driver').val();
     $('#av_option_' + selected).show();
 }
 
 avatar_simplify();
-$('#avatar_driver').on('change', avatar_simplify);
+$('#avatar_driver').bind('change', avatar_simplify);
 
 })(jQuery); // Avoid conflicts with other libraries
