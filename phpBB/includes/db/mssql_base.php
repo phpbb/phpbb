53b095c9e8c58c9a02a23e116d5235c7d4660d0a
Erik Frèrejean <erikfrerejean@phpbb.com>
2012-07-04 14:03:43 +0200

    [ticket/10966] Correct copyright years.
    PHPBB3-10966

--- phpBB/includes/db/mssql_base.php
+++ phpBB/includes/db/mssql_base.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package dbal
-* @copyright (c) 2013 phpBB Group
+* @copyright (c) 2012 phpBB Group
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
