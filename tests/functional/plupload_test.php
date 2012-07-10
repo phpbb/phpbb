6110a615df0efe771c9626a32ab9f9cb42eb3ae2
Fyorl <gaelreth@gmail.com>
2012-07-11 00:34:25 +0100

    [feature/attachment-improvements] Added test for chunked uploads
    Requires newer version of Goutte to work.
    
    PHPBB3-10929

--- tests/functional/plupload_test.php
+++ tests/functional/plupload_test.php
@@ -12,6 +12,7 @@
  */
 class phpbb_functional_plupload_test extends phpbb_functional_test_case
 {
+	const CHUNKS = 4;
 	private $path;
 
 	public function setUp()
@@ -20,6 +21,57 @@ class phpbb_functional_plupload_test extends phpbb_functional_test_case
 		$this->path = __DIR__ . '/fixtures/files/';
 		$this->add_lang('posting');
 		$this->login();
+	}
+
+	public function test_chunked_upload()
+	{
+		$this->markTestIncomplete('Newer version of Goutte needed for headers to work.');
+		$chunk_size = ceil(filesize($this->path . 'valid.jpg') / self::CHUNKS);
+		$handle = fopen($this->path . 'valid.jpg', 'rb');
+
+		for ($i = 0; $i < self::CHUNKS; $i++)
+		{
+			$chunk = fread($handle, $chunk_size);
+			file_put_contents($this-> path . 'chunk', $chunk);
+
+			$file = array(
+				'tmp_name' => $this->path . 'chunk',
+				'name' => 'blob',
+				'type' => 'application/octetstream',
+				'size' => strlen($chunk),
+				'error' => UPLOAD_ERR_OK,
+			);
+
+			$this->client->setServerParameter('HTTP_X_PHPBB_USING_PLUPLOAD', '1');
+
+			$crawler = $this->client->request(
+				'POST',
+				'posting.php?mode=reply&f=2&t=1&sid=' . $this->sid,
+				array(
+					'chunk' => $i,
+					'chunks' => self::CHUNKS,
+					'name' => md5('valid') . '.jpg',
+					'real_filename' => 'valid.jpg',
+					'add_file' => $this->lang('ADD_FILE'),
+				),
+				array('fileupload' => $file),
+				array('X-PHPBB-USING-PLUPLOAD' => '1')
+			);
+
+			if ($i < self::CHUNKS - 1)
+			{
+				$this->assertContains('{"jsonrpc":"2.0","id":"id","result":null}', $this->client->getResponse()->getContent());
+			}
+			else
+			{
+				$this->assertEquals(0, $crawler->filter('p.error')->count());
+				$this->assertContains($this->lang('POSTED_ATTACHMENTS'), $crawler->filter('#postform h3')->eq(1)->text());
+			}
+
+			unlink($this->path . 'chunk');
+		}
+
+		fclose($handle);
 	}
 
 	public function test_normal_upload()
