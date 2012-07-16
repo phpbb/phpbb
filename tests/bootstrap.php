797ee16eaf2e8dc562185cd940351bcfd311cf53
Fyorl <gaelreth@gmail.com>
2012-07-16 17:33:05 +0100

    [ticket/10981] Added goutte via composer
    composer.phar added and autoloaded before tests
    
    PHPBB3-10981

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -22,5 +22,20 @@ require_once 'test_framework/phpbb_database_test_connection_manager.php';
 
 if (version_compare(PHP_VERSION, '5.3.0-dev', '>='))
 {
+	if (getenv('PHPBB_NO_COMPOSER_AUTOLOAD'))
+	{
+		if (getenv('PHPBB_AUTOLOAD'))
+		{
+			require(getenv('PHPBB_AUTOLOAD'));
+		}
+	}
+	else
+	{
+		if (!file_exists($phpbb_root_path . 'vendor/autoload.php'))
+		{
+			trigger_error('You have not set up composer dependencies. See http://getcomposer.org/.', E_USER_ERROR);
+		}
+		require($phpbb_root_path . 'vendor/autoload.php');
+	}
 	require_once 'test_framework/phpbb_functional_test_case.php';
 }
--- tests/test_framework/phpbb_functional_test_case.php
+++ tests/test_framework/phpbb_functional_test_case.php
@@ -36,8 +36,6 @@ class phpbb_functional_test_case extends phpbb_test_case
 		{
 			self::markTestSkipped('phar extension is not loaded');
 		}
-
-		require_once 'phar://' . __DIR__ . '/../../vendor/goutte.phar';
 	}
 
 	public function setUp()
