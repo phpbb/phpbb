c88b5977797b3b90152c84311f345123af04e6b0
David King <imkingdavid@gmail.com>
2012-08-04 15:45:09 -0400

    [feature/revisions] Add comparison class
    PHPBB3-10755

---
+++ phpBB/includes/revisions/comparison.php
@@ -0,0 +1,194 @@
+<?php
+/**
+*
+* @package phpbb_revisions
+* @copyright (c) 2012 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* A class representing the comparison between two strings
+*
+* @package phpbb_revisions
+*/
+class phpbb_revisions_comparison
+{
+	/**
+	* First revision in comparison
+	* @var phpbb_revisions_revision
+	*/
+	private $first;
+
+	/**
+	* Last revision in comparison
+	* @var phpbb_revisions_revision
+	*/
+	private $last;
+
+	/**
+	* Post content diff object
+	* @var phpbb_revisions_diff_engine_base
+	*/
+	private $text_diff;
+	
+	/**
+	* Post subject diff object
+	* @var phpbb_revisions_diff_engine_base
+	*/
+	private $subject_diff;
+
+	/**
+	* Post content diff rendered for display
+	* @var string
+	*/
+	private $text_diff_rendered;
+	
+	/**
+	* Post subject diff rendered for display
+	* @var string
+	*/
+	private $subject_diff_rendered;
+
+	/**
+	* Constructor, initialize some class properties
+	*
+	* @param int $post_id Post ID
+	* @param dbal $dbal phpBB DBAL object
+	*/
+	public function __construct(phpbb_revisions_revision $first, phpbb_revisions_revision $last)
+	{
+		$this->first = $first;
+		$this->last = $last;
+
+		$this->text_diff = new phpbb_revisions_diff_engine_finediff($first->get_text_decoded(),
+			$last->get_text_decoded());
+		$this->subject_diff = new phpbb_revisions_diff_engine_finediff($first->get_subject(),
+			$last->get_subject());
+
+		$this->text_diff_rendered = bbcode_nl2br($this->text_diff->render());
+		$this->subject_diff_rendered = $this->subject_diff->render();
+	}
+
+	public function get_comparison_range_ids(array $revisions)
+	{
+		$first_id = $this->first->get_id();
+		$last_id = $this->last->get_id();
+
+		if ($last_id && $first_id > $last_id)
+		{
+			$revisions = array_reverse($revisions, true);
+		}
+
+		$range_ids = array();
+
+		foreach ($revisions as $revision)
+		{
+			// We continue through the array until we reach
+			// the first revision in the comparison
+			if (!sizeof($range_ids) && $revision->get_id() != $first_id)
+			{
+				continue;
+			}
+
+			$range_ids[] = $revision->get_id();
+
+			// Once we reach the comparison's stopping point,
+			// we no longer need to loop through the array
+			if ($revision->get_id() == $last_id)
+			{
+				break;
+			}
+		}
+
+		return $range_ids;
+	}
+
+	public function output_template_block(phpbb_revisions_post $post, phpbb_template $template, phpbb_user $user, $can_revert, $phpbb_root_path, $phpEx)
+	{
+		$post_data = $post->get_post_data();
+		$revisions = $post->get_revisions();
+		$current = $post->get_current_revision();
+		$range_ids = $this->get_comparison_range_ids($revisions);
+
+		$revision_number = 1;
+		$revision_users = array();
+		$first_id = $this->first->get_id();
+		$last_id = $this->last->get_id();
+
+		foreach ($revisions as $revision)
+		{
+			$this_revision_id = $revision->get_id();
+			$post_id = $revision->get_post_id();
+
+			$template->assign_block_vars('revision', array(
+				'DATE'				=> $user->format_date($revision->get_time()),
+				'ID'				=> $this_revision_id,
+				'IN_RANGE'			=> in_array($this_revision_id, $range_ids),
+				'NUMBER'			=> $revision_number,
+				'REASON'			=> $revision->get_reason(),
+				'USERNAME'			=> $revision->get_username(),
+				'USER_AVATAR'		=> $revision->get_avatar(20, 20),
+				'PROTECTED'			=> $revision->is_protected(),
+
+				'FIRST_IN_COMPARE'	=> $revision->get_id() == $first_id,
+				'LAST_IN_COMPARE'	=> $revision->get_id() == $last_id,
+
+				'U_REVERT_TO'		=> $can_revert ? append_sid("{$phpbb_root_path}revisions.$phpEx", array('p' => $post_id, 'revert' => $this_revision_id)) : '',
+				'U_REVISION_VIEW'	=> append_sid("{$phpbb_root_path}revisions.$phpEx", array('r' => $this_revision_id)),
+				'U_POST'			=> append_sid("{$phpbb_root_path}viewtopic.$phpEx", array('f' => $post_data['forum_id'], 't' => $post_data['topic_id'], 'p' => $post_id)) . '#p' . $post_id,
+			));
+
+			$revision_users[$revision->get_user_id()] = true;
+			$revision_number++;
+		}
+
+		$l_compare_summary = $user->lang('REVISION_COUNT', count($revisions)) . '
+			' . $user->lang('BY') . '
+			' . $user->lang('REVISION_USER_COUNT', sizeof($revision_users));
+		$l_lines_added_removed = $user->lang('REVISION_ADDITIONS', $this->text_diff->additions_count() + $this->subject_diff->additions_count()) . '
+			' . $user->lang('AND') . '
+			' . $user->lang('REVISION_DELETIONS', $this->text_diff->deletions_count() + $this->subject_diff->deletions_count());
+
+		$template->assign_vars(array(
+			'S_DISPLAY_COMPARISON'	=> true,
+			'L_LAST_REVISION_TIME'	=> $user->lang('LAST_REVISION_TIME', $user->format_date($current->get_time())),
+
+			'L_COMPARE_SUMMARY'		=> $l_compare_summary,
+			'L_LINES_ADDED_REMOVED'	=> $l_lines_added_removed,
+
+			'FIRST_REVISION'		=> $this->first->get_id() ? strtolower($user->lang('REVISION')) . ' ' . $this->first->get_id() : $user->lang('CURRENT_REVISION'),
+			'U_FIRST_REVISION'		=> append_sid("{$phpbb_root_path}revisions.$phpEx", ($first_id ? array('r' => $this->first->get_id()) : array('p' => $post_data['post_id']))),
+			'LAST_REVISION'			=> $last_id ? strtolower($user->lang('REVISION')) . ' ' . $last_id : $user->lang('CURRENT_REVISION'),
+			'U_LAST_REVISION'		=> append_sid("{$phpbb_root_path}revisions.$phpEx", ($last_id ? array('r' => $last_id) : array('p' => $post_data['post_id']))),
+		));
+	}
+
+	/**
+	* Return the rendered subject diff for display
+	*
+	* @return string
+	*/
+	public function get_subject_diff_rendered()
+	{
+		return $this->subject_diff_rendered;
+	}
+
+	/**
+	* Return the rendered text diff for display
+	*
+	* @return string
+	*/
+	public function get_text_diff_rendered()
+	{
+		return $this->text_diff_rendered;
+	}
+}
