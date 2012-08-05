6571ea1fc8fa636cb418344f87cc7b010dd8f187
Joas Schilling <nickvergessen@gmx.de>
2012-08-05 14:50:33 +0200

    [feature/php-events] Remove event core.acp_profile_field_edit
    The currently implemented event is useless and we couldn't find a MOD, which
    requires editing this file in that position.
    
    PHPBB3-9550

--- phpBB/includes/acp/acp_profile.php
+++ phpBB/includes/acp/acp_profile.php
@@ -30,7 +30,6 @@ class acp_profile
 		global $config, $db, $user, $auth, $template, $cache;
 		global $phpbb_root_path, $phpbb_admin_path, $phpEx, $table_prefix;
 		global $request;
-		global $phpbb_dispatcher;
 
 		include($phpbb_root_path . 'includes/functions_posting.' . $phpEx);
 		include($phpbb_root_path . 'includes/functions_user.' . $phpEx);
@@ -876,9 +875,6 @@ class acp_profile
 								);
 							}
 						}
-
-						$vars = array('field_row', 'visibility_ary', 'exclude');
-						extract($phpbb_dispatcher->trigger_event('core.acp_profile_field_edit', compact($vars)));
 
 					break;
 				}
