48a0810ea593fc3ed84762ccd9076ea756cda41c
Fyorl <gaelreth@gmail.com>
2012-08-15 14:20:40 +0100

    [ticket/10939] Modified request test slightly to include $_FILES
    PHPBB3-10939

--- tests/request/request_test.php
+++ tests/request/request_test.php
@@ -21,6 +21,13 @@ class phpbb_request_test extends phpbb_test_case
 		$_COOKIE['test'] = 3;
 		$_REQUEST['test'] = 3;
 		$_GET['unset'] = '';
+		$_FILES['test'] = array(
+			'name' => 'file',
+			'tmp_name' => 'tmp',
+			'size' => 256,
+			'type' => 'application/octet-stream',
+			'error' => UPLOAD_ERR_OK,
+		);
 
 		$_SERVER['HTTP_HOST'] = 'example.com';
 		$_SERVER['HTTP_ACCEPT'] = 'application/json';
@@ -42,6 +49,7 @@ class phpbb_request_test extends phpbb_test_case
 		$this->assertEquals(2, $_GET['test'], 'Checking $_GET after enable_super_globals');
 		$this->assertEquals(3, $_COOKIE['test'], 'Checking $_COOKIE after enable_super_globals');
 		$this->assertEquals(3, $_REQUEST['test'], 'Checking $_REQUEST after enable_super_globals');
+		$this->assertEquals(256, $_FILES['test']['size']);
 
 		$_POST['x'] = 2;
 		$this->assertEquals($_POST, $GLOBALS['_POST'], 'Checking whether $_POST can still be accessed via $GLOBALS[\'_POST\']');
