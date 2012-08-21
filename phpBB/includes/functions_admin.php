c6a244a017677bd08fd53bdc8eefdd3de4cac49b
Unknown Bliss <m@michaelcullum.com>
2012-08-21 15:51:14 +0100

    [ticket/11063] Add SSL prototcol for version check
    PHPBB3-11063

--- phpBB/includes/functions_admin.php
+++ phpBB/includes/functions_admin.php
@@ -3316,7 +3316,7 @@ function obtain_latest_version_info($force_update = false, $warn_fail = false, $
 		$errstr = '';
 		$errno = 0;
 
-		$info = get_remote_file('version.phpbb.com', '/phpbb',
+		$info = get_remote_file('https://version.phpbb.com', '/phpbb',
 				((defined('PHPBB_QA')) ? '30x_qa.txt' : '30x.txt'), $errstr, $errno);
 
 		if ($info === false)
