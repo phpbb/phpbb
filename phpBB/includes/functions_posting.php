57afe31af5b0d06e05051e8e1543bfbc866f0a4f
Nathan Guse <nathaniel.guse@gmail.com>
2012-09-09 14:55:40 -0500

    [ticket/11103] Update notifications on post/topic edit
    PHPBB3-11103

--- phpBB/includes/functions_posting.php
+++ phpBB/includes/functions_posting.php
@@ -2223,12 +2223,18 @@ function submit_post($mode, $subject, $username, $topic_type, &$poll, &$data, $u
 	}
 
 	// Send Notifications
-	if (($mode == 'reply' || $mode == 'quote' || $mode == 'post') && $post_approval)
+	if ($post_approval)
 	{
 		$notifications = $phpbb_container->get('notifications');
 
 		switch ($mode)
 		{
+			case 'post' :
+				$notifications->add_notifications('topic', array_merge($data, array(
+					'post_username'		=> $username,
+				)));
+			break;
+
 			case 'reply' :
 			case 'quote' :
 				$notifications->add_notifications('post', array_merge($data, array(
@@ -2236,8 +2242,15 @@ function submit_post($mode, $subject, $username, $topic_type, &$poll, &$data, $u
 				)));
 			break;
 
-			case 'post' :
-				$notifications->add_notifications('topic', array_merge($data, array(
+			case 'edit_topic' :
+			case 'edit_first_post' :
+			case 'edit' :
+			case 'edit_last_post' :
+				$notifications->update_notifications('topic', array_merge($data, array(
+					'post_username'		=> $username,
+					'topic_title'		=> $subject,
+				)));
+				$notifications->update_notifications('post', array_merge($data, array(
 					'post_username'		=> $username,
 				)));
 			break;
