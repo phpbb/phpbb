0d8e7b75842b06a1e74ea2a2872ddd014f2282ff
David King <imkingdavid@gmail.com>
2012-09-16 19:09:53 -0400

    [feature/controller] Added new line for code clarity
    PHPBB3-10864

--- phpBB/includes/controller/resolver.php
+++ phpBB/includes/controller/resolver.php
@@ -20,6 +20,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
+
 /**
 * Controller manager class
 * @package phpBB3
