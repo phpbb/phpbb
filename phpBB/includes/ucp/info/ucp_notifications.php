b052741fcb0b3cab4317a759ec935518bef7c73b
Nathaniel Guse <nathaniel.guse@gmail.com>
2012-09-26 21:50:21 -0500

    [ticket/11103] UCP base files for notification options
    PHPBB3-11103

---
+++ phpBB/includes/ucp/info/ucp_notifications.php
@@ -0,0 +1,34 @@
+<?php
+/**
+*
+* @package notifications
+* @copyright (c) 2012 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @package module_install
+*/
+class ucp_notifications_info
+{
+	function module()
+	{
+		return array(
+			'filename'	=> 'ucp_notifications',
+			'title'		=> 'UCP_NOTIFICATIONS',
+			'version'	=> '1.0.0',
+			'modes'		=> array(
+				'notification_options'		=> array('title' => 'UCP_NOTIFICATION_OPTIONS', 'auth' => '', 'cat' => array('UCP_MAIN')),
+			),
+		);
+	}
+
+	function install()
+	{
+	}
+
+	function uninstall()
+	{
+	}
+}
