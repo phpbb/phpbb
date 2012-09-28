9bf752b31cee4d2c6b90d726d0ab22a9c4420fe1
Nathan Guse <nathaniel.guse@gmail.com>
2012-09-27 19:04:47 -0500

    [ticket/11103] Fix module setup, add explanation to template
    PHPBB3-11103

--- phpBB/install/database_update.php
+++ phpBB/install/database_update.php
@@ -2645,8 +2645,8 @@ function change_database_data(&$no_updates, $version)
 					'auth'		=> 'acl_a_extensions',
 					'cat'		=> 'ACP_EXTENSION_MANAGEMENT',
 				),
-				'notification_options'	=> array(
-					'base'		=> 'ucp_notification_options',
+				'notifications'	=> array(
+					'base'		=> 'ucp_notifications',
 					'class'		=> 'ucp',
 					'title'		=> 'UCP_NOTIFICATION_OPTIONS',
 					'auth'		=> '',
