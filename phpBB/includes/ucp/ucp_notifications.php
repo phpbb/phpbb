868554cbaeaf7a23db2af4c97aa07c77e6ea98b0
Nathan Guse <nathaniel.guse@gmail.com>
2012-10-05 13:50:59 -0500

    [ticket/11103] trigger_error message when preferences updated in UCP
    PHPBB3-11103

--- phpBB/includes/ucp/ucp_notifications.php
+++ phpBB/includes/ucp/ucp_notifications.php
@@ -64,6 +64,10 @@ class ucp_notifications
 					}
 				}
 			}
+
+			meta_refresh(3, $this->u_action);
+			$message = $user->lang['PREFERENCES_UPDATED'] . '<br /><br />' . sprintf($user->lang['RETURN_UCP'], '<a href="' . $this->u_action . '">', '</a>');
+			trigger_error($message);
 		}
 
 		// todo include language files for extensions?
