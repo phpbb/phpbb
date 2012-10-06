95d02d74ac6d71578a98e8850140d9a6cad7e852
Vinny <vinny@suportephpbb.com.br>
2012-10-05 21:18:59 -0300

    [ticket/11135] Full replacement of credit link to https
    PHPBB3-11135

--- phpBB/install/convertors/convert_phpbb20.php
+++ phpBB/install/convertors/convert_phpbb20.php
@@ -33,7 +33,7 @@ $convertor_data = array(
 	'forum_name'	=> 'phpBB 2.0.x',
 	'version'		=> '1.0.3',
 	'phpbb_version'	=> '3.0.11',
-	'author'		=> '<a href="http://www.phpbb.com/">phpBB Group</a>',
+	'author'		=> '<a href="https://www.phpbb.com/">phpBB Group</a>',
 	'dbms'			=> $dbms,
 	'dbhost'		=> $dbhost,
 	'dbport'		=> $dbport,
