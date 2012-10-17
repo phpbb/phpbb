7936740bec9cf9a678b1d491c4521422d077c013
Nathan Guse <nathaniel.guse@gmail.com>
2012-10-16 23:27:49 -0500

    [ticket/11103] Add author name output to post/topic email templates
    For a recent merge:
    https://github.com/phpbb/phpbb3/pull/624
    
    PHPBB3-11103

--- phpBB/includes/notification/type/topic.php
+++ phpBB/includes/notification/type/topic.php
@@ -186,7 +186,19 @@ class phpbb_notification_type_topic extends phpbb_notification_type_base
 	*/
 	public function get_email_template_variables()
 	{
+		if ($this->get_data('post_username'))
+		{
+			$username = $this->get_data('post_username');
+		}
+		else
+		{
+			$user_data = $this->notification_manager->get_user($this->get_data('poster_id'));
+
+			$username = get_username_string('username', $user_data['user_id'], $user_data['username'], $user_data['user_colour']);
+		}
+
 		return array(
+			'AUTHOR_NAME'				=> htmlspecialchars_decode($username),
 			'FORUM_NAME'				=> htmlspecialchars_decode($this->get_data('forum_name')),
 			'TOPIC_TITLE'				=> htmlspecialchars_decode(censor_text($this->get_data('topic_title'))),
 
