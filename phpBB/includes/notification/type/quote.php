f1b6505a991a470bc4a4c6e2cf7c29948717066c
Nathan Guse <nathaniel.guse@gmail.com>
2012-10-19 15:49:49 -0500

    [ticket/11103] Change is_disabled to is_enabled
    If you're following along and would like to update your DB, you can run
    the following queries to do so:
    
    ALTER TABLE phpbb_notifications CHANGE `is_disabled` `is_enabled`
    TINYINT( 1 ) NOT NULL DEFAULT  '1';
    
    UPDATE `phpbb_notifications` SET is_enabled = 1;
    
    PHPBB3-11103

--- phpBB/includes/notification/type/quote.php
+++ phpBB/includes/notification/type/quote.php
@@ -133,7 +133,7 @@ class phpbb_notification_type_quote extends phpbb_notification_type_post
 			WHERE item_type = '" . self::get_item_type() . "'
 				AND item_parent_id = " . (int) self::get_item_parent_id($post) . '
 				AND unread = 1
-				AND is_disabled = 0';
+				AND is_enabled = 1';
 		$result = $this->db->sql_query($sql);
 		while ($row = $this->db->sql_fetchrow($result))
 		{
@@ -163,7 +163,7 @@ class phpbb_notification_type_quote extends phpbb_notification_type_post
 			FROM ' . NOTIFICATIONS_TABLE . "
 			WHERE item_type = '" . self::get_item_type() . "'
 				AND item_id = " . self::get_item_id($post) . '
-				AND is_disabled = 0';
+				AND is_enabled = 1';
 		$result = $this->db->sql_query($sql);
 		while ($row = $this->db->sql_fetchrow($result))
 		{
