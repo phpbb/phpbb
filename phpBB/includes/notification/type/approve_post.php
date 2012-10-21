b32c6b858316fef9f833452ed773752dc51ea23a
Nathan Guse <nathaniel.guse@gmail.com>
2012-10-20 21:45:15 -0500

    [ticket/11103] Correct the comments on the notification classes
    PHPBB3-11103

--- phpBB/includes/notification/type/approve_post.php
+++ phpBB/includes/notification/type/approve_post.php
@@ -16,8 +16,8 @@ if (!defined('IN_PHPBB'))
 }
 
 /**
-* Post notifications class
-* This class handles notifications for replies to a topic
+* Post approved notifications class
+* This class handles notifications for posts when they are approved (to their authors)
 *
 * @package notifications
 */
