7d80c5c83c4729a09a5df17013ecfbf752a91243
Nathan Guse <nathaniel.guse@gmail.com>
2012-10-27 12:03:11 -0500

    [ticket/11103] Working on default notifications on install/update
    PHPBB3-11103

--- phpBB/install/database_update.php
+++ phpBB/install/database_update.php
@@ -3018,6 +3018,109 @@ function change_database_data(&$no_updates, $version)
 			if (!isset($config['load_notifications']))
 			{
 				$config->set('load_notifications', 1);
+
+				// Convert notifications
+				$convert_notifications = array(
+					array(
+						'check'			=> ($config['allow_topic_notify']),
+						'item_type'		=> 'phpbb_notification_type_post',
+					),
+					array(
+						'check'			=> ($config['allow_forum_notify']),
+						'item_type'		=> 'phpbb_notification_type_topic',
+					),
+					array(
+						'check'			=> ($config['allow_bookmarks']),
+						'item_type'		=> 'phpbb_notification_type_bookmark',
+					),
+					array(
+						'check'			=> ($config['allow_privmsg']),
+						'item_type'		=> 'phpbb_notification_type_pm',
+					),
+				);
+
+				foreach ($convert_notifications as $convert_data)
+				{
+					if ($convert_data['check'])
+					{
+						$sql = 'SELECT user_id, user_notify_type
+							FROM ' . USERS_TABLE . '
+								WHERE user_notify = 1';
+						$result = $db->sql_query($sql);
+						while ($row = $db->sql_fetchrow($result))
+						{
+							_sql('INSERT INTO ' . USER_NOTIFICATIONS_TABLE . ' ' . $db->sql_build_array('INSERT', array(
+								'item_type'		=> $convert_data['item_type'],
+								'item_id'		=> 0,
+								'user_id'		=> $row['user_id'],
+								'method'		=> '',
+							)), $errored, $error_ary);
+
+							if ($row['user_notify_type'] == NOTIFY_EMAIL || $row['user_notify_type'] == NOTIFY_BOTH)
+							{
+								_sql('INSERT INTO ' . USER_NOTIFICATIONS_TABLE . ' ' . $db->sql_build_array('INSERT', array(
+									'item_type'		=> $convert_data['item_type'],
+									'item_id'		=> 0,
+									'user_id'		=> $row['user_id'],
+									'method'		=> 'phpbb_notification_method_email',
+								)), $errored, $error_ary);
+							}
+
+							if ($row['user_notify_type'] == NOTIFY_IM || $row['user_notify_type'] == NOTIFY_BOTH)
+							{
+								_sql('INSERT INTO ' . USER_NOTIFICATIONS_TABLE . ' ' . $db->sql_build_array('INSERT', array(
+									'item_type'		=> $convert_data['item_type'],
+									'item_id'		=> 0,
+									'user_id'		=> $row['user_id'],
+									'method'		=> 'phpbb_notification_method_jabber',
+								)), $errored, $error_ary);
+							}
+						}
+						$db->sql_freeresult($result);
+					}
+				}
+/*
+				// Add default notifications
+				$default_notifications = array(
+					array(
+						'check'			=> ($config['allow_topic_notify']),
+						'item_type'		=> 'phpbb_notification_type_post',
+					),
+					array(
+						'check'			=> ($config['allow_forum_notify']),
+						'item_type'		=> 'phpbb_notification_type_topic',
+					),
+					array(
+						'check'			=> ($config['allow_bookmarks']),
+						'item_type'		=> 'phpbb_notification_type_bookmark',
+					),
+					array(
+						'check'			=> ($config['allow_privmsg']),
+						'item_type'		=> 'phpbb_notification_type_pm',
+					),
+				);
+
+				foreach ($default_notifications as $convert_data)
+				{
+					if ($convert_data['check'])
+					{
+						$sql = 'SELECT user_id
+							FROM ' . USERS_TABLE . '
+							WHERE user_id <> ' . ANONYMOUS . '
+								AND user_type <> ' . USER_IGNORE;
+						$result = $db->sql_query($sql);
+						while ($row = $db->sql_fetchrow($result))
+						{
+							_sql('INSERT INTO ' . USER_NOTIFICATIONS_TABLE . ' ' . $db->sql_build_array('INSERT', array(
+								'item_type'		=> $convert_data['item_type'],
+								'item_id'		=> 0,
+								'user_id'		=> $row['user_id'],
+								'method'		=> '',
+							)), $errored, $error_ary);
+						}
+						$db->sql_freeresult($result);
+					}
+				}*/
 			}
 
 			$no_updates = false;
