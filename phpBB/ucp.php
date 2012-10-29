88bd7292f16817d681264b9f06ff30c2a38c1da4
Oleg Pudeyev <oleg@bsdpower.com>
2012-10-29 13:39:45 -0400

    [ticket/11158] Revert old fix in PHPBB3-10186.
    Revert "[ticket/10186] UCP signature panel displays when not authed
    for signatures"
    
    This reverts commit f6fa52540c0fb20d79b4d59cb08c49484d9f3116.
    
    PHPBB3-10186
    PHPBB3-11158

--- phpBB/ucp.php
+++ phpBB/ucp.php
@@ -320,12 +320,6 @@ if (!$config['allow_topic_notify'] && !$config['allow_forum_notify'])
 	$module->set_display('main', 'subscribed', false);
 }
 
-// Do not display signature panel if not authed to do so
-if (!$auth->acl_get('u_sig'))
-{
-	$module->set_display('profile', 'signature', false);
-}
-
 // Select the active module
 $module->set_active($id, $mode);
 
