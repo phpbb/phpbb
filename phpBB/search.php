dac798deffde15f3cb0383f5aa06a266aa9bc6cd
Joas Schilling <nickvergessen@gmx.de>
2012-11-09 12:33:50 +0100

    [feature/soft-delete] Display soft deleted icon on search.php
    PHPBB3-9567

--- phpBB/search.php
+++ phpBB/search.php
@@ -897,7 +897,7 @@ if ($keywords || $author || $author_id || $search_id || $submit)
 					'TOPIC_ICON_IMG_HEIGHT'	=> (!empty($icons[$row['icon_id']])) ? $icons[$row['icon_id']]['height'] : '',
 					'ATTACH_ICON_IMG'		=> ($auth->acl_get('u_download') && $auth->acl_get('f_download', $forum_id) && $row['topic_attachment']) ? $user->img('icon_topic_attach', $user->lang['TOTAL_ATTACHMENTS']) : '',
 					'UNAPPROVED_IMG'		=> ($topic_unapproved || $posts_unapproved) ? $user->img('icon_topic_unapproved', ($topic_unapproved) ? 'TOPIC_UNAPPROVED' : 'POSTS_UNAPPROVED') : '',
-					'DELETED_IMG'           => ($topic_deleted) ? $user->img(/*TODO*/) : '',
+					'DELETED_IMG'			=> ($topic_deleted) ? $user->img('icon_topic_deleted', 'POST_DELETED_RESTORE') : '',
 
 					'S_TOPIC_TYPE'			=> $row['topic_type'],
 					'S_USER_POSTED'			=> (!empty($row['topic_posted'])) ? true : false,
