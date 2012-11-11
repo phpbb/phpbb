f2d318d84da271db80bc0373d424ba3394121936
Fyorl <gaelreth@gmail.com>
2012-11-11 16:22:22 +0000

    [feature/autosave] More work on the autosave.
    Autosaves should now save per window on pages with posting. When
    loading a page, all autosaves of that post will be prompted for,
    organised by the time at which they were created.
    
    Added a language string and a few template variables required by
    javascript for calculating the time correctly for the user's
    timezone.
    
    PHPBB3-11185

--- phpBB/assets/javascript/autosave.js
+++ phpBB/assets/javascript/autosave.js
@@ -1,24 +1,94 @@
-function phpbb_autosave () {
+function phpbb_autosave_prompt (data) {
+	var str = phpbb_autosave_prompt_msg;
+	var dt;
+	var keys = [];
+	
+	for (var key in data) {
+		dt = new Date((key + phpbb_autosave_tz_offset) * 1000);
+		str += "\n" + count + ') ' + phpbb_autosave_timestr(dt);
+
+		keys.push(key);
+	}
+
+	if (keys.length < 2) {
+		return data[keys[0]];
+	} else {
+		var choice;
+
+		while (true) {
+			choice = prompt(str);
+
+			try {
+				choice = parseInt(choice);
+			} catch (e) {
+				continue;
+			}
+
+			if (choice < 1 || > keys.length) {
+				continue;
+			}
+
+			break;
+		}
+
+		return data[keys[choice - 1]];
+	}
+}
+
+function phpbb_autosave_save (key) {
 	try {
-		window.localStorage['phpbb_post'] = $('#message')[0].value;
+		window.localStorage[key] = $('textarea[name=message]').attr('value');
 	} catch (e) {
 		// Quota exceeded, should inform the user that their autosave isn't
 		// working
 	}
 }
 
+function phpbb_autosave_timestr (dt) {
+	var hrs = dt.getUTCHours();
+	var mins = dt.getUTCMinutes();
+
+	if (hrs < 10) {
+		hrs = '0' + hrs;
+	}
+
+	if (mins < 10) {
+		mins = '0' + mins;
+	}
+
+	return hrs + ':' + mins;
+}
+
 jQuery(function($) {
 	// localStorage not supported or no post box on the page
-	if (!window.localStorage || $('#message').length < 1) {
+	if (!window.localStorage || $('textarea[name=message]').length < 1) {
 		return;
 	}
 
-	// If we have data in phpbb_post when the page loads we can assume it's ok
-	// to load their autosave
-	if (window.localStorage['phpbb_post']) {
-		$('#message')[0].value = window.localStorage['phpbb_post'];
+	var key = window.location.host + window.location.pathname;
+	// Extract the f=, t= or p= from the URL and separate with underscores
+	key += window.location.search.replace(/.*?\W(f|t|p)=(\d)+.*?/g, '_$2');
+
+	// Just store a variable without the creation time for convenience
+	var key_no_creation = key;
+	key += '_' + $('input[name=creation_time]').attr('value');
+	
+	// If we have data in localStorage when the page loads we can assume it's
+	// ok to load their autosave
+	var data = {};
+	for (var storage_key in window.localStorage) {
+		var explode = storage_key.split('_');
+		if (key_no_creation === explode.slice(0, -1).join('_')) {
+			data[parseInt(explode.pop())] = window.localStorage[storage_key];
+		}
 	}
 
-	// Autosave every 30 seconds
-	setInterval(phpbb_autosave, 30 * 1000);
+	$('textarea[name=message]').attr('value', phpbb_autosave_prompt(data));
+
+	// Create a closure to pass variables to the setInterval call
+	var bind = function () {
+		return phpbb_autosave_save(key);
+	};
+
+	setInterval(bind, 30 * 1000);
 });
