46cb0fb068feeb89c939d8b145808d2a786de8dd
David King <imkingdavid@gmail.com>
2012-11-13 10:57:24 -0500

    [feature/controller] Removed another empty construct method
    PHPBB3-10864

--- tests/controller/includes/controller/foo.php
+++ tests/controller/includes/controller/foo.php
@@ -5,13 +5,6 @@ use Symfony\Component\HttpFoundation\Response;
 class phpbb_controller_foo
 {
 	/**
-	* Constructor
-	*/
-	public function __construct()
-	{	
-	}
-
-	/**
 	* Bar method
 	*
 	* @return null
