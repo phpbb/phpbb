4cb9ec522c7007b99eb5ef44cb1bfdb369478aff
David King <imkingdavid@gmail.com>
2012-11-14 16:06:12 -0500

    [feature/controller] Separate Kernel listeners into their own classes
    PHPBB3-10864

--- phpBB/includes/event/kernel_request_subscriber.php
+++ phpBB/includes/event/kernel_request_subscriber.php
@@ -17,27 +17,12 @@ if (!defined('IN_PHPBB'))
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\HttpKernel\Event\PostResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\Routing\RequestContext;
 
-class phpbb_event_kernel_subscriber implements EventSubscriberInterface
+class phpbb_event_kernel_request_subscriber implements EventSubscriberInterface
 {
-	/**
-	* Template object
-	* @var phpbb_template
-	*/
-	protected $template;
-
-	/**
-	* User object
-	* @var phpbb_user
-	*/
-	protected $user;
-
 	/**
 	* Extension finder object
 	* @var phpbb_extension_finder
@@ -59,56 +44,17 @@ class phpbb_event_kernel_subscriber implements EventSubscriberInterface
 	/**
 	* Construct method
 	*
-	* @param phpbb_template $template Template object
-	* @param phpbb_user $user User object
 	* @param phpbb_extension_finder $finder Extension finder object
 	* @param string $root_path Root path
 	* @param string $php_ext PHP extension
 	*/
-	public function __construct(phpbb_template $template, phpbb_user $user, phpbb_extension_finder $finder, $root_path, $php_ext)
+	public function __construct(phpbb_extension_finder $finder, $root_path, $php_ext)
 	{
 		$this->template = $template;
 		$this->user = $user;
 		$this->finder = $finder;
 		$this->root_path = $root_path;
 		$this->php_ext = $php_ext;
-	}
-
-	/**
-	* This listener is run when the KernelEvents::TERMINATE event is triggered
-	* This comes after a Response has been sent to the server; this is
-	* primarily cleanup stuff.
-	*
-	* @param PostResponseEvent $event
-	* @return null
-	*/
-	public function on_kernel_terminate(PostResponseEvent $event)
-	{
-		exit_handler();
-	}
-
-	/**
-	* This listener is run when the KernelEvents::EXCEPTION event is triggered
-	*
-	* @param GetResponseForExceptionEvent $event
-	* @return null
-	*/
-	public function on_kernel_exception(GetResponseForExceptionEvent $event)
-	{
-		page_header($this->user->lang('INFORMATION'));
-
-		$this->template->assign_vars(array(
-			'MESSAGE_TITLE'		=> $this->user->lang('INFORMATION'),
-			'MESSAGE_TEXT'		=> $event->getException()->getMessage(),
-		));
-
-		$this->template->set_filenames(array(
-			'body'	=> 'message_body.html',
-		));
-
-		page_footer(true, false, false);
-
-		$event->setResponse(new Response($this->template->assign_display('body'), 404));
 	}
 
 	/**
@@ -125,6 +71,10 @@ class phpbb_event_kernel_subscriber implements EventSubscriberInterface
 		$context = new RequestContext();
 		$context->fromRequest($request);
 
+		if (!function_exists('phpbb_create_url_matcher'))
+		{
+			include($this->root_path . 'includes/functions_url_matcher' . $this->php_ext);
+		}
 		$matcher = phpbb_create_url_matcher($this->finder, $context, $this->root_path, $this->php_ext);
 
 		$router_listener = new RouterListener($matcher, $context);
@@ -135,8 +85,6 @@ class phpbb_event_kernel_subscriber implements EventSubscriberInterface
 	{
 		return array(
 			KernelEvents::REQUEST		=> 'on_kernel_request',
-			KernelEvents::TERMINATE		=> 'on_kernel_terminate',
-			KernelEvents::EXCEPTION		=> 'on_kernel_exception',
 		);
 	}
 }
