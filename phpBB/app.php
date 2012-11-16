c9588572c9f40223de3445a211ca85b18f5294a4
David King <imkingdavid@gmail.com>
2012-11-16 09:20:15 -0500

    [feature/controller] Add period before $phpEx
    PHPBB3-10864

--- phpBB/app.php
+++ phpBB/app.php
@@ -17,7 +17,7 @@ define('IN_PHPBB', true);
 $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
-include($phpbb_root_path . 'includes/functions_url_matcher' . $phpEx);
+include($phpbb_root_path . 'includes/functions_url_matcher.' . $phpEx);
 
 // Start session management
 $user->session_begin();
