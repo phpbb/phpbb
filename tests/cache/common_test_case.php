8595b1f56018d0bfac0b83ac1511175c88a8b9cb
Oleg Pudeyev <oleg@bsdpower.com>
2012-12-01 18:57:09 -0500

    [ticket/9983] Exercise exists also.
    PHPBB3-9983

--- tests/cache/common_test_case.php
+++ tests/cache/common_test_case.php
@@ -9,12 +9,14 @@
 
 abstract class phpbb_cache_common_test_case extends phpbb_database_test_case
 {
-	public function test_get_put()
+	public function test_get_put_exists()
 	{
+		$this->assertFalse($this->driver->_exists('test_key'));
 		$this->assertSame(false, $this->driver->get('test_key'));
 
 		$this->driver->put('test_key', 'test_value');
 
+		$this->assertTrue($this->driver->_exists('test_key'));
 		$this->assertEquals(
 			'test_value',
 			$this->driver->get('test_key'),
