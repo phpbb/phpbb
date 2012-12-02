4a8d74db8b444a5bd8c6ad27c8c12183e436a789
Bruno Ais <brunoaiss@gmail.com>
2012-12-02 17:16:11 +0000

    [feature/only-op-can-view] Moved query logic on mcp_forum
    Moved the logical AND of the query to another place in the code
    and changed the place where the variable of the inserted SQL is.
    
    PHPBB3-10623

--- phpBB/includes/mcp/mcp_forum.php
+++ phpBB/includes/mcp/mcp_forum.php
@@ -158,21 +158,21 @@ function mcp_forum_view($id, $mode, $action, $forum_info)
 	$limit_access_check = '';
 	if (!$auth->acl_get('f_read_other', $forum_id)
 	{
-		$limit_access_check =	'(t.topic_poster = ' . (int) $user->data['user_id'] . ' OR
+		$limit_access_check =	' AND (t.topic_poster = ' . (int) $user->data['user_id'] . ' OR
 									(t.topic_type <> ' . POST_NORMAL . ' AND t.topic_type <> ' . POST_STICKY . ')
-								)
-								AND ';
+								) ';
 	}
 
 	$sql = 'SELECT t.topic_id
 		FROM ' . TOPICS_TABLE . ' t
 		WHERE
-			 ' . $phpbb_content_visibility->get_visibility_sql('topic', $forum_id, 't.') . " AND
+			 ' . $phpbb_content_visibility->get_visibility_sql('topic', $forum_id, 't.') . " 
 			' . $limit_access_check. ' AND
 				(t.topic_poster = ' . $user->data['user_id'] . ' OR 1='. (($auth->acl_get('f_read_other', $forum_id))? '1' : '0') . ')
 				)
 				AND ' : '') . '
 			t.forum_id = ' . $forum_id . '
+			' . $limit_access_check. '
 			$limit_time_sql
 		ORDER BY t.topic_type DESC, $sort_order_sql";
 	$result = $db->sql_query_limit($sql, $topics_per_page, $start);
