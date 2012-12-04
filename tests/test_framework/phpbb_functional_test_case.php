74093d0fd383619ec8b58914ebe2edd68145e070
Oleg Pudeyev <oleg@bsdpower.com>
2012-12-04 04:32:37 -0500

    [ticket/11015] Fix functional test case.
    PHPBB3-11015

--- tests/test_framework/phpbb_functional_test_case.php
+++ tests/test_framework/phpbb_functional_test_case.php
@@ -86,7 +86,7 @@ class phpbb_functional_test_case extends phpbb_test_case
 	{
 		global $phpbb_root_path, $phpEx;
 		// so we don't reopen an open connection
-		if (!($this->db instanceof dbal))
+		if (!($this->db instanceof phpbb_db_driver))
 		{
 			$dbms = self::$config['dbms'];
 			$this->db = new $dbms();
