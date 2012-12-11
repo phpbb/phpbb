12aa6b4b2cfda48f94c44865209f576654632b41
Nathan Guse <nathaniel.guse@gmail.com>
2012-12-11 11:24:18 -0600

    [ticket/8323] More readability in test case
    PHPBB3-8323

--- tests/functions/phpbb_get_banned_user_ids.php
+++ tests/functions/phpbb_get_banned_user_ids.php
@@ -19,9 +19,20 @@ class phpbb_get_banned_user_ids_test extends phpbb_database_test_case
 	public function phpbb_get_banned_user_ids_data()
 	{
 		return array(
-			array(array(array(1, 2, 4, 5, 6), true),	array(2 => 2, 5 => 5)),
-			array(array(array(1, 2, 4, 5, 6), false),	array(2 => 2)),
-			array(array(array(1, 2, 4, 5, 6), 2),		array(2 => 2, 5 => 5, 6 => 6)),
+			// Input to phpbb_get_banned_user_ids (user_id list, ban_end)
+			// Expected output
+			array(
+				array(array(1, 2, 4, 5, 6), true),
+				array(2 => 2, 5 => 5),
+			),
+			array(
+				array(array(1, 2, 4, 5, 6), false),
+				array(2 => 2),
+			),
+			array(
+				array(array(1, 2, 4, 5, 6), 2),
+				array(2 => 2, 5 => 5, 6 => 6),
+			),
 		);
 	}
 
