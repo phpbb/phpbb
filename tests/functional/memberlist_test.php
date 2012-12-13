c5f350906d0470e54ca7ade935b4bee841acdbed
Oleg Pudeyev <oleg@bsdpower.com>
2012-12-13 07:27:40 -0500

    [ticket/10975] Test memberlist, not user creation.
    PHPBB3-10975

--- tests/functional/memberlist_test.php
+++ tests/functional/memberlist_test.php
@@ -12,22 +12,13 @@
 */
 class phpbb_functional_memberlist_test extends phpbb_functional_test_case
 {
-	public function test_create_user()
+	public function test_view()
 	{
-		$this->create_user('user');
+		$this->create_user('memberlist-test-user');
+		// logs in as admin
 		$this->login();
 		$crawler = $this->request('GET', 'memberlist.php?sid=' . $this->sid);
-		$this->assertContains('user', $crawler->filter('#memberlist tr')->eq(1)->text());
-	}
-
-	/**
-	* @depends test_create_user
-	*/
-	public function test_delete_user()
-	{
-		$this->delete_user('user');
-		$this->login();
-		$crawler = $this->request('GET', 'memberlist.php?sid=' . $this->sid);
-		$this->assertEquals(2, $crawler->filter('#memberlist tr')->count());
+		$this->assert_response_success();
+		$this->assertContains('memberlist-test-user', $crawler->text());
 	}
 }
