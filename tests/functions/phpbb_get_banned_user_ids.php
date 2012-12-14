d68778e7ebd801d396a8ec8694499f2ebdbb809f
Nathan Guse <nathaniel.guse@gmail.com>
2012-12-13 20:05:00 -0600

    [ticket/8323] Comments on phpbb_get_banned_user_ids input for test
    PHPBB3-8323

--- tests/functions/phpbb_get_banned_user_ids.php
+++ tests/functions/phpbb_get_banned_user_ids.php
@@ -22,14 +22,17 @@ class phpbb_get_banned_user_ids_test extends phpbb_database_test_case
 			// Input to phpbb_get_banned_user_ids (user_id list, ban_end)
 			// Expected output
 			array(
+				// True to get users currently banned
 				array(array(1, 2, 4, 5, 6), true),
 				array(2 => 2, 5 => 5),
 			),
 			array(
+				// False to only get permanently banned users
 				array(array(1, 2, 4, 5, 6), false),
 				array(2 => 2),
 			),
 			array(
+				// Unix timestamp to get users banned until that time
 				array(array(1, 2, 4, 5, 6), 2),
 				array(2 => 2, 5 => 5, 6 => 6),
 			),
