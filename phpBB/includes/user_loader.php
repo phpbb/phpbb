0e64ca19f455a2481919f7bccacf3832dbf14851
Nathaniel Guse <nathaniel.guse@gmail.com>
2012-12-30 17:24:21 -0600

    [ticket/11103] Load less by default in user loader
    Add events to allow more fields to be added.
    
    Clarify some comments
    
    PHPBB3-11103

--- phpBB/includes/user_loader.php
+++ phpBB/includes/user_loader.php
@@ -18,7 +18,7 @@ if (!defined('IN_PHPBB'))
 * User loader class
 *
 * This handles loading users from the database and
-* storing in them in a temporary cache so we do not
+* storing them in a temporary cache so we do not
 * have to query the same user multiple times in
 * different services.
 */
@@ -64,6 +64,9 @@ class phpbb_user_loader
 	/**
 	* Load user helper
 	*
+	* Loads all users by user_id that are not in the current user cache
+	* Always loads the anonymous user as a fall-back if any user requested does not exist
+	*
 	* @param array $user_ids
 	*/
 	public function load_users(array $user_ids)
@@ -76,11 +79,41 @@ class phpbb_user_loader
 		// Do not load users we already have in $this->users
 		$user_ids = array_diff($user_ids, array_keys($this->users));
 
-		if (sizeof($user_ids))
+		if (!empty($user_ids))
 		{
-			$sql = 'SELECT *
-				FROM ' . $this->users_table . '
-				WHERE ' . $this->db->sql_in_set('user_id', $user_ids);
+			$sql_array = array(
+				'SELECT'	=> implode(', ', array(
+					'u.user_id',
+					'u.user_type',
+					'u.username',
+					'u.username_clean',
+					'u.user_email',
+					'u.user_posts',
+					'u.user_lang',
+					'u.user_rank',
+					'u.user_colour',
+					'u.user_avatar',
+					'u.user_avatar_type',
+					'u.user_avatar_width',
+					'u.user_avatar_height',
+				)),
+				'FROM'		=> array(
+					$this->users_table	=> 'u',
+				),
+				'WHERE'		=> $this->db->sql_in_set('u.user_id', $user_ids),
+			);
+
+			/**
+			* Alter what is loaded by the user loader
+			*
+			* @event core.user_loader.load_users
+			* @var array sql_array SQL array to be sent to sql_build_array
+			* @since 3.1-A1
+			*/
+			$vars = array($sql_array);
+			extract($phpbb_dispatcher->trigger_event('core.user_loader.load_users', compact($vars)));
+
+			$sql = $this->db->sql_build_query('SELECT', $sql_array);
 			$result = $this->db->sql_query($sql);
 
 			while ($row = $this->db->sql_fetchrow($result))
@@ -88,6 +121,15 @@ class phpbb_user_loader
 				$this->users[$row['user_id']] = $row;
 			}
 			$this->db->sql_freeresult($result);
+		}
+
+		// If the user does not exist, set their row to false so we do not query them again
+		foreach ($user_ids as $user_id)
+		{
+			if (!isset($this->users[$user_id]))
+			{
+				$this->users[$user_id] = false;
+			}
 		}
 	}
 
@@ -102,9 +144,40 @@ class phpbb_user_loader
 	*/
 	public function load_user_by_username($username)
 	{
-		$sql = 'SELECT *
-			FROM ' . $this->users_table . "
-			WHERE username_clean = '" . $this->db->sql_escape(utf8_clean_string($username)) . "'";
+		$sql_array = array(
+			'SELECT'	=> implode(', ', array(
+				'u.user_id',
+				'u.user_type',
+				'u.username',
+				'u.username_clean',
+				'u.user_email',
+				'u.user_posts',
+				'u.user_lang',
+				'u.user_rank',
+				'u.user_colour',
+				'u.user_avatar',
+				'u.user_avatar_type',
+				'u.user_avatar_width',
+				'u.user_avatar_height',
+			)),
+			'FROM'		=> array(
+				$this->users_table	=> 'u',
+			),
+			'WHERE'		=> "username_clean = '" . $this->db->sql_escape(utf8_clean_string($username)) . "'",
+		);
+
+		/**
+		* Alter what is loaded by the user loader when load_user_by_username is called
+		*
+		* @event core.user_loader.load_users
+		* @var string username Username requested
+		* @var array sql_array SQL array to be sent to sql_build_array
+		* @since 3.1-A1
+		*/
+		$vars = array($username, $sql_array);
+		extract($phpbb_dispatcher->trigger_event('core.user_loader.load_user_by_username', compact($vars)));
+
+		$sql = $this->db->sql_build_query('SELECT', $sql_array);
 		$result = $this->db->sql_query($sql);
 		$row = $this->db->sql_fetchrow($result);
 		$this->db->sql_freeresult($result);
@@ -124,18 +197,23 @@ class phpbb_user_loader
 	*
 	* @param int $user_id User ID of the user you want to retreive
 	* @param bool $query Should we query the database if this user has not yet been loaded?
-	* 						Typically this should be left as false and you should make sure
-	* 						you load users ahead of time with load_users()
-	* @return array|bool Row from the database of the user or Anonymous if the user wasn't loaded/does not exist
-	* 						or bool False if the anonymous user was not loaded
+	* 			Typically this should be left as false and you should make sure
+	* 			you load users ahead of time with load_users()
+	* @return array Row from the database of the user or Anonymous if the user wasn't loaded/does not exist
 	*/
 	public function get_user($user_id, $query = false)
 	{
 		if (isset($this->users[$user_id]))
 		{
+			if ($this->users[$user_id] === false)
+			{
+				return $this->get_user(ANONYMOUS);
+			}
+
 			return $this->users[$user_id];
 		}
-		else if ($query || $user_id == ANONYMOUS)
+
+		if ($query || $user_id == ANONYMOUS)
 		{
 			// Query them if we must (if ANONYMOUS is sent as the user_id and we have not loaded Anonymous yet, we must load Anonymous as a last resort)
 			$this->load_users(array($user_id));
@@ -147,7 +225,7 @@ class phpbb_user_loader
 	}
 
 	/**
-	* Get username
+	* Get a formatted username (for output to users)
 	*
 	* @param int $user_id User ID of the user you want to retreive the username for
 	* @param string $mode The mode to load (same as get_username_string). One of the following:
@@ -156,12 +234,14 @@ class phpbb_user_loader
 	* 			colour (for obtaining the user colour)
 	* 			full (for obtaining a html string representing a coloured link to the users profile)
 	* 			no_profile (the same as full but forcing no profile link)
-	* @param string $guest_username Optional parameter to specify the guest username. It will be used in favor of the GUEST language variable then.
-	* @param string $custom_profile_url Optional parameter to specify a profile url. The user id get appended to this url as &amp;u={user_id}
+	* @param string $guest_username Optional parameter to specify the guest username.
+	* 			It will be used in favor of the GUEST language variable then.
+	* @param string $custom_profile_url Optional parameter to specify a profile url.
+	* 			The user id get appended to this url as &amp;u={user_id}
 	* @param bool $query Should we query the database if this user has not yet been loaded?
-	* 						Typically this should be left as false and you should make sure
-	* 						you load users ahead of time with load_users()
-	* @return string
+	* 			Typically this should be left as false and you should make sure
+	* 			you load users ahead of time with load_users()
+	* @return string A formatted username based on what mode was desired
 	*/
 	public function get_username($user_id, $mode, $guest_username = false, $custom_profile_url = false, $query = false)
 	{
@@ -171,17 +251,22 @@ class phpbb_user_loader
 			return '';
 		}
 
-		return get_username_string($mode, $user['user_id'], $user['username'], $user['user_colour'], $guest_username, $custom_profile_url);
+		return get_username_string($mode,
+			$user['user_id'],
+			$user['username'],
+			$user['user_colour'],
+			$guest_username,
+			$custom_profile_url);
 	}
 
 	/**
-	* Get avatar
+	* Get HTML code to output the user's avatar
 	*
 	* @param int $user_id User ID of the user you want to retreive the avatar for
 	* @param bool $query Should we query the database if this user has not yet been loaded?
-	* 						Typically this should be left as false and you should make sure
-	* 						you load users ahead of time with load_users()
-	* @return string
+	* 			Typically this should be left as false and you should make sure
+	* 			you load users ahead of time with load_users()
+	* @return string HTML to output the avatar image
 	*/
 	public function get_avatar($user_id, $query = false)
 	{
@@ -196,16 +281,19 @@ class phpbb_user_loader
 			include($this->phpbb_root_path . 'includes/functions_display.' . $this->php_ext);
 		}
 
-		return get_user_avatar($user['user_avatar'], $user['user_avatar_type'], $user['user_avatar_width'], $user['user_avatar_height']);
+		return get_user_avatar($user['user_avatar'],
+			$user['user_avatar_type'],
+			$user['user_avatar_width'],
+			$user['user_avatar_height']);
 	}
 
 	/**
-	* Get rank
+	* Get user rank title and image
 	*
 	* @param int $user_id User ID of the user you want to retreive the rank for
 	* @param bool $query Should we query the database if this user has not yet been loaded?
-	* 						Typically this should be left as false and you should make sure
-	* 						you load users ahead of time with load_users()
+	* 			Typically this should be left as false and you should make sure
+	* 			you load users ahead of time with load_users()
 	* @return array Array with keys 'rank_title', 'rank_img', and 'rank_img_src'
 	*/
 	public function get_rank($user_id, $query = false)
@@ -222,13 +310,28 @@ class phpbb_user_loader
 		}
 
 		$rank = array(
-			'rank_title',
-			'rank_img',
-			'rank_img_src',
+			'rank_title' => '',
+			'rank_img' => '',
+			'rank_img_src' => '',
 		);
 
-		get_user_rank($user['user_rank'], (($user['user_id'] == ANONYMOUS) ? false : $user['user_posts']), $rank['rank_title'], $rank['rank_img'], $rank['rank_img_src']);
+		get_user_rank($user['user_rank'],
+			(($user['user_id'] == ANONYMOUS) ? false : $user['user_posts']),
+			$rank['rank_title'],
+			$rank['rank_img'],
+			$rank['rank_img_src']);
 
 		return $rank;
+	}
+
+	/**
+	* Flush the temporary users cache.
+	*
+	* This should only be used if the user data in the table was altered
+	* and it is absolutely necessary to reload the users
+	*/
+	public function flush()
+	{
+		$this->users = array();
 	}
 }
