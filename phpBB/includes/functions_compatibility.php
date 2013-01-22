79356f54415f901a8ff743817121216cbefb16ee
Marc Alexander <admin@m-a-styles.de>
2013-01-22 17:21:49 +0100

    [feature/avatars] Add compatibility function for get_user_avatar()
    PHPBB3-10018

---
+++ phpBB/includes/functions_compatibility.php
@@ -0,0 +1,41 @@
+<?php
+/**
+*
+* @package phpBB3
+* @copyright (c) 2005 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* Get user avatar
+*
+* @param string $avatar Users assigned avatar name
+* @param int $avatar_type Type of avatar
+* @param string $avatar_width Width of users avatar
+* @param string $avatar_height Height of users avatar
+* @param string $alt Optional language string for alt tag within image, can be a language key or text
+* @param bool $ignore_config Ignores the config-setting, to be still able to view the avatar in the UCP
+*
+* @return string Avatar image
+*/
+function get_user_avatar($avatar, $avatar_type, $avatar_width, $avatar_height, $alt = 'USER_AVATAR', $ignore_config = false)
+{
+	// map arguments to new function phpbb_get_avatar()
+	$row = array(
+		'avatar'		=> $avatar,
+		'avatar_type'	=> $avatar_type,
+		'avatar_width'	=> $avatar_width,
+		'avatar_height'	=> $avatar_height,
+	);
+
+	return phpbb_get_avatar($row, $alt, $ignore_config);
+}
