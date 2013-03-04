0ffabb3a43b217f5a6d4dc9f98d9982f8be28500
erangamapa <erangamapa@gmail.com>
2013-03-04 14:45:12 +0530

    [ticket/11104] Removed unnecessary spaces and lang message.
    Removed lang message 'DELETE_TOP_POST_CONFIRM' with unnecessary spaces
    in front of it.
    
    PHPBB3-11104

--- phpBB/language/en/mcp.php
+++ phpBB/language/en/mcp.php
@@ -74,7 +74,6 @@ $lang = array_merge($lang, array(
 	'DELETE_POSTS'				=> 'Delete posts',
 	'DELETE_POSTS_CONFIRM'		=> 'Are you sure you want to delete these posts?',
 	'DELETE_ENTIRE_TOPIC'		=> 'Delete entire topic',
-    'DELETE_TOP_POST_CONFIRM'	=> 'Are you sure you want to delete this top post in the topic?',
 	'DELETE_POST_CONFIRM'		=> 'Are you sure you want to delete this post?',
 	'DELETE_REPORT'				=> 'Delete report',
 	'DELETE_REPORT_CONFIRM'		=> 'Are you sure you want to delete the selected report?',
