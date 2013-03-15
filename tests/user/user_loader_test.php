ef1caf69332bed24a12fdeda98711f5012c85ebd
Andreas Fischer <bantu@phpbb.com>
2013-03-15 01:32:33 +0100

    [ticket/11441] Use setUp() in phpbb_user_loader_test.
    PHPBB3-11441

--- tests/user/user_loader_test.php
+++ tests/user/user_loader_test.php
@@ -11,38 +11,45 @@ include_once(__DIR__ . '/../../phpBB/includes/utf/utf_tools.php');
 
 class phpbb_user_loader_test extends phpbb_database_test_case
 {
+	protected $db;
+	protected $user_loader;
+
 	public function getDataSet()
 	{
 		return $this->createXMLDataSet(dirname(__FILE__) . '/fixtures/user_loader.xml');
 	}
 
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->db = $this->new_dbal();
+		$this->user_loader = new phpbb_user_loader($this->db, __DIR__ . '/../../phpBB/', 'php', 'phpbb_users');
+	}
+
 	public function test_user_loader()
 	{
-		$db = $this->new_dbal();
+		$this->user_loader->load_users(array(2));
 
-		$user_loader = new phpbb_user_loader($db, __DIR__ . '/../../phpBB/', 'php', 'phpbb_users');
-
-		$user_loader->load_users(array(2));
-
-		$user = $user_loader->get_user(1);
+		$user = $this->user_loader->get_user(1);
 		$this->assertEquals(1, $user['user_id']);
 		$this->assertEquals('Guest', $user['username']);
 
-		$user = $user_loader->get_user(2);
+		$user = $this->user_loader->get_user(2);
 		$this->assertEquals(2, $user['user_id']);
 		$this->assertEquals('Admin', $user['username']);
 
 		// Not loaded
-		$user = $user_loader->get_user(3);
+		$user = $this->user_loader->get_user(3);
 		$this->assertEquals(1, $user['user_id']);
 		$this->assertEquals('Guest', $user['username']);
 
-		$user = $user_loader->get_user(3, true);
+		$user = $this->user_loader->get_user(3, true);
 		$this->assertEquals(3, $user['user_id']);
 		$this->assertEquals('Test', $user['username']);
 
-		$user_id = $user_loader->load_user_by_username('Test');
-		$user = $user_loader->get_user($user_id);
+		$user_id = $this->user_loader->load_user_by_username('Test');
+		$user = $this->user_loader->get_user($user_id);
 		$this->assertEquals(3, $user['user_id']);
 		$this->assertEquals('Test', $user['username']);
 	}
