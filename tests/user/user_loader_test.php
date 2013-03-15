7e0d008a17c47f90575dabd6f981685bcc83ac15
Andreas Fischer <bantu@phpbb.com>
2013-03-15 01:36:05 +0100

    [ticket/11441] Split user loader tests into multiple test methods.
    PHPBB3-11441

--- tests/user/user_loader_test.php
+++ tests/user/user_loader_test.php
@@ -27,7 +27,7 @@ class phpbb_user_loader_test extends phpbb_database_test_case
 		$this->user_loader = new phpbb_user_loader($this->db, __DIR__ . '/../../phpBB/', 'php', 'phpbb_users');
 	}
 
-	public function test_user_loader()
+	public function test_load_get()
 	{
 		$this->user_loader->load_users(array(2));
 
@@ -38,8 +38,12 @@ class phpbb_user_loader_test extends phpbb_database_test_case
 		$user = $this->user_loader->get_user(2);
 		$this->assertEquals(2, $user['user_id']);
 		$this->assertEquals('Admin', $user['username']);
+	}
 
-		// Not loaded
+	public function test_load_get_unloaded()
+	{
+		$this->user_loader->load_users(array(2));
+
 		$user = $this->user_loader->get_user(3);
 		$this->assertEquals(1, $user['user_id']);
 		$this->assertEquals('Guest', $user['username']);
@@ -47,7 +51,10 @@ class phpbb_user_loader_test extends phpbb_database_test_case
 		$user = $this->user_loader->get_user(3, true);
 		$this->assertEquals(3, $user['user_id']);
 		$this->assertEquals('Test', $user['username']);
+	}
 
+	public function test_load_user_by_username()
+	{
 		$user_id = $this->user_loader->load_user_by_username('Test');
 		$user = $this->user_loader->get_user($user_id);
 		$this->assertEquals(3, $user['user_id']);
