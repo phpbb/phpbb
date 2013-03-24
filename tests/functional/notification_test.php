d3decaeedff926f2945285b713e85c09c3de9daf
Andreas Fischer <bantu@phpbb.com>
2013-03-24 23:42:13 +0100

    [ticket/11460] Add default behaviour tests for notification and email types.
    PHPBB3-11460

--- tests/functional/notification_test.php
+++ tests/functional/notification_test.php
@@ -15,12 +15,22 @@ class phpbb_functional_notification_test extends phpbb_functional_test_case
 	static public function user_subscription_data()
 	{
 		return array(
+			// Rows inserted by phpBB/install/schemas/schema_data.sql
+			// Also see PHPBB3-11460
 			array('post_notification', true),
 			array('topic_notification', true),
-
-			// PHPBB3-11460
 			array('post_email', true),
 			array('topic_email', true),
+
+			// Default behaviour for in-board notifications:
+			// If user did not opt-out, in-board notifications are on.
+			array('bookmark_notification', true),
+			array('quote_notification', true),
+
+			// Default behaviour for email notifications:
+			// If user did not opt-in, email notifications are off.
+			array('bookmark_email', false),
+			array('quote_email', false),
 		);
 	}
 
