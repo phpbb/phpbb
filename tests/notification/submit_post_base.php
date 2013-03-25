060876e627db127c72a953d37e83f5176a085e5d
Joas Schilling <nickvergessen@gmx.de>
2013-03-25 15:16:29 +0100

    [ticket/11405] Add a base class to avoid duplicated setUp() code
    PHPBB3-11405

---
+++ tests/notification/submit_post_base.php
@@ -0,0 +1,94 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+require_once dirname(__FILE__) . '/../../phpBB/includes/functions.php';
+require_once dirname(__FILE__) . '/../../phpBB/includes/functions_content.php';
+require_once dirname(__FILE__) . '/../../phpBB/includes/functions_posting.php';
+require_once dirname(__FILE__) . '/../../phpBB/includes/utf/utf_tools.php';
+
+class phpbb_notification_submit_post_base extends phpbb_database_test_case
+{
+	protected $notifications, $db, $container, $user, $config, $auth, $cache;
+
+	public function getDataSet()
+	{
+		return $this->createXMLDataSet(dirname(__FILE__) . '/fixtures/submit_post_notification.xml');
+	}
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		global $auth, $cache, $config, $db, $phpbb_container, $phpbb_dispatcher, $user, $request, $phpEx, $phpbb_root_path;
+
+		// Database
+		$this->db = $this->new_dbal();
+		$db = $this->db;
+
+		// Cache
+		$cache = new phpbb_mock_cache();
+
+		// Auth
+		$auth = $this->getMock('phpbb_auth');
+		$auth->expects($this->any())
+			->method('acl_get')
+			->with($this->stringContains('_'),
+				$this->anything())
+			->will($this->returnValueMap(array(
+				array('f_noapprove', 1, true),
+				array('f_postcount', 1, true),
+				array('m_edit', 1, false),
+			)));
+
+		// Config
+		$config = new phpbb_config(array('num_topics' => 1,'num_posts' => 1,));
+		set_config(null, null, null, $config);
+		set_config_count(null, null, null, $config);
+
+		// Event dispatcher
+		$phpbb_dispatcher = new phpbb_mock_event_dispatcher();
+
+		// User
+		$user = $this->getMock('phpbb_user');
+		$user->ip = '';
+		$user->data = array(
+			'user_id'		=> 2,
+			'username'		=> 'user-name',
+			'is_registered'	=> true,
+			'user_colour'	=> '',
+		);
+
+		// Request
+		$type_cast_helper = $this->getMock('phpbb_request_type_cast_helper_interface');
+		$request = $this->getMock('phpbb_request');
+
+		// Container
+		$phpbb_container = new phpbb_mock_container_builder();
+
+		$user_loader = new phpbb_user_loader($db, $phpbb_root_path, '.' . $phpEx, USERS_TABLE);
+
+		// Notification Manager
+		$phpbb_notifications = new phpbb_notification_manager(array(), array(),
+			$phpbb_container, $user_loader, $db, $user,
+			$phpbb_root_path, '.' . $phpEx,
+			NOTIFICATION_TYPES_TABLE, NOTIFICATIONS_TABLE, USER_NOTIFICATIONS_TABLE);
+		$phpbb_container->set('notification_manager', $phpbb_notifications);
+
+		// Notification Types
+		$notification_types = array('quote', 'bookmark', 'post');
+		foreach ($notification_types as $type)
+		{
+			$class_name = 'phpbb_notification_type_' . $type;
+			$phpbb_container->set('notification.type.' . $type, new $class_name(
+				$user_loader, $db, $cache, $user, $auth, $config,
+				$phpbb_root_path, '.' . $phpEx,
+				NOTIFICATION_TYPES_TABLE, NOTIFICATIONS_TABLE, USER_NOTIFICATIONS_TABLE));
+		}
+	}
+}
