5c24474c2a85d159d2b76d4483c62ab1e9861222
Joas Schilling <nickvergessen@gmx.de>
2013-04-11 22:11:52 +0200

    [ticket/11494] Fix memberlist leaders functional tests
    The scope of the crawler should be reduced to the specific table
    
    PHPBB3-11494

--- tests/functional/memberlist_test.php
+++ tests/functional/memberlist_test.php
@@ -46,33 +46,49 @@ class phpbb_functional_memberlist_test extends phpbb_functional_test_case
 		$this->login();
 		$this->create_user('memberlist-test-moderator');
 
-		// Admin should be listed, user not
 		$crawler = $this->request('GET', 'memberlist.php?mode=leaders&sid=' . $this->sid);
 		$this->assert_response_success();
-		$this->assertContains('admin', $crawler->text());
-		$this->assertNotContains('memberlist-test-user', $crawler->text());
-		$this->assertNotContains('memberlist-test-moderator', $crawler->text());
+
+		// Admin in admin group, but not in moderators
+		$this->assertContains('admin', $crawler->filter('.forumbg-table')->eq(0)->text());
+		$this->assertNotContains('admin', $crawler->filter('.forumbg-table')->eq(1)->text());
+
+		// memberlist-test-user in neither group
+		$this->assertNotContains('memberlist-test-user', $crawler->filter('.forumbg-table')->eq(0)->text());
+		$this->assertNotContains('memberlist-test-user', $crawler->filter('.forumbg-table')->eq(1)->text());
+
+		// memberlist-test-moderator in neither group
+		$this->assertNotContains('memberlist-test-moderator', $crawler->filter('.forumbg-table')->eq(0)->text());
+		$this->assertNotContains('memberlist-test-moderator', $crawler->filter('.forumbg-table')->eq(1)->text());
 	}
 
 	public function test_leaders_remove_users()
 	{
 		$this->login();
 
-		// Remove admin from admins
+		// Remove admin from admins, but is now in moderators
 		$this->remove_user_group('ADMINISTRATORS', array('admin'));
 		$crawler = $this->request('GET', 'memberlist.php?mode=leaders&sid=' . $this->sid);
 		$this->assert_response_success();
-		$this->assertContains('admin', $crawler->text());
+		$this->assertNotContains('admin', $crawler->filter('.forumbg-table')->eq(0)->text());
+		$this->assertContains('admin', $crawler->filter('.forumbg-table')->eq(1)->text());
+
+		// Remove admin from moderators, should not be visible anymore
+		$this->remove_user_group('GLOBAL_MODERATORS', array('admin'));
+		$crawler = $this->request('GET', 'memberlist.php?mode=leaders&sid=' . $this->sid);
+		$this->assertNotContains('admin', $crawler->filter('.forumbg-table')->eq(0)->text());
+		$this->assertNotContains('admin', $crawler->filter('.forumbg-table')->eq(1)->text());
 	}
 
 	public function test_leaders_add_users()
 	{
 		$this->login();
 
-		// Add mod to moderators
+		// Add memberlist-test-moderator to moderators
 		$this->add_user_group('GLOBAL_MODERATORS', array('memberlist-test-moderator'));
 		$crawler = $this->request('GET', 'memberlist.php?mode=leaders&sid=' . $this->sid);
 		$this->assert_response_success();
-		$this->assertContains('memberlist-test-moderator', $crawler->text());
+		$this->assertNotContains('memberlist-test-moderator', $crawler->filter('.forumbg-table')->eq(0)->text());
+		$this->assertContains('memberlist-test-moderator', $crawler->filter('.forumbg-table')->eq(1)->text());
 	}
 }
