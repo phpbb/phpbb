481893df5cfca4ebde72ac86cd07df615e25588c
Joas Schilling <nickvergessen@gmx.de>
2013-04-22 22:14:15 +0200

    [ticket/11415] Remove functions from manager aswell as migrator dependency
    PHPBB3-11415

--- tests/extension/manager_test.php
+++ tests/extension/manager_test.php
@@ -10,6 +10,7 @@
 require_once dirname(__FILE__) . '/ext/bar/ext.php';
 require_once dirname(__FILE__) . '/ext/foo/ext.php';
 require_once dirname(__FILE__) . '/ext/vendor/moo/ext.php';
+require_once dirname(__FILE__) . '/../../phpBB/includes/db/db_tools.php';
 
 class phpbb_extension_manager_test extends phpbb_database_test_case
 {
@@ -24,7 +25,6 @@ class phpbb_extension_manager_test extends phpbb_database_test_case
 	protected function setUp()
 	{
 		parent::setUp();
-
 		$this->extension_manager = $this->create_extension_manager();
 	}
 
@@ -43,43 +43,6 @@ class phpbb_extension_manager_test extends phpbb_database_test_case
 		$this->assertEquals(array('foo', 'vendor/moo'), array_keys($this->extension_manager->all_configured()));
 	}
 
-	public function test_enable()
-	{
-		phpbb_ext_bar_ext::$state = 0;
-
-		$this->assertEquals(array('foo'), array_keys($this->extension_manager->all_enabled()));
-		$this->extension_manager->enable('bar');
-		$this->assertEquals(array('bar', 'foo'), array_keys($this->extension_manager->all_enabled()));
-		$this->assertEquals(array('bar', 'foo', 'vendor/moo'), array_keys($this->extension_manager->all_configured()));
-
-		$this->assertEquals(4, phpbb_ext_bar_ext::$state);
-	}
-
-	public function test_disable()
-	{
-		phpbb_ext_foo_ext::$disabled = false;
-
-		$this->assertEquals(array('foo'), array_keys($this->extension_manager->all_enabled()));
-		$this->extension_manager->disable('foo');
-		$this->assertEquals(array(), array_keys($this->extension_manager->all_enabled()));
-		$this->assertEquals(array('foo', 'vendor/moo'), array_keys($this->extension_manager->all_configured()));
-
-		$this->assertTrue(phpbb_ext_foo_ext::$disabled);
-	}
-
-	public function test_purge()
-	{
-		phpbb_ext_vendor_moo_ext::$purged = false;
-
-		$this->assertEquals(array('foo'), array_keys($this->extension_manager->all_enabled()));
-		$this->assertEquals(array('foo', 'vendor/moo'), array_keys($this->extension_manager->all_configured()));
-		$this->extension_manager->purge('vendor/moo');
-		$this->assertEquals(array('foo'), array_keys($this->extension_manager->all_enabled()));
-		$this->assertEquals(array('foo'), array_keys($this->extension_manager->all_configured()));
-
-		$this->assertTrue(phpbb_ext_vendor_moo_ext::$purged);
-	}
-
 	public function test_enabled_no_cache()
 	{
 		$extension_manager = $this->create_extension_manager(false);
@@ -89,29 +52,15 @@ class phpbb_extension_manager_test extends phpbb_database_test_case
 
 	protected function create_extension_manager($with_cache = true)
 	{
-
 		$config = new phpbb_config(array());
 		$db = $this->new_dbal();
 		$db_tools = new phpbb_db_tools($db);
-		$phpbb_root_path = __DIR__ . './../../phpBB/';
 		$php_ext = 'php';
-		$table_prefix = 'phpbb_';
 
-		$migrator = new phpbb_db_migrator(
-			$config,
-			$db,
-			$db_tools,
-			'phpbb_migrations',
-			$phpbb_root_path,
-			$php_ext,
-			$table_prefix,
-			array()
-		);
 		return new phpbb_extension_manager(
 			new phpbb_mock_container_builder(),
 			$db,
 			$config,
-			$migrator,
 			'phpbb_ext',
 			dirname(__FILE__) . '/',
 			'.' . $php_ext,
