fe02218a2d2f5634e22e494f7b85f9701fa57623
Joas Schilling <nickvergessen@gmx.de>
2013-04-30 18:11:24 +0200

    [ticket/11495] Remove forum_parents from tests where it is not required
    PHPBB3-11495

--- tests/tree/nestedset_forum_insert_delete_test.php
+++ tests/tree/nestedset_forum_insert_delete_test.php
@@ -15,26 +15,26 @@ class phpbb_tests_tree_nestedset_forum_add_remove_test extends phpbb_tests_tree_
 	{
 		return array(
 			array(1, array(1, 2, 3), array(
-				array('forum_id' => 4, 'parent_id' => 0, 'left_id' => 1, 'right_id' => 6, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 5, 'parent_id' => 4, 'left_id' => 2, 'right_id' => 5, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 6, 'parent_id' => 5, 'left_id' => 3, 'right_id' => 4, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 7, 'parent_id' => 0, 'left_id' => 7, 'right_id' => 16, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 8, 'parent_id' => 7, 'left_id' => 8, 'right_id' => 9, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 9, 'parent_id' => 7, 'left_id' => 10, 'right_id' => 13, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 10, 'parent_id' => 9, 'left_id' => 11, 'right_id' => 12, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 11, 'parent_id' => 7, 'left_id' => 14, 'right_id' => 15, 'forum_parents' => 'a:0:{}'),
+				array('forum_id' => 4, 'parent_id' => 0, 'left_id' => 1, 'right_id' => 6),
+				array('forum_id' => 5, 'parent_id' => 4, 'left_id' => 2, 'right_id' => 5),
+				array('forum_id' => 6, 'parent_id' => 5, 'left_id' => 3, 'right_id' => 4),
+				array('forum_id' => 7, 'parent_id' => 0, 'left_id' => 7, 'right_id' => 16),
+				array('forum_id' => 8, 'parent_id' => 7, 'left_id' => 8, 'right_id' => 9),
+				array('forum_id' => 9, 'parent_id' => 7, 'left_id' => 10, 'right_id' => 13),
+				array('forum_id' => 10, 'parent_id' => 9, 'left_id' => 11, 'right_id' => 12),
+				array('forum_id' => 11, 'parent_id' => 7, 'left_id' => 14, 'right_id' => 15),
 			)),
 			array(2, array(2), array(
-				array('forum_id' => 1, 'parent_id' => 0, 'left_id' => 1, 'right_id' => 4, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 3, 'parent_id' => 1, 'left_id' => 2, 'right_id' => 3, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 4, 'parent_id' => 0, 'left_id' => 5, 'right_id' => 10, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 5, 'parent_id' => 4, 'left_id' => 6, 'right_id' => 9, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 6, 'parent_id' => 5, 'left_id' => 7, 'right_id' => 8, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 7, 'parent_id' => 0, 'left_id' => 11, 'right_id' => 20, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 8, 'parent_id' => 7, 'left_id' => 12, 'right_id' => 13, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 9, 'parent_id' => 7, 'left_id' => 14, 'right_id' => 17, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 10, 'parent_id' => 9, 'left_id' => 15, 'right_id' => 16, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 11, 'parent_id' => 7, 'left_id' => 18, 'right_id' => 19, 'forum_parents' => 'a:0:{}'),
+				array('forum_id' => 1, 'parent_id' => 0, 'left_id' => 1, 'right_id' => 4),
+				array('forum_id' => 3, 'parent_id' => 1, 'left_id' => 2, 'right_id' => 3),
+				array('forum_id' => 4, 'parent_id' => 0, 'left_id' => 5, 'right_id' => 10),
+				array('forum_id' => 5, 'parent_id' => 4, 'left_id' => 6, 'right_id' => 9),
+				array('forum_id' => 6, 'parent_id' => 5, 'left_id' => 7, 'right_id' => 8),
+				array('forum_id' => 7, 'parent_id' => 0, 'left_id' => 11, 'right_id' => 20),
+				array('forum_id' => 8, 'parent_id' => 7, 'left_id' => 12, 'right_id' => 13),
+				array('forum_id' => 9, 'parent_id' => 7, 'left_id' => 14, 'right_id' => 17),
+				array('forum_id' => 10, 'parent_id' => 9, 'left_id' => 15, 'right_id' => 16),
+				array('forum_id' => 11, 'parent_id' => 7, 'left_id' => 18, 'right_id' => 19),
 			)),
 		);
 	}
@@ -46,7 +46,7 @@ class phpbb_tests_tree_nestedset_forum_add_remove_test extends phpbb_tests_tree_
 	{
 		$this->assertEquals($expected_deleted, $this->set->delete($forum_id));
 
-		$result = $this->db->sql_query("SELECT forum_id, parent_id, left_id, right_id, forum_parents
+		$result = $this->db->sql_query("SELECT forum_id, parent_id, left_id, right_id
 			FROM phpbb_forums
 			ORDER BY left_id, forum_id ASC");
 		$this->assertEquals($expected, $this->db->sql_fetchrowset($result));
@@ -64,21 +64,21 @@ class phpbb_tests_tree_nestedset_forum_add_remove_test extends phpbb_tests_tree_
 				'right_id'		=> 24,
 				'forum_parents'	=> '',
 			), array(
-				array('forum_id' => 1, 'parent_id' => 0, 'left_id' => 1, 'right_id' => 6, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 2, 'parent_id' => 1, 'left_id' => 2, 'right_id' => 3, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 3, 'parent_id' => 1, 'left_id' => 4, 'right_id' => 5, 'forum_parents' => 'a:0:{}'),
+				array('forum_id' => 1, 'parent_id' => 0, 'left_id' => 1, 'right_id' => 6),
+				array('forum_id' => 2, 'parent_id' => 1, 'left_id' => 2, 'right_id' => 3),
+				array('forum_id' => 3, 'parent_id' => 1, 'left_id' => 4, 'right_id' => 5),
 
-				array('forum_id' => 4, 'parent_id' => 0, 'left_id' => 7, 'right_id' => 12, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 5, 'parent_id' => 4, 'left_id' => 8, 'right_id' => 11, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 6, 'parent_id' => 5, 'left_id' => 9, 'right_id' => 10, 'forum_parents' => 'a:0:{}'),
+				array('forum_id' => 4, 'parent_id' => 0, 'left_id' => 7, 'right_id' => 12),
+				array('forum_id' => 5, 'parent_id' => 4, 'left_id' => 8, 'right_id' => 11),
+				array('forum_id' => 6, 'parent_id' => 5, 'left_id' => 9, 'right_id' => 10),
 
-				array('forum_id' => 7, 'parent_id' => 0, 'left_id' => 13, 'right_id' => 22, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 8, 'parent_id' => 7, 'left_id' => 14, 'right_id' => 15, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 9, 'parent_id' => 7, 'left_id' => 16, 'right_id' => 19, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 10, 'parent_id' => 9, 'left_id' => 17, 'right_id' => 18, 'forum_parents' => 'a:0:{}'),
-				array('forum_id' => 11, 'parent_id' => 7, 'left_id' => 20, 'right_id' => 21, 'forum_parents' => 'a:0:{}'),
+				array('forum_id' => 7, 'parent_id' => 0, 'left_id' => 13, 'right_id' => 22),
+				array('forum_id' => 8, 'parent_id' => 7, 'left_id' => 14, 'right_id' => 15),
+				array('forum_id' => 9, 'parent_id' => 7, 'left_id' => 16, 'right_id' => 19),
+				array('forum_id' => 10, 'parent_id' => 9, 'left_id' => 17, 'right_id' => 18),
+				array('forum_id' => 11, 'parent_id' => 7, 'left_id' => 20, 'right_id' => 21),
 
-				array('forum_id' => 12, 'parent_id' => 0, 'left_id' => 23, 'right_id' => 24, 'forum_parents' => ''),
+				array('forum_id' => 12, 'parent_id' => 0, 'left_id' => 23, 'right_id' => 24),
 			)),
 		);
 	}
@@ -93,7 +93,7 @@ class phpbb_tests_tree_nestedset_forum_add_remove_test extends phpbb_tests_tree_
 			'forum_rules'	=> '',
 		)));
 
-		$result = $this->db->sql_query('SELECT forum_id, parent_id, left_id, right_id, forum_parents
+		$result = $this->db->sql_query('SELECT forum_id, parent_id, left_id, right_id
 			FROM phpbb_forums
 			ORDER BY left_id, forum_id ASC');
 		$this->assertEquals($expected, $this->db->sql_fetchrowset($result));
