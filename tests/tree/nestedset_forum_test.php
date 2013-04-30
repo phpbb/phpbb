d1d59dc5cc3ca779c5e7a971659c8aca5e28215d
Joas Schilling <nickvergessen@gmx.de>
2013-04-30 18:33:28 +0200

    [ticket/11495] Remove unneccessary values from tests
    PHPBB3-11495

--- tests/tree/nestedset_forum_test.php
+++ tests/tree/nestedset_forum_test.php
@@ -46,61 +46,61 @@ class pphpbb_tests_tree_nestedset_forum_test extends phpbb_tests_tree_nestedset_
 	public function get_sql_where_data()
 	{
 		return array(
-			array('SELECT forum_id, parent_id, left_id, right_id
+			array('SELECT forum_id
 				FROM phpbb_forums
 				%s
-				ORDER BY left_id, forum_id ASC',
+				ORDER BY forum_id ASC',
 				'WHERE', '', array(
-				array('forum_id' => 1, 'parent_id' => 0, 'left_id' => 1, 'right_id' => 6),
-				array('forum_id' => 2, 'parent_id' => 1, 'left_id' => 2, 'right_id' => 3),
-				array('forum_id' => 3, 'parent_id' => 1, 'left_id' => 4, 'right_id' => 5),
+				array('forum_id' => 1),
+				array('forum_id' => 2),
+				array('forum_id' => 3),
 
-				array('forum_id' => 4, 'parent_id' => 0, 'left_id' => 7, 'right_id' => 12),
-				array('forum_id' => 5, 'parent_id' => 4, 'left_id' => 8, 'right_id' => 11),
-				array('forum_id' => 6, 'parent_id' => 5, 'left_id' => 9, 'right_id' => 10),
+				array('forum_id' => 4),
+				array('forum_id' => 5),
+				array('forum_id' => 6),
 
-				array('forum_id' => 7, 'parent_id' => 0, 'left_id' => 13, 'right_id' => 22),
-				array('forum_id' => 8, 'parent_id' => 7, 'left_id' => 14, 'right_id' => 15),
-				array('forum_id' => 9, 'parent_id' => 7, 'left_id' => 16, 'right_id' => 19),
-				array('forum_id' => 10, 'parent_id' => 9, 'left_id' => 17, 'right_id' => 18),
-				array('forum_id' => 11, 'parent_id' => 7, 'left_id' => 20, 'right_id' => 21),
+				array('forum_id' => 7),
+				array('forum_id' => 8),
+				array('forum_id' => 9),
+				array('forum_id' => 10),
+				array('forum_id' => 11),
 			)),
-			array('SELECT f.forum_id, f.parent_id, f.left_id, f.right_id
+			array('SELECT f.forum_id
 				FROM phpbb_forums f
 				%s
-				ORDER BY f.left_id, f.forum_id ASC',
+				ORDER BY f.forum_id ASC',
 				'WHERE', 'f.', array(
-				array('forum_id' => 1, 'parent_id' => 0, 'left_id' => 1, 'right_id' => 6),
-				array('forum_id' => 2, 'parent_id' => 1, 'left_id' => 2, 'right_id' => 3),
-				array('forum_id' => 3, 'parent_id' => 1, 'left_id' => 4, 'right_id' => 5),
+				array('forum_id' => 1),
+				array('forum_id' => 2),
+				array('forum_id' => 3),
 
-				array('forum_id' => 4, 'parent_id' => 0, 'left_id' => 7, 'right_id' => 12),
-				array('forum_id' => 5, 'parent_id' => 4, 'left_id' => 8, 'right_id' => 11),
-				array('forum_id' => 6, 'parent_id' => 5, 'left_id' => 9, 'right_id' => 10),
+				array('forum_id' => 4),
+				array('forum_id' => 5),
+				array('forum_id' => 6),
 
-				array('forum_id' => 7, 'parent_id' => 0, 'left_id' => 13, 'right_id' => 22),
-				array('forum_id' => 8, 'parent_id' => 7, 'left_id' => 14, 'right_id' => 15),
-				array('forum_id' => 9, 'parent_id' => 7, 'left_id' => 16, 'right_id' => 19),
-				array('forum_id' => 10, 'parent_id' => 9, 'left_id' => 17, 'right_id' => 18),
-				array('forum_id' => 11, 'parent_id' => 7, 'left_id' => 20, 'right_id' => 21),
+				array('forum_id' => 7),
+				array('forum_id' => 8),
+				array('forum_id' => 9),
+				array('forum_id' => 10),
+				array('forum_id' => 11),
 			)),
-			array('SELECT forum_id, parent_id, left_id, right_id
+			array('SELECT forum_id
 				FROM phpbb_forums
 				WHERE forum_id < 4 %s
-				ORDER BY left_id, forum_id ASC',
+				ORDER BY forum_id ASC',
 				'AND', '', array(
-				array('forum_id' => 1, 'parent_id' => 0, 'left_id' => 1, 'right_id' => 6),
-				array('forum_id' => 2, 'parent_id' => 1, 'left_id' => 2, 'right_id' => 3),
-				array('forum_id' => 3, 'parent_id' => 1, 'left_id' => 4, 'right_id' => 5),
+				array('forum_id' => 1),
+				array('forum_id' => 2),
+				array('forum_id' => 3),
 			)),
-			array('SELECT f.forum_id, f.parent_id, f.left_id, f.right_id
+			array('SELECT f.forum_id
 				FROM phpbb_forums f
 				WHERE f.forum_id < 4 %s
-				ORDER BY f.left_id, f.forum_id ASC',
+				ORDER BY f.forum_id ASC',
 				'AND', 'f.', array(
-				array('forum_id' => 1, 'parent_id' => 0, 'left_id' => 1, 'right_id' => 6),
-				array('forum_id' => 2, 'parent_id' => 1, 'left_id' => 2, 'right_id' => 3),
-				array('forum_id' => 3, 'parent_id' => 1, 'left_id' => 4, 'right_id' => 5),
+				array('forum_id' => 1),
+				array('forum_id' => 2),
+				array('forum_id' => 3),
 			)),
 		);
 	}
