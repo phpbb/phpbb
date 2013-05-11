48e1be58db26ba09f7421082038f1f6353a38e91
OpenShift guest <dachebodt@gmail.com>
2013-05-11 02:38:40 -0400

    [ticket/11458] Update functional test
    Show that the phpbb permission lanuage file is being included
    
    PHPBB3-11458

--- tests/functional/extension_permission_lang_test.php
+++ tests/functional/extension_permission_lang_test.php
@@ -108,6 +108,11 @@ class phpbb_functional_extension_permission_lang_test extends phpbb_functional_t
 		$form->setValues($data);
 		$crawler = $this->client->submit($form);
 		$this->assert_response_success();
+
+		// language from language/en/acp/permissions_phpbb.php
+		$this->assertContains('Can attach files', $crawler->filter('body')->text());
+
+		// language from ext/foo/bar/language/en/permissions_foo.php
 		$this->assertContains('Can view foo', $crawler->filter('body')->text());
 	}
 }
