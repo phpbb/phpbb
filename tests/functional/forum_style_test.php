c2f5ba69f0fb03a30d4f3cbf1e6d49e019edea7c
Vjacheslav Trushkin <cyberalien@gmail.com>
2013-05-12 12:43:15 +0300

    [ticket/develop/10772] Fix copyright year
    PHPBB3-10772

--- tests/functional/forum_style_test.php
+++ tests/functional/forum_style_test.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package testing
-* @copyright (c) 2011 phpBB Group
+* @copyright (c) 2013 phpBB Group
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
