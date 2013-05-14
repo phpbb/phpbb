a0ecfe36a591001ca0996884da3038b3db682991
Joas Schilling <nickvergessen@gmx.de>
2013-05-14 15:55:05 +0200

    [ticket/11539] Add unit tests for language_select()
    PHPBB3-11539

---
+++ tests/functions/language_select_test.php
@@ -0,0 +1,38 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+require_once dirname(__FILE__) . '/../../phpBB/includes/functions.php';
+
+class phpbb_functions_language_select_test extends phpbb_database_test_case
+{
+	public function getDataSet()
+	{
+		return $this->createXMLDataSet(dirname(__FILE__).'/fixtures/language_select.xml');
+	}
+
+	static public function language_select_data()
+	{
+		return array(
+			array('', '<option value="en">English</option><option value="de">Deutsch</option>'),
+			array('en', '<option value="en" selected="selected">English</option><option value="de">Deutsch</option>'),
+			array('de', '<option value="en">English</option><option value="de" selected="selected">Deutsch</option>'),
+		);
+	}
+
+	/**
+	* @dataProvider language_select_data
+	*/
+	public function test_language_select($default, $expected)
+	{
+		global $db;
+		$db = $this->new_dbal();
+
+		$this->assertEquals($expected, language_select($default));
+	}
+}
