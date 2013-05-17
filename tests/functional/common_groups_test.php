0a5988ec1f3d72afb17b87d6cd74f60b646bae42
Marc Alexander <admin@m-a-styles.de>
2013-05-17 12:32:09 +0200

    [ticket/11538] Use abstract class for functional test cases for group colour
    PHPBB3-11538

---
+++ tests/functional/common_groups_test.php
@@ -0,0 +1,50 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @group functional
+*/
+abstract class phpbb_functional_common_groups_test extends phpbb_functional_test_case
+{
+	abstract protected function get_url();
+
+	public function groups_manage_test_data()
+	{
+		return array(
+			array('#AA0000', 'WRONG_DATA_COLOUR'),
+			array('AA0000', 'GROUP_UPDATED'),
+			array('AA0000v', 'WRONG_DATA_COLOUR'),
+			array('vAA0000', 'WRONG_DATA_COLOUR'),
+			array('AAG000','WRONG_DATA_COLOUR'),
+			array('a00', 'GROUP_UPDATED'),
+			array('ag0', 'WRONG_DATA_COLOUR'),
+			array('#aa0', 'WRONG_DATA_COLOUR'),
+			array('AA0000 ', 'GROUP_UPDATED'),
+			array('AA0000 abf', 'WRONG_DATA_COLOUR'),
+			array('AA0000 AA0000', 'WRONG_DATA_COLOUR'),
+		);
+	}
+
+	/**
+	* @dataProvider groups_manage_test_data
+	*/
+	public function test_groups_manage($input, $expected)
+	{
+		$this->login();
+		$this->admin_login();
+		$this->add_lang(array('ucp', 'acp/groups'));
+
+		$crawler = $this->request('GET', $this->get_url() . '&sid=' . $this->sid);
+		$this->assert_response_success();
+		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
+		$form['group_colour']->setValue($input);
+		$crawler = $this->client->submit($form);
+		$this->assertContains($this->lang($expected), $crawler->text());
+	}
+}
--- tests/functional/ucp_groups_test.php
+++ tests/functional/ucp_groups_test.php
@@ -7,41 +7,15 @@
 *
 */
 
+require_once dirname(__FILE__) . '/common_groups_test.php';
+
 /**
 * @group functional
 */
-class phpbb_functional_ucp_groups_test extends phpbb_functional_test_case
+class phpbb_functional_ucp_groups_test extends phpbb_functional_common_groups_test
 {
-	public function groups_manage_test_data()
+	protected function get_url()
 	{
-		return array(
-			array('#AA0000', 'WRONG_DATA_COLOUR'),
-			array('AA0000', 'GROUP_UPDATED'),
-			array('AA0000v', 'WRONG_DATA_COLOUR'),
-			array('vAA0000', 'WRONG_DATA_COLOUR'),
-			array('AAG000','WRONG_DATA_COLOUR'),
-			array('a00', 'GROUP_UPDATED'),
-			array('ag0', 'WRONG_DATA_COLOUR'),
-			array('#aa0', 'WRONG_DATA_COLOUR'),
-			array('AA0000 ', 'GROUP_UPDATED'),
-			array('AA0000 abf', 'WRONG_DATA_COLOUR'),
-			array('AA0000 AA0000', 'WRONG_DATA_COLOUR'),
-		);
-	}
-
-	/**
-	* @dataProvider groups_manage_test_data
-	*/
-	public function test_groups_manage($input, $expected)
-	{
-		$this->login();
-		$this->add_lang(array('ucp', 'acp/groups'));
-
-		$crawler = $this->request('GET', 'ucp.php?i=groups&mode=manage&action=edit&g=5&sid=' . $this->sid);
-		$this->assert_response_success();
-		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
-		$form['group_colour']->setValue($input);
-		$crawler = $this->client->submit($form);
-		$this->assertContains($this->lang($expected), $crawler->text());
+		return 'ucp.php?i=groups&mode=manage&action=edit&g=5';
 	}
 }
