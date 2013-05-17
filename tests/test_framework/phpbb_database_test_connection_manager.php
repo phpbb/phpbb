0a1ca4d3eafda2e9c3baa30e55d825d934f5aed4
Andreas Fischer <bantu@phpbb.com>
2013-05-17 02:31:25 +0200

    [ticket/11547] Add phpbb_db_driver_ prefix to DBMS names.
    PHPBB3-11547

--- tests/test_framework/phpbb_database_test_connection_manager.php
+++ tests/test_framework/phpbb_database_test_connection_manager.php
@@ -145,8 +145,8 @@ class phpbb_database_test_connection_manager
 
 		switch ($this->config['dbms'])
 		{
-			case 'mysql':
-			case 'mysqli':
+			case 'phpbb_db_driver_mysql':
+			case 'phpbb_db_driver_mysqli':
 				$this->pdo->exec('SET NAMES utf8');
 			default:
 		}
