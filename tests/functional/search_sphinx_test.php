36da38f062477e48ddc13cde683515ec9f5921f3
Dhruv <dhruv.goel92@gmail.com>
2013-06-19 02:13:32 +0530

    [ticket/11608] split search tests into separate files
    Tests for each search backend are into their own separate files.
    These separate classes inherit from a common search test case class.
    
    PHPBB3-11608

---
+++ tests/functional/search_sphinx_test.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * @package testing
+ * @copyright (c) 2013 phpBB Group
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+require_once dirname(__FILE__) . '/search_test.php';
+
+/**
+* @group functional
+*/
+class phpbb_functional_search_sphinx_test extends phpbb_functional_search_test
+{
+	protected $search_backend;
+
+	public function setUp()
+	{
+		parent::setUp();
+		$this->search_backend = 'phpbb_search_fulltext_sphinx';
+		$this->markTestIncomplete('Sphinx search not running for the test board');
+	}
+}
