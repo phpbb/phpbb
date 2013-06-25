5c7c61bf9696e05a6ca93665018ed7df8f08a8e3
Andy Chase <asperous2@gmail.com>
2013-06-25 12:40:31 -0700

    [ticket/11615] Rename class in file to match
    PHPBB3-11615

--- tests/session/creation_test.php
+++ tests/session/creation_test.php
@@ -9,7 +9,7 @@
 
 require_once dirname(__FILE__) . '/testable_factory.php';
 
-class phpbb_session_init_test extends phpbb_database_test_case
+class phpbb_session_creation_test extends phpbb_database_test_case
 {
 	public function getDataSet()
 	{
