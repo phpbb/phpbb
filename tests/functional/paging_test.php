91e773a79538a8fa26bea1d56384379b8730ac70
Dhruv <dhruv.goel92@gmail.com>
2013-06-28 01:02:00 +0530

    [ticket/9341] Assert that page doesnt contain next or prev page posts
    PHPBB3-9341

--- tests/functional/paging_test.php
+++ tests/functional/paging_test.php
@@ -24,14 +24,16 @@ class phpbb_functional_paging_test extends phpbb_functional_test_case
 		}
 		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid={$this->sid}");
 		$this->assertContains('post no9', $crawler->text());
+		$this->assertNotContains('post no19', $crawler->text());
 
 		$next_link = $crawler->filter('#viewtopic > fieldset > a.arrow-right')->attr('href');
 		$crawler = self::request('GET', $next_link);
 		$this->assertContains('post no19', $crawler->text());
-
+		$this->assertNotContains('post no9', $crawler->text());
 
 		$prev_link = $crawler->filter('#viewtopic > fieldset > a.arrow-left')->attr('href');
 		$crawler = self::request('GET', $prev_link);
 		$this->assertContains('post no9', $crawler->text());
+		$this->assertNotContains('post no19', $crawler->text());
 	}
 }
