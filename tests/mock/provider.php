3999d7ec7cc0860d3f955db9088558f92d1ba497
Andy Chase <asperous2@gmail.com>
2013-07-08 10:28:40 -0700

    [ticket/11620] More mock provider methods
    The mock provider object now better matches the interface
    given for providers.
    
    PHPBB3-11620

--- tests/mock/provider.php
+++ tests/mock/provider.php
@@ -10,21 +10,43 @@
 /**
  * Mock provider class with basic functions to help test
  * sessions.
+ *
+ * See interface here:
+ *    includes/auth/provider/interface.php
  */
 class phpbb_provider {
+
+	function init()
+	{
+		return null;
+	}
+
+	function login($username, $password)
+	{
+		return array(
+			 'status' => "",
+			 'error_msg' => "",
+			 'user_row' => "",
+			);
+	}
 
 	function autologin()
 	{
 		return array();
 	}
 
-	function kill()
+	function acp($new)
 	{
-
+		return array();
 	}
 
-	function validate_session($data)
+	function logout($data, $new_session)
 	{
-		return true;
+		return null;
+	}
+
+	function validate_session($user)
+	{
+		return null;
 	}
 }
