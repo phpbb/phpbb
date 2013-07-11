f4f29a1c0ae04c3a3d41aee822d7c858f39f5295
Joseph Warner <hardolaf@hardolaf.com>
2013-07-11 17:43:20 -0400

    [feature/auth-refactor] Fix style issue
    PHPBB3-9734

--- phpBB/includes/acp/acp_board.php
+++ phpBB/includes/acp/acp_board.php
@@ -678,7 +678,7 @@ class acp_board
 		$auth_plugins = array();
 		$auth_providers = $phpbb_container->get('auth.provider_collection');
 
-		foreach($auth_providers as $key => $value)
+		foreach ($auth_providers as $key => $value)
 		{
 			if (!($value instanceof phpbb_auth_provider_interface))
 			{
