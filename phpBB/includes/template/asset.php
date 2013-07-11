888e194bfff88a07d3f3439f42d50edcf9803cea
Vjacheslav Trushkin <cyberalien@gmail.com>
2013-07-11 15:23:50 -0400

    [feature/twig] Docblocks for assets
    PHPBB3-11628

--- phpBB/includes/template/asset.php
+++ phpBB/includes/template/asset.php
@@ -22,13 +22,18 @@ class phpbb_template_asset
 	/**
 	* Constructor
 	*
-	* @param string $url phpBB URL
+	* @param string $url URL
 	*/
 	public function __construct($url)
 	{
 		$this->set_url($url);
 	}
 
+	/**
+	* Set URL
+	*
+	* @param string $url URL
+	*/
 	public function set_url($url)
 	{
 		if (version_compare(PHP_VERSION, '5.4.7') < 0 && substr($url, 0, 2) === '//')
@@ -46,6 +51,12 @@ class phpbb_template_asset
 		$this->components = parse_url($url);
 	}
 
+	/**
+	* Convert URL components into string
+	*
+	* @param array $components URL components
+	* @return string URL
+	*/
 	protected function join_url($components)
 	{
 		$path = '';
@@ -99,11 +110,21 @@ class phpbb_template_asset
 		return $path;
 	}
 
+	/**
+	* Get URL
+	*
+	* @return string URL
+	*/
 	public function get_url()
 	{
 		return $this->join_url($this->components);
 	}
 
+	/**
+	* Checks if URL is local and relative
+	*
+	* @return boolean True if URL is local and relative
+	*/
 	public function is_relative()
 	{
 		if (empty($this->components) || !isset($this->components['path']))
@@ -114,11 +135,22 @@ class phpbb_template_asset
 		return !isset($this->components['scheme']) && !isset($this->components['host']) && substr($this->components['path'], 0, 1) !== '/';
 	}
 
+	/**
+	* Get path component of current URL
+	*
+	* @return string Path
+	*/
 	public function get_path()
 	{
 		return isset($this->components['path']) ? $this->components['path'] : '';
 	}
 
+	/**
+	* Set path component
+	*
+	* @param string $path Path component
+	* @param boolean $urlencode If true, parts of path should be encoded with rawurlencode()
+	*/
 	public function set_path($path, $urlencode = false)
 	{
 		if ($urlencode)
@@ -133,6 +165,12 @@ class phpbb_template_asset
 		$this->components['path'] = $path;
 	}
 
+	/**
+	* Add assets_version parameter to URL.
+	* Parameter will not be added if assets_version already exists in URL
+	*
+	* @param string $version Version
+	*/
 	public function add_assets_version($version)
 	{
 		if (!isset($this->components['query']))
