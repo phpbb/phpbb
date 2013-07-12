51ab2c710e91d98bb182dded1e9d5462451151e8
Igor Wiedler <igor@wiedler.ch>
2013-07-12 16:40:20 -0400

    [ticket/11574] Make install language filename less crazy
    PHPBB3-11574

--- phpBB/includes/user.php
+++ phpBB/includes/user.php
@@ -590,17 +590,11 @@ class phpbb_user extends phpbb_session
 				$language_filename = $lang_path . $this->lang_name . '/' . $filename . '.' . $phpEx;
 			}
 
-			if (defined('IN_INSTALL'))
+			// If we are in install, try to use the updated version, when available
+			$install_language_filename = str_replace('language/', 'install/update/new/language/', $language_filename);
+			if (defined('IN_INSTALL') && file_exists($install_language_filename))
 			{
-				// If we are in install, try to use the updated version, when available
-				$orig_language_filename = $language_filename;
-				$language_filename = str_replace('language/', 'install/update/new/language/', $language_filename);
-
-				if (!file_exists($language_filename))
-				{
-					// Not found, go back to the original file name
-					$language_filename = $orig_language_filename;
-				}
+				$language_filename = $install_language_filename;
 			}
 
 			if (!file_exists($language_filename))
