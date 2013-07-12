b93636d42d63bd396a31ff7cf699f8add006d765
Andreas Fischer <bantu@phpbb.com>
2013-07-12 20:56:55 +0200

    [develop-olympus] Increment version number to 3.0.13-dev.

--- phpBB/install/database_update.php
+++ phpBB/install/database_update.php
@@ -8,7 +8,7 @@
 *
 */
 
-define('UPDATES_TO_VERSION', '3.0.12-RC1');
+define('UPDATES_TO_VERSION', '3.0.13-dev');
 
 // Enter any version to update from to test updates. The version within the db will not be updated.
 define('DEBUG_FROM_VERSION', false);
