0ebff82ad31b7f1a59952a6c9b4c45221d214929
Joseph Warner <hardolaf@hardolaf.com>
2013-07-14 15:35:12 -0400

    [feature/oauth] Last five oauth services
    PHPBB3-11673

---
+++ phpBB/phpbb/auth/provider/oauth/service/yammer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+*
+* @package auth
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* Yammer OAuth service
+*
+* @package auth
+*/
+class phpbb_auth_provider_oauth_service_yammer extends phpbb_auth_provider_oauth_service_base
+{
+
+}
