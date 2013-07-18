9e8abf594b6b600baab5060bb7e3fb402f285fbb
Andy Chase <asperous2@gmail.com>
2013-07-18 12:22:04 -0700

    [feature/session-storage-cache] Minor code changes for clarity
    PHPBB3-11705

--- tests/cache/atomic_cache_test.php
+++ tests/cache/atomic_cache_test.php
@@ -27,32 +27,36 @@ class phpbb_cache_atomic_driver_test extends phpbb_database_test_case
 
 	static function run_race($key, $driver_class, $host, $port)
 	{
-		// fork
-		$child_pid = pcntl_fork();
+		// fork process into parent and child
+		$process_id = pcntl_fork();
+		$is_child_process = $process_id == 0;
 		// Set a begin time for the race
 		$start_time = microtime() + 20000;
-		if ($child_pid == 0)
+		if ($is_child_process)
 		{
-			// CHILD
+			// CHILD FORK
 			$driver = new $driver_class($host, $port);
-			// fork 1 writes data
+			// wait to start race
 			usleep($start_time - microtime());
-			$driver->atomic_operation($key, function ($d) {
-				return $d . 'child';
+			// read & write data
+			$driver->atomic_operation($key, function ($data) {
+				return $data . 'child';
 			});
+			// Child ends execution, parent will continue with control flow
 			exit;
 		} else {
-			// PARENT
-			// fork 2 writes data <-- Should fail, re-read and re-write
+			// PARENT FORK
 			$driver = new $driver_class($host, $port);
+			// wait to start race
 			usleep($start_time - microtime());
-			$driver->atomic_operation($key, function ($d) {
-				// Wait so that fork 1 always writes first
+			// read & write data <-- should fail, re-read and re-write
+			$driver->atomic_operation($key, function ($data) {
+				// wait so that child always writes first
 				usleep(10000);
-				return $d . 'parent';
+				return $data . 'parent';
 			});
+			return $driver->get($key);
 		}
-		return $driver->get($key);
 	}
 
 	public function test_redis_race_condition()
@@ -66,8 +70,8 @@ class phpbb_cache_atomic_driver_test extends phpbb_database_test_case
 		$host = isset($config['redis_host']) ? $config['redis_host'] : 'localhost';
 		$port = isset($config['redis_port']) ? $config['redis_port'] : 6379;
 
-		// Prefix with '_' for certain memory cache operations
-		// don't actually cache otherwise
+		// Prefix with '_' because certain memory cache
+		//        operations don't actually hit cache otherwise
 		$key = uniqid('_');
 		$result = self::run_race($key, 'phpbb_cache_driver_redis', $host, $port);
 		$driver = new phpbb_cache_driver_redis($host, $port);
@@ -83,8 +87,8 @@ class phpbb_cache_atomic_driver_test extends phpbb_database_test_case
 			self::markTestSkipped('memcache extension is not loaded');
 		}
 
-		// Prefix with '_' for certain memory cache operations
-		// don't actually cache otherwise
+		// Prefix with '_' because certain memory cache
+		//        operations don't actually hit cache otherwise
 		$key = uniqid('_');
 		$driver = new phpbb_cache_driver_memcache();
 		// Memcache needs key set before atomic_operations
@@ -93,7 +97,7 @@ class phpbb_cache_atomic_driver_test extends phpbb_database_test_case
 		usleep(10000);
 
 		$result = self::run_race($key, 'phpbb_cache_driver_memcache', null, null);
-		$driver->memcache->delete($key);
+		$driver->memcache->delete($driver->key_prefix . $key);
 
 		$this->assertEquals('child' . 'parent', $result);
 	}
