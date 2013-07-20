f1bfbde3f5bdb9191057f28dd623dc2a3a530bf7
Bruno Ais <brunoaiss@gmail.com>
2013-07-20 16:19:27 +0100

    [ticket/11643] Fixed typo in the variable name.
    sub-task of ticket PHPBB3-11635:
    find and fix all bypasses of generate_text_for_*
    
    PHPBB3-11643

--- phpBB/includes/mcp/mcp_queue.php
+++ phpBB/includes/mcp/mcp_queue.php
@@ -132,7 +132,7 @@ class mcp_queue
 				$post_unread = (isset($topic_tracking_info[$post_info['topic_id']]) && $post_info['post_time'] > $topic_tracking_info[$post_info['topic_id']]) ? true : false;
 
 				// Process message, leave it uncensored
-				$message = generate_text_for_display($post_info['message_text'], $post_info['bbcode_uid'], $post_info['bbcode_bitfield'], ($post_info['bbcode_bitfield'] ? OPTION_FLAG_BBCODE : 0) | OPTION_FLAG_SMILIES, false);
+				$message = generate_text_for_display($post_info['post_text'], $post_info['bbcode_uid'], $post_info['bbcode_bitfield'], ($post_info['bbcode_bitfield'] ? OPTION_FLAG_BBCODE : 0) | OPTION_FLAG_SMILIES, false);
 
 				if ($post_info['post_attachment'] && $auth->acl_get('u_download') && $auth->acl_get('f_download', $post_info['forum_id']))
 				{
