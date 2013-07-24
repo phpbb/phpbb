27ea03d3e098ca53f9f49da5024f7d7c64989153
Joseph Warner <hardolaf@hardolaf.com>
2013-07-24 14:55:08 -0400

    [feature/oauth] Initial login_link ucp class
    PHPBB3-11673

---
+++ phpBB/includes/ucp/ucp_login_link.php
@@ -0,0 +1,32 @@
+<?php
+/**
+*
+* @package ucp
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* ucp_login_link
+* Allows users of external accounts link those accounts to their phpBB accounts
+* during an attempted login.
+* @package ucp
+*/
+class ucp_login_link
+{
+	var $u_action;
+
+	function main($id, $mode)
+	{
+
+	}
+}
