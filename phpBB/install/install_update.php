5af63a7860678ac55f84201cf7dbbdf82dcdc8e8
Oliver Schramm <oliver.schramm97@gmail.com>
2013-07-24 18:15:03 +0200

    [ticket/10917] Fixed notice that files are out of date when updating to an unreleased version
    PHPBB3-10917

--- phpBB/install/install_update.php
+++ phpBB/install/install_update.php
@@ -204,7 +204,7 @@ class install_update extends module
 		}
 
 		// Check if the update files stored are for the latest version...
-		if ($this->latest_version != $this->update_info['version']['to'])
+		if (version_compare(strtolower($this->latest_version), strtolower($this->update_info['version']['to']), '>'))
 		{
 			$this->unequal_version = true;
 
