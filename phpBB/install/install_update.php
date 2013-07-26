d770cbf8972d21ab8ebdf3014f766179898dfc9e
Joas Schilling <nickvergessen@gmx.de>
2013-07-26 11:26:57 +0200

    Revert "[ticket/10917] Using phpbb wrapper"
    This reverts commit e80503696350d7410e82e63e9d108d65e3c35696.

--- phpBB/install/install_update.php
+++ phpBB/install/install_update.php
@@ -204,7 +204,7 @@ class install_update extends module
 		}
 
 		// Check if the update files stored are for the latest version...
-		if (phpbb_version_compare($this->latest_version, $this->update_info['version']['to'], '>'))
+		if (version_compare(strtolower($this->latest_version), strtolower($this->update_info['version']['to']), '>'))
 		{
 			$this->unequal_version = true;
 
