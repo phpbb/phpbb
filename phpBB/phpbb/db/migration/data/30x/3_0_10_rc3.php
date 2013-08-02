e667481c6c3d98808ac412531f562d68fad10ae5
Oliver Schramm <oliver.schramm97@gmail.com>
2013-08-02 03:28:32 +0200

    [ticket/11760] Use phpbb_version_compare() wrapper
    PHPBB3-11760

--- phpBB/phpbb/db/migration/data/30x/3_0_10_rc3.php
+++ phpBB/phpbb/db/migration/data/30x/3_0_10_rc3.php
@@ -11,7 +11,7 @@ class phpbb_db_migration_data_30x_3_0_10_rc3 extends phpbb_db_migration
 {
 	public function effectively_installed()
 	{
-		return version_compare($this->config['version'], '3.0.10-RC3', '>=');
+		return phpbb_version_compare($this->config['version'], '3.0.10-RC3', '>=');
 	}
 
 	static public function depends_on()
--- phpBB/phpbb/db/migration/data/30x/3_0_2_rc2.php
+++ phpBB/phpbb/db/migration/data/30x/3_0_2_rc2.php
@@ -11,7 +11,7 @@ class phpbb_db_migration_data_30x_3_0_2_rc2 extends phpbb_db_migration
 {
 	public function effectively_installed()
 	{
-		return version_compare($this->config['version'], '3.0.2-RC2', '>=');
+		return phpbb_version_compare($this->config['version'], '3.0.2-RC2', '>=');
 	}
 
 	static public function depends_on()
