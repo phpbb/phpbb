deb62d51fe4e06e52fbc4042b692071bcd8f9d39
Joseph Warner <hardolaf@hardolaf.com>
2013-08-09 05:12:41 -0400

    [feature/oauth] Start building the template
    PHPBB3-11673

--- phpBB/includes/ucp/ucp_auth_link.php
+++ phpBB/includes/ucp/ucp_auth_link.php
@@ -21,6 +21,34 @@ class ucp_auth_link
 
 	public function main($id, $mode)
 	{
+		global $template, $phpbb_container;
+
+		$error = array();
+		$s_hidden_fields = array();
+		add_form_key('ucp_auth_link');
+
+		$submit	= $request->variable('submit', false, false, phpbb_request_interface::POST);
+
+		if ($submit)
+		{
+			if (!check_form_key('ucp_reg_details'))
+			{
+				$error[] = 'FORM_INVALID';
+			}
+
+			if (!sizeof($error))
+			{
+
+			}
+		}
+
+		$s_hidden_fields = build_hidden_fields($s_hidden_fields);
+
+		$template->assign_vars(array(
+			'S_HIDDEN_FIELDS'	=> $s_hidden_fields,
+			'S_UCP_ACTION'		=> $this->u_action,
+		));
+
 		$this->tpl_name = 'ucp_auth_link';
 		$this->page_title = 'UCP_AUTH_LINK';
 	}
