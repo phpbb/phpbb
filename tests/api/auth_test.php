98c9af7f7cbf55bf3306ba62f4a72b70edf78d8b
Victor Nagy <galaxyAbstractor@gmail.com>
2013-08-12 17:55:28 +0200

    [feature/api] Cleaned up auth test
    Removed unused parameter userid.
    
    PHPBB3-11595

--- tests/api/auth_test.php
+++ tests/api/auth_test.php
@@ -59,18 +59,18 @@ class phpbb_api_auth_test extends phpbb_database_test_case
 	static public function auth_data()
 	{
 		return array(
-			array(1, false, 1, 'guest', '', 'Guest access should give the user id 1 when guests have permissions'),
-			array(2, false, 2, 'aaaaaaaaaaaaaaaa', 'c838ce3a204b683c5a552aba35c7b8f3eb865e30db760dd65677567dba8a7963','Valid key and hash should give user id 2'),
-			array(401, true, 2, 'invalid', 'invalid', 'Non-existant auth_key should give http status code 401'),
-			array(403, true, 2, 'aaaaaaaaaaaaaaaa', 'c838ce3a204b683c5a552aba35c7b8f3eb865e30db760dd65677567dba8a7963','Valid key and hash but no permissions should give 403'),
-			array(400, true, 2, 'aaaaaaaaaaaaaaaa', 'invalid','Invalid hash should give 400'),
+			array(1, false, 'guest', '', 'Guest access should give the user id 1 when guests have permissions'),
+			array(2, false, 'aaaaaaaaaaaaaaaa', 'c838ce3a204b683c5a552aba35c7b8f3eb865e30db760dd65677567dba8a7963','Valid key and hash should give user id 2'),
+			array(401, true, 'invalid', 'invalid', 'Non-existant auth_key should give http status code 401'),
+			array(403, true, 'aaaaaaaaaaaaaaaa', 'c838ce3a204b683c5a552aba35c7b8f3eb865e30db760dd65677567dba8a7963','Valid key and hash but no permissions should give 403'),
+			array(400, true, 'aaaaaaaaaaaaaaaa', 'invalid','Invalid hash should give 400'),
 		);
 	}
 
 	/**
 	 * @dataProvider auth_data
 	 */
-	public function test_auth($status, $nopermission, $userid, $auth_key, $hash, $description)
+	public function test_auth($status, $nopermission, $auth_key, $hash, $description)
 	{
 		if (!$nopermission)
 		{
