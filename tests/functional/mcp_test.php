63535b196dd5d4dcdc9a8fb6af03663638f8d8ce
Joas Schilling <nickvergessen@gmx.de>
2013-08-12 15:31:19 +0200

    [ticket/11775] Split test into multiple steps
    PHPBB3-11775

--- tests/functional/mcp_test.php
+++ tests/functional/mcp_test.php
@@ -22,12 +22,27 @@ class phpbb_functional_mcp_test extends phpbb_functional_test_case
 		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid={$this->sid}");
 		$this->assertContains('Testing move post with "Move posts" option from Quick-Moderator Tools.', $crawler->filter('html')->text());
 
+		return $crawler;
+	}
+
+	/**
+	* @depends test_post_new_topic
+	*/
+	public function test_handle_quickmod($crawler)
+	{
 		// Test moving a post
-		$this->add_lang('mcp');
 		$form = $crawler->selectButton('Go')->eq(1)->form();
 		$form['action']->select('merge');
 		$crawler = self::submit($form);
 
+		return $crawler;
+	}
+
+	/**
+	* @depends test_handle_quickmod
+	*/
+	public function test_move_post_to_topic($crawler)
+	{
 		// Select the post in MCP
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form(array(
 			'to_topic_id'	=> 1,
@@ -36,6 +51,15 @@ class phpbb_functional_mcp_test extends phpbb_functional_test_case
 		$crawler = self::submit($form);
 		$this->assertContains($this->lang('MERGE_POSTS'), $crawler->filter('html')->text());
 
+		return $crawler;
+	}
+
+	/**
+	* @depends test_move_post_to_topic
+	*/
+	public function test_confirm_result($crawler)
+	{
+		$this->add_lang('mcp');
 		$form = $crawler->selectButton('Yes')->form();
 		$crawler = self::submit($form);
 		$this->assertContains($this->lang('POSTS_MERGED_SUCCESS'), $crawler->text());
