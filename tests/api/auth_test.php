cde4cbd454eef72e95945f3a6a507cde0d5bbbf7
Victor Nagy <galaxyAbstractor@gmail.com>
2013-08-26 13:59:53 +0200

    [feature/api] Updated auth unit test to use serials
    PHPBB3-11595

--- tests/api/auth_test.php
+++ tests/api/auth_test.php
@@ -33,7 +33,7 @@ class phpbb_api_auth_test extends phpbb_database_test_case
 	static public function fetchrow_data_allow()
 	{
 		return array(
-			array('key_id = 2', array(array('key_id' => '2', 'auth_key' => 'cccccccccccccccc', 'sign_key' => 'dddddddddddddddd', 'user_id' => '2', 'name' => 'test'))),
+			array('key_id = 2', array(array('key_id' => '2', 'auth_key' => 'cccccccccccccccc', 'sign_key' => 'dddddddddddddddd', 'user_id' => '2', 'name' => 'test', 'serial' => 0))),
 		);
 	}
 
@@ -59,36 +59,29 @@ class phpbb_api_auth_test extends phpbb_database_test_case
 	static public function auth_data()
 	{
 		return array(
-			array(1, false, 'guest', '', 'Guest access should give the user id 1 when guests have permissions'),
-			array(2, false, 'aaaaaaaaaaaaaaaa', 'c838ce3a204b683c5a552aba35c7b8f3eb865e30db760dd65677567dba8a7963','Valid key and hash should give user id 2'),
-			array(401, true, 'invalid', 'invalid', 'Non-existant auth_key should give http status code 401'),
-			array(403, true, 'aaaaaaaaaaaaaaaa', 'c838ce3a204b683c5a552aba35c7b8f3eb865e30db760dd65677567dba8a7963','Valid key and hash but no permissions should give 403'),
-			array(400, true, 'aaaaaaaaaaaaaaaa', 'invalid','Invalid hash should give 400'),
+			array(1, false, 'guest', 1, '', 'Guest access should give the user id 1 when guests have permissions'),
+			array(2, false, 'aaaaaaaaaaaaaaaa', 1, '457dfadb13d252aa7c35c8ee8628eac3028175b7049ade5c8e14b6ddc0d1b63a','Valid key and hash should give user id 2'),
+			array(400, false, 'aaaaaaaaaaaaaaaa', 0, 'c838ce3a204b683c5a552aba35c7b8f3eb865e30db760dd65677567dba8a7963','Valid key and hash but invalid serial should give 400'),
+			array(401, true, 'invalid', 'invalid', 2, 'Non-existant auth_key should give http status code 401'),
+			array(403, true, 'aaaaaaaaaaaaaaaa', 3, 'e14f00de0ad81f1db601ad65f18fb07c4755ebf39540a8e4dc7dcd47f2e6defb','Valid key and hash but no permissions should give 403'),
+			array(400, true, 'aaaaaaaaaaaaaaaa', 4, 'invalid','Invalid hash should give 400'),
+
 		);
 	}
 
 	/**
 	 * @dataProvider auth_data
 	 */
-	public function test_auth($status, $nopermission, $auth_key, $hash, $description)
+	public function test_auth($status, $nopermission, $auth_key, $serial, $hash, $description)
 	{
 		if (!$nopermission)
 		{
-			$this->auth->expects($this->once())
+			$this->auth->expects($this->any())
 			->method('acl_get')
 			->will($this->returnValue(true));
-
-			$response = $this->auth_repository->auth('api/auth/verify', $auth_key, 0, $hash);
-
-			$this->assertEquals($status, $response, $description);
-		}
-		else
-		{
-			$response = $this->auth_repository->auth('api/auth/verify', $auth_key, 0, $hash);
-
-			$this->assertEquals($status, $response['status'], $description);
 		}
 
+		$response = $this->auth_repository->auth('api/auth/verify', $auth_key, $serial, $hash);
+		$this->assertEquals($status, (is_int($response)) ? $response : $response['status'], $description);
 	}
-
 }
