4eab9a8c5361d08bea4d054b44ac0324ece881a4
David King <imkingdavid@gmail.com>
2013-09-06 13:14:07 -0700

    [ticket/11824] Add tests for urls with mod_rewrite enabled
    PHPBB3-11824

--- tests/controller/helper_url_test.php
+++ tests/controller/helper_url_test.php
@@ -12,7 +12,7 @@ require_once dirname(__FILE__) . '/../../phpBB/includes/functions.php';
 class phpbb_controller_helper_url_test extends phpbb_test_case
 {
 
-	public function helper_url_data()
+	public function helper_url_data_no_rewrite()
 	{
 		return array(
 			array('foo/bar?t=1&amp;f=2', false, true, false, 'app.php/foo/bar?t=1&amp;f=2', 'parameters in url-argument'),
@@ -40,10 +40,38 @@ class phpbb_controller_helper_url_test extends phpbb_test_case
 		);
 	}
 
+	public function helper_url_data_with_rewrite()
+	{
+		return array(
+			array('foo/bar?t=1&amp;f=2', false, true, false, 'foo/bar?t=1&amp;f=2', 'parameters in url-argument'),
+			array('foo/bar', 't=1&amp;f=2', true, false, 'foo/bar?t=1&amp;f=2', 'parameters in params-argument using amp'),
+			array('foo/bar', 't=1&f=2', false, false, 'foo/bar?t=1&f=2', 'parameters in params-argument using &'),
+			array('foo/bar', array('t' => 1, 'f' => 2), true, false, 'foo/bar?t=1&amp;f=2', 'parameters in params-argument as array'),
+
+			// Custom sid parameter
+			array('foo/bar', 't=1&amp;f=2', true, 'custom-sid', 'foo/bar?t=1&amp;f=2&amp;sid=custom-sid', 'using session_id'),
+
+			// Testing anchors
+			array('foo/bar?t=1&amp;f=2#anchor', false, true, false, 'foo/bar?t=1&amp;f=2#anchor', 'anchor in url-argument'),
+			array('foo/bar', 't=1&amp;f=2#anchor', true, false, 'foo/bar?t=1&amp;f=2#anchor', 'anchor in params-argument'),
+			array('foo/bar', array('t' => 1, 'f' => 2, '#' => 'anchor'), true, false, 'foo/bar?t=1&amp;f=2#anchor', 'anchor in params-argument (array)'),
+
+			// Anchors and custom sid
+			array('foo/bar?t=1&amp;f=2#anchor', false, true, 'custom-sid', 'foo/bar?t=1&amp;f=2&amp;sid=custom-sid#anchor', 'anchor in url-argument using session_id'),
+			array('foo/bar', 't=1&amp;f=2#anchor', true, 'custom-sid', 'foo/bar?t=1&amp;f=2&amp;sid=custom-sid#anchor', 'anchor in params-argument using session_id'),
+			array('foo/bar', array('t' => 1, 'f' => 2, '#' => 'anchor'), true, 'custom-sid', 'foo/bar?t=1&amp;f=2&amp;sid=custom-sid#anchor', 'anchor in params-argument (array) using session_id'),
+
+			// Empty parameters should not append the &amp;
+			array('foo/bar', false, true, false, 'foo/bar', 'no params using bool false'),
+			array('foo/bar', '', true, false, 'foo/bar', 'no params using empty string'),
+			array('foo/bar', array(), true, false, 'foo/bar', 'no params using empty array'),
+		);
+	}
+
 	/**
-	* @dataProvider helper_url_data
+	* @dataProvider helper_url_data_no_rewrite()
 	*/
-	public function test_helper_url($route, $params, $is_amp, $session_id, $expected, $description)
+	public function test_helper_url_no_rewrite($route, $params, $is_amp, $session_id, $expected, $description)
 	{
 		global $phpbb_dispatcher, $phpbb_root_path, $phpEx;
 
@@ -53,6 +81,23 @@ class phpbb_controller_helper_url_test extends phpbb_test_case
 
 		// We don't use mod_rewrite in tests
 		$config = new phpbb_config(array('enable_mod_rewrite' => '0'));
+		$helper = new phpbb_controller_helper($this->template, $this->user, $config, '', 'php');
+		$this->assertEquals($helper->url($route, $params, $is_amp, $session_id), $expected);
+	}
+
+	/**
+	* @dataProvider helper_url_data_with_rewrite()
+	*/
+	public function test_helper_url_with_rewrite($route, $params, $is_amp, $session_id, $expected, $description)
+	{
+		global $phpbb_dispatcher, $phpbb_root_path, $phpEx;
+
+		$phpbb_dispatcher = new phpbb_mock_event_dispatcher;
+		$this->user = $this->getMock('phpbb_user');
+		$this->template = new phpbb_template_twig($phpbb_root_path, $phpEx, $config, $this->user, new phpbb_template_context());
+
+		// We don't use mod_rewrite in tests
+		$config = new phpbb_config(array('enable_mod_rewrite' => '1'));
 		$helper = new phpbb_controller_helper($this->template, $this->user, $config, '', 'php');
 		$this->assertEquals($helper->url($route, $params, $is_amp, $session_id), $expected);
 	}
