25327cca7d4be71832913f0a5feedcbcdd029816
Dhruv <dhruv.goel92@gmail.com>
2013-09-09 01:47:10 +0530

    [ticket/10533] Remove sql query and instead use $member['user_topics']
    PHPBB3-10533

--- phpBB/memberlist.php
+++ phpBB/memberlist.php
@@ -639,16 +639,8 @@ switch ($mode)
 			$member['posts_in_queue'] = 0;
 		}
 
-		// Give the number of topics a user has along with percentage of all topics, and topics per day.
-		$sql = 'SELECT COUNT(topic_id) as topics
-			FROM ' . TOPICS_TABLE . '
-			WHERE topic_poster = ' . $user_id . '
-				AND topic_approved = 1';
-		$result = $db->sql_query($sql);
-		$topics = (int) $db->sql_fetchfield('topics');
-		$db->sql_freeresult($result);
-		$topics_per_day = $topics / $memberdays;
-		$topic_percentage = ($config['num_topics']) ? min(100, ($topics / $config['num_topics']) * 100) : 0;
+		$topics_per_day = $member['user_topics'] / $memberdays;
+		$topic_percentage = ($config['num_topics']) ? min(100, ($member['user_topics'] / $config['num_topics']) * 100) : 0;
 
 		$template->assign_vars(array(
 			'L_POSTS_IN_QUEUE'	=> $user->lang('NUM_POSTS_IN_QUEUE', $member['posts_in_queue']),
@@ -656,7 +648,7 @@ switch ($mode)
 			'POSTS_DAY'			=> $user->lang('POST_DAY', $posts_per_day),
 			'POSTS_PCT'			=> $user->lang('POST_PCT', $percentage),
 
-			'TOPICS'			=> $topics,
+			'TOPICS'			=> $member['user_topics'],
 			'TOPICS_DAY'		=> $user->lang('TOPIC_DAY', $topics_per_day),
 			'TOPICS_PCT'		=> $user->lang('TOPIC_PCT', $topic_percentage),
 
