d5f93f5ce4c25b4bd0aff9473eb47eeeb1afccd4
Nathan Guse <nathaniel.guse@gmail.com>
2013-09-10 10:06:13 -0500

    Revert "[ticket/11832] Make $phpbb_container a global initiated by the framework"
    This reverts commit 5e09afa0bc3ff9014812282fd4454f0df54b9dfd.

--- tests/test_framework/phpbb_test_case.php
+++ tests/test_framework/phpbb_test_case.php
@@ -11,20 +11,9 @@ class phpbb_test_case extends PHPUnit_Framework_TestCase
 {
 	protected $test_case_helpers;
 
-	protected $phpbb_container;
-
 	public function __construct($name = NULL, array $data = array(), $dataName = '')
 	{
 		parent::__construct($name, $data, $dataName);
-
-		global $phpbb_container, $phpbb_root_path;
-
-		// Setup the container for global usage
-		$this->phpbb_container = $phpbb_container = new phpbb_mock_container_builder();
-
-		// Set some commonly needed systems up
-		$phpbb_container->set('filesystem', new phpbb_filesystem($phpbb_root_path));
-
 		$this->backupStaticAttributesBlacklist += array(
 			'PHP_CodeCoverage' => array('instance'),
 			'PHP_CodeCoverage_Filter' => array('instance'),
