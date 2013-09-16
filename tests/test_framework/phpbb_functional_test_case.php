ee1c15fe7e1d0c823026d2b63ceab276eb878ab0
Nils Adermann <naderman@naderman.de>
2013-09-16 04:47:39 +0200

    [ticket/11700] Fix another case of non-namespaced classnames
    PHPBB3-11700

--- tests/test_framework/phpbb_functional_test_case.php
+++ tests/test_framework/phpbb_functional_test_case.php
@@ -532,7 +532,7 @@ class phpbb_functional_test_case extends phpbb_test_case
 		$phpbb_log = new \phpbb\log\log($db, $user, $auth, $phpbb_dispatcher, $phpbb_root_path, 'adm/', $phpEx, LOG_TABLE);
 		$cache = new phpbb_mock_null_cache;
 
-		$cache_driver = new phpbb_cache_driver_null();
+		$cache_driver = new \phpbb\cache\driver\null();
 		$phpbb_container = new phpbb_mock_container_builder();
 		$phpbb_container->set('cache.driver', $cache_driver);
 		$phpbb_container->set('notification_manager', new phpbb_mock_notification_manager());
