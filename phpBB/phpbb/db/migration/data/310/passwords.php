1970c69c8c9d6b898680aa3e94d5222c8f150d58
Marc Alexander <admin@m-a-styles.de>
2013-09-20 18:10:42 +0200

    [feature/passwords] Rename migrations file to passwords
    Also removed the effectively_installed() check that is not needed according
    to EXreaction.
    
    PHPBB3-11610

---
+++ phpBB/phpbb/db/migration/data/310/passwords.php
@@ -0,0 +1,38 @@
+<?php
+/**
+*
+* @package migration
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License v2
+*
+*/
+
+class phpbb_db_migration_data_310_passwords extends phpbb_db_migration
+{
+	static public function depends_on()
+	{
+		return array('phpbb_db_migration_data_30x_3_0_11');
+	}
+
+	public function update_schema()
+	{
+		return array(
+			'change_columns'	=> array(
+				$this->table_prefix . 'users'			=> array(
+					'user_password'			=> array('VCHAR:255', ''),
+				),
+			),
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+			'change_columns'	=> array(
+				$this->table_prefix . 'users'			=> array(
+					'user_password'			=> array('VCHAR:40', ''),
+				),
+			),
+		);
+	}
+}
