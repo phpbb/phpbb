645bd127dd53300b7877b779226ac53a4891620c
Nils Adermann <naderman@naderman.de>
2013-09-25 16:20:26 +0200

    [ticket/11700] Undo migrations migration, as not an issue for new installs
    PHPBB3-11700

--- phpBB/phpbb/db/migration/data/v310/namespaces.php
+++ phpBB/phpbb/db/migration/data/v310/namespaces.php
@@ -25,61 +25,6 @@ class namespaces extends \phpbb\db\migration\migration
 				(preg_match('#^phpbb_search_#', $this->config['search_type'])),
 				array('config.update', array('search_type', str_replace('phpbb_search_', 'phpbb\\search\\', $this->config['search_type']))),
 			)),
-			array('custom', array(array($this, 'update_migrations'))),
 		);
-	}
-
-	public function update_migrations()
-	{
-		$table = $this->table_prefix . 'migrations';
-
-		$sql = 'SELECT *
-			FROM migrations';
-		$result = $this->db->sql_query($sql);
-
-		while ($row = $this->db->sql_fetchrow($result))
-		{
-			$migration_name = $row['migration_name'];
-
-			$row['migration_name'] = $this->namespacify($row['migration_name']);
-
-			$depends_on = ($row['migration_depends_on']) ? unserialize($row['migration_depends_on']) : false;
-
-			if ($depends_on)
-			{
-				$depends_on_new = array();
-
-				foreach ($depends_on as $migration)
-				{
-					$depends_on_new[] = $this->namespacify($migration);
-				}
-
-				$depends_on = serialize($depends_on_new);
-				$row['migration_depends_on'] = $depends_on;
-			}
-
-			$sql_update = $this->db->sql_build_array('UPDATE', $row);
-
-			$sql = 'UPDATE ' . MODULES_TABLE . '
-				SET ' . $sql_update . '
-				WHERE module_id = ' . $migration_name;
-			$this->sql_query($sql);
-		}
-	}
-
-	public function namespacify($migration_name)
-	{
-		$parts = explode('_', $migration_name);
-
-		$namespace = '';
-		$class = '';
-
-		while (count($parts) > 1 && (!$class || !class_exists($class)))
-		{
-			$namespace = $namespace . '\\' . array_shift($parts);
-			$class = $namespace . '\\' . implode('_', $parts);
-		}
-
-		return $class;
 	}
 }
