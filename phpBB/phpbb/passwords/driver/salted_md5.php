ce51f878eb8ced7d79fb6aadc1ad831fbf73a66e
Marc Alexander <admin@m-a-styles.de>
2013-10-11 09:43:30 +0200

    [feature/passwords] Add phpass copyright notice to salted md5 driver
    PHPBB3-11610

--- phpBB/phpbb/passwords/driver/salted_md5.php
+++ phpBB/phpbb/passwords/driver/salted_md5.php
@@ -18,6 +18,34 @@ if (!defined('IN_PHPBB'))
 }
 
 /**
+*
+* @version Version 0.1 / slightly modified for phpBB 3.1.x (using $H$ as hash type identifier)
+*
+* Portable PHP password hashing framework.
+*
+* Written by Solar Designer <solar at openwall.com> in 2004-2006 and placed in
+* the public domain.
+*
+* There's absolutely no warranty.
+*
+* The homepage URL for this framework is:
+*
+*	http://www.openwall.com/phpass/
+*
+* Please be sure to update the Version line if you edit this file in any way.
+* It is suggested that you leave the main version number intact, but indicate
+* your project name (after the slash) and add your own revision information.
+*
+* Please do not change the "private" password hashing method implemented in
+* here, thereby making your hashes incompatible.  However, if you must, please
+* change the hash type identifier (the "$P$") to something different.
+*
+* Obviously, since this code is in the public domain, the above are not
+* requirements (there can be none), but merely suggestions.
+*
+*/
+
+/**
 * @package passwords
 */
 class salted_md5 extends \phpbb\passwords\driver\base
