457ca9c7633df2bf362f3530952afa15934e0907
Matt Friedman <maf675@gmail.com>
2013-10-15 14:09:47 -0700

    [ticket/11921] Remove "l_" from non-lang variables
    PHPBB3-11921

--- phpBB/includes/functions.php
+++ phpBB/includes/functions.php
@@ -5225,7 +5225,7 @@ function page_header($page_title = '', $display_online_list = true, $item_id = 0
 		$l_online_time = $user->lang('VIEW_ONLINE_TIMES', (int) $config['load_online_time']);
 	}
 
-	$l_privmsgs_count = '';
+	$privmsgs_count = 0;
 	$s_privmsg_new = false;
 
 	// Obtain number of new private messages if user is logged in
@@ -5233,7 +5233,7 @@ function page_header($page_title = '', $display_online_list = true, $item_id = 0
 	{
 		if ($user->data['user_new_privmsg'])
 		{
-			$l_privmsgs_count = (int) $user->data['user_new_privmsg'];
+			$privmsgs_count = (int) $user->data['user_new_privmsg'];
 
 			if (!$user->data['user_last_privmsg'] || $user->data['user_last_privmsg'] > $user->data['session_last_visit'])
 			{
@@ -5251,13 +5251,13 @@ function page_header($page_title = '', $display_online_list = true, $item_id = 0
 		}
 		else
 		{
-			$l_privmsgs_count = 0;
+			$privmsgs_count = 0;
 			$s_privmsg_new = false;
 		}
 
 		if ($user->data['user_unread_privmsg'] && $user->data['user_unread_privmsg'] != $user->data['user_new_privmsg'])
 		{
-			$l_privmsgs_count = (int) $user->data['user_unread_privmsg'];
+			$privmsgs_count = (int) $user->data['user_unread_privmsg'];
 		}
 	}
 
@@ -5348,7 +5348,7 @@ function page_header($page_title = '', $display_online_list = true, $item_id = 0
 		'TOTAL_USERS_ONLINE'			=> $l_online_users,
 		'LOGGED_IN_USER_LIST'			=> $online_userlist,
 		'RECORD_USERS'					=> $l_online_record,
-		'PRIVATE_MESSAGE_COUNT'			=> $l_privmsgs_count,
+		'PRIVATE_MESSAGE_COUNT'			=> $privmsgs_count,
 		'HIDDEN_FIELDS_FOR_JUMPBOX'	=> $hidden_fields_for_jumpbox,
 
 		'UNREAD_NOTIFICATIONS_COUNT'	=> ($notifications !== false) ? $notifications['unread_count'] : '',
