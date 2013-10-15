2adbd08714eb710e7c19481bb94646542bbf0e78
Andreas Fischer <bantu@phpbb.com>
2013-10-16 00:02:51 +0200

    [ticket/11926] Fix plupload migration dependency.
    PHPBB3-11926

--- phpBB/phpbb/db/migration/data/v310/plupload.php
+++ phpBB/phpbb/db/migration/data/v310/plupload.php
@@ -19,7 +19,7 @@ class plupload extends \phpbb\db\migration\migration
 
 	static public function depends_on()
 	{
-		return array('\phpbb\db\migration\data\310\dev');
+		return array('\phpbb\db\migration\data\v310\dev');
 	}
 
 	public function update_data()
