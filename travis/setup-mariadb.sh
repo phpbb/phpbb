cdaea7a4062c463e9536b3ca52207c013d8af408
Andreas Fischer <bantu@phpbb.com>
2013-10-15 16:28:59 +0200

    [ticket/11920] Add MariaDB environment to Travis-CI.
    PHPBB3-11920

---
+++ travis/setup-mariadb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# @copyright (c) 2013 phpBB Group
+# @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+#
+set -e
+
+# MariaDB Series
+VERSION='5.5'
+
+# Operating system codename, e.g. "precise"
+OS_CODENAME=$(lsb_release --codename --short)
+
+if ! which add-apt-repository > /dev/null
+then
+	sudo apt-get update -qq
+	sudo apt-get install -qq python-software-properties
+fi
+
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+sudo add-apt-repository "deb http://ftp.osuosl.org/pub/mariadb/repo/$VERSION/ubuntu $OS_CODENAME main"
+sudo apt-get update -qq
+
+sudo debconf-set-selections <<< "mariadb-server-$VERSION mysql-server/root_password password rootpasswd"
+sudo debconf-set-selections <<< "mariadb-server-$VERSION mysql-server/root_password_again password rootpasswd"
+sudo apt-get install -qq mariadb-server
