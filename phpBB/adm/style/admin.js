43b9314390db962aaef2411e22642f86730a6eb6
Vjacheslav Trushkin <cyberalien@gmail.com>
2013-10-25 19:59:45 +0300

    [ticket/11957] Enhance responsive nav with JS
    PHPBB3-11957

--- phpBB/adm/style/admin.js
+++ phpBB/adm/style/admin.js
@@ -3,6 +3,26 @@
 */
 
 /**
+* Parse document block
+*/
+function parse_document(container) 
+{
+	var test = document.createElement('div'),
+		oldBrowser = (typeof test.style.borderRadius == 'undefined');
+
+	delete test;
+
+	/**
+	* Navigation
+	*/
+	container.find('#menu .menu-block > a.header').click(function() {
+		$(this).parent().toggleClass('active');
+	});
+
+	container.find('#activemenu').parents('.menu-block').addClass('active');
+}
+
+/**
 * Run onload functions
 */
 (function($) {
@@ -14,5 +34,7 @@
 		$('form[data-focus]:first').each(function() {
 			$('#' + this.getAttribute('data-focus')).focus();
 		});
+
+		parse_document($('body'));
 	});
 })(jQuery);
