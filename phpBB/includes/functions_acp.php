7251feab67d8e457c052ac0bcd05f8b685d1d56c
Cesar G <prototech91@gmail.com>
2013-10-30 15:42:35 -0700

    [ticket/11987] Change {ROOT_PATH} to point to board root.
    PHPBB3-11987

--- phpBB/includes/functions_acp.php
+++ phpBB/includes/functions_acp.php
@@ -67,7 +67,7 @@ function adm_page_header($page_title)
 		'SID'					=> $SID,
 		'_SID'					=> $_SID,
 		'SESSION_ID'			=> $user->session_id,
-		'ROOT_PATH'				=> $phpbb_admin_path,
+		'ROOT_PATH'				=> $phpbb_root_path,
 
 		'U_LOGOUT'				=> append_sid("{$phpbb_root_path}ucp.$phpEx", 'mode=logout'),
 		'U_ADM_LOGOUT'			=> append_sid("{$phpbb_admin_path}index.$phpEx", 'action=admlogout'),
