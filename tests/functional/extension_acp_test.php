d17bf9aa273d8cd3dfb730b506f7aac2bd51a97a
Joas Schilling <nickvergessen@gmx.de>
2013-11-03 20:30:09 +0100

    [ticket/11994] "Delete data" is only available for disabled extensions
    PHPBB3-11994

--- tests/functional/extension_acp_test.php
+++ tests/functional/extension_acp_test.php
@@ -170,8 +170,8 @@ class phpbb_functional_extension_acp_test extends phpbb_functional_test_case
 		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&action=delete_data_pre&ext_name=test2&sid=' . $this->sid);
 		$this->assertContains('The required file does not exist', $crawler->filter('.errorbox')->text());
 
-		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&action=delete_data_pre&ext_name=foo&sid=' . $this->sid);
-		$this->assertContains($this->lang('EXTENSION_DELETE_DATA_CONFIRM', 'phpBB Foo Extension'), $crawler->filter('.errorbox')->text());
+		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&action=delete_data_pre&ext_name=vendor%2Fmoo&sid=' . $this->sid);
+		$this->assertContains($this->lang('EXTENSION_DELETE_DATA_CONFIRM', 'phpBB Moo Extension'), $crawler->filter('.errorbox')->text());
 	}
 
 	public function test_actions()
