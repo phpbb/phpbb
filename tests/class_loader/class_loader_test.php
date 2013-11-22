e0c8a81603a6e132c714adbdcb0a8762d01cbdfe
Joel Marcey <joelm@fb.com>
2013-11-22 15:05:39 -0800

    Shouldn't be 3 levels deep. Only two.

--- tests/class_loader/class_loader_test.php
+++ tests/class_loader/class_loader_test.php
@@ -7,7 +7,7 @@
 *
 */
 
-require_once dirname(__FILE__) . '/../../../phpBB/phpbb/cache/driver/driver_interface.php';
+require_once dirname(__FILE__) . '/../../phpBB/phpbb/cache/driver/driver_interface.php';
 
 class phpbb_class_loader_test extends PHPUnit_Framework_TestCase
 {
