981df153d2fbeba5f9aa6a677e11f1df15b3ea15
Andreas Fischer <bantu@phpbb.com>
2013-11-29 14:02:50 +0100

    [ticket/11699] Also exclude the phpBB/cache directory.
    PHPBB3-11699

--- tests/lint_test.php
+++ tests/lint_test.php
@@ -26,6 +26,7 @@ class phpbb_lint_test extends phpbb_test_case
 			dirname(__FILE__) . '/../.git',
 			dirname(__FILE__) . '/../build/new_version',
 			dirname(__FILE__) . '/../build/old_versions',
+			dirname(__FILE__) . '/../phpBB/cache',
 			// PHP Fatal error:  Cannot declare class Container because the name is already in use in /var/www/projects/phpbb3/tests/../phpBB/vendor/symfony/dependency-injection/Symfony/Component/DependencyInjection/Tests/Fixtures/php/services1-1.php on line 20
 			// https://gist.github.com/e003913ffd493da63cbc
 			dirname(__FILE__) . '/../phpBB/vendor',
