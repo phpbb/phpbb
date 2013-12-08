9b536b96520ac730d64a5e867aa8678c1657ac2c
Nathan Guse <nathaniel.guse@gmail.com>
2013-12-08 17:05:16 -0600

    [ticket/12066] Prepare form helper
    PHPBB3-12066

--- phpBB/phpbb/form.php
+++ phpBB/phpbb/form.php
@@ -27,54 +27,112 @@ use Symfony\Component\Validator\Validation;
 */
 class form
 {
-	/** @var phpbb\config\config */
+	/** @var \phpbb\config\config */
 	protected $phpbb_config;
 
-	/** @var phpbb\user */
+	/** @var \phpbb\event\dispatcher */
+	protected $phpbb_dispatcher;
+
+	/** @var \phpbb\request\request */
+	protected $phpbb_request;
+
+	/** @var \phpbb\user */
 	protected $phpbb_user;
+
+	/** @var \phpbb\symfony_request */
+	protected $symfony_request;
 
 	/**
 	* Constructor
 	*
-	* @param phpbb\config\config $phpbb_config
-	* @param phpbb\user $phpbb_user
+	* @param \phpbb\config\config $phpbb_config
+	* @param \phpbb\event\dispatcher $phpbb_dispatcher
+	* @param \phpbb\request\request $phpbb_request
+	* @param \phpbb\user $phpbb_user
+	* @param \phpbb\symfony_request $symfony_request
 	*/
-	public function __construct(\phpbb\config\config $phpbb_config, \phpbb\user $phpbb_user)
+	public function __construct(
+		\phpbb\config\config $phpbb_config,
+		\phpbb\event\dispatcher $phpbb_dispatcher,
+		\phpbb\request\request $phpbb_request,
+		\phpbb\user $phpbb_user,
+		\phpbb\symfony_request $symfony_request)
 	{
 		$this->phpbb_config = $phpbb_config;
+		$this->phpbb_dispatcher = $phpbb_dispatcher;
+		$this->phpbb_request = $phpbb_request;
 		$this->phpbb_user = $phpbb_user;
+		$this->symfony_request = $symfony_request;
 	}
 
 	/**
 	* Get a Symfony Form Builder
 	*
-	* @return Symfony\Component\Form\FormBuilder
+	* @param string $form_name The name of the form
+	* @return \Symfony\Component\Form\FormBuilder
 	*/
-	public function get_builder()
+	public function get_builder($form_name)
 	{
 		$token_sid = ($this->phpbb_user->data['user_id'] == ANONYMOUS && !empty($this->phpbb_config['form_token_sid_guests'])) ? $this->phpbb_user->session_id : '';
 		$secret = $this->phpbb_user->data['user_form_salt'] . $token_sid;
 
 		return Forms::createFormFactoryBuilder()
+			// CSRF Extension
 			->addExtension(
 				new CsrfExtension(
 					new DefaultCsrfProvider($secret)
 				)
 			)
+
+			// Validator Extension
 			->addExtension(
 				new ValidatorExtension(Validation::createValidator())
 			)
 			->getFormFactory()
-			->createBuilder()
-			->setRequestHandler(new HttpFoundationRequestHandler());
+			// Create the form builder
+			->createNamedBuilder($form_name, 'form', null, array(
+				'intention' => $form_name,
+			))
+
+			// Set the request Handler to use Symfony's request
+			->setRequestHandler(new HttpFoundationRequestHandler())
+
+			// Set fields to NOT be required by default
+			->setRequired(false)
+		;
 	}
 
-	public function handle_request($form, $request, $symfony_request)
+	/**
+	* Prepare the form
+	*
+	* This function creates an event to allow modifying the form (form.form_name)
+	* This also gets the form and handles the request
+	*
+	* @param \Symfony\Component\Form\FormBuilder $form
+	* @return null
+	*/
+	public function prepare_form(\Symfony\Component\Form\FormBuilder &$form)
 	{
-		$request->enable_super_globals();
+		/**
+		* This event is used to modify forms
+		*
+		* @event form.(form name)
+		* @var  Symfony\Component\Form\FormBuilder form
+		* @since 3.1-A3
+		*/
+		$vars = array('form');
+		extract($this->phpbb_dispatcher->trigger_event('form.' . $form->getName(), compact($vars)));
 
-		$form->handleRequest($symfony_request);
+		// Get the form
+		$form = $form->getForm();
 
-		$request->disable_super_globals();
+		// Must enable super globals for Symfony's Validators
+		$this->phpbb_request->enable_super_globals();
+
+		// Handle the request
+		$form->handleRequest($this->symfony_request);
+
+		// Disable super globals again
+		$this->phpbb_request->disable_super_globals();
 	}
 }
