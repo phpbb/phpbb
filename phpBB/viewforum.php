4f4512cbac2fe50abe46f16e00bdbad658ed42fa
Joas Schilling <nickvergessen@gmx.de>
2013-12-12 11:59:26 +0100

    [ticket/11849] Update pagination code in viewtopic.php
    PHPBB3-11849

--- phpBB/viewforum.php
+++ phpBB/viewforum.php
@@ -481,8 +481,8 @@ if ($start > $topics_count / 2)
 	// Select the sort order
 	$sql_sort_order = $sort_by_sql[$sort_key] . ' ' . (($sort_dir == 'd') ? 'ASC' : 'DESC');
 
-	$sql_limit = $pagination->reverse_limit($start, $per_page, $num_items);
-	$sql_start = $pagination->reverse_start($start, $sql_limit, $num_items);
+	$sql_limit = $pagination->reverse_limit($start, $sql_limit, $topics_count);
+	$sql_start = $pagination->reverse_start($start, $sql_limit, $topics_count);
 }
 else
 {
