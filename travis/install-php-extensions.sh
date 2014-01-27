a01229346288cbb3118adf159000f51260733606
Andreas Fischer <bantu@phpbb.com>
2014-01-27 01:17:26 +0100

    [ticket/11985] Reorganise travis/install-php-extensions.sh
    PHPBB3-11985

--- travis/install-php-extensions.sh
+++ travis/install-php-extensions.sh
@@ -4,18 +4,32 @@
 # @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 #
 set -e
+set -x
 
-function add_ext_to_php_ini
+function find_php_ini
 {
-	echo "extension=$1.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+	echo $(php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||")
 }
 
+# $1 - PHP extension name
+# $2 - PHP ini file path
+function install_php_extension
+{
+	echo "Installing $1 PHP extension"
+
+	# See http://www.php.net/manual/en/install.pecl.phpize.php
+	cd "$1"
+	phpize
+	./configure
+	make
+	make install
+	cd ..
+
+	echo "extension=$1.so" >> "$2"
+}
+
+php_ini_file=$(find_php_ini)
+
 # redis
-git clone git://github.com/nicolasff/phpredis.git
-cd phpredis
-phpize
-./configure
-make
-make install
-cd ..
-add_ext_to_php_ini 'redis'
+git clone git://github.com/nicolasff/phpredis.git redis
+install_php_extension 'redis' "$php_ini_file"
