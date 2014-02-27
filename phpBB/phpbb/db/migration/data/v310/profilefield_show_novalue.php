de0b7ddce9d0b5b11147cae7d96e0483015b81b2
Joas Schilling <nickvergessen@gmx.de>
2014-02-27 10:00:36 +0100

    [ticket/12194] Ensure that field_show_novalue is present
    The field was not created on boards that were 3.1-dev before 3.0.11 was relesed
    
    PHPBB3-12194

---
+++ phpBB/phpbb/db/migration/data/v310/profilefield_show_novalue.php
@@ -0,0 +1,45 @@
+<?php
+/**
+*
+* @package migration
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\db\migration\data\v310;
+
+class profilefield_show_novalue extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return $this->db_tools->sql_column_exists($this->table_prefix . 'profile_fields', 'field_show_novalue');
+	}
+
+	static public function depends_on()
+	{
+		return array('\phpbb\db\migration\data\v310\profilefield_types');
+	}
+
+	public function update_schema()
+	{
+		return array(
+			'add_columns' => array(
+				$this->table_prefix . 'profile_fields' => array(
+					'field_show_novalue' => array('BOOL', 0),
+				),
+			),
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+			'drop_columns' => array(
+				$this->table_prefix . 'profile_fields' => array(
+					'field_show_novalue',
+				),
+			),
+		);
+	}
+}
