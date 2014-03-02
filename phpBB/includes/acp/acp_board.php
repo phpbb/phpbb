6db1a2bfad121dbe0502869eea275a753d0d3a25
Forumhulp.com <info@forumhulp.com>
2014-03-02 08:19:14 +0100

    [ticket/12241] Event to add and/or modify acp_board configurations
    PHPBB3-12241

--- phpBB/includes/acp/acp_board.php
+++ phpBB/includes/acp/acp_board.php
@@ -455,6 +455,18 @@ class acp_board
 				trigger_error('NO_MODE', E_USER_ERROR);
 			break;
 		}
+		
+		
+		/**
+		* Event to add and/or modify acp_board configurations
+		*
+		* @event core.acp_config_edit_add
+		* @since 3.1-A4
+		*/
+		global $phpbb_dispatcher;
+		$vars = array('display_vars', 'mode');
+		extract($phpbb_dispatcher->trigger_event('core.acp_config_edit_add', compact($vars)));
+
 
 		if (isset($display_vars['lang']))
 		{
