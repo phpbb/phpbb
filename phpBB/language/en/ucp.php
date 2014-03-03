d40fb32dcdc447cd284d8c5dbffa845b142f1eeb
Crizzo <mail@crizzo.de>
2014-03-03 21:11:06 +0100

    [ticket/8785] Rebuilding posting_editor pm head + new language vars
    PHPBB3-8785

--- phpBB/language/en/ucp.php
+++ phpBB/language/en/ucp.php
@@ -472,7 +472,11 @@ $lang = array_merge($lang, array(
 	'TIMEZONE'					=> 'Timezone',
 	'TIMEZONE_DATE_SUGGESTION'	=> 'Suggestion: %s',
 	'TIMEZONE_INVALID'			=> 'The timezone you selected is invalid.',
-	'TO'						=> 'To',
+	'TO'						=> 'Recipient',
+	'TO_MASS'					=> 'Recipients',
+	'TO_ADD'					=> 'Add recipient',
+	'TO_ADD_MASS'				=> 'Add recipients',
+	'TO_ADD_GROUPS'				=> 'Add groups',
 	'TOO_MANY_RECIPIENTS'		=> 'You tried to send a private message to too many recipients.',
 	'TOO_MANY_REGISTERS'		=> 'You have exceeded the maximum number of registration attempts for this session. Please try again later.',
 
