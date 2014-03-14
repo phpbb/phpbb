fb6b350c21f5f63991099ae2ef9f007c9786cb61
Nathan Guse <nathaniel.guse@gmail.com>
2014-03-13 19:38:20 -0500

    [ticket/9871] Split fast and slow version tests
    PHPBB3-9871

--- tests/version/version_test.php
+++ tests/version/version_test.php
@@ -7,9 +7,6 @@
 *
 */
 
-/*
-* @group slow
-*/
 class phpbb_version_helper_test extends phpbb_test_case
 {
 	public function setUp()
@@ -317,28 +314,5 @@ class phpbb_version_helper_test extends phpbb_test_case
 			->will($this->returnValue($versions));
 
 		$this->assertSame($expected, $version_helper->get_latest_on_current_branch());
-	}
-
-	public function test_version_phpbb_com()
-	{
-		global $phpbb_root_path, $phpEx;
-		include_once($phpbb_root_path . 'includes/functions.' . $phpEx);
-
-		if (!phpbb_checkdnsrr('version.phpbb.com', 'A'))
-		{
-			$this->markTestSkipped(sprintf(
-				'Could not find a DNS record for hostname %s. ' .
-				'Assuming network is down.',
-				'version.phpbb.com'
-			));
-		}
-
-		$this->version_helper->get_versions();
-
-		// get_versions checks to make sure we got a valid versions file or
-		// throws an exception if we did not. We don't need to test anything
-		// here, but adding an assertion so we do not get a warning about no
-		// assertions in this test
-		$this->assertSame(true, true);
 	}
 }
