76f7ef5c837345c80f0dcefb6787f23a9c0159e7
geetakshi <geetakshi.19@gmail.com>
2014-03-24 00:12:40 +0530

    [ticket/11360] Updating default value of $display_online_list
    PHPBB3-11360

--- phpBB/ucp.php
+++ phpBB/ucp.php
@@ -353,7 +353,7 @@ $module->load_active();
 $module->assign_tpl_vars(append_sid("{$phpbb_root_path}ucp.$phpEx"));
 
 // Generate the page, do not display/query online list
-$module->display($module->get_page_title(), false);
+$module->display($module->get_page_title());
 
 /**
 * Function for assigning a template var if the zebra module got included
