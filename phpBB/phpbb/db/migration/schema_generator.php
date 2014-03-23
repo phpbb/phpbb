3153ec20d8be3851c6317dca8b34258ecda0f70c
Joas Schilling <nickvergessen@gmx.de>
2014-03-23 13:58:29 +0100

    [ticket/11459] Update doc blocks
    PHPBB3-11459

--- phpBB/phpbb/db/migration/schema_generator.php
+++ phpBB/phpbb/db/migration/schema_generator.php
@@ -2,8 +2,8 @@
 /**
 *
 * @package db
-* @copyright (c) 2011 phpBB Group
-* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
