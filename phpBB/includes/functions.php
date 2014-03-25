4f2a5120520aba766cb8a9e944e695213ec11b18
Crizzo <mail@crizzo.de>
2014-03-25 19:33:38 +0100

    [ticket/9758] Slims the line 4775 in includes/functions.php
    PHPBB3-9758

--- phpBB/includes/functions.php
+++ phpBB/includes/functions.php
@@ -4772,7 +4772,7 @@ function page_header($page_title = '', $display_online_list = false, $item_id = 
 	if ($user->data['user_id'] != ANONYMOUS)
 	{
 		$u_login_logout = append_sid("{$phpbb_root_path}ucp.$phpEx", 'mode=logout', true, $user->session_id);
-		$l_login_logout = sprintf($user->lang['LOGOUT'], $user->data['username']);
+		$l_login_logout = $user->lang['LOGOUT'];
 	}
 	else
 	{
