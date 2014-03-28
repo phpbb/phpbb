368dd23d4babf17e593ea9b16047319296881c46
Matt Friedman <maf675@gmail.com>
2014-03-28 09:08:11 -0700

    [ticket/12311] Use British-English spelling of licence in lang file
    PHPBB3-12311

--- phpBB/language/en/acp/extensions.php
+++ phpBB/language/en/acp/extensions.php
@@ -96,7 +96,7 @@ $lang = array_merge($lang, array(
 	'HOMEPAGE'				=> 'Homepage',
 	'PATH'					=> 'File Path',
 	'TIME'					=> 'Release Time',
-	'LICENSE'				=> 'License',
+	'LICENSE'				=> 'Licence',
 
 	'REQUIREMENTS'			=> 'Requirements',
 	'PHPBB_VERSION'			=> 'phpBB Version',
