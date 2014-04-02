2dc80fa078e38daf545fd88b0164712b038f8951
lucifer4o <lucifer@anavaro.com>
2014-04-03 00:23:57 +0300

    [ticket/12344] UCP PM submission hook
    Add core.pm_pre_submit to funtion submit_pm
    
    There is event request by EXreaction in the forum. As
    I need it for few other extensions I think this fulfills it.
    
    PHPBB3-12344

--- phpBB/includes/functions_privmsgs.php
+++ phpBB/includes/functions_privmsgs.php
@@ -1713,20 +1713,20 @@ function submit_pm($mode, $subject, &$data, $put_in_outbox = true)
 	}
 
 	/**
-	* Get pm_submit info
+	* Get all parts of the PM that is to be submited to the DB.
 	*
-	* @event	core.pm_submit
+	* @event	core.pm_pre_submit
 	* @var	string	mode	PM Post mode - post|reply|quote|quotepost|forward|edit
 	* @var	array	sql_data	sql_data that will be sent to PRIVMSGS_TABLE
 	* @var	array	recipients	array of all recipients of the PM
 	* @var	int	from_user	Who sends the pm
-	* @since 3.1-B3
+	* @since 3.1-b3
 	*/
 	$from_user = $data['from_user_id'];
 	$msg_id = $data['msg_id'];
 	$vars = array('sql_data', 'mode', 'recipients', 'from_user');
-	extract($phpbb_dispatcher->trigger_event('core.pm_submit', compact($vars)));
-
+	extract($phpbb_dispatcher->trigger_event('core.pm_pre_submit', compact($vars)));
+	
 	if (sizeof($sql_data))
 	{
 		$query = '';
