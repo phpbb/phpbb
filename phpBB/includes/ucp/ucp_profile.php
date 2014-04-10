49885e55e74335a6e2a1135bd00f23b01849ae80
Matt Friedman <maf675@gmail.com>
2014-04-10 09:04:35 -0700

    [ticket/12380] Sort Remember Me keys by last login time in UCP
    PHPBB3-12380

--- phpBB/includes/ucp/ucp_profile.php
+++ phpBB/includes/ucp/ucp_profile.php
@@ -672,7 +672,8 @@ class ucp_profile
 
 				$sql = 'SELECT key_id, last_ip, last_login
 					FROM ' . SESSIONS_KEYS_TABLE . '
-					WHERE user_id = ' . (int) $user->data['user_id'];
+					WHERE user_id = ' . (int) $user->data['user_id'] . '
+					ORDER BY last_login';
 
 				$result = $db->sql_query($sql);
 
