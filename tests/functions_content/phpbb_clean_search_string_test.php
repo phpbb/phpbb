601ecd3da132c376be2119ef7bc7126f219859f3
Oliver Schramm <oliver.schramm97@gmail.com>
2014-04-14 18:31:30 +0200

    [ticket/10423] Remove unnecessary include in test
    PHPBB3-10423

--- tests/functions_content/phpbb_clean_search_string_test.php
+++ tests/functions_content/phpbb_clean_search_string_test.php
@@ -7,7 +7,6 @@
 *
 */
 
-require_once dirname(__FILE__) . '/../../phpBB/includes/functions.php';
 require_once dirname(__FILE__) . '/../../phpBB/includes/functions_content.php';
 
 class phpbb_functions_content_phpbb_clean_search_string_test extends phpbb_test_case
