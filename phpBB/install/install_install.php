9c689ef8dd6313ec52cc1b06dcadba79bfd70b96
Joas Schilling <nickvergessen@gmx.de>
2014-04-25 12:45:48 +0200

    [ticket/11917] Replace space with tabs
    PHPBB3-11917

--- phpBB/install/install_install.php
+++ phpBB/install/install_install.php
@@ -1669,13 +1669,13 @@ class install_install extends module
 					WHERE module_basename = 'ucp_notifications'
 						AND module_class = 'ucp'
 						AND module_mode = 'notification_options'";
-                $result = $db->sql_query($sql);
+				$result = $db->sql_query($sql);
 				$row = $db->sql_fetchrow($result);
 				$db->sql_freeresult($result);
 
 				$_module->move_module_by($row, 'move_down', 4);
 
-                // Move OAuth module 5 down...
+				// Move OAuth module 5 down...
 				$sql = 'SELECT *
 					FROM ' . MODULES_TABLE . "
 					WHERE module_basename = 'ucp_auth_link'
