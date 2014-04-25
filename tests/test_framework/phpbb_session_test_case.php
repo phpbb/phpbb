f64b209db0ec7fb7f56de7f8366eee5b331d07e1
Pico88 <luki_9@wp.pl>
2014-04-25 23:01:49 +0200

    [ticket/12099] Add array to class method
    PHPBB3-12099

--- tests/test_framework/phpbb_session_test_case.php
+++ tests/test_framework/phpbb_session_test_case.php
@@ -29,7 +29,7 @@ abstract class phpbb_session_test_case extends phpbb_database_test_case
 			$symfony_request,
 			$phpbb_filesystem,
 			$this->getMock('\phpbb\request\request'),
-			new \phpbb\config\config(),
+			new \phpbb\config\config(array()),
 			$phpbb_root_path,
 			$phpEx
 		);
