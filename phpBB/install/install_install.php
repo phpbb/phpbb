ac8ce42a1297815c0991949ee8340fa682433cc1
n-aleha <nick_aleha@myway.com>
2014-04-27 22:44:41 +0300

    [ticket/12186] Move PM reports below post reports while installing for ascraeus
    In mcp, move PM reports below post reports while installing.
    
    PHPBB3-12186

--- phpBB/install/install_install.php
+++ phpBB/install/install_install.php
@@ -1654,7 +1654,7 @@ class install_install extends module
 				// Move pm report details module 3 down...
 				$sql = 'SELECT *
 					FROM ' . MODULES_TABLE . "
-					WHERE module_basename = 'pm_reports'
+					WHERE module_basename = 'mcp_pm_reports'
 						AND module_class = 'mcp'
 						AND module_mode = 'pm_report_details'";
 				$result = $db->sql_query($sql);
@@ -1666,7 +1666,7 @@ class install_install extends module
 				// Move closed pm reports module 3 down...
 				$sql = 'SELECT *
 					FROM ' . MODULES_TABLE . "
-					WHERE module_basename = 'pm_reports'
+					WHERE module_basename = 'mcp_pm_reports'
 						AND module_class = 'mcp'
 						AND module_mode = 'pm_reports_closed'";
 				$result = $db->sql_query($sql);
@@ -1678,7 +1678,7 @@ class install_install extends module
 				// Move open pm reports module 3 down...
 				$sql = 'SELECT *
 					FROM ' . MODULES_TABLE . "
-					WHERE module_basename = 'pm_reports'
+					WHERE module_basename = 'mcp_pm_reports'
 						AND module_class = 'mcp'
 						AND module_mode = 'pm_reports'";
 				$result = $db->sql_query($sql);
