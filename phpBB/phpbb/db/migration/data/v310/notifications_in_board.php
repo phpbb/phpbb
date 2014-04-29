cc45e6bb98ca121d29ca599615a73129c585d66a
Nicofuma <github@nicofuma.fr>
2014-04-29 16:18:20 +0200

    [ticket/11444] Correct package declaration
    PHPBB3-11444

--- phpBB/phpbb/db/migration/data/v310/notifications_in_board.php
+++ phpBB/phpbb/db/migration/data/v310/notifications_in_board.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package migration
-* @copyright (c) 2012 phpBB Group
+* @copyright (c) 2014 phpBB Group
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
