1982c00706b232e5617e97f1628aacd89dac3061
Nicofuma <github@nicofuma.fr>
2014-04-29 17:43:51 +0200

    [ticket/11444] Fix unit test (wrong config var)
    PHPBB3-11444

--- tests/notification/base.php
+++ tests/notification/base.php
@@ -62,7 +62,7 @@ abstract class phpbb_tests_notification_base extends phpbb_database_test_case
 			'allow_bookmarks'		=> true,
 			'allow_topic_notify'	=> true,
 			'allow_forum_notify'	=> true,
-			'enable_notifications_board'	=> true,
+			'allow_board_notifications'	=> true,
 		));
 		$user = $this->user = new \phpbb\user();
 		$this->user_loader = new \phpbb\user_loader($this->db, $phpbb_root_path, $phpEx, 'phpbb_users');
--- tests/notification/submit_post_base.php
+++ tests/notification/submit_post_base.php
@@ -72,7 +72,7 @@ abstract class phpbb_notification_submit_post_base extends phpbb_database_test_c
 		$config = new \phpbb\config\config(array(
 			'num_topics' => 1,
 			'num_posts' => 1,
-			'enable_notifications_board'	=> true,
+			'allow_board_notifications'	=> true,
 		));
 		set_config(null, null, null, $config);
 		set_config_count(null, null, null, $config);
