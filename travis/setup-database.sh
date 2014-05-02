5cef3254f71f2b0f06c98a4b18a5fc2a23bbac7b
Joas Schilling <nickvergessen@gmx.de>
2014-05-02 16:34:01 +0200

    [ticket/12470] Move setup of database to new .sh
    PHPBB3-12470

---
+++ travis/setup-database.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# @copyright (c) 2014 phpBB Group
+# @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+#
+set -e
+set -x
+
+DB=$1
+TRAVIS_PHP_VERSION=$2
+
+if [ "$DB" == "postgres" ]
+then
+	psql -c 'DROP DATABASE IF EXISTS phpbb_tests;' -U postgres
+	psql -c 'create database phpbb_tests;' -U postgres
+fi
+
+if [ "$TRAVIS_PHP_VERSION" == "5.3" -a "$DB" == "mysql" ]
+then
+	mysql -e 'SET GLOBAL storage_engine=MyISAM;'
+fi
+
+if [ "$DB" == "mysql" -o "$DB" == "mariadb" ]
+then
+	mysql -e 'create database IF NOT EXISTS phpbb_tests;'
+fi
