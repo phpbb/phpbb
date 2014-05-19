6aeb22bfd70ce16fb04d7d4f99cbef6c8d94395a
Matt Friedman <maf675@gmail.com>
2014-05-18 17:43:26 -0700

    [ticket/12564] Remove version definition from ACP/MCP/UCP module info files
    PHPBB3-12564

--- phpBB/includes/ucp/info/ucp_attachments.php
+++ phpBB/includes/ucp/info/ucp_attachments.php
@@ -17,7 +17,6 @@ class ucp_attachments_info
 		return array(
 			'filename'	=> 'ucp_attachments',
 			'title'		=> 'UCP_ATTACHMENTS',
-			'version'	=> '1.0.0',
 			'modes'		=> array(
 				'attachments'	=> array('title' => 'UCP_MAIN_ATTACHMENTS', 'auth' => 'acl_u_attach', 'cat' => array('UCP_MAIN')),
 			),
