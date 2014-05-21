6e478862273946a2088ff3b672ef64034473bcf3
Tristan Darricau <github@nicofuma.fr>
2014-05-22 01:24:11 +0200

    [ticket/12575] Remove  inline assignments
    PHPBB3-12575

--- phpBB/phpbb/di/service_collection.php
+++ phpBB/phpbb/di/service_collection.php
@@ -51,7 +51,8 @@ class service_collection extends \ArrayObject
 	*/
 	public function offsetGet($index)
 	{
-		if (($task = parent::offsetGet($index)) == null)
+		$task = parent::offsetGet($index);
+		if ($task == null)
 		{
 			$task = $this->container->get($index);
 			$this->offsetSet($index, $task);
