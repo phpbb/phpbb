b6135d3d02285542656b179c32ee7297f29fd5e7
Joas Schilling <nickvergessen@gmx.de>
2014-05-30 16:23:44 +0200

    [prep-release-3.1.0-b4] Update version to 3.1.0-b4

--- phpBB/install/convertors/convert_phpbb20.php
+++ phpBB/install/convertors/convert_phpbb20.php
@@ -37,7 +37,7 @@ $dbms = phpbb_convert_30_dbms_to_31($dbms);
 $convertor_data = array(
 	'forum_name'	=> 'phpBB 2.0.x',
 	'version'		=> '1.0.3',
-	'phpbb_version'	=> '3.1.0-b3',
+	'phpbb_version'	=> '3.1.0-b4',
 	'author'		=> '<a href="https://www.phpbb.com/">phpBB Limited</a>',
 	'dbms'			=> $dbms,
 	'dbhost'		=> $dbhost,
