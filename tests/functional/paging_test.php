f2c7890c64082232061ba870ed6781166b1e130d
PayBas <contact@paybas.com>
2014-05-30 02:34:07 +0200

    [ticket/12613] Fix functional test and post sorting bug
    PHPBB3-12613

--- tests/functional/paging_test.php
+++ tests/functional/paging_test.php
@@ -30,12 +30,12 @@ class phpbb_functional_paging_test extends phpbb_functional_test_case
 		$this->assertContains('post no4', $crawler->text());
 		$this->assertNotContains('post no16', $crawler->text());
 
-		$next_link = $crawler->filter('#viewtopic > fieldset > a.arrow-right')->attr('href');
+		$next_link = $crawler->filter('.pagination > ul > li.next > a')->attr('href');
 		$crawler = self::request('GET', $next_link);
 		$this->assertNotContains('post no4', $crawler->text());
 		$this->assertContains('post no16', $crawler->text());
 
-		$prev_link = $crawler->filter('#viewtopic > fieldset > a.arrow-left')->attr('href');
+		$prev_link = $crawler->filter('.pagination > ul > li.previous > a')->attr('href');
 		$crawler = self::request('GET', $prev_link);
 		$this->assertContains('post no4', $crawler->text());
 		$this->assertNotContains('post no16', $crawler->text());
