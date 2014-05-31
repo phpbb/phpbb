fd9c17ca0486c3000e263dcd53848e3610ec07fc
Tristan Darricau <github@nicofuma.fr>
2014-05-31 17:42:40 +0200

    [ticket/12639] Update tests to use a dataProvider
    PHPBB3-12639

--- tests/log/delete_test.php
+++ tests/log/delete_test.php
@@ -1,9 +1,13 @@
 <?php
 /**
 *
-* @package testing
-* @copyright (c) 2012 phpBB Group
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
 *
 */
 
@@ -13,53 +17,146 @@ require_once dirname(__FILE__) . '/../../phpBB/includes/utf/utf_tools.php';
 
 class phpbb_log_delete_test extends phpbb_database_test_case
 {
+	protected $log;
+
 	public function getDataSet()
 	{
 		return $this->createXMLDataSet(dirname(__FILE__) . '/fixtures/delete_log.xml');
 	}
 
-	public function test_log_delete()
+	protected function setUp()
 	{
 		global $phpbb_root_path, $phpEx, $db, $phpbb_dispatcher, $auth;
 
 		$db = $this->new_dbal();
-		$cache = new phpbb_mock_cache;
 		$phpbb_dispatcher = new phpbb_mock_event_dispatcher();
 		$user = $this->getMock('\phpbb\user');
 		$user->data['user_id'] = 1;
 		$auth = $this->getMock('\phpbb\auth\auth');
 
-		$log = new \phpbb\log\log($db, $user, $auth, $phpbb_dispatcher, $phpbb_root_path, 'adm/', $phpEx, LOG_TABLE);
+		$this->log = new \phpbb\log\log($db, $user, $auth, $phpbb_dispatcher, $phpbb_root_path, 'adm/', $phpEx, LOG_TABLE);
 
-		// Delete all admin logs
-		$this->assertCount(2, $log->get_logs('admin'));
-		$log->delete('admin');
-		// One entry is added to the admin log when the logs are purged
-		$this->assertCount(1, $log->get_logs('admin'));
+		parent::setUp();
+	}
 
-		// Delete with keyword
-		$this->assertCount(1, $log->get_logs('mod', false, 0, 0, 0, 0, 0, 0, 'l.log_time DESC', 'guest'));
-		$log->delete('mod', array('keywords' => 'guest'));
-		$this->assertEmpty($log->get_logs('mod', false, 0, 0, 0, 0, 0, 0, 'l.log_time DESC', 'guest'));
+	public function log_delete_data()
+	{
+		return array(
+			array(
+				array(1, 2),
+				array(16),
+				array(),
+				'admin',
+				false,
+				0,
+				0,
+				0,
+				0,
+				0,
+				0,
+				'l.log_time DESC',
+				'',
+			),
+			array(
+				array(11),
+				array(),
+				array('keywords' => 'guest'),
+				'mod',
+				false,
+				0,
+				0,
+				0,
+				0,
+				0,
+				0,
+				'l.log_time DESC',
+				'guest',
+			),
+			array(
+				array(4, 5, 7),
+				array(),
+				array('forum_id' => 12, 'user_id' => 1),
+				'mod',
+				false,
+				0,
+				0,
+				12,
+				0,
+				1,
+				0,
+				'l.log_time DESC',
+				'',
+			),
+			array(
+				array(12, 13),
+				array(),
+				array('forum_id' => array('IN' => array(14, 13))),
+				'mod',
+				false,
+				0,
+				0,
+				array(13, 14),
+				0,
+				0,
+				0,
+				'l.log_time DESC',
+				'',
+			),
+			array(
+				array(3, 14, 15),
+				array(3),
+				array('user_id' => array('>', 1)),
+				'critical',
+				false,
+				0,
+				0,
+				0,
+				0,
+				0,
+				0,
+				'l.log_time DESC',
+				'',
+			),
+			array(
+				array(3, 14, 15),
+				array(),
+				array('keywords' => ''),
+				'critical',
+				false,
+				0,
+				0,
+				0,
+				0,
+				0,
+				0,
+				'l.log_time DESC',
+				'',
+			),
+		);
+	}
 
-		// Delete with simples conditions
-		$this->assertCount(3, $log->get_logs('mod', false, 0, 0, 12, 0, 1, 0, 'l.log_time DESC'));
-		$log->delete('mod', array('forum_id' => 12, 'user_id' => 1));
-		$this->assertEmpty($log->get_logs('mod', false, 0, 0, 12, 0, 1, 0, 'l.log_time DESC'));
-
-		// Delete with IN condition
-		$this->assertCount(2, $log->get_logs('mod', false, 0, 0, array(13, 14), 0, 0, 0, 'l.log_time DESC'));
-		$log->delete('mod', array('forum_id' => array('IN' => array(14, 13))));
-		$this->assertEmpty($log->get_logs('mod', false, 0, 0, array(13, 14), 0, 0, 0, 'l.log_time DESC'));
-
-		// Delete with a custom condition (ie: WHERE x >= 10)
-		$this->assertCount(3, $log->get_logs('critical', false, 0, 0, 0, 0, 0, 0, 'l.log_time DESC'));
-		$log->delete('critical', array('user_id' => array('>', 1)));
-		$this->assertCount(1, $log->get_logs('critical', false, 0, 0, 0, 0, 0, 0, 'l.log_time DESC'));
-
+	/**
+	* @dataProvider log_delete_data
+	*/
+	public function test_log_delete($expected_before, $expected_after, $delete_conditions, $mode, $count_logs, $limit, $offset, $forum_id, $topic_id, $user_id, $log_time, $sort_by, $keywords)
+	{
+		$this->assertEquals($expected_before, $this->get_ids($this->log->get_logs($mode, $count_logs, $limit, $offset, $forum_id, $topic_id, $user_id, $log_time, $sort_by, $keywords)), 'before');
+		$this->log->delete($mode, $delete_conditions);
+		$this->assertEquals($expected_after, $this->get_ids($this->log->get_logs($mode, $count_logs, $limit, $offset, $forum_id, $topic_id, $user_id, $log_time, $sort_by, $keywords)), 'after');
+/*
 		// Delete with an empty keyword list
 		$this->assertCount(1, $log->get_logs('critical', false, 0, 0, 0, 0, 0, 0, 'l.log_time DESC'));
 		$log->delete('critical', array('keywords' => ''));
-		$this->assertEmpty($log->get_logs('critical', false, 0, 0, 0, 0, 0, 0, 'l.log_time DESC'));
+		$this->assertEmpty($log->get_logs('critical', false, 0, 0, 0, 0, 0, 0, 'l.log_time DESC'));*/
+	}
+
+	public function get_ids($logs)
+	{
+		$ids = array();
+		foreach ($logs as $log_entry)
+		{
+			$ids[] = $log_entry['id'];
+		}
+		return $ids;
 	}
 }
