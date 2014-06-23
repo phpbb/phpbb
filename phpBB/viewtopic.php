8984dc610348d7567e5b297c9923286d0c72ae5b
Crizzo <mail@crizzo.de>
2014-06-23 20:12:12 +0200

    [ticket/12680] Brings back get_username_string()
    PHPBB3-12680

--- phpBB/viewtopic.php
+++ phpBB/viewtopic.php
@@ -1197,7 +1197,7 @@ while ($row = $db->sql_fetchrow($result))
 
 				'username'			=> $row['username'],
 				'user_colour'		=> $row['user_colour'],
-				'contact_user' 		=> $user->lang('CONTACT_USER', $row['username']),
+				'contact_user' 		=> $user->lang('CONTACT_USER', get_username_string('username', $poster_id, $row['username'], $row['user_colour'], $row['username'])),
 
 				'online'		=> false,
 				'jabber'		=> ($row['user_jabber'] && $auth->acl_get('u_sendim')) ? append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=contact&amp;action=jabber&amp;u=$poster_id") : '',
