f1856bd0c485f9864343f17ac05df256270470ad
Pico88 <luki_9@wp.pl>
2014-07-08 20:26:57 +0200

    [ticket/12661] Fix path to extension
    PHPBB3-12661

--- tests/template/template_alldir_test.php
+++ tests/template/template_alldir_test.php
@@ -55,7 +55,7 @@ class phpbb_template_alldir_test extends phpbb_template_template_test_case
 		);
 
 		$this->template_path = $this->test_path . '/templates';
-		$this->ext_template_path = 'tests/extension/ext';
+		$this->ext_template_path = 'tests/extension/ext/vendor4/bar/styles';
 		$this->template = new \phpbb\template\twig\twig($path_helper, $config, $this->user, new \phpbb\template\context(), $this->extension_manager);
 		$this->template->set_custom_style('all', array($this->template_path, $this->ext_template_path));
 	}
