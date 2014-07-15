3eafeeb88d173bc4f2b082ee5f09f85bef931ec9
Carlo <carlo@phpbbitalia.net>
2014-07-15 13:31:34 +0200

    [ticket/12685] Removed unused USE statement
    PHPBB3-12685

--- phpBB/phpbb/console/application.php
+++ phpBB/phpbb/console/application.php
@@ -17,7 +17,6 @@ use Symfony\Component\Console\Shell;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class application extends \Symfony\Component\Console\Application
 {
