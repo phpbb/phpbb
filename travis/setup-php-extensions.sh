0a6fdbe6362ad85cc99d479101578789db16403f
Andreas Fischer <bantu@phpbb.com>
2014-08-09 15:24:21 +0200

    [ticket/12948] Remove no longer needed opcache workaround. Travis has 5.5.15.
    PHPBB3-12948

--- travis/setup-php-extensions.sh
+++ travis/setup-php-extensions.sh
@@ -42,12 +42,6 @@ function install_php_extension
 
 php_ini_file=$(find_php_ini)
 
-# disable broken opcache on PHP 5.5.7 and 5.5.8
-if [ `php -r "echo (int) version_compare(PHP_VERSION, '5.5.9', '<');"` == "1" ]
-then
-	sed -i '/opcache.so/d' "$php_ini_file"
-fi
-
 # apc
 if [ `php -r "echo (int) version_compare(PHP_VERSION, '5.5.0-dev', '<');"` == "1" ]
 then
