d720428564ee1982c3087a9433304d630c7d26af
Marc Alexander <admin@m-a-styles.de>
2014-09-19 15:30:24 +0200

    [ticket/13073] Switch $input with $expected and add paths with letters
    PHPBB3-13073

--- tests/functions/get_preg_expression_test.php
+++ tests/functions/get_preg_expression_test.php
@@ -18,20 +18,22 @@ class phpbb_functions_get_preg_expression_test extends phpbb_test_case
 	public function data_path_remove_dot_trailing_slash()
 	{
 		return array(
-			array('/..', '$2', './../'),
-			array('/..', '$2', '/../'),
+			array('./../', '$2', '/..'),
+			array('/../', '$2', '/..'),
 			array('', '$2', ''),
-			array('', '$2', './'),
-			array('', '$2', '/'),
-			array('/../..', '$2', './../../'),
-			array('/../..', '$2', '/../../'),
+			array('./', '$2', ''),
+			array('/', '$2', ''),
+			array('./../../', '$2', '/../..'),
+			array('/../../', '$2', '/../..'),
+			array('./dir/', '$2', '/dir'),
+			array('./../dir/', '$2', '/../dir'),
 		);
 	}
 
 	/**
 	 * @dataProvider data_path_remove_dot_trailing_slash
 	 */
-	public function test_path_remove_dot_trailing_slash($expected, $replace, $input)
+	public function test_path_remove_dot_trailing_slash($input, $replace, $expected)
 	{
 		$this->assertSame($expected, preg_replace(get_preg_expression('path_remove_dot_trailing_slash'), $replace, $input));
 	}
