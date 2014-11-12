efd4b43c1bbea587a480c145d8a79066ae78d9a3
omniError <omniError@github>
2014-11-11 20:20:52 -0600

    [ticket/13306] constructor sets error types
    https://tracker.phpbb.com/browse/PHPBB3-13306
    
    PHPBB3-13306

--- phpBB/phpbb/error_collector.php
+++ phpBB/phpbb/error_collector.php
@@ -16,15 +16,27 @@ namespace phpbb;
 class error_collector
 {
 	var $errors;
+	var $error_types;
 
-	function __construct()
+	/**
+	 * Constructor.
+	 *
+	 * The variable $error_types may be set to a mask of PHP error types that
+	 * the collector should keep, e.g. `E_ALL`. If unset, the current value of
+	 * the error_reporting() function will be used to determine which errors
+	 * the collector will keep.
+	 *
+	 * @param int|null $error_types
+	 */
+	function __construct($error_types = null)
 	{
 		$this->errors = array();
+		$this->error_types = !empty($error_types) ? $error_types : error_reporting();
 	}
 
 	function install()
 	{
-		set_error_handler(array(&$this, 'error_handler'), error_reporting());
+		set_error_handler(array(&$this, 'error_handler'), $this->error_types);
 	}
 
 	function uninstall()
