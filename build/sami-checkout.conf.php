b55d744e775d548f451384422d9954309a2bc4b5
Andreas Fischer <bantu@phpbb.com>
2014-11-12 21:26:35 +0100

    [ticket/13331] Use docs target for this branch, add docs-all for dev branches.
    PHPBB3-13331

--- build/sami-checkout.conf.php
+++ build/sami-checkout.conf.php
@@ -1,7 +1,14 @@
 <?php
 /**
-* @copyright (c) 2014 phpBB Group
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
 */
 
 // Prevent 'Class "acm" does not exist.' exception on removeClass().
@@ -24,27 +31,14 @@ $iterator = Symfony\Component\Finder\Finder::create()
 	->notPath('data')
 ;
 
-$versions = Sami\Version\GitVersionCollection::create(__DIR__ . '/../')
-	/*
-	This would be nice, but currently causes various problems that need
-	debugging.
-	->addFromTags('release-3.0.*')
-	->add('develop-olympus', '3.0-next (olympus)')
-	->addFromTags('release-3.1.*')
-	->add('develop-ascraeus', '3.1-next (ascraeus)')
-	->add('develop')
-	*/
-	->add('develop-olympus')
-	->add('develop-ascraeus')
-;
-
-return new Sami\Sami($iterator, array(
+$config = array(
 	'theme'                => 'enhanced',
-	'versions'             => $versions,
 	'title'                => 'phpBB API Documentation',
 	'build_dir'            => __DIR__.'/api/output/%version%',
 	'cache_dir'            => __DIR__.'/api/cache/%version%',
 	'default_opened_level' => 2,
 	// Do not use JsonStore. See https://github.com/fabpot/Sami/issues/79
 	'store'                => new PhpbbArrayStore,
-));
+);
+
+return new Sami\Sami($iterator, $config);
