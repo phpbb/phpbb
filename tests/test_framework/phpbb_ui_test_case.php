8df63a44997a1e34575dd7f2a0d93eb47a51a562
Andreas Fischer <bantu@phpbb.com>
2014-11-12 20:32:36 +0100

    [ticket/13325] Remove whitespace at end of line.
    PHPBB3-13325

--- tests/test_framework/phpbb_ui_test_case.php
+++ tests/test_framework/phpbb_ui_test_case.php
@@ -49,7 +49,7 @@ class phpbb_ui_test_case extends phpbb_test_case
 		{
 			try {
 				$capabilities = array(\WebDriverCapabilityType::BROWSER_NAME => 'firefox');
-				self::$webDriver = RemoteWebDriver::create(self::$host . ':' . self::$port, $capabilities);	
+				self::$webDriver = RemoteWebDriver::create(self::$host . ':' . self::$port, $capabilities);
 			} catch (WebDriverCurlException $e) {
 				self::markTestSkipped('PhantomJS webserver is not running.');
 			}
