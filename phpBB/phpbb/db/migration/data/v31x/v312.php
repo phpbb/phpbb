eb83bd5797d3fb7e6c1380b5ecaa2d0a2e69dd82
Joas Schilling <nickvergessen@gmx.de>
2014-11-25 22:42:52 +0100

    [ticket/13381] Fix header intendation of v312.php
    PHPBB3-13381

--- phpBB/phpbb/db/migration/data/v31x/v312.php
+++ phpBB/phpbb/db/migration/data/v31x/v312.php
@@ -1,15 +1,15 @@
 <?php
 /**
- *
- * This file is part of the phpBB Forum Software package.
- *
- * @copyright (c) phpBB Limited <https://www.phpbb.com>
- * @license GNU General Public License, version 2 (GPL-2.0)
- *
- * For full copyright and license information, please see
- * the docs/CREDITS.txt file.
- *
- */
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
 
 namespace phpbb\db\migration\data\v31x;
 
