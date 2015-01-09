d2a4d6624f9e994469fff8b37af51caa5de3287c
Joas Schilling <nickvergessen@gmx.de>
2015-01-09 19:46:14 +0100

    [ticket/13307] Update mysql_upgrader.php to work without the schema_data.php
    The file has been delete quite some time ago.
    
    PHPBB3-13307

--- phpBB/develop/mysql_upgrader.php
+++ phpBB/develop/mysql_upgrader.php
@@ -62,10 +62,14 @@ echo "USE $dbname;$newline$newline";
 
 @set_time_limit(0);
 
-require($phpbb_root_path . 'includes/db/schema_data.' . $phpEx);
-require($phpbb_root_path . 'phpbb/db/tools.' . $phpEx);
+$finder = new \phpbb\finder(new \phpbb\filesystem(), $phpbb_root_path);
+$classes = $finder->core_path('phpbb/')
+	->directory('/db/migration/data')
+	->get_classes();
 
-$dbms_type_map = phpbb\db\tools::get_dbms_type_map();
+$schema_generator = new \phpbb\db\migration\schema_generator($classes, $config, $db, new \phpbb\db\tools($db, true), $phpbb_root_path, $phpEx, $table_prefix);
+$schema_data = $schema_generator->get_schema();
+$dbms_type_map = \phpbb\db\tools::get_dbms_type_map();
 
 foreach ($schema_data as $table_name => $table_data)
 {
