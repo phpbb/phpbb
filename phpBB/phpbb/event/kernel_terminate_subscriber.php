ffc5a73a0cb8d7f6a10aace54be8c4a12fe0a0d3
Nicofuma <github@nicofuma.fr>
2015-02-22 16:36:18 +0100

    [ticket/13643] Change the priority of kernel_terminate_subscriber to PHP_INT_MIN
    PHP_INT_MIN isn't a defined constant but it can be calculed by
    ~PHP_INT_MAX (two's complement of PHP_INT_MAX)
    
    PHPBB3-13643

--- phpBB/phpbb/event/kernel_terminate_subscriber.php
+++ phpBB/phpbb/event/kernel_terminate_subscriber.php
@@ -35,7 +35,7 @@ class kernel_terminate_subscriber implements EventSubscriberInterface
 	public static function getSubscribedEvents()
 	{
 		return array(
-			KernelEvents::TERMINATE		=> 'on_kernel_terminate',
+			KernelEvents::TERMINATE		=> array('on_kernel_terminate', ~PHP_INT_MAX),
 		);
 	}
 }
