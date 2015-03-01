5fa6871ebf486eb70052c2e5a07b96d6b23d88c0
brunoais <brunoaiss@gmail.com>
2015-03-01 19:56:50 +0000

    [ticket/13664] Add event to allow changing number of unapproved posts listed
    PHPBB3-13664

--- phpBB/includes/mcp/mcp_front.php
+++ phpBB/includes/mcp/mcp_front.php
@@ -49,6 +49,18 @@ function mcp_front_view($id, $mode, $action)
 				'WHERE' => $db->sql_in_set('forum_id', $forum_list) . '
 					AND ' . $db->sql_in_set('post_visibility', array(ITEM_UNAPPROVED, ITEM_REAPPROVE))
 			);
+
+			/**
+			* Allow altering the query to get the number of unapproved posts
+			*
+			* @event core.mcp_front_queue_unapproved_total_before
+			* @var	int		sql_ary						Query to get the total number of unapproved posts
+			* @var	array	forum_list					List of forums to look for unapproved posts
+			* @since 3.1.4-RC1
+			*/
+			$vars = array('sql_ary', 'forum_list');
+			extract($phpbb_dispatcher->trigger_event('core.mcp_front_queue_unapproved_total_before', compact($vars)));
+
 			$sql = $db->sql_build_query('SELECT', $sql_ary);
 			$result = $db->sql_query($sql);
 			$total = (int) $db->sql_fetchfield('total');
