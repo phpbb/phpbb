0e5d1decbf64eb1192c0ad697c38f368e8f970e7
Marc Alexander <admin@m-a-styles.de>
2015-05-20 09:03:59 +0200

    [ticket/13832] Use preg_replace_callback in check_flash_bbcodes
    PHPBB3-13832

--- phpBB/develop/check_flash_bbcodes.php
+++ phpBB/develop/check_flash_bbcodes.php
@@ -140,8 +140,13 @@ function html_entity_decode_utf8($string)
 	static $trans_tbl;
 
 	// replace numeric entities
-	$string = preg_replace('~&#x([0-9a-f]+);~ei', 'code2utf8(hexdec("\\1"))', $string);
+	$string = preg_replace_callback('~&#x([0-9a-f]+);~i', function ($match) {
+		return code2utf8(hexdec($match[1]));
+	}, $string);
 	$string = preg_replace('~&#([0-9]+);~e', 'code2utf8(\\1)', $string);
+	$string = preg_replace_callback('~&#([0-9]+);~', function ($match) {
+		return code2utf8($match[1]);
+	}, $string);
 
 	// replace literal entities
 	if (!isset($trans_tbl))
