3f19453cd1d28ed68ae0e0fe673c40c042922a28
alf007 <alf007@free.fr>
2015-05-28 20:55:59 +0200

    [ticket/13311] Add core event on acp_forums for forum content deletes
    Event added for the function delete_forum_content() in
    includes/acp/acp_forums.php, so that you can insert any extension's table(s)
    to be updated when forum content is deleted
    Incude parameters forum_id, topic_ids and post_counts
    (better with more than the comment ^^)
    
    PHPBB3-13311

--- phpBB/includes/acp/acp_forums.php
+++ phpBB/includes/acp/acp_forums.php
@@ -1930,6 +1930,9 @@ class acp_forums
 		 */
 		$vars = array(
 				'table_ary',
+				'forum_id',
+				'topic_ids'
+				'post_counts',
 		);
 		extract($phpbb_dispatcher->trigger_event('core.delete_forum_content_before_query', compact($vars)));
 
