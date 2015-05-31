a8368e29a293464908a23bc67b82236a0860cd07
brunoais <brunoaiss@gmail.com>
2015-05-31 19:42:15 +0100

    [feature/SCEditor] Update DB migration to accommodate the new idea
    PHPBB3-10418

--- phpBB/phpbb/db/migration/data/v320/add_wysiwyg.php
+++ phpBB/phpbb/db/migration/data/v320/add_wysiwyg.php
@@ -27,10 +27,9 @@ class add_wysiwyg extends \phpbb\db\migration\migration
 			'add_columns'		=> array(
 				$this->table_prefix . 'users'	=> array(
 					'COLUMNS'			=> array(
-						'user_wysiwyg_editor'			=> array('VCHAR:15', ''),
-						'user_wysiwyg_default_mode'		=> array('TINT:2', 0),
-						'user_wysiwyg_buttons_mode'		=> array('TINT:2', 1),
-						'user_wysiwyg_override_data'	=> array('VCHAR:255', ''),
+						'user_wysiwyg_editor'			=> array('VCHAR:60', 'wysiwyg.converters.sce'),
+						'user_wysiwyg_default_mode'		=> array('USINT:5', 1),
+						'user_wysiwyg_buttons_mode'		=> array('USINT:5', 2),
 					),
 				),
 			),
