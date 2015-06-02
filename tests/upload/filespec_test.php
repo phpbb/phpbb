25df7f814920a67b1d9b1e65e797c44960026585
Marc Alexander <admin@m-a-styles.de>
2015-06-02 12:24:31 +0200

    [ticket/13904] Remove functions_fileupload.php
    PHPBB3-13904

--- tests/upload/filespec_test.php
+++ tests/upload/filespec_test.php
@@ -13,7 +13,6 @@
 
 require_once __DIR__ . '/../../phpBB/includes/functions.php';
 require_once __DIR__ . '/../../phpBB/includes/utf/utf_tools.php';
-require_once __DIR__ . '/../../phpBB/includes/functions_upload.php';
 
 class phpbb_filespec_test extends phpbb_test_case
 {
