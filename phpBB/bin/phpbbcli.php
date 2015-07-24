be7e1ba7a0d35a86c4b41ab037e82a3a114108cb
Mate Bartus <mate.bartus@gmail.com>
2015-07-24 18:05:10 +0200

    [ticket/13740] Use language service in console application
    PHPBB3-13740

--- phpBB/bin/phpbbcli.php
+++ phpBB/bin/phpbbcli.php
@@ -68,7 +68,8 @@ require($phpbb_root_path . 'includes/compatibility_globals.' . $phpEx);
 $user = $phpbb_container->get('user');
 $user->add_lang('acp/common');
 $user->add_lang('cli');
+$lang = $phpbb_container->get('language');
 
-$application = new \phpbb\console\application('phpBB Console', PHPBB_VERSION, $user);
+$application = new \phpbb\console\application('phpBB Console', PHPBB_VERSION, $lang);
 $application->register_container_commands($phpbb_container->get('console.command_collection'));
 $application->run($input);
