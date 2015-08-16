ec032733de6e693874a6cc74ad5e11f49303a362
rxu <rxu@mail.ru>
2015-08-16 19:55:00 +0700

    [ticket/14106] Fix topics sorting in MCP for topics merging.
    The "bug" in the deleted code comment is non-existent now.
    The PR is for 3.1 only.
    
    PHPBB3-14106

--- phpBB/includes/mcp/mcp_forum.php
+++ phpBB/includes/mcp/mcp_forum.php
@@ -35,15 +35,6 @@ function mcp_forum_view($id, $mode, $action, $forum_info)
 	// merge_topic is the quickmod action, merge_topics is the mcp_forum action, and merge_select is the mcp_topic action
 	$merge_select = ($action == 'merge_select' || $action == 'merge_topic' || $action == 'merge_topics') ? true : false;
 
-	if ($merge_select)
-	{
-		// Fixes a "bug" that makes forum_view use the same ordering as topic_view
-		$request->overwrite('sk', null);
-		$request->overwrite('sd', null);
-		$request->overwrite('sk', null, \phpbb\request\request_interface::POST);
-		$request->overwrite('sd', null, \phpbb\request\request_interface::POST);
-	}
-
 	$forum_id			= $forum_info['forum_id'];
 	$start				= request_var('start', 0);
 	$topic_id_list		= request_var('topic_id_list', array(0));
