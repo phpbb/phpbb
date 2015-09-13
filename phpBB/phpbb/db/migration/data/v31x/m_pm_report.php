aa01ee1bbccd415591771635179991c9f1fc8379
Zoddo <zoddo.ino@gmail.com>
2015-09-13 14:06:10 +0200

    [ticket/8920] Revert update_module_auth() on revert_data()
    PHPBB3-8920

--- phpBB/phpbb/db/migration/data/v31x/m_pm_report.php
+++ phpBB/phpbb/db/migration/data/v31x/m_pm_report.php
@@ -31,6 +31,17 @@ class m_pm_report extends \phpbb\db\migration\migration
 		);
 	}
 
+	public function revert_data()
+	{
+		return array(
+			array('permission.remove', array('m_pm_report')),
+			array('custom', array(
+					array($this, 'revert_module_auth'),
+				),
+			),
+		);
+	}
+
 	public function update_module_auth()
 	{
 		$sql = 'UPDATE ' . MODULES_TABLE . "
@@ -38,6 +49,16 @@ class m_pm_report extends \phpbb\db\migration\migration
 			WHERE module_class = 'mcp'
 				AND module_basename = 'mcp_pm_reports'
 				AND module_auth = 'aclf_m_report'";
+		$this->db->sql_query($sql);
+	}
+
+	public function revert_module_auth()
+	{
+		$sql = 'UPDATE ' . MODULES_TABLE . "
+			SET module_auth = 'aclf_m_report'
+			WHERE module_class = 'mcp'
+				AND module_basename = 'mcp_pm_reports'
+				AND module_auth = 'acl_m_pm_report'";
 		$this->db->sql_query($sql);
 	}
 }
