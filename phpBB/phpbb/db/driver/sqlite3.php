63534d655bb93774529bb75d17769f7bf10c1680
Marc Alexander <admin@m-a-styles.de>
2015-10-23 17:44:16 +0200

    [ticket/14044] Use preg_replace() instead of str_replace()
    PHPBB3-14044

--- phpBB/phpbb/db/driver/sqlite3.php
+++ phpBB/phpbb/db/driver/sqlite3.php
@@ -136,7 +136,7 @@ class sqlite3 extends \phpbb\db\driver\driver
 			{
 				if ($this->transaction === true && strpos($query, 'INSERT') === 0)
 				{
-					$query = str_replace('INSERT INTO', 'INSERT OR ROLLBACK INTO', $query);
+					$query = preg_replace('/^INSERT INTO/', 'INSERT OR ROLLBACK INTO', $query);
 				}
 
 				if (($this->query_result = @$this->dbo->query($query)) === false)
