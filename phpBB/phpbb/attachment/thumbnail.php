7fdd7dd394ab6c1d4b39abf987abcfe9dcff167b
Marc Alexander <admin@m-a-styles.de>
2015-11-12 13:29:16 +0100

    [ticket/14286] Further split up thumbnail class
    PHPBB3-14286

--- phpBB/phpbb/attachment/thumbnail.php
+++ phpBB/phpbb/attachment/thumbnail.php
@@ -31,6 +31,12 @@ class thumbnail
 	/** @var FastImageSize */
 	protected $image_size;
 
+	/** @var string Source file path */
+	protected $source;
+
+	/** @var string Destination file path */
+	protected $destination;
+
 	/** @var int Source image height */
 	protected $height;
 
@@ -45,6 +51,9 @@ class thumbnail
 
 	/** @var int Source image width */
 	protected $new_width;
+
+	/** @var bool Flag whether thumbnail was created */
+	private $thumbnail_created = false;
 
 	/**
 	 * Thumbnail constructor
@@ -71,134 +80,29 @@ class thumbnail
 	 */
 	function create($source, $destination, $mime_type)
 	{
-		if (!$this->set_size($source, $mime_type))
+		$this->set_paths($source, $destination);
+		$this->thumbnail_created = false;
+
+		if (!$this->set_size($mime_type))
 		{
 			return false;
 		}
 
-		$used_imagick = false;
+		$this->create_imagick();
 
-		// Only use imagemagick if defined and the passthru function not disabled
-		if ($this->config['img_imagick'] && function_exists('passthru'))
+		if (!$this->thumbnail_created && !$this->create_gd())
 		{
-			if (substr($this->config['img_imagick'], -1) !== '/')
-			{
-				$this->config['img_imagick'] .= '/';
-			}
-
-			@passthru(escapeshellcmd($this->config['img_imagick']) . 'convert' . ((defined('PHP_OS') && preg_match('#^win#i', PHP_OS)) ? '.exe' : '') . ' -quality 85 -geometry ' . $this->new_width . 'x' . $this->new_height . ' "' . str_replace('\\', '/', $source) . '" "' . str_replace('\\', '/', $destination) . '"');
-
-			if (file_exists($destination))
-			{
-				$used_imagick = true;
-			}
+			return false;
 		}
 
-		if (!$used_imagick)
-		{
-			$type = get_supported_image_types($this->type);
-
-			if ($type['gd'])
-			{
-				// If the type is not supported, we are not able to create a thumbnail
-				if ($type['format'] === false)
-				{
-					return false;
-				}
-
-				switch ($type['format'])
-				{
-					case IMG_GIF:
-						$image = @imagecreatefromgif($source);
-						break;
-
-					case IMG_JPG:
-						@ini_set('gd.jpeg_ignore_warning', 1);
-						$image = @imagecreatefromjpeg($source);
-						break;
-
-					case IMG_PNG:
-						$image = @imagecreatefrompng($source);
-						break;
-
-					case IMG_WBMP:
-						$image = @imagecreatefromwbmp($source);
-						break;
-				}
-
-				if (empty($image))
-				{
-					return false;
-				}
-
-				if ($type['version'] == 1)
-				{
-					$new_image = imagecreate($this->new_width, $this->new_height);
-
-					if ($new_image === false)
-					{
-						return false;
-					}
-
-					imagecopyresized($new_image, $image, 0, 0, 0, 0, $this->new_width, $this->new_height, $this->width, $this->height);
-				}
-				else
-				{
-					$new_image = imagecreatetruecolor($this->new_width, $this->new_height);
-
-					if ($new_image === false)
-					{
-						return false;
-					}
-
-					// Preserve alpha transparency (png for example)
-					@imagealphablending($new_image, false);
-					@imagesavealpha($new_image, true);
-
-					imagecopyresampled($new_image, $image, 0, 0, 0, 0, $this->new_width, $this->new_height, $this->width, $this->height);
-				}
-
-				// If we are in safe mode create the destination file prior to using the gd functions to circumvent a PHP bug
-				if (@ini_get('safe_mode') || @strtolower(ini_get('safe_mode')) == 'on')
-				{
-					@touch($destination);
-				}
-
-				switch ($type['format'])
-				{
-					case IMG_GIF:
-						imagegif($new_image, $destination);
-						break;
-
-					case IMG_JPG:
-						imagejpeg($new_image, $destination, 90);
-						break;
-
-					case IMG_PNG:
-						imagepng($new_image, $destination);
-						break;
-
-					case IMG_WBMP:
-						imagewbmp($new_image, $destination);
-						break;
-				}
-
-				imagedestroy($new_image);
-			}
-			else
-			{
-				return false;
-			}
-		}
-
-		if (!file_exists($destination))
+		if (!file_exists($this->destination))
 		{
 			return false;
 		}
 
 		try
 		{
-			$this->filesystem->phpbb_chmod($destination, CHMOD_READ | CHMOD_WRITE);
+			$this->filesystem->phpbb_chmod($this->destination, CHMOD_READ | CHMOD_WRITE);
 		}
 		catch (\phpbb\filesystem\exception\filesystem_exception $e)
 		{
@@ -209,23 +113,34 @@ class thumbnail
 	}
 
 	/**
-	 * Set image size info
+	 * Set file paths
 	 *
 	 * @param string $source Source file path
+	 * @param string $destination Destination file path
+	 */
+	protected function set_paths($source, $destination)
+	{
+		$this->source = $source;
+		$this->destination = $destination;
+	}
+
+	/**
+	 * Set image size info
+	 *
 	 * @param string $mime_type Image mime type
 	 *
 	 * @return bool True if valid size could be set, false if not
 	 */
-	protected function set_size($source, $mime_type)
+	protected function set_size($mime_type)
 	{
-		$img_filesize = (file_exists($source)) ? @filesize($source) : false;
+		$img_filesize = (file_exists($this->source)) ? @filesize($this->source) : false;
 
 		if (!$img_filesize || $img_filesize <= (int) $this->config['img_min_thumb_filesize'])
 		{
 			return false;
 		}
 
-		$dimension = $this->image_size->getImageSize($source, $mime_type);
+		$dimension = $this->image_size->getImageSize($this->source, $mime_type);
 
 		if ($dimension === false)
 		{
@@ -243,6 +158,132 @@ class thumbnail
 
 		// Do not create a thumbnail if the resulting width/height is bigger than the original one
 		if ($this->new_width >= $this->width && $this->new_height >= $this->height)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Create thumbnail using imagemagick
+	 */
+	protected function create_imagick()
+	{
+		// Only use imagemagick if defined and the passthru function not disabled
+		if ($this->config['img_imagick'] && function_exists('passthru'))
+		{
+			if (substr($this->config['img_imagick'], -1) !== '/')
+			{
+				$this->config['img_imagick'] .= '/';
+			}
+
+			@passthru(escapeshellcmd($this->config['img_imagick']) . 'convert' . ((defined('PHP_OS') && preg_match('#^win#i', PHP_OS)) ? '.exe' : '') . ' -quality 85 -geometry ' . $this->new_width . 'x' . $this->new_height . ' "' . str_replace('\\', '/', $this->source) . '" "' . str_replace('\\', '/', $this->destination) . '"');
+
+			if (file_exists($this->destination))
+			{
+				$this->thumbnail_created = true;
+			}
+		}
+	}
+
+	/**
+	 * Create thumbnail using GD
+	 *
+	 * @return bool True if thumbnail might have been created, false if not
+	 */
+	protected function create_gd()
+	{
+		$type = get_supported_image_types($this->type);
+
+		if ($type['gd'])
+		{
+			// If the type is not supported, we are not able to create a thumbnail
+			if ($type['format'] === false)
+			{
+				return false;
+			}
+
+			switch ($type['format'])
+			{
+				case IMG_GIF:
+					$image = @imagecreatefromgif($this->source);
+				break;
+
+				case IMG_JPG:
+					@ini_set('gd.jpeg_ignore_warning', 1);
+					$image = @imagecreatefromjpeg($this->source);
+				break;
+
+				case IMG_PNG:
+					$image = @imagecreatefrompng($this->source);
+				break;
+
+				case IMG_WBMP:
+					$image = @imagecreatefromwbmp($this->source);
+				break;
+			}
+
+			if (empty($image))
+			{
+				return false;
+			}
+
+			if ($type['version'] == 1)
+			{
+				$new_image = imagecreate($this->new_width, $this->new_height);
+
+				if ($new_image === false)
+				{
+					return false;
+				}
+
+				imagecopyresized($new_image, $image, 0, 0, 0, 0, $this->new_width, $this->new_height, $this->width, $this->height);
+			}
+			else
+			{
+				$new_image = imagecreatetruecolor($this->new_width, $this->new_height);
+
+				if ($new_image === false)
+				{
+					return false;
+				}
+
+				// Preserve alpha transparency (png for example)
+				@imagealphablending($new_image, false);
+				@imagesavealpha($new_image, true);
+
+				imagecopyresampled($new_image, $image, 0, 0, 0, 0, $this->new_width, $this->new_height, $this->width, $this->height);
+			}
+
+			// If we are in safe mode create the destination file prior to using the gd functions to circumvent a PHP bug
+			if (@ini_get('safe_mode') || @strtolower(ini_get('safe_mode')) == 'on')
+			{
+				@touch($this->destination);
+			}
+
+			switch ($type['format'])
+			{
+				case IMG_GIF:
+					imagegif($new_image, $this->destination);
+				break;
+
+				case IMG_JPG:
+					imagejpeg($new_image, $this->destination, 90);
+				break;
+
+				case IMG_PNG:
+					imagepng($new_image, $this->destination);
+				break;
+
+				case IMG_WBMP:
+					imagewbmp($new_image, $this->destination);
+				break;
+			}
+
+			imagedestroy($new_image);
+		}
+		else
 		{
 			return false;
 		}
