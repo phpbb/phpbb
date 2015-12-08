d37d3ed3ed91cc16271c57ab7210c7c0aa19910b
Marc Alexander <admin@m-a-styles.de>
2015-12-08 15:32:45 +0100

    [ticket/14213] Add pending to core.group_add_user_after
    PHPBB3-14213

--- phpBB/includes/functions_user.php
+++ phpBB/includes/functions_user.php
@@ -2783,9 +2783,16 @@ function group_user_add($group_id, $user_id_ary = false, $username_ary = false, 
 	* @var	string group_name		Name of the group
 	* @var	array	user_id_ary		IDs of the users which are added
 	* @var	array	username_ary	names of the users which are added
+	* @var	int		pending			Pending setting, 1 if user(s) added are pending
 	* @since 3.1.7-RC1
 	*/
-	$vars = array('group_id', 'group_name', 'user_id_ary', 'username_ary');
+	$vars = array(
+		'group_id',
+		'group_name',
+		'user_id_ary',
+		'username_ary',
+		'pending',
+	);
 	extract($phpbb_dispatcher->trigger_event('core.group_add_user_after', compact($vars)));
 
 	if (!$group_name)
