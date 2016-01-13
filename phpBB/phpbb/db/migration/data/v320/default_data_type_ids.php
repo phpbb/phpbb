7cd7bb3c015be1355ab634d3cb704c1d764bf8b0
Marc Alexander <admin@m-a-styles.de>
2016-01-13 19:35:04 +0100

    [ticket/11875] Change more columns to use UINT and add missings ones
    PHPBB3-11875

--- phpBB/phpbb/db/migration/data/v320/default_data_type_ids.php
+++ phpBB/phpbb/db/migration/data/v320/default_data_type_ids.php
@@ -24,21 +24,35 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 	{
 		return array(
 			'change_columns'	=> array(
-				$this->table_prefix . 'attachments'			=> array(
-					'topic_id'		=> array('ULINT', 0),
-				),
 				$this->table_prefix . 'acl_users'			=> array(
 					'user_id'		=> array('ULINT', 0),
+				),
+				$this->table_prefix . 'attachments'			=> array(
+					'attach_id'		=> array('ULINT', null, 'auto_increment'),
+					'post_msg_id'	=> array('ULINT', 0),
+					'poster_id'		=> array('ULINT', 0),
+					'topic_id'		=> array('ULINT', 0),
+				),
+				$this->table_prefix . 'banlist'				=> array(
+					'ban_id'		=> array('ULINT', null, 'auto_increment'),
+					'ban_userid'	=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'bookmarks'			=> array(
 					'topic_id'		=> array('ULINT', 0),
 					'user_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'bots'				=> array(
+					'bot_id'		=> array('ULINT', null, 'auto_increment'),
 					'user_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'drafts'				=> array(
+					'draft_id'		=> array('ULINT', null, 'auto_increment'),
+					'user_id'		=> array('ULINT', 0),
 					'topic_id'		=> array('ULINT', 0),
+				),
+				$this->table_prefix . 'forums'				=> array(
+					'forum_last_post_id'	=> array('ULINT', 0),
+					'forum_last_poster_id'	=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'forums_access'		=> array(
 					'user_id'		=> array('ULINT', 0),
@@ -50,6 +64,9 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 					'user_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'log'					=> array(
+					'log_id'		=> array('ULINT', null, 'auto_increment'),
+					'post_id'		=> array('ULINT', 0),
+					'reportee_id'	=> array('ULINT', 0),
 					'user_id'		=> array('ULINT', 0),
 					'topic_id'		=> array('ULINT', 0),
 				),
@@ -65,6 +82,9 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 				$this->table_prefix . 'oauth_accounts'		=> array(
 					'user_id'		=> array('ULINT', 0),
 				),
+				$this->table_prefix . 'oauth_states'		=> array(
+					'user_id'		=> array('ULINT', 0),
+				),
 				$this->table_prefix . 'oauth_tokens'		=> array(
 					'user_id'		=> array('ULINT', 0),
 				),
@@ -72,36 +92,63 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 					'topic_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'poll_votes'			=> array(
-					'topicr_id'		=> array('ULINT', 0),
+					'topic_id'		=> array('ULINT', 0),
+					'vote_user_id'	=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'posts'				=> array(
-					'post_id'		=> array('ULINT', null, 'auto_increment'),
-					'topic_id'		=> array('ULINT', 0),
+					'post_id'			=> array('ULINT', null, 'auto_increment'),
+					'poster_id'			=> array('ULINT', 0),
+					'post_delete_user'	=> array('ULINT', 0),
+					'post_edit_user'	=> array('ULINT', 0),
+					'topic_id'			=> array('ULINT', 0),
+				),
+				$this->table_prefix . 'privmsgs'			=> array(
+					'author_id'			=> array('ULINT', 0),
+					'message_edit_user'	=> array('ULINT', 0),
+					'msg_id'			=> array('ULINT', null, 'auto_increment'),
 				),
 				$this->table_prefix . 'privmsgs_folder'		=> array(
 					'user_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'privmsgs_rules'		=> array(
+					'rule_user_id'	=> array('ULINT', 0),
 					'user_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'privmsgs_to'			=> array(
+					'author_id'		=> array('ULINT', 0),
+					'msg_id'		=> array('ULINT', 0),
 					'user_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'profile_fields_data'	=> array(
 					'user_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'reports'				=> array(
+					'report_id'		=> array('ULINT', 0),
+					'pm_id'			=> array('ULINT', 0),
 					'post_id'		=> array('ULINT', 0),
 					'user_id'		=> array('ULINT', 0),
 				),
+				$this->table_prefix . 'search_wordlist'		=> array(
+					'word_id'		=> array('ULINT', null, 'auto_increment'),
+				),
 				$this->table_prefix . 'search_wordmatch'	=> array(
 					'post_id'		=> array('ULINT', 0),
+					'word_id'		=> array('ULINT', 0),
+				),
+				$this->table_prefix . 'sessions'			=> array(
+					'session_user_id'	=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'sessions_keys'		=> array(
 					'user_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'topics'				=> array(
-					'topic_id'		=> array('ULINT', null, 'auto_increment'),
+					'topic_id'				=> array('ULINT', null, 'auto_increment'),
+					'topic_poster'			=> array('ULINT', 0),
+					'topic_first_post_id'	=> array('ULINT', 0),
+					'topic_last_post_id'	=> array('ULINT', 0),
+					'topic_last_poster_id'	=> array('ULINT', 0),
+					'topic_moved_id'		=> array('ULINT', 0),
+					'topic_delete_user'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'topics_track'		=> array(
 					'user_id'		=> array('ULINT', 0),
@@ -116,6 +163,7 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 					'topic_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'user_notifications'	=> array(
+					'item_id'		=> array('ULINT', 0),
 					'user_id'		=> array('ULINT', 0),
 				),
 				$this->table_prefix . 'user_group'			=> array(
@@ -125,11 +173,16 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 					'user_id'		=> array('ULINT', null, 'auto_increment'),
 				),
 				$this->table_prefix . 'warnings'			=> array(
+					'log_id'		=> array('ULINT', 0),
 					'user_id'		=> array('ULINT', 0),
-					'post_Id'		=> array('ULINT', 0),
+					'post_id'		=> array('ULINT', 0),
+				),
+				$this->table_prefix . 'words'				=> array(
+					'word_id'		=> array('ULINT', null, 'auto_increment'),
 				),
 				$this->table_prefix . 'zebra'			=> array(
 					'user_id'		=> array('ULINT', 0),
+					'zebra_id'		=> array('ULINT', 0),
 				),
 			),
 		);
@@ -139,21 +192,35 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 	{
 		return array(
 			'change_columns'	=> array(
-				$this->table_prefix . 'attachments'			=> array(
-					'topic_id'		=> array('UINT', 0),
-				),
 				$this->table_prefix . 'acl_users'			=> array(
 					'user_id'		=> array('UINT', 0),
+				),
+				$this->table_prefix . 'attachments'			=> array(
+					'attach_id'		=> array('UINT', null, 'auto_increment'),
+					'post_msg_id'	=> array('UINT', 0),
+					'poster_id'		=> array('UINT', 0),
+					'topic_id'		=> array('UINT', 0),
+				),
+				$this->table_prefix . 'banlist'				=> array(
+					'ban_id'		=> array('UINT', null, 'auto_increment'),
+					'ban_userid'	=> array('UINT', 0),
 				),
 				$this->table_prefix . 'bookmarks'			=> array(
 					'topic_id'		=> array('UINT', 0),
 					'user_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'bots'				=> array(
+					'bot_id'		=> array('UINT', null, 'auto_increment'),
 					'user_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'drafts'				=> array(
+					'draft_id'		=> array('UINT', null, 'auto_increment'),
+					'user_id'		=> array('UINT', 0),
 					'topic_id'		=> array('UINT', 0),
+				),
+				$this->table_prefix . 'forums'				=> array(
+					'forum_last_post_id'	=> array('UINT', 0),
+					'forum_last_poster_id'	=> array('UINT', 0),
 				),
 				$this->table_prefix . 'forums_access'		=> array(
 					'user_id'		=> array('UINT', 0),
@@ -165,6 +232,9 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 					'user_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'log'					=> array(
+					'log_id'		=> array('UINT', null, 'auto_increment'),
+					'post_id'		=> array('UINT', 0),
+					'reportee_id'	=> array('UINT', 0),
 					'user_id'		=> array('UINT', 0),
 					'topic_id'		=> array('UINT', 0),
 				),
@@ -180,6 +250,9 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 				$this->table_prefix . 'oauth_accounts'		=> array(
 					'user_id'		=> array('UINT', 0),
 				),
+				$this->table_prefix . 'oauth_states'		=> array(
+					'user_id'		=> array('UINT', 0),
+				),
 				$this->table_prefix . 'oauth_tokens'		=> array(
 					'user_id'		=> array('UINT', 0),
 				),
@@ -187,36 +260,63 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 					'topic_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'poll_votes'			=> array(
-					'topicr_id'		=> array('UINT', 0),
+					'topic_id'		=> array('UINT', 0),
+					'vote_user_id'	=> array('UINT', 0),
 				),
 				$this->table_prefix . 'posts'				=> array(
-					'post_id'		=> array('UINT', null, 'auto_increment'),
-					'topic_id'		=> array('UINT', 0),
+					'post_id'			=> array('UINT', null, 'auto_increment'),
+					'poster_id'			=> array('UINT', 0),
+					'post_delete_user'	=> array('UINT', 0),
+					'post_edit_user'	=> array('UINT', 0),
+					'topic_id'			=> array('UINT', 0),
+				),
+				$this->table_prefix . 'privmsgs'			=> array(
+					'author_id'			=> array('UINT', 0),
+					'message_edit_user'	=> array('UINT', 0),
+					'msg_id'			=> array('UINT', null, 'auto_increment'),
 				),
 				$this->table_prefix . 'privmsgs_folder'		=> array(
 					'user_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'privmsgs_rules'		=> array(
+					'rule_user_id'	=> array('UINT', 0),
 					'user_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'privmsgs_to'			=> array(
+					'author_id'		=> array('UINT', 0),
+					'msg_id'		=> array('UINT', 0),
 					'user_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'profile_fields_data'	=> array(
 					'user_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'reports'				=> array(
+					'report_id'		=> array('UINT', 0),
+					'pm_id'			=> array('UINT', 0),
 					'post_id'		=> array('UINT', 0),
 					'user_id'		=> array('UINT', 0),
 				),
+				$this->table_prefix . 'search_wordlist'		=> array(
+					'word_id'		=> array('UINT', null, 'auto_increment'),
+				),
 				$this->table_prefix . 'search_wordmatch'	=> array(
 					'post_id'		=> array('UINT', 0),
+					'word_id'		=> array('UINT', 0),
+				),
+				$this->table_prefix . 'sessions'			=> array(
+					'session_user_id'	=> array('UINT', 0),
 				),
 				$this->table_prefix . 'sessions_keys'		=> array(
 					'user_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'topics'				=> array(
-					'topic_id'		=> array('UINT', null, 'auto_increment'),
+					'topic_id'				=> array('UINT', null, 'auto_increment'),
+					'topic_poster'			=> array('UINT', 0),
+					'topic_first_post_id'	=> array('UINT', 0),
+					'topic_last_post_id'	=> array('UINT', 0),
+					'topic_last_poster_id'	=> array('UINT', 0),
+					'topic_moved_id'		=> array('UINT', 0),
+					'topic_delete_user'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'topics_track'		=> array(
 					'user_id'		=> array('UINT', 0),
@@ -231,6 +331,7 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 					'topic_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'user_notifications'	=> array(
+					'item_id'		=> array('UINT', 0),
 					'user_id'		=> array('UINT', 0),
 				),
 				$this->table_prefix . 'user_group'			=> array(
@@ -240,11 +341,16 @@ class default_data_type_ids extends \phpbb\db\migration\migration
 					'user_id'		=> array('UINT', null, 'auto_increment'),
 				),
 				$this->table_prefix . 'warnings'			=> array(
+					'log_id'		=> array('UINT', 0),
 					'user_id'		=> array('UINT', 0),
-					'post_Id'		=> array('UINT', 0),
+					'post_id'		=> array('UINT', 0),
+				),
+				$this->table_prefix . 'words'				=> array(
+					'word_id'		=> array('UINT', null, 'auto_increment'),
 				),
 				$this->table_prefix . 'zebra'			=> array(
 					'user_id'		=> array('UINT', 0),
+					'zebra_id'		=> array('UINT', 0),
 				),
 			),
 		);
