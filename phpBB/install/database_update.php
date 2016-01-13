77298b6d03f0f6317e04c644e59d2c88e8e48d34
Dragos-Valentin Radulescu <rdragos18@yahoo.com>
2016-01-13 13:06:05 +0200

    Remove span corners
    Shouldnt these tags have been removed and replaced with css border radius even from the updater?

--- phpBB/install/database_update.php
+++ phpBB/install/database_update.php
@@ -31,7 +31,6 @@ function phpbb_end_update($cache, $config)
 								</p>
 							</div>
 						</div>
-					<span class="corners-bottom"><span></span></span>
 				</div>
 			</div>
 		</div>
@@ -152,7 +151,6 @@ header('Content-type: text/html; charset=UTF-8');
 		<div id="page-body">
 			<div id="acp">
 				<div class="panel">
-					<span class="corners-top"><span></span></span>
 						<div id="content">
 							<div id="main" class="install-body">
 
