abae70079bef84c92692d481b62fdb164e03414c
Tristan Darricau <tristan.darricau@sensiolabs.com>
2016-02-01 22:16:08 +0100

    Prepare sources

--- docker/scripts/prepare-sources.sh
+++ docker/scripts/prepare-sources.sh
@@ -14,9 +14,9 @@ docker run \
     --volume ${COMPOSER_HOME}:/composer/ \
     --workdir /data \
     phpbb/build${IMAGES_TAG} sh -c '
-    cd phpBB &&
     git config user.email "no-reply@phpbb.com" &&
     git config user.name "phpBB CI" &&
+    cd phpBB &&
     COMPOSER_HOME=/composer php ../composer.phar install --dev'
 
 docker run \
