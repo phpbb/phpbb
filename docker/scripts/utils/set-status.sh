2248d591814911a9481f33a8c4bd3139d4f3d31f
Tristan Darricau <tristan.darricau@sensiolabs.com>
2016-02-02 11:46:33 +0100

    Echo set_status

--- docker/scripts/utils/set-status.sh
+++ docker/scripts/utils/set-status.sh
@@ -6,5 +6,7 @@ status=$1
 description=$2
 step=$3
 
+echo '---------------------'
 echo curl -H "Authorization: token ${GITHUB_TOKEN}" --request POST --data "{\"state\": \"${status}\", \"description\": \"${description}\", \"target_url\": \"${BUILD_RESULT_URL}\", \"context\": \"phpBB continuous integration - ${step}\"}" https://api.github.com/repos/phpbb/phpbb/statuses/${last_commit}
+echo '---------------------'
 curl -H "Authorization: token ${GITHUB_TOKEN}" --request POST --data "{\"state\": \"${status}\", \"description\": \"${description}\", \"target_url\": \"${BUILD_RESULT_URL}\", \"context\": \"phpBB continuous integration - ${step}\"}" https://api.github.com/repos/phpbb/phpbb/statuses/${last_commit}
