8fa531b3c340f7e98d56b2d0cb64eef20cde285b
Tristan Darricau <tristan.darricau@sensiolabs.com>
2016-02-02 12:48:23 +0100

    Echo set_status

--- docker/scripts/jobs/unit-tests.sh
+++ docker/scripts/jobs/unit-tests.sh
@@ -11,14 +11,14 @@ start_db
 
 docker run \
     --user $(id -u):$(id -g) \
-    --link mysql:mysql \
-    --env PHPBB_TEST_DBMS="phpbb\db\driver\mysqli" \
-    --env PHPBB_TEST_DBHOST="mysql" \
-    --env PHPBB_TEST_DBPORT="3306" \
-    --env PHPBB_TEST_DBNAME="phpbb_tests" \
-    --env PHPBB_TEST_DBUSER="root" \
-    --env PHPBB_TEST_DBPASSWD="" \
-    --env PHPBB_TEST_TABLE_PREFIX="phpbb_" \
-    --volume ${bamboo.working.directory}:/data \
+    ${DOCKER_LINK} \
+    --env PHPBB_TEST_DBMS="${PHPBB_TEST_DBMS}" \
+    --env PHPBB_TEST_DBHOST="${PHPBB_TEST_DBHOST}" \
+    --env PHPBB_TEST_DBPORT="${PHPBB_TEST_DBPORT}" \
+    --env PHPBB_TEST_DBNAME="${PHPBB_TEST_DBNAME}" \
+    --env PHPBB_TEST_DBUSER="${PHPBB_TEST_DBUSER}" \
+    --env PHPBB_TEST_DBPASSWD="${PHPBB_TEST_DBPASSWD}" \
+    --env PHPBB_TEST_TABLE_PREFIX="${PHPBB_TEST_TABLE_PREFIX}" \
+    --volume ${WORKING_DIR}:/data \
     --workdir /data \
-    phpbb/php-ut-5.6-mysql php -d memory_limit=-1 phpBB/vendor/bin/phpunit --group __nogroup__ --log-junit build/logs/phpunit.xml
+    phpbb/php-ut-${php}-${db} php -d memory_limit=-1 phpBB/vendor/bin/phpunit --group __nogroup__ --log-junit build/logs/phpunit.xml
