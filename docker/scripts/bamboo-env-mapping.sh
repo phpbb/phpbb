aeb6fb57423d629e34e5cb71433a4628f1eaecd2
Tristan Darricau <tristan.darricau@sensiolabs.com>
2016-02-02 12:58:28 +0100

    Github token

--- docker/scripts/bamboo-env-mapping.sh
+++ docker/scripts/bamboo-env-mapping.sh
@@ -5,5 +5,4 @@ export IMAGES_TAG=${bamboo_images_tag}
 export WORKING_DIR=${bamboo_working_directory}
 export PR_NUMBER=${bamboo_PRnumber}
 export COMPOSER_HOME=${bamboo_composer_home}
-export GITHUB_TOKEN=${bamboo_github_token_password}
 export BUILD_RESULT_URL=${bamboo_buildResultsUrl}
