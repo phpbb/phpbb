c4a4f35a3abff53ec0820bd5f631661823a80cec
Tristan Darricau <tristan.darricau@sensiolabs.com>
2016-02-03 21:21:47 +0100

    Func tests

--- docker/scripts/jobs/functional-tests.sh
+++ docker/scripts/jobs/functional-tests.sh
@@ -10,7 +10,8 @@ php=$2
 start_db
 
 docker run \
-    --user $(id -u):$(id -g) \
+    --env TEST_UID=$(id -u) \
+    --env TEST_GID=$(id -g) \
     ${DOCKER_LINK} \
     --env PHPBB_TEST_DBMS="${PHPBB_TEST_DBMS}" \
     --env PHPBB_TEST_DBHOST="${PHPBB_TEST_DBHOST}" \
@@ -19,6 +20,8 @@ docker run \
     --env PHPBB_TEST_DBUSER="${PHPBB_TEST_DBUSER}" \
     --env PHPBB_TEST_DBPASSWD="${PHPBB_TEST_DBPASSWD}" \
     --env PHPBB_TEST_TABLE_PREFIX="${PHPBB_TEST_TABLE_PREFIX}" \
+    --env PHPBB_FUNCTIONAL_URL="http://localhost/" \
     --volume ${WORKING_DIR}:/data \
     --workdir /data \
-    phpbb/php-ut-${php}-${db}${IMAGES_TAG} php -d memory_limit=-1 phpBB/vendor/bin/phpunit --group __nogroup__ --log-junit build/logs/phpunit.xml
+
+    phpbb/php-ft-${php}-${db}${IMAGES_TAG} php -d memory_limit=-1 phpBB/vendor/bin/phpunit --group functional --log-junit build/logs/phpunit.xml
