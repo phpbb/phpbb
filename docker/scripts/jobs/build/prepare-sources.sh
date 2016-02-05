f8b9a4f4b64fb311399073d988993d98cd82e601
Tristan Darricau <tristan.darricau@sensiolabs.com>
2016-02-05 16:50:01 +0100

    echo debug

--- docker/scripts/jobs/build/prepare-sources.sh
+++ docker/scripts/jobs/build/prepare-sources.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
 # Ensure the github oauth token is set
+echo docker run \
+    --user $(id -u):$(id -g) \
+    --volume ${WORKING_DIR}:/data \
+    --volume ${COMPOSER_HOME}:/composer/ \
+    --workdir /data \
+    phpbb/build${IMAGES_TAG} sh -c "COMPOSER_HOME=/composer php composer.phar config -g github-oauth.github.com ${GITHUB_TOKEN}"
+
+echo docker run \
+    --user $(id -u):$(id -g) \
+    --volume ${WORKING_DIR}:/data \
+    --volume ${COMPOSER_HOME}:/composer/ \
+    --workdir /data \
+    phpbb/build${IMAGES_TAG} sh -c '
+    git config user.email "no-reply@phpbb.com" &&
+    git config user.name "phpBB CI" &&
+    cd phpBB &&
+    COMPOSER_HOME=/composer php ../composer.phar install --dev'
 docker run \
     --user $(id -u):$(id -g) \
     --volume ${WORKING_DIR}:/data \
