b95e246b76e21db7a83516362328b842b11a45f7
Tristan Darricau <tristan.darricau@sensiolabs.com>
2016-02-07 12:22:13 +0100

    Add Oracle

--- docker/scripts/db/oracle.sh
+++ docker/scripts/db/oracle.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+export PHPBB_TEST_DBMS='phpbb\db\driver\oracle'
+export PHPBB_TEST_DBHOST='oracle'
+export PHPBB_TEST_DBPORT='1521'
+export PHPBB_TEST_DBNAME='xe'
+export PHPBB_TEST_DBUSER='system'
+export PHPBB_TEST_DBPASSWD='oracle'
+export PHPBB_TEST_TABLE_PREFIX='phpbb_'
+export DOCKER_LINK='--link oracle:oracle'
+
+function start_db {
+    docker run -d --name oracle wnameless/oracle-xe-11g
+    docker run --rm --link oracle:oracle -e TARGETS=oracle:1521 waisbrot/wait
+}
--- docker/scripts/db/sqlite.sh
+++ docker/scripts/db/sqlite.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+export PHPBB_TEST_DBMS='phpbb\db\driver\sqlite3'
+export PHPBB_TEST_DBHOST='/dev/shm/phpbb_unit_tests.sqlite3'
+export PHPBB_TEST_DBPORT=''
+export PHPBB_TEST_DBNAME=''
+export PHPBB_TEST_DBUSER=''
+export PHPBB_TEST_DBPASSWD=''
+export PHPBB_TEST_TABLE_PREFIX='phpbb_'
+export DOCKER_LINK=''
+
+function start_db {
+    true
+}
