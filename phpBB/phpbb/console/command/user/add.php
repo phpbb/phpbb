637b02690db0d3b7af2915c1da4bacc8fa054f6c
Matt Friedman <maf675@gmail.com>
2016-02-29 13:28:42 -0800

    [ticket/12684] Update to use non-deprecated methods
    PHPBB3-12684

--- phpBB/phpbb/console/command/user/add.php
+++ phpBB/phpbb/console/command/user/add.php
@@ -1,15 +1,15 @@
 <?php
 /**
-*
-* This file is part of the phpBB Forum Software package.
-*
-* @copyright (c) phpBB Limited <https://www.phpbb.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-* For full copyright and license information, please see
-* the docs/CREDITS.txt file.
-*
-*/
+ *
+ * This file is part of the phpBB Forum Software package.
+ *
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the docs/CREDITS.txt file.
+ *
+ */
 
 namespace phpbb\console\command\user;
 
@@ -17,6 +17,7 @@ use phpbb\exception\runtime_exception;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class add extends \phpbb\console\command\command
@@ -27,108 +28,124 @@ class add extends \phpbb\console\command\command
 	/** @var \phpbb\config\config */
 	protected $config;
 
+	/** @var \phpbb\language\language */
+	protected $language;
+
 	/** @var \phpbb\passwords\manager */
 	protected $password_manager;
 
 	/**
-	* phpBB root path
-	* @var string
-	*/
+	 * phpBB root path
+	 *
+	 * @var string
+	 */
 	protected $phpbb_root_path;
 
 	/**
-	* PHP extension.
-	*
-	* @var string
-	*/
+	 * PHP extension.
+	 *
+	 * @var string
+	 */
 	protected $php_ext;
 
 	/**
-	* Construct method
-	*
-	* @param \phpbb\user $user The user object used for language information
-	* @param \phpbb\db\driver\driver_interface $db The database in wich will be inserted the user
-	* @param \phpbb\config\config $config The config object used to get default language and timezone
-	* @param \phpbb\passwords\manager $password_manager The password manager used to store the user's password
-	* @param string $phpbb_root_path Root path
-	* @param string $php_ext PHP extension
-	*/
-	public function __construct(\phpbb\user $user, \phpbb\db\driver\driver_interface $db, \phpbb\config\config $config, \phpbb\passwords\manager $password_manager, $phpbb_root_path, $php_ext)
+	 * Construct method
+	 *
+	 * @param \phpbb\user                       $user
+	 * @param \phpbb\db\driver\driver_interface $db
+	 * @param \phpbb\config\config              $config
+	 * @param \phpbb\language\language          $language
+	 * @param \phpbb\passwords\manager          $password_manager
+	 * @param string                            $phpbb_root_path
+	 * @param string                            $php_ext
+	 */
+	public function __construct(\phpbb\user $user, \phpbb\db\driver\driver_interface $db, \phpbb\config\config $config, \phpbb\language\language $language, \phpbb\passwords\manager $password_manager, $phpbb_root_path, $php_ext)
 	{
 		$this->db = $db;
 		$this->config = $config;
+		$this->language = $language;
 		$this->password_manager = $password_manager;
 		$this->phpbb_root_path = $phpbb_root_path;
 		$this->php_ext = $php_ext;
 
-		$user->add_lang('ucp');
+		$language->add_lang('ucp');
 		parent::__construct($user);
 	}
 
 	/**
-	* Sets the command name and description
-	*
-	* @return null
-	*/
+	 * Sets the command name and description
+	 *
+	 * @return null
+	 */
 	protected function configure()
 	{
 		$this
 			->setName('user:add')
-			->setDescription($this->user->lang('CLI_DESCRIPTION_USER_ADD'))
-			->addOption('username', null, InputOption::VALUE_REQUIRED, $this->user->lang('CLI_DESCRIPTION_USER_ADD_OPTION_USERNAME'))
-			->addOption('password', null, InputOption::VALUE_REQUIRED, $this->user->lang('CLI_DESCRIPTION_USER_ADD_OPTION_PASSWORD'))
-			->addOption('email', null, InputOption::VALUE_REQUIRED, $this->user->lang('CLI_DESCRIPTION_USER_ADD_OPTION_EMAIL'))
-			->addOption('send-email', null, InputOption::VALUE_NONE, $this->user->lang('CLI_CONFIG_PRINT_WITHOUT_NEWLINE'))
+			->setDescription($this->language->lang('CLI_DESCRIPTION_USER_ADD'))
+			->addOption('username', null, InputOption::VALUE_REQUIRED, $this->language->lang('CLI_DESCRIPTION_USER_ADD_OPTION_USERNAME'))
+			->addOption('password', null, InputOption::VALUE_REQUIRED, $this->language->lang('CLI_DESCRIPTION_USER_ADD_OPTION_PASSWORD'))
+			->addOption('email', null, InputOption::VALUE_REQUIRED, $this->language->lang('CLI_DESCRIPTION_USER_ADD_OPTION_EMAIL'))
+			->addOption('send-email', null, InputOption::VALUE_NONE, $this->language->lang('CLI_CONFIG_PRINT_WITHOUT_NEWLINE'))
 		;
 	}
 
 	/**
-	* Executes the command user:add
-	*
-	* If not given in option, asks the username, password and email.
-	* Then a new user is added in the database, with language and timezone found in the $config passed to the constructor, and the group_id found in the database.
-	*
-	* @param InputInterface $input The input stream used to get the options
-	* @param OutputInterface $output The output stream, used to print messages
-	*
-	* @return int 0 if all is well, 1 if a database error occured while trying to get the group_id
-	*/
+	 * Executes the command user:add
+	 *
+	 * Adds a new user to the database. If options are not provided, it will ask for the username, password and email.
+	 * User is added to the registered user group. Language and timezone default to $config settings.
+	 *
+	 * @param InputInterface  $input  The input stream used to get the options
+	 * @param OutputInterface $output The output stream, used to print messages
+	 *
+	 * @return int 0 if all is well, 1 if any errors occurred
+	 */
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		$io = new SymfonyStyle($input, $output);
 
-		$dialog = $this->getHelperSet()->get('dialog');
-
-		$username = $input->getOption('username');
-		if (!$username) {
-			$username = $dialog->ask(
-				$output,
-				$this->user->lang('USERNAME') . $this->user->lang('COLON') . ' ',
-				null
-			);
-		}
-
-		$password = $input->getOption('password');
-		if (!$password)
-		{
-			$password = $this->get_password($output, $dialog);
-		}
-
-		$email = $input->getOption('email');
-		if (!$email)
-		{
-			$email = $dialog->ask(
-				$output,
-				$this->user->lang('EMAIL_ADDRESS') . $this->user->lang('COLON') . ' ',
-				null
-			);
-		}
+		$helper = $this->getHelper('question');
 
 		$data = array(
-			'username'		=> $username,
-			'new_password'	=> $password,
-			'email'			=> $email,
+			'username'     => $input->getOption('username'),
+			'new_password' => $input->getOption('password'),
+			'email'        => $input->getOption('email'),
 		);
+
+		if (!$data['username'])
+		{
+			$question = new Question($this->ask_user('USERNAME'), null);
+			$data['username'] = $helper->ask($input, $output, $question);
+		}
+
+		if (!$data['new_password'])
+		{
+			$self = $this;
+			$question = new Question($this->ask_user('PASSWORD'));
+			$question->setValidator(function ($value) use ($self, $helper, $input, $output) {
+				$question = new Question($self->ask_user('CONFIRM_PASSWORD'));
+				$question->setHidden(true);
+				$question->setHiddenFallback(false);
+
+				$confirm = $helper->ask($input, $output, $question);
+				if ($confirm != $value)
+				{
+					throw new runtime_exception($self->language->lang('NEW_PASSWORD_ERROR'));
+				}
+				return $value;
+			});
+			$question->setHidden(true);
+			$question->setHiddenFallback(false);
+			$question->setMaxAttempts(5);
+
+			$data['new_password'] = $helper->ask($input, $output, $question);
+		}
+
+		if (!$data['email'])
+		{
+			$question = new Question($this->ask_user('EMAIL_ADDRESS'), null);
+			$data['email'] = $helper->ask($input, $output, $question);
+		}
 
 		try
 		{
@@ -151,14 +168,14 @@ class add extends \phpbb\console\command\command
 		}
 
 		$user_row = array(
-			'username'				=> $username,
-			'user_password'			=> $this->password_manager->hash($password),
-			'user_email'			=> $email,
-			'group_id'				=> $group_id,
-			'user_timezone'			=> $this->config['board_timezone'],
-			'user_lang'				=> $this->config['default_lang'],
-			'user_type'				=> USER_NORMAL,
-			'user_regdate'			=> time(),
+			'username'      => $data['username'],
+			'user_password' => $this->password_manager->hash($data['new_password']),
+			'user_email'    => $data['email'],
+			'group_id'      => $group_id,
+			'user_timezone' => $this->config['board_timezone'],
+			'user_lang'     => $this->config['default_lang'],
+			'user_type'     => USER_NORMAL,
+			'user_regdate'  => time(),
 		);
 
 		if (!function_exists('user_add'))
@@ -166,60 +183,25 @@ class add extends \phpbb\console\command\command
 			require($this->phpbb_root_path . 'includes/functions_user.' . $this->php_ext);
 		}
 
-		$user_id = user_add($user_row);
+		$user_id = (int) user_add($user_row);
 
 		if ($input->getOption('send-email') && $this->config['email_enable'])
 		{
 			$this->send_activation_email($user_id, $data);
 		}
 
-		$io->success($this->user->lang('SUCCESS_ADD_USER', $username));
+		$io->success($this->language->lang('SUCCESS_ADD_USER', $data['username']));
 
 		return 0;
 	}
 
 	/**
-	* Get the password
-	*
-	* Asks a password to the user and asks for confirmation.
-	* This is repeated until the password match is confirmed.
-	*
-	* @param OutputInterface $output The output stream, where messages are printed
-	* @param \Symfony\Component\Console\Helper\DialogHelper $dialog The dialog helper used to get answers to questions asked to the user
-	*
-	* @return null
-	*/
-	protected function get_password($output, $dialog)
-	{
-		$current_user = $this->user;
-		return $dialog->askHiddenResponseAndValidate(
-			$output,
-			$current_user->lang('PASSWORD') . $current_user->lang('COLON') . ' ',
-			function ($answer) use ($dialog, $output, $current_user)
-			{
-				$confirm = $dialog->askHiddenResponse(
-					$output,
-					$current_user->lang('CONFIRM_PASSWORD') . $current_user->lang('COLON') . ' ',
-					null
-				);
-				if ($confirm != $answer)
-				{
-					throw new runtime_exception($current_user->lang('NEW_PASSWORD_ERROR'));
-				}
-				return $answer;
-			},
-			false,
-			null
-		);
-	}
-
-	/**
-	* Validate the submitted user data
-	*
-	* @param array $data The user data array
-	* @throws runtime_exception if any data fails validation
-	* @return null
-	*/
+	 * Validate the submitted user data
+	 *
+	 * @param array $data The user data array
+	 * @throws runtime_exception if any data fails validation
+	 * @return null
+	 */
 	protected function validate_user_data($data)
 	{
 		if (!function_exists('validate_data'))
@@ -228,13 +210,13 @@ class add extends \phpbb\console\command\command
 		}
 
 		$error = validate_data($data, array(
-			'username'		=> array(
+			'username'     => array(
 				array('string', false, $this->config['min_name_chars'], $this->config['max_name_chars']),
 				array('username', '')),
-			'new_password'		=> array(
+			'new_password' => array(
 				array('string', false, $this->config['min_pass_chars'], $this->config['max_pass_chars']),
 				array('password')),
-			'email'			=> array(
+			'email'        => array(
 				array('string', false, 6, 60),
 				array('user_email')),
 		));
@@ -246,13 +228,13 @@ class add extends \phpbb\console\command\command
 	}
 
 	/**
-	* Get the group id
-	*
-	* Go and find in the database the group_id corresponding to 'REGISTERED'
-	*
-	* @throws runtime_exception if the group id does not exist in database.
-	* @return null
-	*/
+	 * Get the group id
+	 *
+	 * Go and find in the database the group_id corresponding to 'REGISTERED'
+	 *
+	 * @throws runtime_exception if the group id does not exist in database.
+	 * @return null
+	 */
 	protected function get_group_id()
 	{
 		$sql = 'SELECT group_id
@@ -265,19 +247,19 @@ class add extends \phpbb\console\command\command
 
 		if (!$row || !$row['group_id'])
 		{
-			throw new runtime_exception($this->user->lang('NO_GROUP'));
+			throw new runtime_exception($this->language->lang('NO_GROUP'));
 		}
 
 		return $row['group_id'];
 	}
 
 	/**
-	* Send account activation email
-	*
-	* @param int $user_id The new user's id
-	* @param array $data  The user data array
-	* @return null
-	*/
+	 * Send account activation email
+	 *
+	 * @param int   $user_id The new user's id
+	 * @param array $data    The user data array
+	 * @return null
+	 */
 	protected function send_activation_email($user_id, $data)
 	{
 		if ($this->config['require_activation'] == USER_ACTIVATION_SELF)
@@ -310,12 +292,23 @@ class add extends \phpbb\console\command\command
 		$messenger->anti_abuse_headers($this->config, $this->user);
 
 		$messenger->assign_vars(array(
-			'WELCOME_MSG'	=> htmlspecialchars_decode($this->user->lang('WELCOME_SUBJECT', $this->config['sitename'])),
-			'USERNAME'		=> htmlspecialchars_decode($data['username']),
-			'PASSWORD'		=> htmlspecialchars_decode($data['new_password']),
-			'U_ACTIVATE'	=> generate_board_url() . "/ucp.{$this->php_ext}?mode=activate&u=$user_id&k=$user_actkey")
+				'WELCOME_MSG' => htmlspecialchars_decode($this->language->lang('WELCOME_SUBJECT', $this->config['sitename'])),
+				'USERNAME'    => htmlspecialchars_decode($data['username']),
+				'PASSWORD'    => htmlspecialchars_decode($data['new_password']),
+				'U_ACTIVATE'  => generate_board_url() . "/ucp.{$this->php_ext}?mode=activate&u=$user_id&k=$user_actkey")
 		);
 
 		$messenger->send(NOTIFY_EMAIL);
+	}
+
+	/**
+	 * Helper to translate questions to the user
+	 *
+	 * @param string $key The language key
+	 * @return string The language key translated with a colon and space appended
+	 */
+	protected function ask_user($key)
+	{
+		return $this->language->lang($key) . $this->language->lang('COLON') . ' ';
 	}
 }
