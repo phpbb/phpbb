d713ce94ff218c2464823cb23dae1007354c60f3
Matt Friedman <maf675@gmail.com>
2016-04-12 10:56:23 -0700

    [ticket/14569] Extract CLI progress bar creation to a method
    PHPBB3-14569

--- phpBB/phpbb/console/command/command.php
+++ phpBB/phpbb/console/command/command.php
@@ -13,6 +13,10 @@
 
 namespace phpbb\console\command;
 
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
 abstract class command extends \Symfony\Component\Console\Command\Command
 {
 	/** @var \phpbb\user */
@@ -27,5 +31,46 @@ abstract class command extends \Symfony\Component\Console\Command\Command
 	{
 		$this->user = $user;
 		parent::__construct();
+	}
+
+	/**
+	 * Create a styled progress bar
+	 *
+	 * @param int             $max     Max value for the progress bar
+	 * @param SymfonyStyle    $io      Symfony style output decorator
+	 * @param OutputInterface $output  The output stream, used to print messages
+	 * @param bool            $message Should we display message output under the progress bar?
+	 * @return ProgressBar
+	 */
+	public function create_progress_bar($max, SymfonyStyle $io, OutputInterface $output, $message = false)
+	{
+		$progress = $io->createProgressBar($max);
+		if ($output->getVerbosity() === OutputInterface::VERBOSITY_VERBOSE)
+		{
+			$progress->setFormat('<info>[%percent:3s%%]</info> %message%');
+			$progress->setOverwrite(false);
+		}
+		else if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERY_VERBOSE)
+		{
+			$progress->setFormat('<info>[%current:s%/%max:s%]</info><comment>[%elapsed%/%estimated%][%memory%]</comment> %message%');
+			$progress->setOverwrite(false);
+		}
+		else
+		{
+			$io->newLine(2);
+			$progress->setFormat(
+				"    %current:s%/%max:s% %bar%  %percent:3s%%\n" .
+				"        " . ($message ? '%message%' : '                ') . " %elapsed:6s%/%estimated:-6s% %memory:6s%\n");
+			$progress->setBarWidth(60);
+		}
+
+		if (!defined('PHP_WINDOWS_VERSION_BUILD'))
+		{
+			$progress->setEmptyBarCharacter('░'); // light shade character \u2591
+			$progress->setProgressCharacter('');
+			$progress->setBarCharacter('▓'); // dark shade character \u2593
+		}
+
+		return $progress;
 	}
 }
