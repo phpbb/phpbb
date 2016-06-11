42b6dc38c5f173e98d27b8f5ffdc19e9bacbddc6
Marc Alexander <admin@m-a-styles.de>
2016-06-11 17:46:51 +0200

    [ticket/14670] Fix incompatibilities with container definitions
    PHPBB3-14670

--- tests/mock/container_builder.php
+++ tests/mock/container_builder.php
@@ -23,11 +23,11 @@ class phpbb_mock_container_builder implements ContainerInterface
 	*
 	* @param string $id      The service identifier
 	* @param object $service The service instance
-	* @param string $scope   The scope of the service
+	* @param bool $shared Whether service is shared
 	*
 	* @api
 	*/
-	public function set($id, $service, $scope = self::SCOPE_CONTAINER)
+	public function set($id, $service, $shared = false)
 	{
 		$this->services[$id] = $service;
 	}
@@ -192,5 +192,10 @@ class phpbb_mock_container_builder implements ContainerInterface
 	public function isFrozen()
 	{
 		return false;
+	}
+
+	public function initialized($id)
+	{
+		return true;
 	}
 }
