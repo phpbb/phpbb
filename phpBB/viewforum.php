f991a99a14da4d7fc79df9429d6f0e5f6d0f8914
upstrocker <upstrocker@gmail.com>
2017-03-23 12:03:33 +0300

    [ticket/15139] Assigned 'topics_per_page' before template included
    The element $config['topics_per_page'] was assigned a value after the
    template was included, which led to incorrect page number display in the
    header when pagination.
    Now this var is assigned the correct value before the template is included
    
    PHPBB3-15139

--- phpBB/viewforum.php
+++ phpBB/viewforum.php
@@ -146,6 +146,13 @@ else
 	}
 }
 
+// Is a forum specific topic count required?
+if ($forum_data['forum_topics_per_page'])
+{
+	$config['topics_per_page'] = $forum_data['forum_topics_per_page'];
+}
+
+/* @var $phpbb_content_visibility \phpbb\content_visibility */
 $phpbb_content_visibility = $phpbb_container->get('content.visibility');
 
 // Dump out the page header and load viewforum template
@@ -207,12 +214,6 @@ if ($mark_read == 'topics')
 	}
 
 	trigger_error($user->lang['TOPICS_MARKED'] . '<br /><br />' . sprintf($user->lang['RETURN_FORUM'], '<a href="' . $redirect_url . '">', '</a>'));
-}
-
-// Is a forum specific topic count required?
-if ($forum_data['forum_topics_per_page'])
-{
-	$config['topics_per_page'] = $forum_data['forum_topics_per_page'];
 }
 
 // Do the forum Prune thang - cron type job ...
