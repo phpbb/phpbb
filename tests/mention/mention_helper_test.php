310fdfd087d4c531be1842e109e97b0486554262
flash1452 <vishpandey2014@gmail.com>
2017-07-14 17:31:04 +0530

    [ticket/13713] Initialize request
    Mock a request to call server function
    
    PHPBB3-13713

--- tests/mention/mention_helper_test.php
+++ tests/mention/mention_helper_test.php
@@ -41,6 +41,7 @@ class phpbb_mention_helper_test extends phpbb_test_case {
 
 	public function test_get_regex_substituted_text() {
 
+		global $request;
 		$sample_text = 'cbwkf kfbwkef wkfwb @robdyrdek vghjgjdsrshc ftfkyugb buykyuf yiglyftydtrf gyg ygiy @RickyBahner vgjdrs jfytg jbhjv ygfjh @HackMurphy kdnkwe [url = #]@andream[/url] [url = http://www.google.com]google[/url]wifhr [url = http://localhost/phpbb/phpBB/memberlist.php?mode=viewprofile&u=13]LindsayM[/url] wewe [url = http://localhost/phpbb/phpBB/memberlist.php?mode=viewprofile&u=13] @LindsayM [/url][mention]admin[/mention][mention]admin[/mention] dwjpfw [mention]admin[/mention]';
 		$regular_expression_match = '#\[mention\](.*?)\[/mention\]#';
 		$db = $this->getMock('\phpbb\db\driver\driver_interface');
@@ -49,6 +50,7 @@ class phpbb_mention_helper_test extends phpbb_test_case {
 		$start_tag_length = strlen("[mention]");
 		$end_tag_length = strlen("[\mention]");
 		$userid_list = array("admin" => 2);
+		$request = new phpbb_mock_request;
 		$matches = $log->get_regex_match($regular_expression_match, $sample_text);
 		$new_text = $log->get_regex_substituted_text($matches, $sample_text, $start_tag_length, $end_tag_length, $userid_list);
 		$expected_text = "cbwkf kfbwkef wkfwb @robdyrdek vghjgjdsrshc ftfkyugb buykyuf yiglyftydtrf gyg ygiy @RickyBahner vgjdrs jfytg jbhjv ygfjh @HackMurphy kdnkwe [url = #]@andream[/url] [url = http://www.google.com]google[/url]wifhr [url = http://localhost/phpbb/phpBB/memberlist.php?mode=viewprofile&u=13]LindsayM[/url] wewe [url = http://localhost/phpbb/phpBB/memberlist.php?mode=viewprofile&u=13] @LindsayM [/url][url=http://memberlist.php?mode=viewprofile&u=2]admin[/url][url=http://memberlist.php?mode=viewprofile&u=2]admin[/url] dwjpfw [url=http://memberlist.php?mode=viewprofile&u=2]admin[/url]";
