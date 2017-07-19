af21b8b6c147f19e6c4aba1e0d4f558200dc2411
Rubén Calvo <rubencm@gmail.com>
2017-07-19 14:26:07 +0200

    [ticket/15287] Update storage
    PHPBB3-15287

--- phpBB/phpbb/storage/adapter/adapter_interface.php
+++ phpBB/phpbb/storage/adapter/adapter_interface.php
@@ -16,6 +16,13 @@ namespace phpbb\storage\adapter;
 interface adapter_interface
 {
 	/**
+	 * Set adapter parameters
+	 *
+	 * @param array	options		Storage-specific options.
+	 */
+	public function configure($options);
+
+	/**
 	 * Dumps content into a file.
 	 *
 	 * @param string	path		The file to be written to.
