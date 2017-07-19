c18c40aaa5c555d4d837a9163cc209f47e284b27
Rubén Calvo <rubencm@gmail.com>
2017-07-19 14:07:29 +0200

    [ticket/15253] Add create dir method
    PHPBB3-15253

--- phpBB/phpbb/storage/adapter/adapter_interface.php
+++ phpBB/phpbb/storage/adapter/adapter_interface.php
@@ -77,4 +77,13 @@ interface adapter_interface
 	 * 												When the file cannot be copied
 	 */
 	public function copy($path_orig, $path_dest);
+
+	/**
+	 * Creates a directory recursively.
+ 	 *
+	 * @param string	$path	The directory path
+ 	 *
+	 * @throws \phpbb\storage\exception\exception	On any directory creation failure
+ 	 */
+	public function create_dir($path);
 }
