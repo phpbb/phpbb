6ab124472c7508afae65c9433c14be9733e85a29
flash1452 <vishpandey2014@gmail.com>
2017-07-22 14:34:29 +0530

    [ticket/13713] Add tabs to user_mention
    Add tabs instead of spaces in user_mention
    
    PHPBB3-13713

--- phpBB/phpbb/mention/controller/user_mention.php
+++ phpBB/phpbb/mention/controller/user_mention.php
@@ -23,50 +23,50 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class user_mention
 {
-    /**
-    * @var driver_interface
-    */
-    private $db;
+	/**
+	* @var driver_interface
+	*/
+	private $db;
 
-    /**
-    * @var request_interface
-    */
-    private $request;
+	/**
+	* @var request_interface
+	*/
+	private $request;
 
-    /**
-    * @var container_interface
-    */
-    private $container;
+	/**
+	* @var container_interface
+	*/
+	private $container;
 
-    /**
-    * User Mention Controller Constructor
-    *
-    * @param $db       Database Driver Interface Object.
-    * @param $request  Request Interface Object.
-    *
-    * @return \phpbb\mention\controller\user_mention
-    */
-    public function __construct(driver_interface $db, request_interface $request, ContainerInterface $container)
-    {
-        $this->db = $db;
-        $this->request = $request;
-        $this->container = $container;
-    }
-    /**
-    *
-    * Get a list of users matching on a username.
-    *
-    * @return \Symfony\Component\HttpFoundation\Response A Symfony Response object
-    */
-    public function handle()
-    {
-        $user_search_keyword = utf8_clean_string($this->request->variable('q', '', true));
-        if (strlen($user_search_keyword) < 3)
-        {
-            return new JsonResponse(['usernames' => []]);
-        }
-        $helper_container = $this->container->get('phpbb_mention_helper');
-        $return_usernames_userid = $helper_container->get_allusers($user_search_keyword);
-        return new JsonResponse($return_usernames_userid);
-    }
+	/**
+	* User Mention Controller Constructor
+	*
+	* @param $db       Database Driver Interface Object.
+	* @param $request  Request Interface Object.
+	*
+	* @return \phpbb\mention\controller\user_mention
+	*/
+	public function __construct(driver_interface $db, request_interface $request, ContainerInterface $container)
+	{
+		$this->db = $db;
+		$this->request = $request;
+		$this->container = $container;
+	}
+	/**
+	*
+	* Get a list of users matching on a username.
+	*
+	* @return \Symfony\Component\HttpFoundation\Response A Symfony Response object
+	*/
+	public function handle()
+	{
+		$user_search_keyword = utf8_clean_string($this->request->variable('q', '', true));
+		if (strlen($user_search_keyword) < 3)
+		{
+			return new JsonResponse(['usernames' => []]);
+		}
+		$helper_container = $this->container->get('phpbb_mention_helper');
+		$return_usernames_userid = $helper_container->get_allusers($user_search_keyword);
+		return new JsonResponse($return_usernames_userid);
+	}
 }
