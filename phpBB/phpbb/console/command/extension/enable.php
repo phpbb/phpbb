aa23af38d4d3a13918e859005277955debd76acd
Rubén Calvo <rubencm@gmail.com>
2017-09-07 12:53:44 +0200

    [ticket/15349] Check if extension is enableable before enable
    PHPBB3-15346

--- phpBB/phpbb/console/command/extension/enable.php
+++ phpBB/phpbb/console/command/extension/enable.php
@@ -37,6 +37,13 @@ class enable extends command
 		$io = new SymfonyStyle($input, $output);
 
 		$name = $input->getArgument('extension-name');
+		$extension = $this->manager->get_extension($name);
+
+		if (!$extension->is_enableable())
+		{
+			$io->error($this->user->lang('CLI_EXTENSION_NOT_ENABLEABLE', $name));
+			return 1;
+		}
 
 		if ($this->manager->is_enabled($name))
 		{
