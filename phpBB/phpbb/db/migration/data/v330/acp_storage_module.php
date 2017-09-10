c10f1b38ac492463e5ac594757dc9eac535099c2
Rubén Calvo <rubencm@gmail.com>
2017-09-10 14:10:56 +0200

    [ticket/15289] Add migration for storage module
    PHPBB3-15289

---
+++ phpBB/phpbb/db/migration/data/v330/acp_storage_module.php
@@ -0,0 +1,31 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+
+namespace phpbb\db\migration\data\v330;
+
+class acp_storage_module extends \phpbb\db\migration\migration
+{
+	public function update_data()
+	{
+		return array(
+			array('module.add', array(
+				'acp',
+				'ACP_SERVER_CONFIGURATION',
+				array(
+					'module_basename'	=> 'acp_storage',
+					'modes'				=> array('settings'),
+				),
+			)),
+		);
+	}
+}
