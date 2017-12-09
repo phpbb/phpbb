37a55052e77fc8d9f6b929917b93d1a0d7d8ec90
kasimi <mail@kasimi.net>
2017-12-09 10:56:26 +0100

    [ticket/15440] Reverted changes to core.ucp_register_agreement
    PHPBB3-15440

--- phpBB/includes/ucp/ucp_register.php
+++ phpBB/includes/ucp/ucp_register.php
@@ -166,7 +166,7 @@ class ucp_register
 					->format($user->lang['DATE_FORMAT'], true);
 				unset($now);
 
-				$template_vars = array(
+				$template->assign_vars(array(
 					'S_LANG_OPTIONS'	=> (sizeof($lang_row) > 1) ? language_select($user_lang) : '',
 					'L_COPPA_NO'		=> sprintf($user->lang['UCP_COPPA_BEFORE'], $coppa_birthday),
 					'L_COPPA_YES'		=> sprintf($user->lang['UCP_COPPA_ON_AFTER'], $coppa_birthday),
@@ -180,11 +180,11 @@ class ucp_register
 
 					'COOKIE_NAME'		=> $config['cookie_name'],
 					'COOKIE_PATH'		=> $config['cookie_path'],
-				);
+				));
 			}
 			else
 			{
-				$template_vars = array(
+				$template->assign_vars(array(
 					'S_LANG_OPTIONS'	=> (sizeof($lang_row) > 1) ? language_select($user_lang) : '',
 					'L_TERMS_OF_USE'	=> sprintf($user->lang['TERMS_OF_USE_CONTENT'], $config['sitename'], generate_board_url()),
 
@@ -195,12 +195,10 @@ class ucp_register
 
 					'COOKIE_NAME'		=> $config['cookie_name'],
 					'COOKIE_PATH'		=> $config['cookie_path'],
+					)
 				);
 			}
-
-			$template->assign_vars($template_vars);
-
-			$tpl_name = 'ucp_agreement';
+			unset($lang_row);
 
 			/**
 			* Allows to modify the agreements.
@@ -208,19 +206,11 @@ class ucp_register
 			* To assign data to the template, use $template->assign_vars()
 			*
 			* @event core.ucp_register_agreement
-			* @var	string	tpl_name			Template file
-			* @var	array	template_vars		Array with data assigned to the template, read only
-			* @var	array	lang_row			Array with available languages, read only
-			* @var	array	s_hidden_fields		Array with hidden form elements, read only
 			* @since 3.1.6-RC1
-			* @changed 3.2.2-RC1 Added tpl_name, template_vars, lang_row, s_hidden_fields
 			*/
-			$vars = array('tpl_name', 'template_vars', 'lang_row', 's_hidden_fields');
-			extract($phpbb_dispatcher->trigger_event('core.ucp_register_agreement', compact($vars)));
+			$phpbb_dispatcher->dispatch('core.ucp_register_agreement');
 
-			unset($lang_row);
-
-			$this->tpl_name = $tpl_name;
+			$this->tpl_name = 'ucp_agreement';
 			return;
 		}
 
