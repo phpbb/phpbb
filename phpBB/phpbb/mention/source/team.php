3840ba94401fe346100f6b7239de23f62ff6d11e
lavigor <lavigor@users.noreply.github.com>
2018-06-09 18:56:26 +0300

    [ticket/13713] Add two new sources: member and team
    PHPBB3-13713

---
+++ phpBB/phpbb/mention/source/team.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ * This file is part of the phpBB Forum Software package.
+ *
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license       GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the docs/CREDITS.txt file.
+ *
+ */
+
+namespace phpbb\mention\source;
+
+class team extends user
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function query($keyword, $topic_id)
+	{
+		$query = $this->db->sql_build_query('SELECT', [
+			'SELECT'    => 'u.username, u.user_id',
+			'FROM'      => [
+				USERS_TABLE => 'u',
+				USER_GROUP_TABLE => 'ug',
+				TEAMPAGE_TABLE => 't',
+			],
+			'WHERE'     => 'ug.group_id = t.group_id AND ug.user_id = u.user_id AND ug.user_pending = 0
+				AND u.user_id <> ' . ANONYMOUS . '
+				AND ' . $this->db->sql_in_set('u.user_type', [USER_NORMAL, USER_FOUNDER]) . '
+				AND u.username_clean ' . $this->db->sql_like_expression($keyword . $this->db->get_any_char()),
+			'ORDER_BY'  => 'u.user_lastvisit DESC'
+		]);
+		return $query;
+	}
+}
