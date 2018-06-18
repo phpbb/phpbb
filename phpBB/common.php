bdff0f55907d4a9a08bf570df392adb8f17e3587
Jakub Senko <jakubsenko@gmail.com>
2018-06-18 20:42:16 +0200

    [ticket/12627] Add debug.sql_explain parameter
    PHPBB3-12627

--- phpBB/common.php
+++ phpBB/common.php
@@ -131,6 +131,8 @@ catch (InvalidArgumentException $e)
 $phpbb_class_loader->set_cache($phpbb_container->get('cache.driver'));
 $phpbb_class_loader_ext->set_cache($phpbb_container->get('cache.driver'));
 
+$phpbb_container->get('dbal.conn')->set_debug_sql_explain($phpbb_container->getParameter('debug.sql_explain'));
+
 require($phpbb_root_path . 'includes/compatibility_globals.' . $phpEx);
 
 register_compatibility_globals();
--- phpBB/phpbb/db/driver/factory.php
+++ phpBB/phpbb/db/driver/factory.php
@@ -68,6 +68,14 @@ class factory implements driver_interface
 	/**
 	* {@inheritdoc}
 	*/
+	public function set_debug_sql_explain($value)
+	{
+		$this->get_driver()->set_debug_sql_explain($value);
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
 	public function get_sql_layer()
 	{
 		return $this->get_driver()->get_sql_layer();
