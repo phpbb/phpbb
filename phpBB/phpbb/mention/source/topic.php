db31066a1d61f4a618f3b27fa0e7b6320a261a4a
lavigor <lavigor@users.noreply.github.com>
2018-07-22 03:58:00 +0300

    [ticket/13713] Fix priorities
    PHPBB3-13713

--- phpBB/phpbb/mention/source/topic.php
+++ phpBB/phpbb/mention/source/topic.php
@@ -18,15 +18,35 @@ class topic extends base_user
 	/**
 	 * {@inheritdoc}
 	 */
+	public function get_priority($row)
+	{
+		/*
+		 * Topic's open poster is probably the most mentionable user in the topic
+		 * so we give him a significant priority
+		 */
+		if ($row['user_id'] === $row['topic_poster'])
+		{
+			return 5;
+		}
+
+		return 1;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
 	protected function query($keyword, $topic_id)
 	{
 		/*
+		 * Select poster's username together with topic author's ID
+		 * that will be later used for priotirisation
+		 *
 		 * For optimization purposes all users are returned regardless of the keyword
 		 * Names filtering is done on the frontend
 		 * Results will be cached on a per-topic basis
 		 */
 		$query = $this->db->sql_build_query('SELECT', [
-			'SELECT'    => 'u.username, u.user_id',
+			'SELECT'    => 'u.username, u.user_id, t.topic_poster',
 			'FROM'      => [
 				USERS_TABLE => 'u',
 			],
@@ -34,7 +54,11 @@ class topic extends base_user
 				[
 					'FROM' => [POSTS_TABLE => 'p'],
 					'ON'   => 'u.user_id = p.poster_id'
-				]
+				],
+				[
+					'FROM' => [TOPICS_TABLE => 't'],
+					'ON'   => 't.topic_id = p.topic_id'
+				],
 			],
 			'WHERE'     => 'p.topic_id = ' . $topic_id . '
 				AND ' . $this->db->sql_in_set('u.user_type', [USER_NORMAL, USER_FOUNDER]),
--- phpBB/phpbb/mention/source/user.php
+++ phpBB/phpbb/mention/source/user.php
@@ -18,16 +18,28 @@ class user extends base_user
 	/**
 	 * {@inheritdoc}
 	 */
+	public function get_priority($row)
+	{
+		/*
+		 * Presence in array with all names for this type should not increase the priority
+		 * Otherwise names will not be properly sorted because we fetch them in batches
+		 * and the name from 'special' source can be absent from the array with all names
+		 * and therefore it will appear lower than needed
+		 */
+		return 0;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
 	protected function query($keyword, $topic_id)
 	{
-		// TODO: think about caching ALL users: 1m users results to ~40MB file
 		$query = $this->db->sql_build_query('SELECT', [
 			'SELECT'    => 'u.username_clean, u.username, u.user_id',
 			'FROM'      => [
 				USERS_TABLE => 'u',
 			],
-			'WHERE'     => $this->db->sql_in_set('u.user_type', [USER_NORMAL, USER_FOUNDER])/* . '
-				AND u.username_clean ' . $this->db->sql_like_expression($keyword . $this->db->get_any_char())*/,
+			'WHERE'     => $this->db->sql_in_set('u.user_type', [USER_NORMAL, USER_FOUNDER]),
 			'ORDER_BY'  => 'u.user_lastvisit DESC'
 		]);
 		return $query;
