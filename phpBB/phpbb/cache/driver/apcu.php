7f1f64b1dcebb28ab575e1905676471b473d08ed
v12mike <github@ingenious.co.nz>
2018-07-26 06:09:02 +0100

    [ticket/15726] Implement selective purge in APCu cache driver
    Fix whitespace
    
    PHPBB3-15726

--- phpBB/phpbb/cache/driver/apcu.php
+++ phpBB/phpbb/cache/driver/apcu.php
@@ -25,9 +25,9 @@ class apcu extends \phpbb\cache\driver\memory
 	*/
 	function purge()
 	{
-	/* 
-		Use an iterator to selectively clear our cache entries without disturbing
-		any other cache users (e.g. other phpBB boards hosted on this server)
+	/*
+	* Use an iterator to selectively clear our cache entries without disturbing
+	* any other cache users (e.g. other phpBB boards hosted on this server)
 	*/
 		apcu_delete(new \APCUIterator('#^' . $this->key_prefix . '#'));
 
