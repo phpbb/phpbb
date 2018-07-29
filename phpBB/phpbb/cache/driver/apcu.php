2bc36d491706530201d2e34b42af944f09120c1c
root <root@louis.frenchcarforum.co.uk>
2018-07-29 09:15:12 +0100

    [ticket/15726] Implement selective purge in APCu
    fix indentation of comment
    
    PHPBB3-15726

--- phpBB/phpbb/cache/driver/apcu.php
+++ phpBB/phpbb/cache/driver/apcu.php
@@ -25,10 +25,10 @@ class apcu extends \phpbb\cache\driver\memory
 	*/
 	function purge()
 	{
-	/*
-	* Use an iterator to selectively delete our cache entries without disturbing
-	* any other cache users (e.g. other phpBB boards hosted on this server)
-	*/
+		/*
+		* Use an iterator to selectively delete our cache entries without disturbing
+		* any other cache users (e.g. other phpBB boards hosted on this server)
+		*/
 		apcu_delete(new \APCUIterator('#^' . $this->key_prefix . '#'));
 
 		parent::purge();
