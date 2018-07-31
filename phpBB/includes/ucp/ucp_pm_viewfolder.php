1ac2802dfad4ff87132c49bdd5ce0f57325a8ba6
akbarhashmi <akbar6127@gmail.com>
2018-07-31 21:45:30 +0530

    [ticket/15682] Change query parameter for pagination
    PHPBB3-15682

--- phpBB/includes/ucp/ucp_pm_viewfolder.php
+++ phpBB/includes/ucp/ucp_pm_viewfolder.php
@@ -449,8 +449,8 @@ function get_pm_from($folder_id, $folder, $user_id)
 
 	$base_url = append_sid("{$phpbb_root_path}ucp.$phpEx", "i=pm&amp;mode=view&amp;action=view_folder&amp;f=$folder_id&amp;$u_sort_param");
 	$start = $pagination->validate_start($start, $config['topics_per_page'], $pm_count);
-	$pagination->generate_template_pagination($base_url, 'paginations', 'start', $pm_count, $config['topics_per_page'], $start);
-//temporary
+	$pagination->generate_template_pagination($base_url, 'pagination', 'start', $pm_count, $config['topics_per_page'], $start);
+
 	$template_vars = array(
 		'TOTAL_MESSAGES'	=> $user->lang('VIEW_PM_MESSAGES', (int) $pm_count),
 
