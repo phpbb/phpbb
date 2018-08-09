0c1bf3c561beb7892a7fe008e27742784e420d83
Rubén Calvo <rubencm@gmail.com>
2018-08-09 02:28:58 +0200

    [ticket/15692] Fix full_check in exist method
    PHPBB3-15692

--- phpBB/phpbb/storage/storage.php
+++ phpBB/phpbb/storage/storage.php
@@ -146,7 +146,7 @@ class storage
 	 */
 	public function exists($path, $full_check = false)
 	{
-		return ($this->is_tracked($path) && !$full_check || $this->get_adapter()->exists($path));
+		return ($this->is_tracked($path) && (!$full_check || $this->get_adapter()->exists($path)));
 	}
 
 	/**
