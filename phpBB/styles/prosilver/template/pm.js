01c233e7e844c98a99e47859332af8b46cd5c147
akbarhashmi <akbar6127@gmail.com>
2018-08-09 17:47:21 +0530

    [ticket/15682] JS for bbcode model
    PHPBB3-15682

--- phpBB/styles/prosilver/template/pm.js
+++ phpBB/styles/prosilver/template/pm.js
@@ -6,6 +6,10 @@ var to_modal = document.getElementById('pm-to-model');
 var to_btn = document.getElementById("pm-to-button");
 var to_span = document.getElementsByClassName("pm-to-button-close")[0];
 
+var bb_modal = document.getElementById('pm-bbcode-model');
+var bb_btn = document.getElementById("pm-bbcode-button");
+var bb_span = document.getElementsByClassName("pm-to-button-close")[0];
+
 /**
  * When the user clicks the button, open the modal
  */
@@ -49,5 +53,28 @@ to_span.onclick = function() {
 window.onclick = function(event) {
 	if (event.target == to_modal) {
 		to_modal.style.display = "none";
+	}
+}
+
+/**
+ * When the user clicks the button, open the modal
+ */
+bb_btn.onclick = function() {
+	bb_modal.style.display = "block";
+}
+
+/**
+ * When the user clicks on <span> (x), close the modal
+ */
+bb_span.onclick = function() {
+	bb_modal.style.display = "none";
+}
+
+/**
+ * When the user clicks anywhere outside of the modal, close it
+ */
+window.onclick = function(event) {
+	if (event.target == bb_modal) {
+		bb_modal.style.display = "none";
 	}
 }
