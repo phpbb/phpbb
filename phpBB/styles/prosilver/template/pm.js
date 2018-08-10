7e8194216c13f76550996dfc8e2ca9b8c7847f76
akbarhashmi <akbar6127@gmail.com>
2018-08-10 16:50:38 +0530

    [ticket/15682] Use jQuery for popups and change colors CSS
    PHPBB3-15682

--- phpBB/styles/prosilver/template/pm.js
+++ phpBB/styles/prosilver/template/pm.js
@@ -1,80 +1,31 @@
-var modal = document.getElementById('pm-smile-model');
-var btn = document.getElementById("pm-smile-button");
-var span = document.getElementsByClassName("pm-button-close")[0];
+/**
+ * Open Pm smiley popup
+ */
+$(document).ready(function(){
+	$('#pm-smile-button').click(function(){
+		var $alert = phpbb.alert("Pm To: Box", $('#pm-smile-model').html());
 
-var to_modal = document.getElementById('pm-to-model');
-var to_btn = document.getElementById("pm-to-button");
-var to_span = document.getElementsByClassName("pm-to-button-close")[0];
-
-var bb_modal = document.getElementById('pm-bbcode-model');
-var bb_btn = document.getElementById("pm-bbcode-button");
-var bb_span = document.getElementsByClassName("pm-to-button-close")[0];
+		$alert.on('click', '.pm-smiley-code', function() {
+			phpbb.alert.close($alert, true);
+		});
+	});
+});
 
 /**
- * When the user clicks the button, open the modal
+ * Open PM:To popup
  */
-btn.onclick = function() {
-	modal.style.display = "block";
-}
+$(document).ready(function(){
+	$('#pm-to-button').click(function(){
+		var $alert = phpbb.alert("Smiley Box", $('#pm-to-model').html());
+
+	});
+});
 
 /**
- * When the user clicks on <span> (x), close the modal
+ * Open BBCode popup
  */
-span.onclick = function() {
-	modal.style.display = "none";
-}
-
-/**
- * When the user clicks anywhere outside of the modal, close it
- */
-window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
-}
-
-/**
- * When the user clicks the button, open the modal
- */
-to_btn.onclick = function() {
-	to_modal.style.display = "block";
-}
-
-/**
- * When the user clicks on <span> (x), close the modal
- */
-to_span.onclick = function() {
-	to_modal.style.display = "none";
-}
-
-/**
- * When the user clicks anywhere outside of the modal, close it
- */
-window.onclick = function(event) {
-	if (event.target == to_modal) {
-		to_modal.style.display = "none";
-	}
-}
-
-/**
- * When the user clicks the button, open the modal
- */
-bb_btn.onclick = function() {
-	bb_modal.style.display = "block";
-}
-
-/**
- * When the user clicks on <span> (x), close the modal
- */
-bb_span.onclick = function() {
-	bb_modal.style.display = "none";
-}
-
-/**
- * When the user clicks anywhere outside of the modal, close it
- */
-window.onclick = function(event) {
-	if (event.target == bb_modal) {
-		bb_modal.style.display = "none";
-	}
-}
+$(document).ready(function(){
+	$('#pm-bbcode-button').click(function(){
+		phpbb.alert("BBCode Box", $('#pm-bbcode-model').html());
+	});
+});
