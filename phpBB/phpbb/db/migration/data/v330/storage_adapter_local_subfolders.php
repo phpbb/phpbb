225a6e2cbeeb9e851b43b069ee34cd5dee91649c
Rubén Calvo <rubencm@gmail.com>
2018-08-13 15:55:14 +0200

    [ticket/15747] Replace migration
    PHPBB3-15747

--- phpBB/phpbb/db/migration/data/v330/storage_adapter_local_subfolders.php
+++ phpBB/phpbb/db/migration/data/v330/storage_adapter_local_subfolders.php
@@ -13,12 +13,14 @@
 
 namespace phpbb\db\migration\data\v330;
 
-class storage_adapter_local_depth_rename extends \phpbb\db\migration\migration
+class storage_adapter_local_subfolders extends \phpbb\db\migration\migration
 {
 	static public function depends_on()
 	{
 		return array(
-			'\phpbb\db\migration\data\v330\storage_adapter_local_depth',
+			'\phpbb\db\migration\data\v330\storage_attachment',
+			'\phpbb\db\migration\data\v330\storage_avatar',
+			'\phpbb\db\migration\data\v330\storage_backup',
 		);
 	}
 
@@ -27,17 +29,14 @@ class storage_adapter_local_depth_rename extends \phpbb\db\migration\migration
 		return array(
 			array('if', array(
 				($this->config['storage\\attachment\\provider'] == \phpbb\storage\provider\local::class),
-				array('config.delete', array('storage\\attachment\\config\\depth')),
 				array('config.add', array('storage\\attachment\\config\\subfolders', '0')),
 			)),
 			array('if', array(
 				($this->config['storage\\avatar\\provider'] == \phpbb\storage\provider\local::class),
-				array('config.delete', array('storage\\avatar\\config\\depth')),
 				array('config.add', array('storage\\avatar\\config\\subfolders', '0')),
 			)),
 			array('if', array(
 				($this->config['storage\\backup\\provider'] == \phpbb\storage\provider\local::class),
-				array('config.delete', array('storage\\backup\\config\\depth')),
 				array('config.add', array('storage\\backup\\config\\subfolders', '0')),
 			)),
 		);
