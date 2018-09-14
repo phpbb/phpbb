4ac3c545e1b95fb2abfdd6f4343865e7646c978e
Marc Alexander <admin@m-a-styles.de>
2018-09-14 15:02:07 +0200

    [ticket/15770] Make sure sphinx limits are cast to int
    PHPBB3-15770

--- phpBB/phpbb/search/fulltext_sphinx.php
+++ phpBB/phpbb/search/fulltext_sphinx.php
@@ -648,7 +648,7 @@ class fulltext_sphinx
 
 		$this->sphinx->SetFilter('deleted', array(0));
 
-		$this->sphinx->SetLimits($start, (int) $per_page, SPHINX_MAX_MATCHES);
+		$this->sphinx->SetLimits((int) $start, (int) $per_page, SPHINX_MAX_MATCHES);
 		$result = $this->sphinx->Query($search_query_prefix . $this->sphinx->EscapeString(str_replace('&quot;', '"', $this->search_query)), $this->indexes);
 
 		// Could be connection to localhost:9312 failed (errno=111,
