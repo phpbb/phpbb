db36318ad0f0a12a63b83fe54526103f17f3ffdc
rubencm <rubencm@gmail.com>
2018-09-15 09:13:01 +0000

    [ticket/15772] Hide warning in acp when allow_install_dir is true
    PHPBB3-15772

--- phpBB/includes/acp/acp_main.php
+++ phpBB/includes/acp/acp_main.php
@@ -657,7 +657,7 @@ class acp_main
 		}
 
 		// Warn if install is still present
-		if (file_exists($phpbb_root_path . 'install') && !is_file($phpbb_root_path . 'install'))
+		if (!defined('IN_INSTALL') && !$phpbb_container->getParameter('allow_install_dir') && file_exists($phpbb_root_path . 'install') && !is_file($phpbb_root_path . 'install'))
 		{
 			$template->assign_var('S_REMOVE_INSTALL', true);
 		}
