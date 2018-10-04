e981202a573a6c0d997ec4922f0247bb609c2cc2
Jakub Senko <jakubsenko@gmail.com>
2018-10-04 11:05:18 +0200

    [feature/1clickupdate] Fix .php ext on incomplete update link

--- phpBB/includes/acp/acp_update.php
+++ phpBB/includes/acp/acp_update.php
@@ -74,7 +74,7 @@ class acp_update
 		// Incomplete update?
 		if (phpbb_version_compare($config['version'], PHPBB_VERSION, '<'))
 		{
-			$database_update_link = $phpbb_root_path . 'install/app.php/update';
+			$database_update_link = $phpbb_root_path . 'install/app.' . $phpEx . '/update';
 
 			$template->assign_vars(array(
 				'S_UPDATE_INCOMPLETE'		=> true,
