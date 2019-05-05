9a58166ae281338673a7dc28b1bba3d8637fbb90
rubencm <rubencm@gmail.com>
2019-05-05 17:08:08 +0000

    [ticket/16044] Fix vagrant in 3.2.x
    PHPBB3-16044

--- vagrant/after.sh
+++ vagrant/after.sh
@@ -6,28 +6,22 @@ PHPBB_INSTALL="${PHPBB_PATH}/vagrant/phpbb-install-config.yml"
 
 # Ensure composer deps are installed
 cd ${PHPBB_PATH}/phpBB
-php ../composer.phar install
+php7.2 ../composer.phar install --ignore-platform-reqs
 
 # Backup current config.php file
 if [ -e ${PHPBB_CONFIG} ]
 then
     cp --backup=numbered ${PHPBB_CONFIG} ${PHPBB_CONFIG}.bak
+    rm -rf ${PHPBB_CONFIG}
 fi
 
-# Delete any sqlite db and config file
-rm -rf /tmp/phpbb.sqlite3
-rm -rf ${PHPBB_CONFIG}
-
 # Install phpBB
-php ${PHPBB_PATH}/phpBB/install/phpbbcli.php install ${PHPBB_INSTALL}
-
-# Update sqlite db file permissions
-sudo chown -R vagrant /tmp/phpbb.sqlite3
+php7.2 ${PHPBB_PATH}/phpBB/install/phpbbcli.php install ${PHPBB_INSTALL}
 
 # Add DEBUG mode to phpBB to remove annoying installer warnings
 sed -i "$ a @define('DEBUG', true);" ${PHPBB_CONFIG}
 
 # Update the PHP memory limits (enough to allow phpunit tests to run)
-sed -i "s/memory_limit = .*/memory_limit = 1024M/" /etc/php5/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 1024M/" /etc/php7.2/fpm/php.ini
 
 echo "Your board is ready at http://192.168.10.10/"
