78a913581c727801240758c0a8c3bfae86d124ca
v12mike <github@ingenious.co.nz>
2019-09-10 16:32:13 -0400

    [ticket/9837] Display unapproved posts to posters
    Add tests and improve style template
    
    PHPBB3-9837

--- phpBB/phpbb/content_visibility.php
+++ phpBB/phpbb/content_visibility.php
@@ -76,7 +76,7 @@ class content_visibility
 	* @param	string		$topics_table		Topics table name
 	* @param	string		$users_table		Users table name
 	*/
-	public function __construct(\phpbb\auth\auth $auth, \phpbb\config\config $config, \phpbb\event\dispatcher_interface $phpbb_dispatcher, \phpbb\db\driver\driver_interface $db, \phpbb\user $user, $phpbb_root_path, $php_ext, $forums_table, $posts_table, $topics_table, $users_table)
+	public function __construct(\phpbb\auth\auth $auth, \phpbb\config\config $config, \phpbb\event\dispatcher_interface $phpbb_dispatcher, \phpbb\db\driver\driver_interface $db, \phpbb\user $user, $phpbb_root_path, $php_ext, $forums_table, $posts_table, $topics_table,	$users_table)
 	{
 		$this->auth = $auth;
 		$this->config = $config;
@@ -198,8 +198,8 @@ class content_visibility
 		* @var	array		forum_id						The forum id in which the search is made.
 		* @var	string		table_alias						Table alias to prefix in SQL queries
 		* @var	mixed		get_visibility_sql_overwrite	If a string, forces the function to return get_forums_visibility_sql_overwrite after executing the event
-		* 													If false, get_visibility_sql continues normally
-		* 													It must be either boolean or string
+		*   												If false, get_visibility_sql continues normally
+		*   												It must be either boolean or string
 		* @since 3.1.4-RC1
 		*/
 		$vars = array(
@@ -225,10 +225,10 @@ class content_visibility
 			$visibility_query = $table_alias . $mode . '_visibility = ';
 
 			$where_sql .= '(' . $visibility_query . ITEM_APPROVED . ')';
-            if ($this->config['display_unapproved_posts'] && ($this->user->data['user_id'] <> ANONYMOUS))
+			if ($this->config['display_unapproved_posts'] && ($this->user->data['user_id'] <> ANONYMOUS))
 			{
 				$poster_key = ($mode === 'topic') ? 'topic_poster' : 'poster_id';
-				$where_sql .= ' OR (' . $visibility_query . ITEM_UNAPPROVED;
+				$where_sql .= ' OR ((' . $visibility_query . ITEM_UNAPPROVED . ' OR ' . $visibility_query . ITEM_REAPPROVE .')';
 				$where_sql .= ' AND ' . $table_alias . $poster_key . ' = ' . ((int) $this->user->data['user_id']) . ')';
 			}
 		}
@@ -268,8 +268,8 @@ class content_visibility
 		* @var	string		table_alias							Table alias to prefix in SQL queries
 		* @var	array		approve_forums						Array of forums where the user has m_approve permissions
 		* @var	mixed		get_forums_visibility_sql_overwrite	If a string, forces the function to return get_forums_visibility_sql_overwrite after executing the event
-		* 														If false, get_forums_visibility_sql continues normally
-		* 														It must be either boolean or string
+		*   													If false, get_forums_visibility_sql continues normally
+		*   													It must be either boolean or string
 		* @since 3.1.3-RC1
 		*/
 		$vars = array(
