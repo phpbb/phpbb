de33efd93b0accaa5a1d1b16e65cf554e91c2179
3D-I <480857+3D-I@users.noreply.github.com>
2019-10-30 14:47:05 +0100

    [ticket/16153] Enable Emojis and rich text in Topic title
    PHPBB3-16153

--- phpBB/posting.php
+++ phpBB/posting.php
@@ -1033,7 +1033,7 @@ if ($submit || $preview || $refresh || $save)
 	}
 
 	/**
-	 * Replace Emojis and other 4bit UTF-8 chars not allowed by MySql to UCR / NCR.
+	 * Replace Emojis and other 4bit UTF-8 chars not allowed by MySQL to UCR/NCR.
 	 * Using their Numeric Character Reference's Hexadecimal notation.
 	 */
 	$post_data['post_subject'] = utf8_encode_ucr($post_data['post_subject']);
