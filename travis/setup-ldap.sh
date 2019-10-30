51bb05f2860ed086ea8ef502eb4120c952e110dd
Marc Alexander <admin@m-a-styles.de>
2019-10-30 18:08:17 +0100

    [ticket/12574] Introduce files for LDAP testing on travis
    PHPBB3-12574

---
+++ travis/setup-ldap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# This file is part of the phpBB Forum Software package.
+#
+# @copyright (c) phpBB Limited <https://www.phpbb.com>
+# @license GNU General Public License, version 2 (GPL-2.0)
+#
+# For full copyright and license information, please see
+# the docs/CREDITS.txt file.
+#
+set -e
+set -x
+
+SLOWTESTS=$1
+
+if [ "$SLOWTESTS" == '1' ]
+then
+	sudo apt-get -y install ldap-utils slapd
+	echo "extension = ldap.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+	mkdir /tmp/slapd
+	slapd -f travis/ldap/slapd.conf -h ldap://localhost:3389 &
+	sleep 3
+	ldapadd -h localhost:3389 -D "cn=admin,dc=example,dc=com" -w adminadmin -f travis/ldap/base.ldif
+fi
