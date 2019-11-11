6320da67e4f031e5d47c74ecaea477c2e721f99a
Marc Alexander <admin@m-a-styles.de>
2019-11-11 20:35:09 +0100

    [ticket/security-249] Do not handle avatar submit on invalid token
    SECURITY-249

--- phpBB/includes/ucp/ucp_groups.php
+++ phpBB/includes/ucp/ucp_groups.php
@@ -534,7 +534,12 @@ class ucp_groups
 								'teampage'	=> $group_row['group_teampage'],
 							);
 
-							if ($config['allow_avatar'])
+							if (!check_form_key('ucp_groups'))
+							{
+								$error[] = $user->lang['FORM_INVALID'];
+							}
+
+							if (!count($error) && $config['allow_avatar'])
 							{
 								// Handle avatar
 								$driver_name = $phpbb_avatar_manager->clean_driver_name($request->variable('avatar_driver', ''));
@@ -554,11 +559,6 @@ class ucp_groups
 
 								// Merge any avatars errors into the primary error array
 								$error = array_merge($error, $phpbb_avatar_manager->localize_errors($user, $avatar_error));
-							}
-
-							if (!check_form_key('ucp_groups'))
-							{
-								$error[] = $user->lang['FORM_INVALID'];
 							}
 
 							// Validate submitted colour value
