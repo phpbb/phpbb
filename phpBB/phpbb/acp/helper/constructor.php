79143f4186ec39d79a45af86abe6e47fcbc0953c
mrgoldy <gijsmartens1@gmail.com>
2019-11-23 14:00:51 +0100

    [ticket/16218] CP Services: ACP Constructor
    PHPBB3-16218

--- phpBB/phpbb/acp/helper/constructor.php
+++ phpBB/phpbb/acp/helper/constructor.php
@@ -1,90 +1,125 @@
 <?php
 /**
-*
-* This file is part of the phpBB Forum Software package.
-*
-* @copyright (c) phpBB Limited <https://www.phpbb.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-* For full copyright and license information, please see
-* the docs/CREDITS.txt file.
-*
-*/
+ *
+ * This file is part of the phpBB Forum Software package.
+ *
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the docs/CREDITS.txt file.
+ *
+ */
 
-/**
-*/
-define('IN_PHPBB', true);
-define('ADMIN_START', true);
-define('NEED_SID', true);
+namespace phpbb\acp\helper;
 
-// Include files
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './../';
-$phpEx = substr(strrchr(__FILE__, '.'), 1);
-require($phpbb_root_path . 'common.' . $phpEx);
-require($phpbb_root_path . 'includes/functions_acp.' . $phpEx);
-require($phpbb_root_path . 'includes/functions_admin.' . $phpEx);
-require($phpbb_root_path . 'includes/functions_module.' . $phpEx);
+use phpbb\exception\http_exception;
 
-// Start session management
-$user->session_begin();
-$auth->acl($user->data);
-$user->setup('acp/common');
-// End session management
-
-// Have they authenticated (again) as an admin for this session?
-if (!isset($user->data['session_admin']) || !$user->data['session_admin'])
+class constructor implements \phpbb\cp\constructor_interface
 {
-	login_box('', $user->lang['LOGIN_ADMIN_CONFIRM'], $user->lang['LOGIN_ADMIN_SUCCESS'], true, false);
+	/** @var \phpbb\auth\auth */
+	protected $auth;
+
+	/** @var \phpbb\controller\helper */
+	protected $helper;
+
+	/** @var \phpbb\language\language */
+	protected $language;
+
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	/** @var string phpBB admin path */
+	protected $admin_path;
+
+	/** @var string phpBB root path */
+	protected $root_path;
+
+	/** @var string phpBB web path */
+	protected $web_path;
+
+	/** @var string php File extension */
+	protected $php_ext;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param \phpbb\auth\auth			$auth			Auth object
+	 * @param \phpbb\controller\helper	$helper			Controller helper object
+	 * @param \phpbb\language\language	$language		Language object
+	 * @param \phpbb\path_helper		$path_helper	Path helper object
+	 * @param \phpbb\template\template	$template		Template object
+	 * @param \phpbb\user				$user			User object
+	 * @param string					$admin_path		phpBB admin path
+	 * @param string					$root_path		phpBB root path
+	 * @param string					$php_ext		php File extension
+	 */
+	public function __construct(
+		\phpbb\auth\auth $auth,
+		\phpbb\controller\helper $helper,
+		\phpbb\language\language $language,
+		\phpbb\path_helper $path_helper,
+		\phpbb\template\template $template,
+		\phpbb\user $user,
+		$admin_path,
+		$root_path,
+		$php_ext
+	)
+	{
+		$this->auth			= $auth;
+		$this->helper		= $helper;
+		$this->language		= $language;
+		$this->template		= $template;
+		$this->user			= $user;
+
+		$this->admin_path	= $admin_path;
+		$this->root_path	= $root_path;
+		$this->web_path		= $path_helper->update_web_root_path($root_path);
+		$this->php_ext		= $php_ext;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setup()
+	{
+		define('ADMIN_START', true);
+		define('NEED_SID', true);
+
+		$this->language->add_lang('acp/common');
+
+		// Include files
+		require($this->root_path . 'includes/functions_acp.' . $this->php_ext);
+		require($this->root_path . 'includes/functions_admin.' . $this->php_ext);
+
+		// Have they authenticated (again) as an admin for this session?
+		if (!isset($this->user->data['session_admin']) || !$this->user->data['session_admin'])
+		{
+			login_box($this->helper->route('acp_index'), $this->language->lang('LOGIN_ADMIN_CONFIRM'), $this->language->lang('LOGIN_ADMIN_SUCCESS'), true, false);
+		}
+
+		// Is user any type of admin? No, then stop here, each script needs to
+		// check specific permissions but this is a catchall
+		if (!$this->auth->acl_get('a_'))
+		{
+			throw new http_exception(403, 'NO_ADMIN');
+		}
+
+		// We define the admin variables now, because the user is now able to use the admin related features...
+		define('IN_ADMIN', true);
+
+		// Set custom style for admin area
+		$this->template->set_custom_style([[
+			'name'		=> 'adm',
+			'ext_path'	=> 'adm/style/',
+		]], $this->admin_path . 'style');
+
+		$this->template->assign_vars([
+			'T_ASSETS_PATH'		=> $this->web_path . 'assets',
+			'T_TEMPLATE_PATH'	=> $this->web_path . $this->admin_path . 'style',
+		]);
+	}
 }
-
-// Is user any type of admin? No, then stop here, each script needs to
-// check specific permissions but this is a catchall
-if (!$auth->acl_get('a_'))
-{
-	send_status_line(403, 'Forbidden');
-	trigger_error('NO_ADMIN');
-}
-
-// We define the admin variables now, because the user is now able to use the admin related features...
-define('IN_ADMIN', true);
-
-// Some oft used variables
-$file_uploads	= (@ini_get('file_uploads') == '1' || strtolower(@ini_get('file_uploads')) === 'on') ? true : false;
-$module_id		= $request->variable('i', '');
-$mode			= $request->variable('mode', '');
-
-// Set custom style for admin area
-$template->set_custom_style(array(
-	array(
-		'name' 		=> 'adm',
-		'ext_path' 	=> 'adm/style/',
-	),
-), $phpbb_admin_path . 'style');
-
-$template->assign_var('T_ASSETS_PATH', $phpbb_root_path . 'assets');
-$template->assign_var('T_TEMPLATE_PATH', $phpbb_admin_path . 'style');
-
-// Instantiate new module
-$module = new p_master();
-
-// Instantiate module system and generate list of available modules
-$module->list_modules('acp');
-
-// Select the active module
-$module->set_active($module_id, $mode);
-
-// Assign data to the template engine for the list of modules
-// We do this before loading the active module for correct menu display in trigger_error
-$module->assign_tpl_vars(append_sid("{$phpbb_admin_path}index.$phpEx"));
-
-// Load and execute the relevant module
-$module->load_active();
-
-// Generate the page
-adm_page_header($module->get_page_title());
-
-$template->set_filenames(array(
-	'body' => $module->get_tpl_name(),
-));
-
-adm_page_footer();
