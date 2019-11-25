dac910d27ab7c8a95841f3bb28a3dfa6391fda84
Marc Alexander <admin@m-a-styles.de>
2019-11-25 22:19:03 +0100

    [ticket/16223] Do not use install_php_extension for memcached
    PHPBB3-16223

--- travis/setup-php-extensions.sh
+++ travis/setup-php-extensions.sh
@@ -58,7 +58,7 @@ fi
 phpenv config-rm xdebug.ini || true
 
 # memcached
-install_php_extension 'memcached' "$php_ini_file"
+echo "extension = memcached.so" >> "$php_ini_file"
 
 # redis
 # Disabled redis for now as it causes travis to fail
