c7d47e34e82128399e1b20b9f80a85f46c4cfd75
Marc Alexander <admin@m-a-styles.de>
2019-11-25 21:27:43 +0100

    [ticket/16223] Ensure memcached tests are only run when available
    PHPBB3-16223

--- tests/cache/memcached_test.php
+++ tests/cache/memcached_test.php
@@ -29,6 +29,25 @@ class phpbb_cache_memcached_driver_test extends \phpbb_cache_common_test_case
 			self::markTestSkipped('memcached extension is not loaded');
 		}
 
+		$config = phpbb_test_case_helpers::get_test_config();
+		if (isset($config['memcached_host']) || isset($config['memcached_port']))
+		{
+			$host = isset($config['memcached_host']) ? $config['memcached_host'] : 'localhost';
+			$port = isset($config['memcached_port']) ? $config['memcached_port'] : 11211;
+			self::$config = array('host' => $host, 'port' => $port);
+		}
+		else
+		{
+			self::markTestSkipped('Test memcached host/port is not specified');
+		}
+
+		$memcached = new \Memcached();
+		$memcached->addServer(self::$config['host'], self::$config['port']);
+		if (empty($memcached->getStats()))
+		{
+			self::markTestSkipped('Test memcached server is not available');
+		}
+
 		parent::setUpBeforeClass();
 	}
 
@@ -40,7 +59,7 @@ class phpbb_cache_memcached_driver_test extends \phpbb_cache_common_test_case
 
 		$phpbb_container = new phpbb_mock_container_builder();
 		$phpbb_container->setParameter('core.cache_dir', $phpbb_root_path . 'cache/' . PHPBB_ENVIRONMENT . '/');
-		$this->driver = new \phpbb\cache\driver\memcached();
+		$this->driver = new \phpbb\cache\driver\memcached(self::$config['host'], self::$config['port']);
 		$this->driver->purge();
 	}
 }
