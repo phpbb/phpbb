8c42a9d9000ff06d4ea10ab04e47ab2a998e260a
3D-I <480857+3D-I@users.noreply.github.com>
2019-12-05 11:56:41 +0100

    [ticket/16247] Code consistency
    PHPBB3-16247

--- phpBB/phpbb/textformatter/s9e/factory.php
+++ phpBB/phpbb/textformatter/s9e/factory.php
@@ -89,9 +89,9 @@ class factory implements \phpbb\textformatter\cache_interface
 				author={TEXT1;optional}
 				post_id={UINT;optional}
 				post_url={URL;optional;postFilter=#false}
-				profile_url={URL;optional;postFilter=#false}
 				msg_id={UINT;optional}
 				msg_url={URL;optional;postFilter=#false}
+				profile_url={URL;optional;postFilter=#false}
 				time={UINT;optional}
 				url={URL;optional}
 				user_id={UINT;optional}
