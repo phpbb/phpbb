73741e547b6a45c225cdc77cf56580c2e2730b70
3D-I <480857+3D-I@users.noreply.github.com>
2020-05-16 14:38:12 +0200

    [ticket/16487] Fix URL for fontawesome 3.2.0 migration
    PHPBB3-16487

--- phpBB/phpbb/db/migration/data/v320/font_awesome_update.php
+++ phpBB/phpbb/db/migration/data/v320/font_awesome_update.php
@@ -15,22 +15,22 @@ namespace phpbb\db\migration\data\v320;
 
 class font_awesome_update extends \phpbb\db\migration\migration
 {
-	static public function depends_on()
-	{
-		return array(
-			'\phpbb\db\migration\data\v320\dev',
-		);
-	}
-
 	public function effectively_installed()
 	{
 		return isset($this->config['load_font_awesome_url']);
 	}
 
+	static public function depends_on()
+	{
+		return [
+			'\phpbb\db\migration\data\v320\dev',
+		];
+	}
+
 	public function update_data()
 	{
-		return array(
-			array('config.add', array('load_font_awesome_url', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css')),
-		);
+		return [
+			['config.add', ['load_font_awesome_url', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css']],
+		];
 	}
 }
