800bd98ae65779adfa6339b6bf1ac516e474648c
Marc Alexander <admin@m-a-styles.de>
2020-08-02 18:04:28 +0200

    [prep-release-3.3.1] Update changelog for 3.3.1

--- build/build_changelog.php
+++ build/build_changelog.php
@@ -1,16 +1,16 @@
 #!/usr/bin/env php
 <?php
 /**
-*
-* This file is part of the phpBB Forum Software package.
-*
-* @copyright (c) phpBB Limited <https://www.phpbb.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-* For full copyright and license information, please see
-* the docs/CREDITS.txt file.
-*
-*/
+ *
+ * This file is part of the phpBB Forum Software package.
+ *
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the docs/CREDITS.txt file.
+ *
+ */
 
 if ($_SERVER['argc'] != 2)
 {
