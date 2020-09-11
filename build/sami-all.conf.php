0d2e9455aa0d8c7c9d7bd6ee3d72a6cca0697ea5
Marc Alexander <admin@m-a-styles.de>
2020-09-11 19:31:43 +0200

    [ticket/16597] Stop building docs with sami for 3.3 and above
    PHPBB3-16597

--- build/sami-all.conf.php
+++ build/sami-all.conf.php
@@ -26,8 +26,6 @@ $config['versions'] = Sami\Version\GitVersionCollection::create(__DIR__ . '/../'
 	->add('3.0.x')
 	->add('3.1.x')
 	->add('3.2.x')
-	->add('3.3.x')
-	->add('master')
 ;
 
 return new Sami\Sami($iterator, $config);
