395716112475b312edc0bae14e5ea51ccce1c72a
Marc Alexander <admin@m-a-styles.de>
2020-10-23 20:38:51 +0200

    [ticket/security-264] Ensure HTML entity state after removing formatting
    SECURITY-264

--- tests/text_processing/strip_bbcode_test.php
+++ tests/text_processing/strip_bbcode_test.php
@@ -13,27 +13,26 @@
 
 class phpbb_text_processing_strip_bbcode_test extends phpbb_test_case
 {
-	public function test_legacy()
+
+	public function data_strip_bbcode()
 	{
-		$original = '[b:20m4ill1]bold[/b:20m4ill1]';
-		$expected = ' bold ';
-
-		$actual = $original;
-		strip_bbcode($actual);
-
-		$this->assertSame($expected, $actual, '20m4ill1');
+		return [
+			['[b:20m4ill1]bold[/b:20m4ill1]', ' bold '],
+			['<r><B><s>[b]</s>bold<e>[/b]</e></B></r>', ' bold '],
+			['[b:20m4ill1]bo &amp; ld[/b:20m4ill1]', ' bo &amp; ld '],
+			['<r><B><s>[b]</s>bo &amp; ld<e>[/b]</e></B></r>', ' bo &amp; ld ']
+		];
 	}
 
-	public function test_s9e()
+	/**
+	 * @dataProvider data_strip_bbcode
+	 */
+	public function test_strip_bbcode($input, $expected)
 	{
 		$phpbb_container = $this->get_test_case_helpers()->set_s9e_services();
 
-		$original = '<r><B><s>[b]</s>bold<e>[/b]</e></B></r>';
-		$expected = ' bold ';
+		strip_bbcode($input);
 
-		$actual = $original;
-		strip_bbcode($actual);
-
-		$this->assertSame($expected, $actual);
+		$this->assertSame($expected, $input);
 	}
 }
