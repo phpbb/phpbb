ef22a029b25e0bf986c9fccc2c83cef665a1dfbf
Marc Alexander <admin@m-a-styles.de>
2020-12-16 21:46:57 +0100

    [ticket/16659] Split up tests for each database type
    PHPBB3-16659

---
+++ .github/setup-exiftool.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# This file is part of the phpBB Forum Software package.
+#
+# @copyright (c) phpBB Limited <https://www.phpbb.com>
+# @license GNU General Public License, version 2 (GPL-2.0)
+#
+# For full copyright and license information, please see
+# the docs/CREDITS.txt file.
+#
+set -e
+
+sudo apt-get update
+sudo apt-get install -y parallel libimage-exiftool-perl
---
+++ .github/setup-phpbb.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# This file is part of the phpBB Forum Software package.
+#
+# @copyright (c) phpBB Limited <https://www.phpbb.com>
+# @license GNU General Public License, version 2 (GPL-2.0)
+#
+# For full copyright and license information, please see
+# the docs/CREDITS.txt file.
+#
+set -e
+set -x
+
+DB=$1
+PHP_VERSION=$2
+NOTESTS=$3
+
+if [ "$NOTESTS" == '1' ]
+then
+	.github/setup-exiftool.sh
+	.github/setup-unbuffer.sh
+fi
+
+if [ "$NOTESTS" != '1' ]
+then
+	.github/setup-webserver.sh
+fi
+
+cd phpBB
+php ../composer.phar install --dev --no-interaction
+if [[ "$PHP_VERSION" =~ ^nightly$ || "$PHP_VERSION" =~ ^8 ]]
+then
+	php ../composer.phar remove phpunit/dbunit --dev --update-with-dependencies \
+	&& php ../composer.phar require symfony/yaml:~4.4 misantron/dbunit:~5.0 phpunit/phpunit:^9.3 --dev --update-with-all-dependencies --ignore-platform-reqs
+fi
+cd ..
