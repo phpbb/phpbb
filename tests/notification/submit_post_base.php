a271d0cacbe33eb4fd65f0802f8fae398fa7c299
Marc Alexander <admin@m-a-styles.de>
2021-01-28 20:58:35 +0100

    [ticket/13713] Fix incorrect parameter to mention_helper constructor
    PHPBB3-13713

--- tests/notification/submit_post_base.php
+++ tests/notification/submit_post_base.php
@@ -140,7 +140,7 @@ abstract class phpbb_notification_submit_post_base extends phpbb_database_test_c
 			new \phpbb\textformatter\s9e\mention_helper(
 				$this->db,
 				$auth,
-				$this->user,
+				$user,
 				$phpbb_root_path,
 				$phpEx
 			)
