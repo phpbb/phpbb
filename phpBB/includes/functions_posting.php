bcea34ee7c48f711cde30cb69ea8884aaa49f17d
toxyy <arr70017@uga.edu>
2021-05-07 19:25:35 -0400

    [ticket/15925] Update events for 3.3.5-RC1
    PHPBB3-15925

--- phpBB/includes/functions_posting.php
+++ phpBB/includes/functions_posting.php
@@ -356,7 +356,7 @@ function update_post_information($type, $ids, $return_update_sql = false)
 		* @event core.update_post_info_modify_posts_sql
 		* @var	string	type				The table being updated (forum or topic)
 		* @var	array	sql_ary				SQL array to get some of the last posts' data
-		* @since 3.3.4-RC1
+		* @since 3.3.5-RC1
 		*/
 		$vars = [
 			'type',
@@ -385,7 +385,7 @@ function update_post_information($type, $ids, $return_update_sql = false)
 		* @var	string	type				The table being updated (forum or topic)
 		* @var	array	rowset				Array with the posts data
 		* @var	array	update_sql			Array with SQL data to update the forums or topics table with
-		* @since 3.3.4-RC1
+		* @since 3.3.5-RC1
 		*/
 		$vars = [
 			'type',
