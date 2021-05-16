57b0547fd6901cea3b2dfb3976faeacc847e367c
Michael Miday <hanakin@users.noreply.github.com>
2021-05-16 08:24:11 -1000

    [ticket/16775] use gulp 4.0 syntax
    PHPBB3-16775

--- gulpfile.js
+++ gulpfile.js
@@ -3,42 +3,35 @@
 const del = require('del');
 const gulp = require('gulp');
 const autoprefixer = require('autoprefixer');
-// const sass = require('gulp-sass');
 const rename = require('gulp-rename');
-const sourcemaps = require('gulp-sourcemaps');
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
 const sorting = require('postcss-sorting');
 const atimport = require('postcss-import');
-// const torem = require('postcss-pxtorem');
 const sortOrder = require('./.postcss-sorting.json');
 // const pkg = require('./package.json');
 
 // Config
-const build = {
-	css: './phpBB/styles/prosilver/theme/',
+const paths = {
+	styles: {
+		src: './phpBB/styles/prosilver/theme/*.css',
+		css: './phpBB/styles/prosilver/theme/',
+	}
 };
 
-gulp.task('css', gulp.series(() => {
-	return gulp
-		.src(build.css + '*.css')
+function css () {
+	return gulp.src(paths.styles.src)
 		.pipe(
 			postcss([
 				autoprefixer(),
 				sorting(sortOrder),
 			]),
 		)
-		.pipe(gulp.dest(build.css));
-}));
+		.pipe(gulp.dest(paths.styles.css));
+}
 
-gulp.task('clean', gulp.series(() => {
-	del([ 'dist' ]);
-}));
-
-gulp.task('minify', gulp.series(() => {
-	return gulp
-		.src(build.css + '/bidi.css')
-		.pipe(sourcemaps.init())
+function minify () {
+	return gulp.src(paths.styles.src, { sourcemaps: true })
 		.pipe(
 			postcss([
 				atimport(),
@@ -49,12 +42,15 @@ gulp.task('minify', gulp.series(() => {
 			suffix: '.min',
 			extname: '.css',
 		}))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(build.css));
-}));
+		.pipe(gulp.dest(paths.styles.css));
+}
 
-gulp.task('watch', gulp.series(() => {
-	gulp.watch('phpBB/styles/prosilver/theme/*.css', gulp.series('css'));
-}));
+function watch() {
+	gulp.watch(paths.styles.src, css);
+}
 
-exports.default = gulp.series('css', 'watch');
+exports.css = css;
+exports.minify = minify;
+exports.watch = watch;
+
+exports.default = gulp.series('css', 'minify', 'watch');
