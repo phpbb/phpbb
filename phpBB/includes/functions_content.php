21759572c26a3431014355915762aa6b911190e4
Marc Alexander <admin@m-a-styles.de>
2021-07-01 21:06:09 +0200

    [ticket/14771] Remove not needed code from parse_attachments()
    PHPBB3-14771

--- phpBB/includes/functions_content.php
+++ phpBB/includes/functions_content.php
@@ -1318,14 +1318,10 @@ function parse_attachments($forum_id, &$message, &$attachments, &$update_count_a
 
 				// Audio files
 				case ATTACHMENT_CATEGORY_AUDIO:
-					$inline_link = append_sid("{$phpbb_root_path}download/file.$phpEx", 'id=' . $attachment['attach_id']);
 					$block_array += [
 						'S_AUDIO_FILE'			=> true,
-						'U_FORUM'			=> generate_board_url(),
-						'ATTACH_ID'			=> $attachment['attach_id'],
 					];
 
-					// Heard File ... update the download count
 					$update_count_ary[] = $attachment['attach_id'];
 				break;
 
