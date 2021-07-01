9099bdb2ad4eedc0798d88da373e843e17972d05
Marc Alexander <admin@m-a-styles.de>
2021-07-01 21:09:31 +0200

    [ticket/14771] Make phing sniff happy
    PHPBB3-14771

--- phpBB/phpbb/db/migration/data/v400/add_audio_files_attachment_group.php
+++ phpBB/phpbb/db/migration/data/v400/add_audio_files_attachment_group.php
@@ -15,7 +15,7 @@ namespace phpbb\db\migration\data\v400;
 
 class add_audio_files_attachment_group extends \phpbb\db\migration\migration
 {
-	static public function depends_on()
+	public static function depends_on()
 	{
 		return ['\phpbb\db\migration\data\v400\dev'];
 	}
