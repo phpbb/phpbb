1db2d9c3c14ba491fd83a05f9c8a6825e61f7537
Marc Alexander <admin@m-a-styles.de>
2021-09-08 22:37:11 +0200

    [prep-release-3.3.5] Add migration for 3.3.5-RC1

---
+++ phpBB/phpbb/db/migration/data/v33x/v335rc1.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ * This file is part of the phpBB Forum Software package.
+ *
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the docs/CREDITS.txt file.
+ *
+ */
+
+namespace phpbb\db\migration\data\v33x;
+
+class v335rc1 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return version_compare($this->config['version'], '3.3.5-RC1', '>=');
+	}
+
+	public static function depends_on()
+	{
+		return [
+			'\phpbb\db\migration\data\v33x\extend_bbcode_tooltip',
+			'\phpbb\db\migration\data\v33x\bot_update_v2',
+			'\phpbb\db\migration\data\v33x\jquery_update_v2',
+		];
+	}
+
+	public function update_data()
+	{
+		return [
+			['config.update', ['version', '3.3.5-RC1']],
+		];
+	}
+}
