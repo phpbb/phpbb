0d3f9911197c10eb0f6a06216de8d3c89f2750bf
Stanislav Atanasov <lucifer@anavaro.com>
2021-09-09 02:56:56 +0300

    [ticket/12426] Merge conflict resolution
    PHPBB3-12426

--- phpBB/phpbb/profilefields/type/type_dropdown.php
+++ phpBB/phpbb/profilefields/type/type_dropdown.php
@@ -68,7 +68,6 @@ class type_dropdown extends type_base
 	*/
 	public function get_options($default_lang_id, $field_data)
 	{
-		$profile_row = [];
 		$profile_row[0] = [
 			'var_name'				=> 'field_default_value',
 			'field_id'				=> 1,
