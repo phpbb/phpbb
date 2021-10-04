1fd19787cedf38b097195bc5b046c7d973b9bf37
3D-I <eataly3d@gmail.com>
2021-10-04 14:33:42 +0200

    [ticket/16882] Add file_exists() function to Twig
    PHPBB3-16882

--- phpBB/phpbb/template/twig/extension.php
+++ phpBB/phpbb/template/twig/extension.php
@@ -93,7 +93,7 @@ class extension extends \Twig\Extension\AbstractExtension
 			new \Twig\TwigFunction('lang', array($this, 'lang')),
 			new \Twig\TwigFunction('lang_defined', array($this, 'lang_defined')),
 			new \Twig\TwigFunction('lang_js', [$this, 'lang_js']),
-			new \Twig\TwigFunction('file_exists', [$this, 'on_disk']),
+			new \Twig\TwigFunction('file_exists', 'file_exists'),
 			new \Twig\TwigFunction('get_class', 'get_class'),
 		);
 	}
@@ -201,20 +201,6 @@ class extension extends \Twig\Extension\AbstractExtension
 	public function lang_defined($key)
 	{
 		return call_user_func_array([$this->language, 'is_set'], [$key]);
-	}
-
-	/**
-	 * Check if a file exists
-	 *
-	 * Example:
-	 * <samp>{% if file_exists('./ext/vendor/name/styles/all/theme/images/blah.png') %}</samp>
-	 *
-	 * @param string	$path	The relative path to be checked
-	 * @return bool				True if exists, false otherwise
-	 */
-	public function on_disk($path): bool
-	{
-		return call_user_func_array('file_exists', [$path]);
 	}
 
 	/**
