7d586b04202c281fe252228963513d232d247eff
Marc Alexander <admin@m-a-styles.de>
2022-10-16 16:22:36 +0200

    [ticket/17049] Remove unused extensions manager parameter
    PHPBB3-17049

--- phpBB/includes/acp/acp_extensions.php
+++ phpBB/includes/acp/acp_extensions.php
@@ -189,7 +189,7 @@ class acp_extensions
 
 				$this->list_enabled_exts($phpbb_extension_manager, $managed_packages);
 				$this->list_disabled_exts($phpbb_extension_manager, $managed_packages);
-				$this->list_available_exts($phpbb_extension_manager, $managed_packages);
+				$this->list_available_exts($managed_packages);
 
 				$this->tpl_name = 'acp_ext_list';
 
@@ -928,12 +928,11 @@ class acp_extensions
 	/**
 	 * Lists all the available extensions and dumps to the template
 	 *
-	 * @param \phpbb\extension\manager  $phpbb_extension_manager     An instance of the extension manager
 	 * @param array                     $managed_packages            List of managed packages
 	 *
 	 * @return null
 	 */
-	public function list_available_exts(\phpbb\extension\manager $phpbb_extension_manager, array $managed_packages)
+	public function list_available_exts(array $managed_packages)
 	{
 		$uninstalled = array_diff_key($this->ext_manager->all_available(), $this->ext_manager->all_configured());
 
