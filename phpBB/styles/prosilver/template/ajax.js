637844f264f4a98e175e8bcfa1fce0cc20da2c8c
Marc Alexander <admin@m-a-styles.de>
2022-10-27 00:01:36 +0200

    [ticket/13291] Close notifications dropdown during mark all read
    PHPBB3-13291

--- phpBB/styles/prosilver/template/ajax.js
+++ phpBB/styles/prosilver/template/ajax.js
@@ -101,6 +101,7 @@ phpbb.addAjaxCallback('mark_topics_read', function(res, updateTopicLinks) {
 phpbb.addAjaxCallback('notification.mark_all_read', function(res) {
 	if (typeof res.success !== 'undefined') {
 		phpbb.markNotifications($('#notification_list li.bg2'), 0);
+		phpbb.toggleDropdown.call($('#notification_list_button'));
 		phpbb.closeDarkenWrapper(3000);
 	}
 });
