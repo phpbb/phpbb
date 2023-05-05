f6351f30670bc11d4e50aabe901aac8a20bdfdf0
rxu <rxu@mail.ru>
2023-05-05 19:59:45 +0700

    [ticket/17135] Introduce Symfony Mailer for emails backend
    PHPBB3-17135

--- tests/email/email_parsing_test.php
+++ tests/email/email_parsing_test.php
@@ -67,6 +67,7 @@ class phpbb_email_parsing_test extends phpbb_test_case
 		$phpbb_container->set('ext.manager', $extension_manager);
 
 		$context = new \phpbb\template\context();
+		$dispatcher = new \phpbb\event\dispatcher();
 		$twig = new \phpbb\template\twig\environment(
 			$config,
 			$filesystem,
@@ -74,7 +75,7 @@ class phpbb_email_parsing_test extends phpbb_test_case
 			$cache_path,
 			null,
 			new \phpbb\template\twig\loader(''),
-			new \phpbb\event\dispatcher(),
+			$dispatcher,
 			array(
 				'cache'			=> false,
 				'debug'			=> false,
@@ -91,6 +92,16 @@ class phpbb_email_parsing_test extends phpbb_test_case
 
 		$twig->addExtension($twig_extension);
 		$phpbb_container->set('template.twig.lexer', new \phpbb\template\twig\lexer($twig));
+		$phpbb_container->set('dispatcher', $dispatcher);
+		$phpbb_container->set('language', $lang);
+		$phpbb_container->set('request', $request);
+
+		$db = $this->getMockBuilder('\phpbb\db\driver\mysqli')
+			->disableOriginalConstructor()
+			->getMock();
+		$auth = $this->createMock('\phpbb\auth\auth');
+		$log = new \phpbb\log\log($db, $user, $auth, $dispatcher, $phpbb_root_path, 'adm/', $phpEx, LOG_TABLE);
+		$phpbb_container->set('log', $log);
 
 		if (!class_exists('messenger'))
 		{
