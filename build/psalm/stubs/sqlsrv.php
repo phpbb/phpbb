fa522d24606c8a9ceda6e41f641f117fb5b3e572
Marc Alexander <admin@m-a-styles.de>
2023-08-20 09:49:54 +0200

    [ticket/17176] Add more test stubs for psalm
    PHPBB3-17176

---
+++ build/psalm/stubs/sqlsrv.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * This file is part of the phpBB Forum Software package.
+ *
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the docs/CREDITS.txt file.
+ *
+ */
+
+/** @link https://www.php.net/manual/en/sqlsrv.constants.php */
+define('SQLSRV_ERR_ERRORS', 0);
+define('SQLSRV_FETCH_ASSOC', 2);
+define('SQLSRV_CURSOR_STATIC', 'static');
