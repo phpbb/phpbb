384405d8e049633d798e2bced20958b2cf949dfb
Marc Alexander <admin@m-a-styles.de>
2023-09-21 15:48:02 +0200

    [ticket/17176] Fix our misuse of trigger_error() to work again
    PHPBB3-17176

--- phpBB/phpbb/debug/error_handler.php
+++ phpBB/phpbb/debug/error_handler.php
@@ -31,7 +31,7 @@ class error_handler extends ErrorHandler
 	 */
 	public function handleError(int $type, string $message, string $file, int $line): bool
 	{
-		if (!$this->debug)
+		if (!$this->debug || $type === E_USER_WARNING || $type === E_USER_NOTICE)
 		{
 			$handler = defined('PHPBB_MSG_HANDLER') ? PHPBB_MSG_HANDLER : 'msg_handler';
 
