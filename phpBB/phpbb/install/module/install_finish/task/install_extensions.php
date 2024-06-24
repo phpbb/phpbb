b3ddd87b7b65d5026d1bcb5e79f1753d442c94c6
Daniel James <danielt70@icloud.com>
2024-06-24 14:54:39 +0100

    [ticket/17350] Add IP Address of user during install process
    Adds the IP address of the user completing the installation process
    while extensions are being installed or updated. Prior to this change
    the IP address field was being left blank however other logs created
    during the install process for other actions does record the users IP.
    
    PHPBB-17350

--- phpBB/phpbb/install/module/install_finish/task/install_extensions.php
+++ phpBB/phpbb/install/module/install_finish/task/install_extensions.php
@@ -144,7 +144,7 @@ class install_extensions extends \phpbb\install\task_base
 				if (isset($extensions[$ext_name]) && $extensions[$ext_name]['ext_active'])
 				{
 					// Create log
-					$this->log->add('admin', ANONYMOUS, '', 'LOG_EXT_ENABLE', time(), array($ext_name));
+					$this->log->add('admin', ANONYMOUS, $this->user->ip, 'LOG_EXT_ENABLE', time(), array($ext_name));
 					$this->iohandler->add_success_message(array('CLI_EXTENSION_ENABLE_SUCCESS', $ext_name));
 				}
 				else
