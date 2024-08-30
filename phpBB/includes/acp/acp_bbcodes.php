fe3750bfb2a45884a71e91ce3d40f3ff0b13dea3
Marc Alexander <admin@m-a-styles.de>
2024-08-30 21:39:53 +0200

    [ticket/17326] Move HTML to acp_bbcodes.html
    PHPBB-17326

--- phpBB/includes/acp/acp_bbcodes.php
+++ phpBB/includes/acp/acp_bbcodes.php
@@ -104,8 +104,6 @@ class acp_bbcodes
 					'U_BACK'			=> $this->u_action,
 					'U_ACTION'			=> $this->u_action . '&amp;action=' . (($action == 'add') ? 'create' : 'modify') . (($bbcode_id) ? "&amp;bbcode=$bbcode_id" : ''),
 
-					'L_BBCODE_USAGE_EXPLAIN'		=> sprintf($user->lang['BBCODE_USAGE_EXPLAIN'], '<a href="#down">', '</a>'),
-					'L_BBCODE_FONT_ICON_EXPLAIN'	=> sprintf($user->lang['BBCODE_USAGE_EXPLAIN'], '<a href="https://fontawesome.com/v6/icons/" target="_blank">', '</a>'),
 					'BBCODE_MATCH'					=> $bbcode_match,
 					'BBCODE_TPL'					=> $bbcode_tpl,
 					'BBCODE_HELPLINE'				=> $bbcode_helpline,
