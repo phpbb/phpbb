8619ff35268365852bd71b4e80f87cbb917c67be
rxu <rxu@mail.ru>
2024-11-30 11:46:16 +0700

    [ticket/17445] Add missing class property definitions
    PHPBB-17445

--- phpBB/phpbb/notification/method/webpush.php
+++ phpBB/phpbb/notification/method/webpush.php
@@ -39,8 +39,17 @@ class webpush extends base implements extended_method_interface
 	/** @var log_interface */
 	protected $log;
 
+	/** @var user_loader */
+	protected $user_loader;
+
 	/** @var user */
 	protected $user;
+
+	/** @var string */
+	protected $phpbb_root_path;
+
+	/** @var string */
+	protected $php_ext;
 
 	/** @var string Notification Web Push table */
 	protected $notification_webpush_table;
