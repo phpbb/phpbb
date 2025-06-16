78dcb0c86207b182d8abd517880e5057ca3a3f85
rxu <rxu@mail.ru>
2025-06-17 01:01:19 +0700

    [ticket/17525] Avoid more index name duplication
    PHPBB-17525

--- phpBB/phpbb/db/migration/data/v400/rename_auth_role_id_index.php
+++ phpBB/phpbb/db/migration/data/v400/rename_auth_role_id_index.php
@@ -40,8 +40,15 @@ class rename_auth_role_id_index extends migration
 					'poster_id' => 'attchmnts_poster_id',
 					'topic_id' => 'attchmnts_topic_id',
 				],
+				$this->table_prefix . 'bbcodes' => [
+					'display_on_post' => 'bbcds_display_on_post',
+				],
+				$this->table_prefix . 'forums' => [
+					'left_right_id' => 'frms_left_right_id',
+				],
 				$this->table_prefix . 'forums_watch' => [
 					'forum_id' => 'frmswtch_forum_id',
+					'notify_stat' => 'frmswtch_notify_stat',
 					'user_id' => 'frmswtch_user_id',
 				],
 				$this->table_prefix . 'log' => [
@@ -55,10 +62,15 @@ class rename_auth_role_id_index extends migration
 				$this->table_prefix . 'moderator_cache' => [
 					'forum_id' => 'mdrtrcch_forum_id',
 				],
+				$this->table_prefix . 'modules' => [
+					'left_right_id' => 'mdls_left_right_id',
+				],
 				$this->table_prefix . 'oauth_states' => [
+					'provider' => 'oauthsts_provider',
 					'user_id' => 'oauthsts_user_id',
 				],
 				$this->table_prefix . 'oauth_tokens' => [
+					'provider' => 'oauthtkns_provider',
 					'user_id' => 'oauthtkns_user_id',
 				],
 				$this->table_prefix . 'poll_options' => [
@@ -72,11 +84,26 @@ class rename_auth_role_id_index extends migration
 					'poster_id' => 'psts_poster_id',
 					'topic_id' => 'psts_topic_id',
 				],
+				$this->table_prefix . 'privmsgs' => [
+					'author_id' => 'pms_author_id',
+				],
 				$this->table_prefix . 'privmsgs_folder' => [
-					'user_id' => 'pmfldr_user_id',
+					'user_id' => 'pmsfldr_user_id',
 				],
 				$this->table_prefix . 'privmsgs_rules' => [
-					'user_id' => 'pmrls_user_id',
+					'user_id' => 'pmsrls_user_id',
+				],
+				$this->table_prefix . 'privmsgs_to' => [
+					'author_id' => 'pmsto_author_id',
+				],
+				$this->table_prefix . 'reports' => [
+					'post_id' => 'rprts_post_id',
+				],
+				$this->table_prefix . 'search_wordmatch' => [
+					'post_id' => 'wrdmtch_post_id',
+				],
+				$this->table_prefix . 'smilies' => [
+					'display_on_post' => 'smls_display_on_post',
 				],
 				$this->table_prefix . 'topics' => [
 					'forum_id' => 'tpcs_forum_id',
@@ -87,6 +114,7 @@ class rename_auth_role_id_index extends migration
 				],
 				$this->table_prefix . 'topics_watch' => [
 					'topic_id' => 'tpcswtch_topic_id',
+					'notify_stat' => 'tpcswtch_notify_stat',
 					'user_id' => 'tpcswtch_user_id',
 				],
 				$this->table_prefix . 'user_group' => [
