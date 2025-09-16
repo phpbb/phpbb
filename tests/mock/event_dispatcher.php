dac58f53d735e6bb41402c390bd2ac1192446f68
Matt Friedman <maf675@gmail.com>
2025-09-16 13:14:39 -0700

    [ticket/17542] Optimize and simplify logic
    PHPBB-17542

--- tests/mock/event_dispatcher.php
+++ tests/mock/event_dispatcher.php
@@ -24,18 +24,13 @@ class phpbb_mock_event_dispatcher extends \phpbb\event\dispatcher
 
 	public function trigger_event($eventName, $data = array()): array
 	{
-		// Ensure tests never hard-exit when phpBB calls exit_handler()
+		$data = (array) $data;
+		
 		if ($eventName === 'core.exit_handler')
 		{
-			// Set the override flag so exit_handler() returns instead of exit;
-			if (is_array($data))
-			{
-				$data['exit_handler_override'] = true;
-			}
-			return (array) $data;
+			$data['exit_handler_override'] = true;
 		}
 
-		// Default behaviour of the mock: return the input data unchanged
-		return (array) $data;
+		return $data;
 	}
 }
