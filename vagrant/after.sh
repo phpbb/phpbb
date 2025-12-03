39c55c66b7968627341776b7aeea3706cf01a7d1
Marc Alexander <admin@m-a-styles.de>
2025-12-03 20:22:49 +0100

    [ticket/17556] Increase minimum PHP version to 8.2
    PHPBB-17556

--- vagrant/after.sh
+++ vagrant/after.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PHP_VERSION="8.1"
+PHP_VERSION="8.2"
 PHPBB_PATH="/home/vagrant/phpbb"
 PHPBB_CONFIG="${PHPBB_PATH}/phpBB/config.php"
 PHPBB_INSTALL="${PHPBB_PATH}/vagrant/phpbb-install-config.yml"
