c8a5130c19e2fef40a6c915bb988a355524ad148
Marc Alexander <admin@m-a-styles.de>
2025-12-25 14:32:13 +0100

    [ticket/17592] Move html to template file for no match template
    PHPBB-17592

--- phpBB/assets/javascript/mentions.js
+++ phpBB/assets/javascript/mentions.js
@@ -291,8 +291,7 @@
 				itemClass: 'mention-item',
 				menuItemTemplate,
 				noMatchTemplate() {
-					const returnTemplate = (typeof mention_no_match_found === 'undefined') ? 'No match found' : mention_no_match_found;
-					return '<li>' + returnTemplate + '</li>';
+					return (typeof mention_no_match_found === 'undefined') ? 'No match found' : mention_no_match_found;
 				},
 				selectTemplate(item) {
 					return '[mention ' + (item.type === 'g' ? 'group_id=' : 'user_id=') + item.id + ']' + item.name + '[/mention]';
