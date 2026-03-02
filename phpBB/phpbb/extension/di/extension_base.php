35f4f0eaf00d538de2390812710f62f1f2aca51d
Matt Friedman <maf675@gmail.com>
2026-03-02 09:54:15 -0800

    [ticket/17618] Replacing deprecated symfony classes or method calls
    PHPBB-17618
    
    Fix
    
    fix

--- phpBB/phpbb/extension/di/extension_base.php
+++ phpBB/phpbb/extension/di/extension_base.php
@@ -18,7 +18,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use phpbb\filesystem\helper as filesystem_helper;
 
 /**
