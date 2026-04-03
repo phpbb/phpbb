3be5a75c14a7cb33b6033471207adb71a91bd946
Marc Alexander <admin@m-a-styles.de>
2026-04-03 11:17:38 +0200

    [ticket/17616] Add develop script for previewiing maintenance page
    PHPBB-17616

---
+++ phpBB/develop/maintenance_preview.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ *
+ * This file is part of the phpBB Forum Software package.
+ *
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the docs/CREDITS.txt file.
+ *
+ */
+
+/**
+ * Creates a preview of the maintenance page.
+ *
+ * DO NOT USE FOR PRODUCTION
+ */
+die("Please read the first lines of this script for instructions on how to enable it");
+
+/**
+ * @ignore
+ */
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './../';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup('app');
+
+/** @var \phpbb\template\template $template */
+$template = $phpbb_container->get('template');
+$template->set_style();
+$template->set_filenames([
+	'body' => 'maintenance_page.html',
+]);
+
+/** @var \phpbb\update\maintenance_generator $maintenance_generator */
+$maintenance_generator = $phpbb_container->get('update.maintenance_generator');
+
+// Generate the lock file
+$maintenance_generator->write_maintenance_lock([
+	[
+		'ICON'	=> '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M415.9 344L225 344C227.9 408.5 242.2 467.9 262.5 511.4C273.9 535.9 286.2 553.2 297.6 563.8C308.8 574.3 316.5 576 320.5 576C324.5 576 332.2 574.3 343.4 563.8C354.8 553.2 367.1 535.8 378.5 511.4C398.8 467.9 413.1 408.5 416 344zM224.9 296L415.8 296C413 231.5 398.7 172.1 378.4 128.6C367 104.2 354.7 86.8 343.3 76.2C332.1 65.7 324.4 64 320.4 64C316.4 64 308.7 65.7 297.5 76.2C286.1 86.8 273.8 104.2 262.4 128.6C242.1 172.1 227.8 231.5 224.9 296zM176.9 296C180.4 210.4 202.5 130.9 234.8 78.7C142.7 111.3 74.9 195.2 65.5 296L176.9 296zM65.5 344C74.9 444.8 142.7 528.7 234.8 561.3C202.5 509.1 180.4 429.6 176.9 344L65.5 344zM463.9 344C460.4 429.6 438.3 509.1 406 561.3C498.1 528.6 565.9 444.8 575.3 344L463.9 344zM575.3 296C565.9 195.2 498.1 111.3 406 78.7C438.3 130.9 460.4 210.4 463.9 296L575.3 296z"/></svg>',
+		'URL'  => 'https://www.phpbb.com',
+		'NAME' => 'phpBB.com',
+	],
+	[
+		'ICON'	=> '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 440 146.7 540.8 258.2 568.5L258.2 398.2L205.4 398.2L205.4 320L258.2 320L258.2 286.3C258.2 199.2 297.6 158.8 383.2 158.8C399.4 158.8 427.4 162 438.9 165.2L438.9 236C432.9 235.4 422.4 235 409.3 235C367.3 235 351.1 250.9 351.1 292.2L351.1 320L434.7 320L420.3 398.2L351 398.2L351 574.1C477.8 558.8 576 450.9 576 320z"/></svg>',
+		'URL'  => 'https://www.facebook.com/phpBB/',
+		'NAME' => 'phpBB',
+	],
+	[
+		'ICON'	=> '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M453.2 112L523.8 112L369.6 288.2L551 528L409 528L297.7 382.6L170.5 528L99.8 528L264.7 339.5L90.8 112L236.4 112L336.9 244.9L453.2 112zM428.4 485.8L467.5 485.8L215.1 152L173.1 152L428.4 485.8z"/></svg>',
+		'URL'  => 'https://x.com/phpbb',
+		'NAME' => 'phpbb',
+	],
+]);
+
+$lock_file = $phpbb_root_path . 'store/UPDATE_LOCK.' . $phpEx;
+
+if (file_exists($lock_file))
+{
+	$data = include($lock_file);
+
+	echo $data['content'];
+
+	// Delete the file after previewing
+	@unlink($lock_file);
+}
+else
+{
+	echo "Failed to generate maintenance lock file.";
+}
