b82764b55fbfccf882c5a2b50b1bc8b852401890
Marc <>
2026-05-09 16:37:58 +0200

    [ticket/PHPBB-17652] Use $phpbb_admin_path in acp_extensions

--- phpBB/includes/acp/acp_extensions.php
+++ phpBB/includes/acp/acp_extensions.php
@@ -42,7 +42,7 @@ class acp_extensions
 	function main($id, $mode)
 	{
 		// Start the page
-		global $config, $user, $template, $request, $phpbb_extension_manager, $phpbb_root_path, $phpbb_log, $phpbb_dispatcher, $phpbb_container;
+		global $config, $user, $template, $request, $phpbb_extension_manager, $phpbb_root_path, $phpbb_log, $phpbb_dispatcher, $phpbb_container, $phpbb_admin_path;
 
 		$this->config = $config;
 		$this->template = $template;
@@ -224,7 +224,7 @@ class acp_extensions
 							'name' 		=> 'adm',
 							'ext_path' 	=> 'adm/style/',
 						),
-					), array($phpbb_root_path . 'adm/style'));
+					), array($phpbb_admin_path . 'style'));
 
 					$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'LOG_EXT_ENABLE', time(), array($ext_name));
 				}
