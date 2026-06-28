6be30a395b16021ae10e9baf1f45fadda1519e33
Ruben Calvo <rubencm@google.com>
2026-06-28 15:19:08 +0200

    [ticket/17672] Use DI Extension instead of HttpKernel Extension
    HttpKernel\DependencyInjection\Extension was marked @internal since
    Symfony 7.1. Replace with DependencyInjection\Extension\Extension.
    Add : void return type to load() overrides to match ExtensionInterface.
    
    PHPBB-17672

--- phpBB/phpbb/extension/di/extension_base.php
+++ phpBB/phpbb/extension/di/extension_base.php
@@ -17,8 +17,8 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use phpbb\filesystem\helper as filesystem_helper;
 
 /**
