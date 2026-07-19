22020bc538d971d563a39bf33efe03268fa47554
Marc Alexander <admin@m-a-styles.de>
2026-07-19 20:08:38 +0200

    [ticket/security-303] Update tests to not rely on weird setups
    SECURITY-303

--- tests/profilefields/type_url_test.php
+++ tests/profilefields/type_url_test.php
@@ -30,7 +30,9 @@ class phpbb_profilefield_type_url_test extends phpbb_test_case
 	{
 		global $config, $request, $user, $cache, $phpbb_root_path, $phpEx;
 
-		$config = new \phpbb\config\config([]);
+		$config = new \phpbb\config\config([
+			'server_name'	=> 'localhost',
+		]);
 		$cache = new phpbb_mock_cache;
 		$user = $this->createMock('\phpbb\user', array(), array(
 			new \phpbb\language\language(new \phpbb\language\language_file_loader($phpbb_root_path, $phpEx)),
@@ -194,13 +196,13 @@ class phpbb_profilefield_type_url_test extends phpbb_test_case
 			array(
 				'http://foobar.com',
 				array('field_show_novalue' => true),
-				'<!-- l --><a class="postlink-local" href="http://foobar.com">foobar.com</a><!-- l -->',
+				'<!-- m --><a class="postlink" href="http://foobar.com">http://foobar.com</a><!-- m -->',
 				'Field should output the given value',
 			),
 			array(
 				'http://foobar.com',
 				array('field_show_novalue' => false),
-				'<!-- l --><a class="postlink-local" href="http://foobar.com">foobar.com</a><!-- l -->',
+				'<!-- m --><a class="postlink" href="http://foobar.com">http://foobar.com</a><!-- m -->',
 				'Field should output the given value',
 			),
 			array(
