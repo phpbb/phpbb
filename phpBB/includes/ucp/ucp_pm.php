689d23efe4a7e9d10acd5a257f9faaee800e460b
Marc Alexander <admin@m-a-styles.de>
2026-08-19 21:13:47 +0200

    [ticket/security/308] Check link hash when releasing on hold PMs
    SECURITY-308

--- phpBB/includes/ucp/ucp_pm.php
+++ phpBB/includes/ucp/ucp_pm.php
@@ -240,7 +240,8 @@ class ucp_pm
 
 				// If new messages arrived, place them into the appropriate folder
 				$num_not_moved = $num_removed = 0;
-				$release = $request->variable('release', 0);
+				$release = $request->variable('release', 0) === 1 &&
+					check_link_hash($request->variable('hash', ''), 'release_pm_messages');
 
 				if ($user->data['user_new_privmsg'] && ($action == 'view_folder' || $action == 'view_message'))
 				{
@@ -364,7 +365,7 @@ class ucp_pm
 					'CUR_FOLDER_NAME'		=> $folder_status ? $folder_status['folder_name'] : false,
 					'NUM_NOT_MOVED'			=> $num_not_moved,
 					'NUM_REMOVED'			=> $num_removed,
-					'RELEASE_MESSAGE_INFO'	=> sprintf($user->lang['RELEASE_MESSAGES'], '<a href="' . $this->u_action . '&amp;folder=' . $folder_id . '&amp;release=1">', '</a>'),
+					'RELEASE_MESSAGE_INFO'	=> sprintf($user->lang['RELEASE_MESSAGES'], '<a href="' . $this->u_action . '&amp;folder=' . $folder_id . '&amp;release=1&amp;hash=' . generate_link_hash('release_pm_messages') .'">', '</a>'),
 					'NOT_MOVED_MESSAGES'	=> $user->lang('NOT_MOVED_MESSAGES', (int) $num_not_moved),
 					'RULE_REMOVED_MESSAGES'	=> $user->lang('RULE_REMOVED_MESSAGES', (int) $num_removed),
 
