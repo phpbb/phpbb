2252a252c28255e874ac44cd608b8a44c2937622
Christian Schnegelberger <mail@crizzo.de>
2026-09-18 10:00:38 +0100

    [ticket/17406] Display reason for soft deleted topic
    PHPBB-17406

--- phpBB/viewtopic.php
+++ phpBB/viewtopic.php
@@ -1553,6 +1553,14 @@ while ($row = $db->sql_fetchrow($result))
 }
 $db->sql_freeresult($result);
 
+// Get delete reason for soft deleted topic
+$sql = 'SELECT topic_delete_reason FROM ' . TOPICS_TABLE . '
+	WHERE topic_id = ' . (int) $rowset_data['topic_id'];
+$result = $db->sql_query($sql);
+$topic_delete_reason = $db->sql_fetchrow($result);
+
+$db->sql_freeresult($result);
+
 // Load custom profile fields
 if ($config['load_cpf_viewtopic'])
 {
@@ -1882,9 +1890,9 @@ for ($i = 0, $end = count($post_list); $i < $end; ++$i)
 			$display_username = get_username_string('full', $row['post_delete_user'], $user_cache[$row['post_delete_user']]['username'], $user_cache[$row['post_delete_user']]['user_colour']);
 		}
 
-		if ($row['post_delete_reason'])
+		if ($row['post_delete_reason'] || $topic_delete_reason['topic_delete_reason'])
 		{
-			$l_deleted_message = $user->lang('POST_DELETED_BY_REASON', $display_postername, $display_username, $user->format_date($row['post_delete_time'], false, true), $row['post_delete_reason']);
+			$l_deleted_message = $user->lang('POST_DELETED_BY_REASON', $display_postername, $display_username, $user->format_date($row['post_delete_time'], false, true), $row['post_delete_reason']?: $topic_delete_reason['topic_delete_reason']);
 		}
 		else
 		{
@@ -2054,7 +2062,7 @@ for ($i = 0, $end = count($post_list); $i < $end; ++$i)
 		'EDITED_MESSAGE'	=> $l_edited_by,
 		'EDIT_REASON'		=> $row['post_edit_reason'],
 		'DELETED_MESSAGE'	=> $l_deleted_by,
-		'DELETE_REASON'		=> $row['post_delete_reason'],
+		'DELETE_REASON'		=> $row['post_delete_reason'] ?: $topic_delete_reason['topic_delete_reason'],
 		'BUMPED_MESSAGE'	=> $l_bumped_by,
 
 		'MINI_POST_IMG'			=> ($post_unread) ? $user->img('icon_post_target_unread', 'UNREAD_POST') : $user->img('icon_post_target', 'POST'),
